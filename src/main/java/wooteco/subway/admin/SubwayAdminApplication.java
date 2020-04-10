4565df61e4f0ce0fadf2b2281f5089970fd82879
gracefulBrown <sunghyun.ryu@woowahan.com>
2020-04-10 23:57:46 +0900

    init

---
+++ src/main/java/wooteco/subway/admin/SubwayAdminApplication.java
@@ -0,0 +1,13 @@
+package wooteco.subway.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubwayAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubwayAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wooteco/subway/admin/SubwayAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package wooteco.subway.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubwayAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
