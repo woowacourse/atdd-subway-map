304f92cbab200dc5d32b546b312fff760dfd94ad
gracefulBrown <sunghyun.ryu@woowahan.com>
2020-04-13 02:55:33 +0900

    feat(line): acceptance test for step1

---
+++ src/main/java/wooteco/subway/admin/dto/LineResponse.java
@@ -0,0 +1,79 @@
+package wooteco.subway.admin.dto;
+
+import wooteco.subway.domain.Line;
+import wooteco.subway.domain.Station;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class LineResponse {
+    private Long id;
+    private String name;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private int intervalTime;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    private Set<Station> stations;
+
+    public LineResponse() {
+    }
+
+    public LineResponse(Long id, String name, LocalTime startTime, LocalTime endTime, int intervalTime, LocalDateTime createdAt, LocalDateTime updatedAt, Set<Station> stations) {
+        this.id = id;
+        this.name = name;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.intervalTime = intervalTime;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.stations = stations;
+    }
+
+    public static LineResponse of(Line line, Set<Station> stations) {
+        return new LineResponse(line.getId(), line.getName(), line.getStartTime(), line.getEndTime(), line.getIntervalTime(), line.getCreatedAt(), line.getUpdatedAt(), stations);
+    }
+
+    public static LineResponse of(Line line) {
+        return new LineResponse(line.getId(), line.getName(), line.getStartTime(), line.getEndTime(), line.getIntervalTime(), line.getCreatedAt(), line.getUpdatedAt(), new HashSet<>());
+    }
+
+    public static List<LineResponse> listOf(List<Line> lines) {
+        return lines.stream()
+                .map(it -> LineResponse.of(it))
+                .collect(Collectors.toList());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public int getIntervalTime() {
+        return intervalTime;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+}
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,3 +5,15 @@ create table if not exists STATION
    created_at datetime,
    primary key(id)
 );
+
+create table if not exists LINE
+(
+   id bigint auto_increment not null,
+   name varchar(255) not null,
+   start_time time not null,
+   end_time time not null,
+   interval_time int not null,
+   created_at datetime,
+   updated_at datetime,
+   primary key(id)
+);
