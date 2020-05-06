87ee415c64d706088e6d6b5a89c6e6d8f08b4b97
Sony <sonypark0204@gmail.com>
2020-05-06 17:25:39 +0900

    feat: 지하철 노선 제거

--- src/main/java/wooteco/subway/admin/controller/LineController.java
+++ src/main/java/wooteco/subway/admin/controller/LineController.java
@@ -1,16 +1,15 @@
 package wooteco.subway.admin.controller;
 
 import java.net.URI;
-import java.time.LocalTime;
 import java.util.NoSuchElementException;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import wooteco.subway.admin.domain.Line;
@@ -52,8 +51,13 @@ public class LineController {
     public ResponseEntity updateLine(@PathVariable Long id, @RequestBody LineRequest view) {
         Line line = lineRepository.findById(id)
             .orElseThrow(() -> new NoSuchElementException("id 에 해당하는 노선이 존재하지 않습니다."));
-        // line.update(new Line(null, startTime, endTime, 0));
         line.update(view.toLine());
         return ResponseEntity.ok().body(lineRepository.save(line));
+    }
+
+    @DeleteMapping("/lines/{id}")
+    public ResponseEntity deleteLine(@PathVariable Long id) {
+        lineRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
