b1952ee47f3dea7232b3876699059cd91865c10a
Sony <sonypark0204@gmail.com>
2020-05-06 17:20:32 +0900

    feat: 지하철 노선 수정 구현

--- src/main/java/wooteco/subway/admin/controller/LineController.java
+++ src/main/java/wooteco/subway/admin/controller/LineController.java
@@ -1,11 +1,14 @@
 package wooteco.subway.admin.controller;
 
 import java.net.URI;
+import java.time.LocalTime;
+import java.util.NoSuchElementException;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,5 +46,14 @@ public class LineController {
     @GetMapping("/lines/{id}")
     public ResponseEntity showLine(@PathVariable Long id) {
         return ResponseEntity.ok().body(lineRepository.findById(id));
+    }
+
+    @PutMapping("/lines/{id}")
+    public ResponseEntity updateLine(@PathVariable Long id, @RequestBody LineRequest view) {
+        Line line = lineRepository.findById(id)
+            .orElseThrow(() -> new NoSuchElementException("id 에 해당하는 노선이 존재하지 않습니다."));
+        // line.update(new Line(null, startTime, endTime, 0));
+        line.update(view.toLine());
+        return ResponseEntity.ok().body(lineRepository.save(line));
     }
 }
