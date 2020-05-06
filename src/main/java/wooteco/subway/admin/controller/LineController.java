4de6ca8ed40e5aaca7896367655e93c7f474f7ea
juyeongjung <juyeong.dev@gmail.com>
2020-05-06 17:20:21 +0900

    feat : 지하철 노선 수정 api 구현

--- src/main/java/wooteco/subway/admin/controller/LineController.java
+++ src/main/java/wooteco/subway/admin/controller/LineController.java
@@ -4,9 +4,11 @@ import java.net.URI;
 import java.util.NoSuchElementException;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -41,6 +43,16 @@ public class LineController {
 	public ResponseEntity showLine(@PathVariable Long id) {
 		Line line = lineRepository.findById(id)
 			.orElseThrow(() -> new NoSuchElementException("해당하는 id 의 노선이 존재하지 않습니다."));
+
+		return ResponseEntity.ok().body(LineResponse.of(line));
+	}
+
+	@PutMapping("/lines/{id}")
+	public ResponseEntity updateLine(@PathVariable Long id, @RequestBody LineRequest view) {
+		Line line = lineRepository.findById(id)
+			.orElseThrow(() -> new NoSuchElementException("해당하는 id 의 노선이 존재하지 않습니다."));
+		line.update(view.toLine());
+		lineRepository.save(line);
 
 		return ResponseEntity.ok().body(LineResponse.of(line));
 	}
