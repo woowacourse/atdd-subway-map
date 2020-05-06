dba48e446efcdabf57a1d0ed768d44b1f660e92d
hamliet <hamliet@hanyang.ac.kr>
2020-05-06 16:39:05 +0900

    feat : createLine 기능 추가

---
+++ src/main/java/wooteco/subway/admin/controller/LineController.java
@@ -0,0 +1,33 @@
+package wooteco.subway.admin.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import wooteco.subway.admin.domain.Line;
+import wooteco.subway.admin.dto.LineRequest;
+import wooteco.subway.admin.dto.LineResponse;
+import wooteco.subway.admin.service.LineService;
+
+import java.net.URI;
+
+@RestController
+public class LineController {
+
+    private LineService lineService;
+
+    public LineController(LineService lineService) {
+        this.lineService = lineService;
+    }
+
+    @PostMapping("/lines")
+    public ResponseEntity createLine(@RequestBody LineRequest lineRequest) {
+//        return new ResponseEntity();
+        Line line = lineRequest.toLine();
+        Line persistLine = lineService.save(line);
+
+        return ResponseEntity
+                .created(URI.create("/lines/" + persistLine.getId()))
+                .body(LineResponse.of(persistLine));
+    }
+}
--- src/main/java/wooteco/subway/admin/service/LineService.java
+++ src/main/java/wooteco/subway/admin/service/LineService.java
@@ -2,16 +2,14 @@ package wooteco.subway.admin.service;
 
 import org.springframework.stereotype.Service;
 import wooteco.subway.admin.domain.Line;
-import wooteco.subway.admin.domain.LineStation;
-import wooteco.subway.admin.domain.Station;
 import wooteco.subway.admin.dto.LineResponse;
 import wooteco.subway.admin.dto.LineStationCreateRequest;
 import wooteco.subway.admin.repository.LineRepository;
 import wooteco.subway.admin.repository.StationRepository;
 
 import java.util.List;
-import java.util.Set;
 
+@Service
 public class LineService {
     private LineRepository lineRepository;
     private StationRepository stationRepository;
