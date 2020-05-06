3842bf780059ee505ddc6b8f9c065d4d620ec693
yebink <yebin825@gmail.com>
2020-05-06 20:05:28 +0900

    feat: 페이지 연결하는 Controller 생성

--- src/main/resources/static/api/index.js
+++ src/main/resources/static/api/index.js
@@ -1,50 +1,50 @@
 const BASE_URL = "localhost:8080";
 
 const METHOD = {
-  PUT() {
-    return {
-      method: "PUT"
-    };
-  },
-  DELETE() {
-    return {
-      method: "DELETE"
-    };
-  },
-  POST(data) {
-    return {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        content: data
-      })
-    };
-  }
+    PUT() {
+        return {
+            method: "PUT"
+        };
+    },
+    DELETE() {
+        return {
+            method: "DELETE"
+        };
+    },
+    POST(data) {
+        return {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                content: data
+            })
+        };
+    }
 };
 
 const api = (() => {
-  const request = (uri, config) => fetch(uri, config).then(data => data.json());
+    const request = (uri, config) => fetch(uri, config).then(data => data.json());
 
-  const station = {
-    get() {
-      request(`${BASE_URL}/stations`);
-    },
-    create(data) {
-      request(`${BASE_URL}/station`, METHOD.POST(data));
-    },
-    update(data) {
-      request(`${BASE_URL}/station/${id}`, METHOD.PUT(data));
-    },
-    delete(id) {
-      request(`${BASE_URL}/station/${id}`, METHOD.DELETE);
-    }
-  };
+    const station = {
+        get() {
+            request(`${BASE_URL}/stations`);
+        },
+        create(data) {
+            request(`${BASE_URL}/station`, METHOD.POST(data));
+        },
+        update(data) {
+            request(`${BASE_URL}/station/${id}`, METHOD.PUT(data));
+        },
+        delete(id) {
+            request(`${BASE_URL}/station/${id}`, METHOD.DELETE);
+        }
+    };
 
-  return {
-    station
-  };
+    return {
+        station
+    };
 })();
 
 export default api;
