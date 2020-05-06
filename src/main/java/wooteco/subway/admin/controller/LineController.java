91815d08f2afcfd03bc8b83a2041888910172bc2
moon <mjj920930@gmail.com>
2020-05-06 16:55:19 +0900

    feat : LineController의 create메서드 구현

---
+++ src/main/java/wooteco/subway/admin/controller/LineController.java
@@ -0,0 +1,32 @@
+package wooteco.subway.admin.controller;
+
+import java.net.URI;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import wooteco.subway.admin.domain.Line;
+import wooteco.subway.admin.dto.LineRequest;
+import wooteco.subway.admin.dto.LineResponse;
+import wooteco.subway.admin.repository.LineRepository;
+
+@RestController
+public class LineController {
+	private final LineRepository lineRepository;
+
+	public LineController(LineRepository lineRepository) {
+		this.lineRepository = lineRepository;
+	}
+
+	@PostMapping("/lines")
+	public ResponseEntity createLine(@RequestBody LineRequest lineRequest) {
+		Line line = lineRequest.toLine();
+		Line persistLine = lineRepository.save(line);
+
+		return ResponseEntity
+			.created(URI.create("/lines/" + persistLine.getId()))
+			.body(LineResponse.of(persistLine));
+	}
+}
