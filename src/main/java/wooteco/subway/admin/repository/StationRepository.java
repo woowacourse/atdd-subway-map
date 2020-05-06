fb754d842af3b6130097e164d7b6aa502160a94c
giantim <giantim132@gmail.com>
2020-05-06 17:09:44 +0900

    feat: 라인 업데이트 라우터 구현 style: 컨벤션 수정
    LineController 업데이트 라우터 구현

--- src/main/java/wooteco/subway/admin/repository/StationRepository.java
+++ src/main/java/wooteco/subway/admin/repository/StationRepository.java
@@ -1,7 +1,6 @@
 package wooteco.subway.admin.repository;
 
 import org.springframework.data.repository.CrudRepository;
-
 import wooteco.subway.admin.domain.Station;
 
 public interface StationRepository extends CrudRepository<Station, Long> {
--- src/test/java/wooteco/subway/admin/repository/LineRepositoryTest.java
+++ src/test/java/wooteco/subway/admin/repository/LineRepositoryTest.java
@@ -1,34 +1,33 @@
 package wooteco.subway.admin.repository;
 
-import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import wooteco.subway.admin.domain.Line;
 
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
-
-import wooteco.subway.admin.domain.Line;
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 @DataJdbcTest
 class LineRepositoryTest {
-	@Autowired
-	private LineRepository lineRepository;
+    @Autowired
+    private LineRepository lineRepository;
 
-	@DisplayName("displayName")
-	@Test
-	void findAll() {
-		List<Line> lines = new ArrayList<>();
-		lines.add(new Line("역1", LocalTime.of(5, 30), LocalTime.of(23, 30), 10));
-		lines.add(new Line("역2", LocalTime.of(5, 30), LocalTime.of(23, 30), 10));
-		lines.add(new Line("역3", LocalTime.of(5, 30), LocalTime.of(23, 30), 10));
-		lineRepository.saveAll(lines);
-		assertThatCode(() -> {
-			List<Line> all = lineRepository.findAll();
+    @DisplayName("displayName")
+    @Test
+    void findAll() {
+        List<Line> lines = new ArrayList<>();
+        lines.add(new Line("역1", LocalTime.of(5, 30), LocalTime.of(23, 30), 10));
+        lines.add(new Line("역2", LocalTime.of(5, 30), LocalTime.of(23, 30), 10));
+        lines.add(new Line("역3", LocalTime.of(5, 30), LocalTime.of(23, 30), 10));
+        lineRepository.saveAll(lines);
+        assertThatCode(() -> {
+            List<Line> all = lineRepository.findAll();
 
-		}).doesNotThrowAnyException();
-	}
+        }).doesNotThrowAnyException();
+    }
 }
