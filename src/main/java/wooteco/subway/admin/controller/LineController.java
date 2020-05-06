042826da13a13ee436140188cedeceb5c1e025d8
giantim <giantim132@gmail.com>
2020-05-06 17:12:11 +0900

    feat: 라인 업데이트 라우터 구현 style: 컨벤션 수정
    LineController 업데이트 라우터 구현

--- src/main/java/wooteco/subway/admin/controller/LineController.java
+++ src/main/java/wooteco/subway/admin/controller/LineController.java
@@ -1,41 +1,48 @@
 package wooteco.subway.admin.controller;
 
-import java.net.URI;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import wooteco.subway.admin.domain.Line;
 import wooteco.subway.admin.dto.LineRequest;
 import wooteco.subway.admin.dto.LineResponse;
 import wooteco.subway.admin.repository.LineRepository;
 
+import java.net.URI;
+
 @RestController
 @RequestMapping("/lines")
 public class LineController {
-	private LineRepository lineRepository;
+    private LineRepository lineRepository;
 
-	public LineController(LineRepository lineRepository) {
-		this.lineRepository = lineRepository;
-	}
+    public LineController(LineRepository lineRepository) {
+        this.lineRepository = lineRepository;
+    }
 
-	@PostMapping
-	public ResponseEntity createLines(@RequestBody LineRequest view) {
-		Line line = view.toLine();
-		Line persistLine = lineRepository.save(line);
+    @PostMapping
+    public ResponseEntity createLines(@RequestBody LineRequest view) {
+        Line line = view.toLine();
+        Line persistLine = lineRepository.save(line);
 
-		return ResponseEntity
-			.created(URI.create("/lines/" + persistLine.getId()))
-			.body(LineResponse.of(persistLine));
-	}
+        return ResponseEntity
+                .created(URI.create("/lines/" + persistLine.getId()))
+                .body(LineResponse.of(persistLine));
+    }
 
-	@GetMapping
-	public ResponseEntity showLines() {
-		return ResponseEntity.ok().body(lineRepository.findAll());
-	}
+    @GetMapping
+    public ResponseEntity showLines() {
+        return ResponseEntity.ok().body(lineRepository.findAll());
+    }
 
-	@GetMapping("/{id}")
-	public ResponseEntity getLine(@PathVariable Long id) {
-		return ResponseEntity.ok().body((lineRepository.findById(id)));
-	}
+    @GetMapping("/{id}")
+    public ResponseEntity getLine(@PathVariable Long id) {
+        return ResponseEntity.ok().body((lineRepository.findById(id)));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity updateLine(@PathVariable Long id, @RequestBody LineRequest lineRequest) {
+        Line line = lineRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("잘못된 아이디를 입력하였습니다."));
+        line.update(lineRequest.toLine());
+        return ResponseEntity.ok().body(lineRepository.save(line));
+    }
 }
