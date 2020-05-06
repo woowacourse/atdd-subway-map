26e790643a79798a2b559288ad39dd3012f63682
yebink <yebin825@gmail.com>
2020-05-06 15:44:53 +0900

    feat: 지하철 노선 관리 - 조회 요청

--- src/main/java/wooteco/subway/admin/controller/LineController.java
+++ src/main/java/wooteco/subway/admin/controller/LineController.java
@@ -2,6 +2,7 @@ package wooteco.subway.admin.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +22,7 @@ public class LineController {
     }
 
     @PostMapping("/lines")
-    public ResponseEntity createLine(@RequestBody LineRequest request) {
+    public ResponseEntity showLine(@RequestBody LineRequest request) {
         Line line = request.toLine();
         Line persistLine = lineRepository.save(line);
 
@@ -30,12 +31,10 @@ public class LineController {
                 .body(LineResponse.of(persistLine));
     }
 
-//    @GetMapping("/lines/{id}")
-//    public ResponseEntity createLine(@PathVariable String line) {
-//        return ResponseEntity
-//                .created(URI.create("/stations/" + persistStation.getId()))
-//                .body(StationResponse.of(persistStation));
-//    }
+    @GetMapping("/lines/{id}")
+    public ResponseEntity showLine(@PathVariable("id") Long id) {
+        return ResponseEntity.ok().body(lineRepository.findById(id));
+    }
 
     @GetMapping("/lines")
     public ResponseEntity showLines() {
