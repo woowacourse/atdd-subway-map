3f328358f4438e2ea99469039837a8003d022092
Andy H. Jung <commaniakr@gmail.com>
2020-05-06 18:59:20 +0900

    feat: 프론트엔드 관련 파일 추가

---
+++ src/main/resources/static/js/views/AdminLine.js
@@ -0,0 +1,105 @@
+import { EVENT_TYPE } from "../../utils/constants.js";
+import {
+  subwayLinesTemplate,
+  colorSelectOptionTemplate
+} from "../../utils/templates.js";
+import { defaultSubwayLines } from "../../utils/subwayMockData.js";
+import { subwayLineColorOptions } from "../../utils/defaultSubwayData.js";
+import Modal from "../../ui/Modal.js";
+
+function AdminLine() {
+  const $subwayLineList = document.querySelector("#subway-line-list");
+  const $subwayLineNameInput = document.querySelector("#subway-line-name");
+  const $subwayLineColorInput = document.querySelector("#subway-line-color");
+
+  const $createSubwayLineButton = document.querySelector(
+    "#subway-line-create-form #submit-button"
+  );
+  const subwayLineModal = new Modal();
+
+  const onCreateSubwayLine = event => {
+    event.preventDefault();
+    const newSubwayLine = {
+      title: $subwayLineNameInput.value,
+      bgColor: $subwayLineColorInput.value
+    };
+    $subwayLineList.insertAdjacentHTML(
+      "beforeend",
+      subwayLinesTemplate(newSubwayLine)
+    );
+    subwayLineModal.toggle();
+    $subwayLineNameInput.value = "";
+    $subwayLineColorInput.value = "";
+  };
+
+  const onDeleteSubwayLine = event => {
+    const $target = event.target;
+    const isDeleteButton = $target.classList.contains("mdi-delete");
+    if (isDeleteButton) {
+      $target.closest(".subway-line-item").remove();
+    }
+  };
+
+  const onUpdateSubwayLine = event => {
+    const $target = event.target;
+    const isUpdateButton = $target.classList.contains("mdi-pencil");
+    if (isUpdateButton) {
+      subwayLineModal.toggle();
+    }
+  };
+
+  const onEditSubwayLine = event => {
+    const $target = event.target;
+    const isDeleteButton = $target.classList.contains("mdi-pencil");
+  };
+
+  const initDefaultSubwayLines = () => {
+    defaultSubwayLines.map(line => {
+      $subwayLineList.insertAdjacentHTML(
+        "beforeend",
+        subwayLinesTemplate(line)
+      );
+    });
+  };
+
+  const initEventListeners = () => {
+    $subwayLineList.addEventListener(EVENT_TYPE.CLICK, onDeleteSubwayLine);
+    $subwayLineList.addEventListener(EVENT_TYPE.CLICK, onUpdateSubwayLine);
+    $createSubwayLineButton.addEventListener(
+      EVENT_TYPE.CLICK,
+      onCreateSubwayLine
+    );
+  };
+
+  const onSelectColorHandler = event => {
+    event.preventDefault();
+    const $target = event.target;
+    if ($target.classList.contains("color-select-option")) {
+      document.querySelector("#subway-line-color").value =
+        $target.dataset.color;
+    }
+  };
+
+  const initCreateSubwayLineForm = () => {
+    const $colorSelectContainer = document.querySelector(
+      "#subway-line-color-select-container"
+    );
+    const colorSelectTemplate = subwayLineColorOptions
+      .map((option, index) => colorSelectOptionTemplate(option, index))
+      .join("");
+    $colorSelectContainer.insertAdjacentHTML("beforeend", colorSelectTemplate);
+    $colorSelectContainer.addEventListener(
+      EVENT_TYPE.CLICK,
+      onSelectColorHandler
+    );
+  };
+
+  this.init = () => {
+    initDefaultSubwayLines();
+    initEventListeners();
+    initCreateSubwayLineForm();
+  };
+}
+
+const adminLine = new AdminLine();
+adminLine.init();
