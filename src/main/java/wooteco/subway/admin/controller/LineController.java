d62b9eb67f3ca37d452974a2cebb9bba0e47d918
kouz <lastpuzly@gmail.com>
2020-05-06 17:41:55 +0900

    feat: 노선 추가 API 구현

---
+++ src/main/java/wooteco/subway/admin/controller/LineController.java
@@ -0,0 +1,32 @@
+package wooteco.subway.admin.controller;
+
+import java.net.URI;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import wooteco.subway.admin.domain.Line;
+import wooteco.subway.admin.dto.LineRequest;
+import wooteco.subway.admin.dto.LineResponse;
+import wooteco.subway.admin.service.LineService;
+
+@RestController
+public class LineController {
+    private LineService lineService;
+
+    public LineController(LineService lineService) {
+        this.lineService = lineService;
+    }
+
+    @PostMapping("/lines")
+    public ResponseEntity<LineResponse> createLine(@RequestBody LineRequest request) {
+        Line line = request.toLine();
+        Line persistLine = lineService.save(line);
+
+        return ResponseEntity
+            .created(URI.create("/lines/" + persistLine.getId()))
+            .body(LineResponse.of(persistLine));
+    }
+}
