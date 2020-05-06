38715d1baacb6caf38dbe8d6b08bb07233a806e0
taejinseok <stj930@gmail.com>
2020-05-06 15:10:13 +0900

    feat: 라인 목록 조회 라우터 구현
    LineController get lines 라우터 구현

--- src/main/java/wooteco/subway/admin/controller/LineController.java
+++ src/main/java/wooteco/subway/admin/controller/LineController.java
@@ -2,8 +2,10 @@ package wooteco.subway.admin.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import wooteco.subway.admin.domain.Line;
 import wooteco.subway.admin.dto.LineRequest;
@@ -13,6 +15,7 @@ import wooteco.subway.admin.repository.LineRepository;
 import java.net.URI;
 
 @RestController
+@RequestMapping("/lines")
 public class LineController {
     private LineRepository lineRepository;
 
@@ -20,7 +23,7 @@ public class LineController {
         this.lineRepository = lineRepository;
     }
 
-    @PostMapping("/lines")
+    @PostMapping
     public ResponseEntity createLines(@RequestBody LineRequest view) {
         Line line = view.toLine();
         Line persistLine = lineRepository.save(line);
@@ -28,5 +31,10 @@ public class LineController {
         return ResponseEntity
                 .created(URI.create("/lines/"+ persistLine.getId()))
                 .body(LineResponse.of(persistLine));
+    }
+
+    @GetMapping
+    public ResponseEntity showLines() {
+        return ResponseEntity.ok().body(lineRepository.findAll());
     }
 }
