ebda52a03e88e919247674aebd0bc45f6cd16ce0
Andy H. Jung <commaniakr@gmail.com>
2020-05-06 16:12:07 +0900

    feat: 지하철 노선을 추가하는 Controller 메소드 추가

---
+++ src/main/java/wooteco/subway/admin/controller/LineController.java
@@ -0,0 +1,32 @@
+package wooteco.subway.admin.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import wooteco.subway.admin.domain.Line;
+import wooteco.subway.admin.dto.LineRequest;
+import wooteco.subway.admin.dto.LineResponse;
+import wooteco.subway.admin.repository.LineRepository;
+
+import java.net.URI;
+
+@RestController
+public class LineController {
+    private final LineRepository lineRepository;
+
+    public LineController(LineRepository lineRepository) {
+        this.lineRepository = lineRepository;
+    }
+
+    @PostMapping("/lines")
+    public ResponseEntity createLine(@RequestBody LineRequest view) {
+        Line line = view.toLine();
+        Line persistLine = lineRepository.save(line);
+
+        return ResponseEntity
+                .created(URI.create("/lines/" + persistLine.getId()))
+                .body(LineResponse.of(persistLine));
+    }
+}
