52fdff5b9779836a24e5063836e0b96388866cb8
giantim <giantim132@gmail.com>
2020-05-06 16:53:43 +0900

    feat: line id 로 라인 찾는 라우터 구현
    LineController line id 를 받는 라우터 구현

--- src/main/java/wooteco/subway/admin/controller/LineController.java
+++ src/main/java/wooteco/subway/admin/controller/LineController.java
@@ -3,11 +3,7 @@ package wooteco.subway.admin.controller;
 import java.net.URI;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import wooteco.subway.admin.domain.Line;
 import wooteco.subway.admin.dto.LineRequest;
@@ -36,5 +32,10 @@ public class LineController {
 	@GetMapping
 	public ResponseEntity showLines() {
 		return ResponseEntity.ok().body(lineRepository.findAll());
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity getLine(@PathVariable Long id) {
+		return ResponseEntity.ok().body((lineRepository.findById(id)));
 	}
 }
