3c57780f545c7fb147262a056dc1d07b4468a20e
kimjunseok <jnunseok@gmail.com>
2020-05-06 16:44:36 +0900

    feat: update test success

--- src/main/java/wooteco/subway/admin/controller/LineController.java
+++ src/main/java/wooteco/subway/admin/controller/LineController.java
@@ -1,18 +1,13 @@
 package wooteco.subway.admin.controller;
 
 import java.net.URI;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import wooteco.subway.admin.domain.Line;
-import wooteco.subway.admin.domain.Station;
 import wooteco.subway.admin.dto.LineRequest;
 import wooteco.subway.admin.dto.LineResponse;
 import wooteco.subway.admin.repository.LineRepository;
@@ -45,4 +40,13 @@ public class LineController {
         return ResponseEntity.ok().body(LineResponse.of(lineRepository.findById(id).get()));
     }
 
+
+    @PutMapping("/lines/{id}")
+    public ResponseEntity updateLine(@PathVariable Long id, @RequestBody LineRequest lineRequest) {
+        Line line = lineRepository.findById(id).get();
+        line.update(lineRequest.toLine());
+        Line persistLine = lineRepository.save(line);
+
+        return ResponseEntity.ok().body(LineResponse.of(persistLine));
+    }
 }
