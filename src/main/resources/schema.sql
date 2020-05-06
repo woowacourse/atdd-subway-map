1eac67cb9546d5c37fe71fd218d15498682c7640
hongbin23 <khbasd@gmail.com>
2020-05-06 17:42:51 +0900

    docs : README 작성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -8,12 +8,23 @@ create table if not exists STATION
 
 create table if not exists LINE
 (
-   id bigint auto_increment not null,
-   name varchar(255) not null,
-   start_time time not null,
-   end_time time not null,
-   interval_time int not null,
-   created_at datetime,
-   updated_at datetime,
-   primary key(id)
+    id            bigint auto_increment not null,
+    name          varchar(255)          not null,
+    start_time    time                  not null,
+    end_time      time                  not null,
+    interval_time int                   not null,
+    created_at    datetime,
+    updated_at    datetime,
+    primary key (id)
+);
+
+create table if not exists LINE_STATION
+(
+    line        bigint not null,
+    station     bigint not null,
+    pre_station bigint,
+    distance    int,
+    duration    int,
+    created_at  datetime,
+    updated_at  datetime
 );
