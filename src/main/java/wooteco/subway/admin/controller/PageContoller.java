5c147eaa673cc8aeb268d4f4dd7ca11027e226a2
junk-sound <wom2277@naver.com>
2020-05-06 21:51:18 +0900

    feat: 지하철 노선 관리 페이지 연동

---
+++ src/main/java/wooteco/subway/admin/controller/PageContoller.java
@@ -0,0 +1,28 @@
+package wooteco.subway.admin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageContoller {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/admin-line.html")
+    public String adminLine() {
+        return "admin-line";
+    }
+
+    @GetMapping("/admin-edge.html")
+    public String adminEdge() {
+        return "admin-edge";
+    }
+
+    @GetMapping("/admin-station.html")
+    public String adminStation() {
+        return "admin-station";
+    }
+}
