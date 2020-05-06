04e08d9dd7e27070b7116268a8bb719559096e6e
KIMSIYOUNG <tldud2404@gmail.com>
2020-05-06 16:19:27 +0900

    feat : 지하철 변경 및 삭제 로직 구현

--- README.md
+++ README.md
@@ -31,5 +31,7 @@ Feature: 지하철 노선 관리
 -[x] read : 지하철 노선을 조회할 수 있다.
     -[x] `findAll`
     -[x] `findById`
--[ ] update : 지하철 노선 및 시간을 변경할 수 있다.
--[ ] delete : 등록된 노선을 삭제할 수 있다.
+-[x] update : 지하철 노선 및 시간을 변경할 수 있다.
+-[x] delete : 등록된 노선을 삭제할 수 있다.
+
+-[ ] refactor
--- src/main/java/wooteco/subway/admin/controller/LineController.java
+++ src/main/java/wooteco/subway/admin/controller/LineController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,4 +47,19 @@ public class LineController {
         return ResponseEntity.ok(line);
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity updateById(@PathVariable Long id, @RequestBody LineRequest lineRequest) {
+        final Line line = lineService.showLine(id);
+        line.update(lineRequest.toLine());
+        lineService.updateLine(id, line);
+
+        return ResponseEntity.ok(line);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteById(@PathVariable Long id) {
+        lineService.deleteLineById(id);
+        
+        return ResponseEntity.ok().build();
+    }
 }
