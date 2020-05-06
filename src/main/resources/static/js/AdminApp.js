dbdbdf8f8a22881838dd54d1c91b7300dcbaaf4d
moon <mjj920930@gmail.com>
2020-05-06 20:29:58 +0900

    feat : 프론트엔드 코드 반영

---
+++ src/main/resources/static/js/AdminApp.js
@@ -0,0 +1,17 @@
+import {initNavigation} from "../utils/templates.js";
+
+function AdminApp() {
+    const init = () =
+>
+    {
+        initNavigation();
+    }
+    ;
+
+    return {
+        init
+    };
+}
+
+const adminApp = new AdminApp();
+adminApp.init();
