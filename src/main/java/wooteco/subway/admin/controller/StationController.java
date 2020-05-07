aff005c8a65c61473ed4be62b6dfdd32309a54f6
Sony <sonypark0204@gmail.com>
2020-05-07 12:00:16 +0900

    feat: 벡엔드에 지하철 역이름 중복 체크 로직 추가

--- src/main/java/wooteco/subway/admin/controller/StationController.java
+++ src/main/java/wooteco/subway/admin/controller/StationController.java
@@ -1,13 +1,20 @@
 package wooteco.subway.admin.controller;
 
+import java.net.URI;
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import wooteco.subway.admin.domain.Station;
 import wooteco.subway.admin.dto.StationCreateRequest;
 import wooteco.subway.admin.dto.StationResponse;
 import wooteco.subway.admin.repository.StationRepository;
-
-import java.net.URI;
 
 @RestController
 public class StationController {
@@ -19,12 +26,18 @@ public class StationController {
 
     @PostMapping("/stations")
     public ResponseEntity createStation(@RequestBody StationCreateRequest view) {
+        List<Station> persistStations = (List<Station>)stationRepository.findAll();
+        boolean hasDuplicateName = persistStations.stream()
+            .anyMatch(station -> station.getName().equals(view.toStation().getName()));
+        if (hasDuplicateName) {
+            return ResponseEntity.badRequest().body("역이름은 중복될 수 없습니다.");
+        }
         Station station = view.toStation();
         Station persistStation = stationRepository.save(station);
 
         return ResponseEntity
-                .created(URI.create("/stations/" + persistStation.getId()))
-                .body(StationResponse.of(persistStation));
+            .created(URI.create("/stations/" + persistStation.getId()))
+            .body(StationResponse.of(persistStation));
     }
 
     @GetMapping("/stations")
