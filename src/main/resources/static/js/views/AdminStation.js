b22c7d538500f3218f9ee1a0d9688d9921614a04
dd <dqrd123@gmail.com>
2020-05-07 11:40:17 +0900

    feat: station 페이지 연동

--- src/main/resources/static/js/views/AdminStation.js
+++ src/main/resources/static/js/views/AdminStation.js
@@ -3,22 +3,68 @@ import { listItemTemplate } from "../../utils/templates.js";
 
 function AdminStation() {
   const $stationInput = document.querySelector("#station-name");
+  const $stationInputButton = document.querySelector("#station-add-btn");
   const $stationList = document.querySelector("#station-list");
 
   const onAddStationHandler = event => {
-    if (event.key !== KEY_TYPE.ENTER) {
+    if (event.key !== KEY_TYPE.ENTER && event.type !== EVENT_TYPE.CLICK) {
       return;
     }
     event.preventDefault();
     const $stationNameInput = document.querySelector("#station-name");
     const stationName = $stationNameInput.value;
-    if (!stationName) {
-      alert(ERROR_MESSAGE.NOT_EMPTY);
-      return;
-    }
+
+    validate(stationName);
+
+    let data = {
+      name: stationName
+    };
+
+    fetch("/stations", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    }).then(response => response.json())
+    .then(response=>{
+      console.log(JSON.stringify(response));
+    }).catch(error=>{
+      alert(error);
+    });
+
     $stationNameInput.value = "";
     $stationList.insertAdjacentHTML("beforeend", listItemTemplate(stationName));
   };
+
+  function validate(stationName) {
+    if (!stationName) {
+      alert(ERROR_MESSAGE.NOT_EMPTY);
+      throw new Error();
+    }
+    if (stationName.includes(" ")) {
+      alert(ERROR_MESSAGE.NOT_BLANK);
+      throw new Error();
+    }
+    //myCode
+    if (/\d/.test(stationName)) {
+      alert(ERROR_MESSAGE.CONTAIN_NUMBER);
+      throw new Error();
+    }
+    //myCode
+    if (duplicatedName(stationName)) {
+      alert(ERROR_MESSAGE.DUPLICATE_NAME);
+      throw new Error();
+    }
+  }
+
+  function duplicatedName(input) {
+    const names = document.querySelectorAll(".list-item");
+    const namesArr = Array.from(names);
+    return namesArr.some(element => {
+      return element.innerText === input;
+    });
+  }
 
   const onRemoveStationHandler = event => {
     const $target = event.target;
@@ -31,6 +77,7 @@ function AdminStation() {
   const initEventListeners = () => {
     $stationInput.addEventListener(EVENT_TYPE.KEY_PRESS, onAddStationHandler);
     $stationList.addEventListener(EVENT_TYPE.CLICK, onRemoveStationHandler);
+    $stationInputButton.addEventListener(EVENT_TYPE.CLICK, onAddStationHandler);
   };
 
   const init = () => {
@@ -43,4 +90,17 @@ function AdminStation() {
 }
 
 const adminStation = new AdminStation();
+const $stationList = document.querySelector("#station-list");
 adminStation.init();
+window.onload = async function(event) {
+  const response = await fetch("/stations", {
+      method: "GET",
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  const jsonResponse = await response.json();
+  for(const station of jsonResponse) {
+    $stationList.insertAdjacentHTML("beforeend", listItemTemplate(station.name));
+  }
+};
