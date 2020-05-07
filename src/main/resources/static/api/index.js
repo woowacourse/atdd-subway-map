2d150807e4f5e4baaf776bc3f9dc3447a2485db7
moon <mjj920930@gmail.com>
2020-05-07 10:13:35 +0900

    fix : api/index.js 오류 수정

--- src/main/resources/static/api/index.js
+++ src/main/resources/static/api/index.js
@@ -1,53 +1,21 @@
-const BASE_URL = "localhost:8080";
-
-const METHOD = {
-    PUT() {
-        return {
-            method: "PUT"
-        };
-    },
-    DELETE() {
-        return {
-            method: "DELETE"
-        };
-    },
-    POST(data) {
-        return {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                content: data
-            })
-        };
-    }
-};
-
 const api = (() => {
-    const request = (uri, config) => fetch(uri, config).then(data => data.json()
-)
-;
-
-const station = {
-    get() {
-        request(`${BASE_URL}/stations`);
-    },
-    create(data) {
-        request(`${BASE_URL}/station`, METHOD.POST(data));
-    },
-    update(data) {
-        request(`${BASE_URL}/station/${id}`, METHOD.PUT(data));
-    },
-    delete(id) {
-        request(`${BASE_URL}/station/${id}`, METHOD.DELETE);
-    }
-};
-
-return {
-    station
-};
-})
-();
-
+    const request = (uri, config) => fetch(uri, config).then(data => data.json());
+    const station = {
+        get() {
+            return request(`/stations`);
+        },
+        create(data) {
+            return request(`/station`, METHOD.POST(data));
+        },
+        update(data, id) {
+            return request(`/station/${id}`, METHOD.PUT(data));
+        },
+        delete(id) {
+            return request(`/station/${id}`, METHOD.DELETE);
+        }
+    };
+    return {
+        station
+    };
+})();
 export default api;
