3178428032e04db9fd5cc7c07cea4403210d4100
Sony <sonypark0204@gmail.com>
2020-05-07 11:14:46 +0900

    feat: 지하철 역 추가 기능 페이지 연동

--- src/main/resources/static/js/views/AdminStation.js
+++ src/main/resources/static/js/views/AdminStation.js
@@ -1,45 +1,65 @@
-import { EVENT_TYPE, ERROR_MESSAGE, KEY_TYPE } from "../../utils/constants.js";
-import { listItemTemplate } from "../../utils/templates.js";
+import {ERROR_MESSAGE, EVENT_TYPE, KEY_TYPE} from "../../utils/constants.js";
+import {listItemTemplate} from "../../utils/templates.js";
+import api from "../../api/index.js";
+
 
 function AdminStation() {
-  const $stationInput = document.querySelector("#station-name");
-  const $stationList = document.querySelector("#station-list");
+    let stations = [];
+    const $stationInput = document.querySelector('#station-name');
+    const $stationList = document.querySelector('#station-list');
+    const $stationAddButton = document.querySelector('#station-add-btn');
 
-  const onAddStationHandler = event => {
-    if (event.key !== KEY_TYPE.ENTER) {
-      return;
-    }
-    event.preventDefault();
-    const $stationNameInput = document.querySelector("#station-name");
-    const stationName = $stationNameInput.value;
-    if (!stationName) {
-      alert(ERROR_MESSAGE.NOT_EMPTY);
-      return;
-    }
-    $stationNameInput.value = "";
-    $stationList.insertAdjacentHTML("beforeend", listItemTemplate(stationName));
-  };
+    const onAddStationHandler = (event) => {
+        if (event.key !== KEY_TYPE.ENTER && event.key !== KEY_TYPE.CLICK) {
+            return;
+        }
+        event.preventDefault();
+        const $stationNameInput = document.querySelector('#station-name');
+        const stationName = $stationNameInput.value;
+        const blank_pattern = /^[^\d\s]+$/;
+        if (!stationName) {
+            alert(ERROR_MESSAGE.NOT_EMPTY);
+            return;
+        }
+        if (!blank_pattern.test(stationName)) {
+            alert(ERROR_MESSAGE.NOT_BLANK);
+            return;
+        }
+        if (stations.includes(stationName)) {
+            alert(ERROR_MESSAGE.NOT_DUPLICATE);
+            return;
+        }
 
-  const onRemoveStationHandler = event => {
-    const $target = event.target;
-    const isDeleteButton = $target.classList.contains("mdi-delete");
-    if (isDeleteButton) {
-      $target.closest(".list-item").remove();
-    }
-  };
+        stations = [...stations, stationName];
 
-  const initEventListeners = () => {
-    $stationInput.addEventListener(EVENT_TYPE.KEY_PRESS, onAddStationHandler);
-    $stationList.addEventListener(EVENT_TYPE.CLICK, onRemoveStationHandler);
-  };
+        $stationNameInput.value = '';
+        $stationList.insertAdjacentHTML('beforeend', listItemTemplate(stationName));
+        api.station.create(stationName);
+    };
 
-  const init = () => {
-    initEventListeners();
-  };
+    const onRemoveStationHandler = (event) => {
+        const $target = event.target;
+        const isDeleteButton = $target.classList.contains('mdi-delete');
+        const stationName = $target.closest('.list-item').innerText;
+        if (isDeleteButton && confirm('정말 삭제하시겠습니까?')) {
+            $target.closest('.list-item').remove();
+            stations = stations.filter((station) => station !== stationName);
+        }
+    };
 
-  return {
-    init
-  };
+    const initEventListeners = () => {
+        $stationInput.addEventListener(EVENT_TYPE.KEY_PRESS, onAddStationHandler);
+        $stationAddButton.addEventListener(EVENT_TYPE.CLICK, onAddStationHandler);
+        $stationList.addEventListener(EVENT_TYPE.CLICK, onRemoveStationHandler);
+    };
+
+    const init = () => {
+        initEventListeners();
+    };
+
+    return {
+        init
+    };
 }
 
 const adminStation = new AdminStation();
--- src/main/resources/static/utils/constants.js
+++ src/main/resources/static/utils/constants.js
@@ -1,12 +1,14 @@
 export const EVENT_TYPE = {
-  CLICK: "click",
-  KEY_PRESS: "keypress"
+    CLICK: "click",
+    KEY_PRESS: "keypress"
 };
 
 export const ERROR_MESSAGE = {
-  NOT_EMPTY: "🤔 값을 입력해주세요"
+    NOT_EMPTY: '🤔 값을 입력해주세요',
+    NOT_BLANK: '역 이름에 공백이 포함될 수 없습니다.',
+    NOT_DUPLICATE: '중복된 역은 입력할 수 없습니다.',
 };
 
 export const KEY_TYPE = {
-  ENTER: "Enter"
+    ENTER: "Enter"
 };
