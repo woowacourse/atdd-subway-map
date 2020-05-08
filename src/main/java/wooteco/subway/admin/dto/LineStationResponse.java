a496e3162ee696ce2d0ce1a4311f3dcb4a37a47d
moonyoungCHAE <xpf_fl@naver.com>
2020-05-08 16:26:49 +0900

    test: 노선 별 지하철 역 조회

---
+++ src/main/java/wooteco/subway/admin/dto/LineStationResponse.java
@@ -0,0 +1,36 @@
+package wooteco.subway.admin.dto;
+
+import wooteco.subway.admin.domain.Line;
+import wooteco.subway.admin.domain.Station;
+
+import java.util.Set;
+
+public class LineStationResponse {
+    private Long id;
+    private String name;
+    private Set<Station> stations;
+
+    public LineStationResponse(Long id, String name, Set<Station> stations) {
+        this.id = id;
+        this.name = name;
+        this.stations = stations;
+    }
+
+    public LineStationResponse(Line line, Set<Station> stations) {
+        this.id = line.getId();
+        this.name = line.getName();
+        this.stations = stations;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Set<Station> getStations() {
+        return stations;
+    }
+}
