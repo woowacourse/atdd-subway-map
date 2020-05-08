7df40b201778a55ad080d00b955e7b82a91fe695
Seyun <ksy90101@gmail.com>
2020-05-09 01:25:22 +0900

    feat : 숫자와 공백, 중복값이 들어올 경우 예외처리

--- src/main/resources/static/js/views/AdminStation.js
+++ src/main/resources/static/js/views/AdminStation.js
@@ -1,26 +1,62 @@
-import { EVENT_TYPE, ERROR_MESSAGE, KEY_TYPE } from "../../utils/constants.js";
+import { EVENT_TYPE, ERROR_MESSAGE, KEY_TYPE, CONFIRM_MESSAGE } from "../../utils/constants.js";
 import { listItemTemplate } from "../../utils/templates.js";
 
 function AdminStation() {
   const $stationInput = document.querySelector("#station-name");
   const $stationList = document.querySelector("#station-list");
+  const $stationAddButtn = document.querySelector("#station-add-btn");
 
   const onAddStationHandler = event => {
-    if (event.key !== KEY_TYPE.ENTER) {
+    if (event.type !== EVENT_TYPE.CLICK && event.key !== KEY_TYPE.ENTER) {
       return;
     }
     event.preventDefault();
     const $stationNameInput = document.querySelector("#station-name");
     const stationName = $stationNameInput.value;
+    const regExp = new RegExp(/^[^\d\s]+$/);
+
     if (!stationName) {
       alert(ERROR_MESSAGE.NOT_EMPTY);
       return;
     }
+
+    if(validateReduplication(stationName)){
+      $stationNameInput.value = "";
+      alert(ERROR_MESSAGE.REDUPLCATE_STATION_NAME);
+      return;
+    }
+
+    if(!regExp.test(stationName)){
+      alert(ERROR_MESSAGE.INCORRECT_STATION_NAME);
+      $stationNameInput.value = "";
+      return;
+    }
+
     $stationNameInput.value = "";
     $stationList.insertAdjacentHTML("beforeend", listItemTemplate(stationName));
   };
 
+  function validateReduplication(stationName) {
+    const items = document.querySelectorAll('.list-item');
+    let isReduplicate = false;
+
+    items.forEach(item => {
+      if(item.innerText === stationName){
+        isReduplicate = true;
+        return;
+      }
+    });
+
+    return isReduplicate;
+  }
+
   const onRemoveStationHandler = event => {
+    const isRemove = confirm(CONFIRM_MESSAGE.REMOVE);
+
+    if(!isRemove){
+      return;
+    }
+
     const $target = event.target;
     const isDeleteButton = $target.classList.contains("mdi-delete");
     if (isDeleteButton) {
@@ -31,6 +67,7 @@ function AdminStation() {
   const initEventListeners = () => {
     $stationInput.addEventListener(EVENT_TYPE.KEY_PRESS, onAddStationHandler);
     $stationList.addEventListener(EVENT_TYPE.CLICK, onRemoveStationHandler);
+    $stationAddButtn.addEventListener(EVENT_TYPE.CLICK, onAddStationHandler);
   };
 
   const init = () => {
