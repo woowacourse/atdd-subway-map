90981369e06bba327726666b84c1c77d955999d0
junk-sound <wom2277@naver.com>
2020-05-09 03:10:04 +0900

    chore: 데이터 초기화

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,6 +6,21 @@ create table if not exists STATION
    primary key(id)
 );
 
+-- 1호선
+insert into STATION (title) values ('수원');
+insert into STATION (title) values ('화서');
+insert into STATION (title) values ('성균관대');
+
+-- 2호선
+insert into STATION (title) values ('교대');
+insert into STATION (title) values ('강남');
+insert into STATION (title) values ('역삼');
+insert into STATION (title) values ('선릉');
+insert into STATION (title) values ('삼성');
+insert into STATION (title) values ('종합운동장');
+insert into STATION (title) values ('잠실새내');
+insert into STATION (title) values ('잠실');
+
 create table if not exists LINE
 (
    id bigint auto_increment not null,
@@ -19,6 +34,16 @@ create table if not exists LINE
    primary key(id)
 );
 
+insert into LINE (name, start_time, end_time, interval_time, bg_color) values ('1호선', '06:00:00', '22:00:00', '10', 'bg-blue-700');
+insert into LINE (name, start_time, end_time, interval_time, bg_color) values ('2호선', '06:00:00', '22:00:00', '10', 'bg-green-500');
+insert into LINE (name, start_time, end_time, interval_time, bg_color) values ('3호선', '06:00:00', '22:00:00', '10', 'bg-orange-500');
+insert into LINE (name, start_time, end_time, interval_time, bg_color) values ('4호선', '06:00:00', '22:00:00', '10', 'bg-blue-500');
+insert into LINE (name, start_time, end_time, interval_time, bg_color) values ('5호선', '06:00:00', '22:00:00', '10', 'bg-purple-500');
+insert into LINE (name, start_time, end_time, interval_time, bg_color) values ('6호선', '06:00:00', '22:00:00', '10', 'bg-yellow-500');
+insert into LINE (name, start_time, end_time, interval_time, bg_color) values ('7호선', '06:00:00', '22:00:00', '10', 'bg-green-500');
+insert into LINE (name, start_time, end_time, interval_time, bg_color) values ('8호선', '06:00:00', '22:00:00', '10', 'bg-pink-500');
+insert into LINE (name, start_time, end_time, interval_time, bg_color) values ('9호선', '06:00:00', '22:00:00', '10', '신분당선');
+
 create table if not exists LINE_STATION
 (
     line bigint not null,
@@ -29,3 +54,7 @@ create table if not exists LINE_STATION
     created_at datetime,
     updated_at datetime
 );
+
+insert into LINE_STATION (line, station, pre_station, distance, duration) values (1, 1, null, 10, 10);
+insert into LINE_STATION (line, station, pre_station, distance, duration) values (1, 2, 1, 10, 10);
+insert into LINE_STATION (line, station, pre_station, distance, duration) values (1, 3, 2, 10, 10);
