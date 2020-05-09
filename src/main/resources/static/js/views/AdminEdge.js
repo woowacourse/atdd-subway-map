364b9ed40095aa743471720a1518cec51ceb3b6d
hongbin23 <khbasd@gmail.com>
2020-05-09 14:32:47 +0900

    feat: 뷰에서 LineStation을 삭제하는 기능 구현

--- src/main/resources/static/js/views/AdminEdge.js
+++ src/main/resources/static/js/views/AdminEdge.js
@@ -1,7 +1,4 @@
-import {
-    optionTemplate,
-    subwayLinesItemTemplate
-} from "../../utils/templates.js";
+import {optionTemplate, subwayLinesItemTemplate} from "../../utils/templates.js";
 import {defaultSubwayLines} from "../../utils/subwayMockData.js";
 import tns from "../../lib/slider/tiny-slider.js";
 import {EVENT_TYPE} from "../../utils/constants.js";
@@ -15,8 +12,8 @@ function AdminEdge() {
     const initSubwayLinesSlider = () => {
         api.line.get("/stations").then(res => {
             $subwayLinesSlider.innerHTML = res
-            .map(line => subwayLinesItemTemplate(line))
-            .join("");
+                .map(line => subwayLinesItemTemplate(line))
+                .join("");
             tns({
                 container: ".subway-lines-slider",
                 loop: true,
@@ -34,8 +31,8 @@ function AdminEdge() {
 
     const initSubwayLineOptions = () => {
         const subwayLineOptionTemplate = defaultSubwayLines
-        .map(line => optionTemplate(line.name))
-        .join("");
+            .map(line => optionTemplate(line.name))
+            .join("");
         const $stationSelectOptions = document.querySelector(
             "#station-select-options"
         );
@@ -48,9 +45,16 @@ function AdminEdge() {
     const onRemoveStationHandler = event => {
         const $target = event.target;
         const isDeleteButton = $target.classList.contains("mdi-delete");
-        if (isDeleteButton) {
-            $target.closest(".list-item").remove();
+        if (!isDeleteButton) {
+            return;
         }
+        const lineId = $target.closest(".slider-list").querySelector(".lint-title").dataset.lineId;
+        const stationId = $target.closest(".list-item").dataset.stationId;
+
+        api.line.delete(`/${lineId}/stations/${stationId}`)
+            .then(() =>
+                $target.closest(".list-item").remove()
+            ).catch(err => alert(err));
     };
 
     const initEventListeners = () => {
