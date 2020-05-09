725c67dcba3ed00ff9610ce16cbbf0ab37758493
yebink <yebin825@gmail.com>
2020-05-09 16:47:48 +0900

    refactor: 테스트에 truncate.sql 적용

--- src/test/java/wooteco/subway/admin/domain/LineTest.java
+++ src/test/java/wooteco/subway/admin/domain/LineTest.java
@@ -4,18 +4,21 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.test.context.jdbc.Sql;
 
 import java.time.LocalTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Sql("/truncate.sql")
 public class LineTest {
     private Line line;
 
     @BeforeEach
     void setUp() {
         line = new Line(1L, "2호선", "bg-green-500", LocalTime.of(05, 30), LocalTime.of(22, 30), 5);
+        line.addLineStation(new LineStation(null, 1L, 10, 10));
         line.addLineStation(new LineStation(1L, 2L, 10, 10));
         line.addLineStation(new LineStation(2L, 3L, 10, 10));
     }
@@ -34,6 +37,6 @@ public class LineTest {
     void removeLineStation(Long stationId) {
         line.removeLineStationById(stationId);
 
-        assertThat(line.getStations()).hasSize(1);
+        assertThat(line.getStations()).hasSize(2);
     }
 }
