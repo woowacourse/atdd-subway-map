8efe13a051b830495877e35a3cbf4d05add61505
lowoon <sangbo0229@gmail.com>
2020-05-09 19:54:01 +0900

    refactor: 컨벤션 체크

--- src/main/java/wooteco/subway/admin/dto/LineStationResponse.java
+++ src/main/java/wooteco/subway/admin/dto/LineStationResponse.java
@@ -20,7 +20,7 @@ public class LineStationResponse {
     }
 
     public LineStationResponse(Long id, String name, Long preStationId, String preStationName,
-            int distance, int duration) {
+        int distance, int duration) {
         this.id = id;
         this.name = name;
         this.preStationId = preStationId;
@@ -29,17 +29,20 @@ public class LineStationResponse {
         this.duration = duration;
     }
 
-    public static LineStationResponse of(LineStation lineStation, Station station, Station preStation) {
-        return new LineStationResponse(lineStation.getStationId(), station.getName(), lineStation.getPreStationId(),
-                preStation.getName(), lineStation.getDistance(), lineStation.getDuration());
+    public static LineStationResponse of(LineStation lineStation, Station station,
+        Station preStation) {
+        return new LineStationResponse(lineStation.getStationId(), station.getName(),
+            lineStation.getPreStationId(),
+            preStation.getName(), lineStation.getDistance(), lineStation.getDuration());
     }
 
     public static LineStationResponse of(LineStation lineStation, Station station) {
         return new LineStationResponse(lineStation.getStationId(), station.getName(), null,
-                null, lineStation.getDistance(), lineStation.getDuration());
+            null, lineStation.getDistance(), lineStation.getDuration());
     }
 
-    public static List<LineStationResponse> listOf(List<LineStation> lineStations, Map<Long, Station> stations) {
+    public static List<LineStationResponse> listOf(List<LineStation> lineStations,
+        Map<Long, Station> stations) {
         if (lineStations.isEmpty()) {
             return Collections.emptyList();
         }
@@ -47,18 +50,18 @@ public class LineStationResponse {
         List<LineStationResponse> lineStationResponses = new ArrayList<>();
         LineStation first = lineStations.get(0);
         lineStationResponses.add(LineStationResponse.of(
-                first, stations.get(first.getStationId()))
+            first, stations.get(first.getStationId()))
         );
 
         for (int index = 1; index < lineStations.size(); index++) {
             LineStation lineStation = lineStations.get(index);
-            System.err.println(lineStation.getStationId());
             lineStationResponses.add(LineStationResponse.of(
-                    lineStation,
-                    stations.get(lineStation.getStationId()),
-                    stations.get(lineStation.getPreStationId()))
+                lineStation,
+                stations.get(lineStation.getStationId()),
+                stations.get(lineStation.getPreStationId()))
             );
         }
+        
         return lineStationResponses;
     }
 
