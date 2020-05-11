8e81e1dad9fba4637998e469aec91698645e0a79
hj <a301dks@naver.com>
2020-05-11 16:22:57 +0900

    feat: 프론트 미션 추가

--- src/main/resources/static/utils/constants.js
+++ src/main/resources/static/utils/constants.js
@@ -4,7 +4,10 @@ export const EVENT_TYPE = {
 };
 
 export const ERROR_MESSAGE = {
-    NOT_EMPTY: "🤔 값을 입력해주세요"
+    NOT_EMPTY: "🤔 값을 입력해주세요",
+    NOT_SPACE: "🤔 공백이 포함될 수 없어요",
+    NOT_NUMBER: "🤔 숫자가 포함될 수 없어요",
+    NOT_DUPLICATE: "🤔 동일한 이름의 역이 있어요"
 };
 
 export const KEY_TYPE = {
