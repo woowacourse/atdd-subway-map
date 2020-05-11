52d3abae71e8c0b386feb377bca9ee0a92607c10
hwookim <gusdn9754@gmail.com>
2020-05-11 14:33:39 +0900

    feat: Station.validateName() 추가

--- src/main/java/wooteco/subway/admin/domain/Station.java
+++ src/main/java/wooteco/subway/admin/domain/Station.java
@@ -1,18 +1,35 @@
 package wooteco.subway.admin.domain;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
 import org.springframework.data.annotation.Id;
 
-import java.time.LocalDateTime;
-
 public class Station {
+
     @Id
     private Long id;
     private String name;
     private LocalDateTime createdAt;
 
     public Station(String name) {
+        validateName(name);
         this.name = name;
         this.createdAt = LocalDateTime.now();
+    }
+
+    private void validateName(String name) {
+        if (Objects.isNull(name)) {
+            throw new IllegalArgumentException("Station.name은 null이 올 수 없습니다.");
+        }
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException("Station.name은 빈 값이 올 수 없습니다.");
+        }
+        if (name.matches(".*[ ].*")) {
+            throw new IllegalArgumentException("Station.name은 공백이 포함 될 수 없습니다.");
+        }
+        if (name.matches(".*[0-9].*")) {
+            throw new IllegalArgumentException("Station.name은 숫자가 포함 될 수 없습니다.");
+        }
     }
 
     public Long getId() {
