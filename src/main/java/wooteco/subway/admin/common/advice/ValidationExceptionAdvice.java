7a437c01a8c58d678680b9c485bcb26899851207
chanin <pci2676@gmail.com>
2020-05-11 17:20:24 +0900

    refactor : 잘못된 타입의 json 인자 입력시 excpetion 설정

--- src/main/java/wooteco/subway/admin/common/advice/ValidationExceptionAdvice.java
+++ src/main/java/wooteco/subway/admin/common/advice/ValidationExceptionAdvice.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import wooteco.subway.admin.common.advice.dto.MethodArgumentExceptionDto;
 
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,7 +24,7 @@ public class ValidationExceptionAdvice {
 
     @ExceptionHandler(InvalidFormatException.class)
     public ResponseEntity<List<MethodArgumentExceptionDto>> invalidJsonInput(InvalidFormatException e) {
-        return ResponseEntity.badRequest().body(Arrays.asList(new MethodArgumentExceptionDto(e.getPath().get(0).getFieldName(),
+        return ResponseEntity.badRequest().body(Collections.singletonList(new MethodArgumentExceptionDto(e.getPath().get(0).getFieldName(),
                 String.format("%s가 올바르지 않은 값(%s)입니다.", e.getPath().get(0).getFieldName(), e.getValue()))));
     }
 }
