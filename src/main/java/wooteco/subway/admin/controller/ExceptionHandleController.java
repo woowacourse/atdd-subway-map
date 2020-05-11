17aac000a97725d08106b195c348c7d67537736e
minuyim <minu_yim@naver.com>
2020-05-11 14:22:47 +0900

    refactor : 예외처리 시 DBActionExecutionException도 처리하도록 수정

--- src/main/java/wooteco/subway/admin/controller/ExceptionHandleController.java
+++ src/main/java/wooteco/subway/admin/controller/ExceptionHandleController.java
@@ -1,6 +1,7 @@
 package wooteco.subway.admin.controller;
 
 import org.springframework.dao.DataAccessException;
+import org.springframework.data.relational.core.conversion.DbActionExecutionException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -23,9 +24,9 @@ public class ExceptionHandleController {
         return new ErrorMessage(HttpStatus.BAD_REQUEST, e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
     }
 
-    @ExceptionHandler(DataAccessException.class)
-    public ErrorMessage handleDBException(DataAccessException e, HttpServletResponse httpServletResponse) {
+    @ExceptionHandler({DataAccessException.class, DbActionExecutionException.class})
+    public ErrorMessage handleDBException(HttpServletResponse httpServletResponse) {
         httpServletResponse.setStatus(500);
-        return new ErrorMessage(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+        return new ErrorMessage(HttpStatus.INTERNAL_SERVER_ERROR, "데이터 요청 처리 중 오류가 발생했습니다.");
     }
 }
