639f04f47250b45550f7cc5e342060ecc66d8942
toneyparky <qkrtmddhks95@gmail.com>
2020-05-11 16:33:37 +0900

    refactor: 불필요한 코드 제거

--- src/main/resources/static/ui/Modal.js
+++ src/main/resources/static/ui/Modal.js
@@ -6,22 +6,10 @@ export default function Modal() {
   const $body = document.querySelector("body");
   const $modal = document.querySelector(".modal");
 
-  // const $subwayLineNameInput = document.querySelector("#subway-line-name");
-  // const $subwayLineFirstTimeInput = document.querySelector("#first-time");
-  // const $subwayLineLastTimeInput = document.querySelector("#last-time");
-  // const $subwayLineIntervalTimeInput = document.querySelector("#interval-time");
-  // const $subwayLineColorInput = document.querySelector("#subway-line-color");
-
   const toggle = event => {
     if (event) {
       event.preventDefault();
     }
-
-    // $subwayLineNameInput.value = "";
-    // $subwayLineFirstTimeInput.value = "";
-    // $subwayLineLastTimeInput.value = "";
-    // $subwayLineIntervalTimeInput.value = "";
-    // $subwayLineColorInput.value = "";
 
     $body.classList.toggle("modal-active");
     $modal.classList.toggle("opacity-0");
