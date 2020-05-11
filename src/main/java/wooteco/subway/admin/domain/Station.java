290998c85503d9673e8dc2bc37a3ef90b2f90e0f
lalize <printf@kakao.com>
2020-05-11 19:50:16 +0900

    feat: 역 이름 유효성 검사 추가

--- src/main/java/wooteco/subway/admin/domain/Station.java
+++ src/main/java/wooteco/subway/admin/domain/Station.java
@@ -1,6 +1,7 @@
 package wooteco.subway.admin.domain;
 
 import org.springframework.data.annotation.Id;
+import wooteco.subway.admin.exception.InvalidStationNameException;
 
 import java.time.LocalDateTime;
 
@@ -14,9 +15,22 @@ public class Station {
     }
 
     public Station(Long id, String name) {
+        validateName(name);
         this.id = id;
         this.name = name;
         this.createdAt = LocalDateTime.now();
+    }
+
+    private void validateName(String name) {
+        if (name == null || name.equals("")) {
+            throw new InvalidStationNameException("빈 이름");
+        }
+        if (name.contains(" ")) {
+            throw new InvalidStationNameException("이름에 공백이 포함");
+        }
+        if (name.matches(".*[0-9].*")) {
+            throw new InvalidStationNameException("이름에 숫자가 포함");
+        }
     }
 
     public Station(String name) {
---
+++ src/main/java/wooteco/subway/admin/exception/InvalidStationNameException.java
@@ -0,0 +1,7 @@
+package wooteco.subway.admin.exception;
+
+public class InvalidStationNameException extends RuntimeException {
+    public InvalidStationNameException(String message) {
+        super(message);
+    }
+}
