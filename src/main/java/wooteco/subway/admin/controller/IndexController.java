fd0fd5804320980a9c06c36320cae248d3a4043c
moonyoungCHAE <xpf_fl@naver.com>
2020-05-11 16:10:05 +0900

    refactor: 불필요한 주입 제거

--- src/main/java/wooteco/subway/admin/controller/IndexController.java
+++ src/main/java/wooteco/subway/admin/controller/IndexController.java
@@ -2,16 +2,9 @@ package wooteco.subway.admin.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import wooteco.subway.admin.repository.StationRepository;
 
 @Controller
 public class IndexController {
-    private final StationRepository stationRepository;
-
-    public IndexController(StationRepository stationRepository) {
-        this.stationRepository = stationRepository;
-    }
-
     @GetMapping("/")
     public String index() {
         return "index";
