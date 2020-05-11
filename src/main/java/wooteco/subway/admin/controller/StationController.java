ac5b5d57ea91e97b9901bd1554272bdd2de1f6db
yebink <yebin825@gmail.com>
2020-05-11 12:31:39 +0900

    refactor: StationService 분리

--- src/main/java/wooteco/subway/admin/controller/StationController.java
+++ src/main/java/wooteco/subway/admin/controller/StationController.java
@@ -7,39 +7,37 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import wooteco.subway.admin.domain.Station;
 import wooteco.subway.admin.dto.request.StationCreateRequest;
 import wooteco.subway.admin.dto.response.StationResponse;
-import wooteco.subway.admin.repository.StationRepository;
+import wooteco.subway.admin.service.StationService;
 
 import java.net.URI;
 
 @RestController
 public class StationController {
-    private final StationRepository stationRepository;
+    private final StationService stationService;
 
-    public StationController(StationRepository stationRepository) {
-        this.stationRepository = stationRepository;
+    public StationController(StationService stationService) {
+        this.stationService = stationService;
     }
 
     @PostMapping("/stations")
-    public ResponseEntity createStation(@RequestBody StationCreateRequest view) {
-        Station station = view.toStation();
-        Station persistStation = stationRepository.save(station);
+    public ResponseEntity createStation(@RequestBody StationCreateRequest request) {
+        StationResponse response = stationService.createStation(request);
 
         return ResponseEntity
-                .created(URI.create("/stations/" + persistStation.getId()))
-                .body(StationResponse.of(persistStation));
+                .created(URI.create("/stations/" + response.getId()))
+                .body(response);
     }
 
     @GetMapping("/stations")
     public ResponseEntity showStations() {
-        return ResponseEntity.ok().body(stationRepository.findAll());
+        return ResponseEntity.ok().body(stationService.findAll());
     }
 
     @DeleteMapping("/stations/{id}")
     public ResponseEntity deleteStation(@PathVariable("id") Long id) {
-        stationRepository.deleteById(id);
+        stationService.deleteById(id);
         return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/wooteco/subway/admin/repository/StationRepository.java
+++ src/main/java/wooteco/subway/admin/repository/StationRepository.java
@@ -5,7 +5,12 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import wooteco.subway.admin.domain.Station;
 
+import java.util.List;
+
 public interface StationRepository extends CrudRepository<Station, Long> {
+    @Override
+    List<Station> findAll();
+
     @Query("SELECT id FROM station WHERE name = :name ")
     Long findIdByName(@Param("name") String stationName);
 }
