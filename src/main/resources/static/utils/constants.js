587695a1475449f149b9fd3aea68b56306e5ecb2
lalize <printf@kakao.com>
2020-05-12 10:47:23 +0900

    refactor: 중복된 메시지 제거

--- src/main/resources/static/utils/constants.js
+++ src/main/resources/static/utils/constants.js
@@ -6,7 +6,6 @@ export const EVENT_TYPE = {
 export const ERROR_MESSAGE = {
   NOT_EMPTY: "🤔 값을 입력해주세요",
   NOT_EXIST: "🤔 이미 등록된 이름이에요",
-  NOT_EMPTY: "🤔 값을 입력해주세요",
   NOT_BLANK: "🤔 공백이 포함될 수 없어요",
   NOT_NUMBER: "🤔 숫자를 포함할 수 없어요",
   FAIL: "🤔 요청이 실패했어요",
