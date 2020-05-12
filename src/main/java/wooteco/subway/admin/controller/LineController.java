6d98883dc2f54bcafc9c6b3d289843f3fbba7783
Sony <sonypark0204@gmail.com>
2020-05-12 11:34:35 +0900

    refactor: 공통 url 주소 컨트롤러 RequestMapping에 선언

--- src/main/java/wooteco/subway/admin/controller/LineController.java
+++ src/main/java/wooteco/subway/admin/controller/LineController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import wooteco.subway.admin.domain.Line;
@@ -19,6 +20,7 @@ import wooteco.subway.admin.dto.LineStationCreateRequest;
 import wooteco.subway.admin.service.LineService;
 
 @RestController
+@RequestMapping("/lines")
 public class LineController {
 
     private final LineService lineService;
@@ -27,7 +29,7 @@ public class LineController {
         this.lineService = lineService;
     }
 
-    @PostMapping("/lines")
+    @PostMapping
     public ResponseEntity<Long> createLine(@RequestBody LineRequest lineRequest) {
 
         Line line = lineRequest.toLine();
@@ -37,43 +39,43 @@ public class LineController {
             .body(persistLine.getId());
     }
 
-    @GetMapping("/lines")
+    @GetMapping
     public ResponseEntity<List<LineResponse>> showLines() {
         return ResponseEntity.ok().body(lineService.showLines());
     }
 
-    @GetMapping("/lines/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<LineResponse> findLineById(@PathVariable Long id) {
         return ResponseEntity.ok().body(LineResponse.of(lineService.showLine(id)));
     }
 
-    @PutMapping("/lines/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<Void> updateLine(@PathVariable Long id,
         @RequestBody LineRequest view) {
         lineService.updateLine(id, view.toLine());
         return ResponseEntity.ok().build();
     }
 
-    @DeleteMapping("/lines/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteLine(@PathVariable Long id) {
         lineService.deleteLineById(id);
         return ResponseEntity.noContent().build();
     }
 
-    @PostMapping("/lines/{id}/stations")
+    @PostMapping("/{id}/stations")
     public ResponseEntity<Void> addLineStation(@PathVariable Long id,
         @RequestBody LineStationCreateRequest lineStationCreateRequest) {
         lineService.addLineStation(id, lineStationCreateRequest);
         return ResponseEntity.created(URI.create("/line/" + id + "/stations")).build();
     }
 
-    @GetMapping("/lines/{id}/stations")
+    @GetMapping("/{id}/stations")
     public ResponseEntity<LineResponse> findLine(@PathVariable Long id) {
         LineResponse lineResponse = lineService.findLineWithStationsById(id);
         return ResponseEntity.ok().body(lineResponse);
     }
 
-    @DeleteMapping("/lines/{lineId}/stations/{stationId}")
+    @DeleteMapping("/{lineId}/stations/{stationId}")
     public ResponseEntity<Void> deleteLineStation(@PathVariable Long lineId,
         @PathVariable Long stationId) {
         lineService.removeLineStation(lineId, stationId);
--- src/main/java/wooteco/subway/admin/controller/StationController.java
+++ src/main/java/wooteco/subway/admin/controller/StationController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import wooteco.subway.admin.domain.Station;
@@ -17,6 +18,7 @@ import wooteco.subway.admin.dto.StationResponse;
 import wooteco.subway.admin.service.StationService;
 
 @RestController
+@RequestMapping("/stations")
 public class StationController {
     private final StationService stationService;
 
@@ -24,7 +26,7 @@ public class StationController {
         this.stationService = stationService;
     }
 
-    @PostMapping("/stations")
+    @PostMapping
     public ResponseEntity<Long> createStation(@RequestBody StationCreateRequest view) {
         Station station = view.toStation();
         Station persistStation = stationService.save(station);
@@ -33,12 +35,12 @@ public class StationController {
             .body(persistStation.getId());
     }
 
-    @GetMapping("/stations")
+    @GetMapping
     public ResponseEntity<List<StationResponse>> showStations() {
         return ResponseEntity.ok().body(StationResponse.listOf(stationService.findAll()));
     }
 
-    @DeleteMapping("/stations/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteStation(@PathVariable Long id) {
         stationService.removeStation(id);
         return ResponseEntity.noContent().build();
