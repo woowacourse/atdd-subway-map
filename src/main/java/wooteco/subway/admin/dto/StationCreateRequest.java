62d095032097e6de6cd2a6d2ba4536641a7e0bef
lalize <printf@kakao.com>
2020-05-12 10:32:21 +0900

    fix: StationCreateRequest 기본 생성자 추가

--- src/main/java/wooteco/subway/admin/dto/StationCreateRequest.java
+++ src/main/java/wooteco/subway/admin/dto/StationCreateRequest.java
@@ -6,6 +6,9 @@ import wooteco.subway.admin.domain.Station;
 public class StationCreateRequest {
     private String name;
 
+    public StationCreateRequest() {
+    }
+
     public StationCreateRequest(String name) {
         this.name = name;
     }
