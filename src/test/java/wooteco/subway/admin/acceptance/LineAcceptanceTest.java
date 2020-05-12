40b104e907b47ba4f063dd63281ed7ad36b1f1c1
Woonjang Ahn <woonjangahn@gmail.com>
2020-05-12 15:23:30 +0900

    refactor : 인수 테스트 DynamicTest 구조로 변경

--- src/test/java/wooteco/subway/admin/acceptance/LineAcceptanceTest.java
+++ src/test/java/wooteco/subway/admin/acceptance/LineAcceptanceTest.java
@@ -3,16 +3,19 @@ package wooteco.subway.admin.acceptance;
 import static org.assertj.core.api.Assertions.*;
 import static org.hamcrest.Matchers.anyOf;
 import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.DynamicTest.*;
 
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
@@ -40,42 +43,64 @@ public class LineAcceptanceTest {
         return RestAssured.given().log().all();
     }
 
-    @DisplayName("지하철 노선을 관리한다")
-    @Test
-    void manageLine() {
-        // when
-        createLine("신분당선");
-        createLine("1호선");
-        createLine("2호선");
-        createLine("3호선");
-        createLine("3호선");
-        // then
-        List<LineResponse> lines = getLines();
-        assertThat(lines.size()).isEqualTo(4);
+    @DisplayName("노선을 관리한다.")
+    @TestFactory
+    Stream<DynamicTest> dynamicTestsFromCollection() {
+        return Stream.of(
+            dynamicTest("노선을 만드는 요청을 보내 추가한다.", () -> {
+                // when
+                createLine("신분당선");
+                createLine("1호선");
+                createLine("2호선");
+                createLine("3호선");
+                createLine("3호선");
 
-        // when
-        LineResponse line = getLine(lines.get(0).getId());
-        // then
-        assertThat(line.getId()).isNotNull();
-        assertThat(line.getName()).isNotNull();
-        assertThat(line.getStartTime()).isNotNull();
-        assertThat(line.getEndTime()).isNotNull();
-        assertThat(line.getIntervalTime()).isNotNull();
+                // then
+                List<LineResponse> lines = getLines();
+                assertThat(lines.size()).isEqualTo(4);
+            }),
+            dynamicTest("노선에 접근한다.", () -> {
+                // given
+                List<LineResponse> lines = getLines();
 
-        // when
-        LocalTime startTime = LocalTime.of(8, 00);
-        LocalTime endTime = LocalTime.of(22, 00);
-        updateLine(line.getId(), startTime, endTime);
-        // then
-        LineResponse updatedLine = getLine(line.getId());
-        assertThat(updatedLine.getStartTime()).isEqualTo(startTime);
-        assertThat(updatedLine.getEndTime()).isEqualTo(endTime);
+                // when
+                LineResponse line = lines.get(0);
 
-        // when
-        deleteLine(line.getId());
-        // then
-        List<LineResponse> linesAfterDelete = getLines();
-        assertThat(linesAfterDelete.size()).isEqualTo(3);
+                // then
+                assertThat(line.getId()).isNotNull();
+                assertThat(line.getName()).isNotNull();
+                assertThat(line.getStartTime()).isNotNull();
+                assertThat(line.getEndTime()).isNotNull();
+                assertThat(line.getIntervalTime()).isNotNull();
+            }),
+            dynamicTest("노선을 업데이트한다.", () -> {
+                // given
+                List<LineResponse> lines = getLines();
+                LineResponse line = lines.get(0);
+
+                // when
+                LocalTime startTime = LocalTime.of(8, 00);
+                LocalTime endTime = LocalTime.of(22, 00);
+                updateLine(line.getId(), startTime, endTime);
+
+                // then
+                LineResponse updatedLine = getLine(line.getId());
+                assertThat(updatedLine.getStartTime()).isEqualTo(startTime);
+                assertThat(updatedLine.getEndTime()).isEqualTo(endTime);
+            }),
+            dynamicTest("노선을 삭제한다.", () -> {
+                // given
+                List<LineResponse> lines = getLines();
+                LineResponse line = lines.get(0);
+
+                // when
+                deleteLine(line.getId());
+
+                // then
+                List<LineResponse> linesAfterDelete = getLines();
+                assertThat(linesAfterDelete.size()).isEqualTo(3);
+            })
+        );
     }
 
     private LineResponse getLine(Long id) {
--- src/test/java/wooteco/subway/admin/acceptance/LineStationAcceptanceTest.java
+++ src/test/java/wooteco/subway/admin/acceptance/LineStationAcceptanceTest.java
@@ -3,16 +3,19 @@ package wooteco.subway.admin.acceptance;
 import static org.assertj.core.api.Assertions.*;
 import static org.hamcrest.Matchers.anyOf;
 import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.DynamicTest.*;
 
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
@@ -56,103 +59,116 @@ public class LineStationAcceptanceTest {
      *     <br/>Then 지하철역 목록을 응답 받는다.
      *     <br/>And 제외한 지하철역이 목록에 존재하지 않는다.
      */
-
     @DisplayName("지하철 노선에서 지하철역 추가 / 제외")
-    @Test
-    void manageLineStation() {
+    @TestFactory
+    Stream<DynamicTest> manageLineStation() {
         // Given 지하철역이 여러 개 추가되어있다.
         createStation("낙성대역");
         createStation("압구정로데오역");
         createStation("오리역");
         createStation("잠실역");
-        createStation("삼송역");
-        createStation("행신역");
-        createStation("대구역");
 
         // And 지하철 노선이 추가되어있다.
         createLine("1호선");
 
-        // When 지하철 노선에 지하철역을 등록하는 요청을 한다.
-        StationResponse naksungdae = getStations().get(0);
-        StationResponse apgujeongRodeo = getStations().get(1);
-        StationResponse ori = getStations().get(2);
-        StationResponse jamsil = getStations().get(3);
-        LineResponse line = getLine(1L);
-        addStationToLine(1L, new LineStationCreateRequest(null, apgujeongRodeo.getId(), 1, 1));
+        return Stream.of(
+            dynamicTest("지하철 노선 추가", () -> {
+                // When 지하철 노선에 지하철역을 등록하는 요청을 한다.
+                StationResponse station1 = getStations().get(1);
+                LineResponse line = getLine(1L);
+                addStationToLine(1L, new LineStationCreateRequest(null, station1.getId(), 1, 1));
 
-        LineResponse updatedLine = getLine(1L);
-        // Then 지하철역이 노선에 추가 되었다. (압구정로데오역)
-        assertThat(updatedLine.getStations()).hasSize(line.getStations().size() + 1);
+                LineResponse updatedLine = getLine(1L);
+                // Then 지하철역이 노선에 추가 되었다. (압구정로데오역)
+                assertThat(updatedLine.getStations()).hasSize(line.getStations().size() + 1);
 
-        // And 새로 추가한 지하철역을 목록에서 찾는다.
-        StationResponse stationAdded = updatedLine.getStations().get(0);
-        assertThat(stationAdded.getName()).isEqualTo(apgujeongRodeo.getName());
+                // And 새로 추가한 지하철역을 목록에서 찾는다.
+                StationResponse stationAdded = updatedLine.getStations().get(0);
+                assertThat(stationAdded.getName()).isEqualTo(station1.getName());
+            }),
+            dynamicTest("지하철역을 노선의 마지막 지점으로 추가한다.", () -> {
+                // When 지하철역을 노선의 마지막 지점으로 추가한다. (압구정로데오역 - 낙성대역)
+                StationResponse station1 = getStations().get(1);
+                StationResponse station0 = getStations().get(0);
+                addStationToLine(1L, new LineStationCreateRequest(station1.getId(), station0.getId(), 1, 1));
 
-        // When 지하철역을 노선의 마지막 지점으로 추가한다. (압구정로데오역 - 낙성대역)
-        addStationToLine(1L, new LineStationCreateRequest(apgujeongRodeo.getId(), naksungdae.getId(), 1, 1));
+                // When 지하철역 노선의 지하철역 목록 조회 요청을 한다.
+                LineResponse linePushed = getLine(1L);
 
-        // When 지하철역 노선의 지하철역 목록 조회 요청을 한다.
-        LineResponse linePushed = getLine(1L);
+                // Then 노선 마지막에 지하철역이 추가되었다.
+                StationResponse stationPushed = linePushed.getStations().get(linePushed.getStations().size() - 1);
+                assertThat(stationPushed.getName()).isEqualTo(station0.getName());
+            }),
+            dynamicTest("지하철역을 노선의 시작 지점으로 추가한다.", () -> {
+                // When 지하철역을 노선의 시작 지점으로 추가한다. (오리역 - 압구정로데오역 - 낙성대역)
+                StationResponse station2 = getStations().get(2);
+                addStationToLine(1L, new LineStationCreateRequest(null, station2.getId(), 1, 1));
 
-        // Then 노선 마지막에 지하철역이 추가되었다.
-        StationResponse stationPushed = linePushed.getStations().get(linePushed.getStations().size() - 1);
-        assertThat(stationPushed.getName()).isEqualTo(naksungdae.getName());
+                // When 지하철역 노선의 지하철역 목록 조회 요청을 한다.
+                LineResponse lineUnshifted = getLine(1L);
 
-        // When 지하철역을 노선의 시작 지점으로 추가한다. (오리역 - 압구정로데오역 - 낙성대역)
-        addStationToLine(1L, new LineStationCreateRequest(null, ori.getId(), 1, 1));
+                // Then 노선 맨 앞에 지하철역이 추가되었다.
+                StationResponse unshiftedStation = lineUnshifted.getStations().get(0);
+                assertThat(unshiftedStation.getName()).isEqualTo(station2.getName());
+            }),
+            dynamicTest("지하철역을 두 역 사이에 추가한다.", () -> {
+                // When 지하철역을 두 역 사이에 추가한다. (오리역 - 압구정로데오역 - 잠실역 - 낙성대역)
+                StationResponse station1 = getStations().get(1);
+                StationResponse station3 = getStations().get(3);
+                addStationToLine(1L, new LineStationCreateRequest(station1.getId(), station3.getId(), 1, 1));
 
-        // When 지하철역 노선의 지하철역 목록 조회 요청을 한다.
-        LineResponse lineUnshifted = getLine(1L);
+                // When 지하철역 노선의 지하철역 목록 조회 요청을 한다.
+                LineResponse lineInsertedWithinTwoStations = getLine(1L);
 
-        // Then 노선 맨 앞에 지하철역이 추가되었다.
-        StationResponse unshiftedStation = lineUnshifted.getStations().get(0);
-        assertThat(unshiftedStation.getName()).isEqualTo(ori.getName());
+                // Then 두 역 사이에 지하철역이 추가되었다.
+                StationResponse insertedStation = lineInsertedWithinTwoStations.getStations().get(2);
+                assertThat(insertedStation.getName()).isEqualTo(station3.getName());
+            }),
+            dynamicTest("존재하지 않는 지하철역을 추가할 수 없다.", () -> {
+                // When id가 빈 값인 지하철역을 추가한다.
+                StationResponse station1 = getStations().get(1);
+                addStationToLine(1L, new LineStationCreateRequest(station1.getId(), null, 1, 1));
 
-        // When 지하철역을 두 역 사이에 추가한다. (오리역 - 압구정로데오역 - 잠실역 - 낙성대역)
-        addStationToLine(1L, new LineStationCreateRequest(apgujeongRodeo.getId(), jamsil.getId(), 1, 1));
+                // Then 지하철 역이 추가되지 않는다.
+                LineResponse lineWithEmptyInsertion = getLine(1L);
+                assertThat(lineWithEmptyInsertion.getStations()).hasSize(4);
 
-        // When 지하철역 노선의 지하철역 목록 조회 요청을 한다.
-        LineResponse lineInsertedWithinTwoStations = getLine(1L);
+                // When 존재하지 않는 지하철 역을 노선에 추가하였다.
+                addStationToLine(1L, new LineStationCreateRequest(station1.getId(), Long.MAX_VALUE, 1, 1));
 
-        // Then 두 역 사이에 지하철역이 추가되었다.
-        StationResponse insertedStation = lineInsertedWithinTwoStations.getStations().get(2);
-        assertThat(insertedStation.getName()).isEqualTo(jamsil.getName());
+                // Then 404 상태와 함께 지하철 역이 추가되지 않는다.
+                LineResponse lineWithInvalidInsertion = getLine(1L);
+                assertThat(lineWithInvalidInsertion.getStations()).hasSize(4);
+            }),
+            dynamicTest("이미 노선에 존재하는 두 역을 추가할 수 없다.", () -> {
+                // When 이미 존재하는 두 역을 노선에 추가하였다.
+                StationResponse station2 = getStations().get(2);
+                StationResponse station0 = getStations().get(0);
+                addStationToLine(1L, new LineStationCreateRequest(station2.getId(), station0.getId(), 1, 1));
 
-        // When id가 빈 값인 지하철역을 추가한다.
-        addStationToLine(1L, new LineStationCreateRequest(apgujeongRodeo.getId(), null, 1, 1));
+                // Then 400 상태와 함께 지하철 역이 추가되지 않는다.
+                LineResponse lineWithDuplicate = getLine(1L);
+                assertThat(lineWithDuplicate.getStations()).hasSize(4);
+            }),
+            dynamicTest("지하철역을 노선에서 삭제한다.", () -> {
+                // When 지하철 노선에 포함된 특정 지하철역을 제외하는 요청을 한다.
+                LineResponse line = getLine(1L);
+                StationResponse station0 = getStations().get(0);
+                deleteStationFromLine(line.getId(), station0.getId());
 
-        // Then 지하철 역이 추가되지 않는다.
-        LineResponse lineWithEmptyInsertion = getLine(1L);
-        assertThat(lineWithEmptyInsertion.getStations()).hasSize(4);
+                // When 지하철 노선의 지하철역 목록 조회 요청을 한다.
+                LineResponse stationExcludedLine = getLine(1L);
 
-        // When 존재하지 않는 지하철 역을 노선에 추가하였다.
-        addStationToLine(1L, new LineStationCreateRequest(apgujeongRodeo.getId(), Long.MAX_VALUE, 1, 1));
+                // Then 지하철역이 노선에서 제거 되었다.
+                assertThat(stationExcludedLine.getStations()).hasSize(3);
 
-        // Then 404 상태와 함께 지하철 역이 추가되지 않는다.
-        LineResponse lineWithInvalidInsertion = getLine(1L);
-        assertThat(lineWithInvalidInsertion.getStations()).hasSize(4);
-
-        // When 이미 존재하는 두 역을 노선에 추가하였다.
-        addStationToLine(1L, new LineStationCreateRequest(ori.getId(), naksungdae.getId(), 1, 1));
-
-        // Then 400 상태와 함께 지하철 역이 추가되지 않는다.
-        LineResponse lineWithDuplicate = getLine(1L);
-        assertThat(lineWithDuplicate.getStations()).hasSize(4);
-
-        // When 지하철 노선에 포함된 특정 지하철역을 제외하는 요청을 한다.
-        deleteStationFromLine(line.getId(), naksungdae.getId());
-
-        // When 지하철 노선의 지하철역 목록 조회 요청을 한다.
-        LineResponse stationExcludedLine = getLine(1L);
-
-        // Then 지하철역이 노선에서 제거 되었다.
-        assertThat(stationExcludedLine.getStations()).hasSize(3);
-
-        // And 제외한 지하철역이 목록에 존재하지 않는다.
-        assertThat(stationExcludedLine.getStations()
-            .stream()
-            .map(StationResponse::getName))
-            .doesNotContain(naksungdae.getName());
+                // And 제외한 지하철역이 목록에 존재하지 않는다.
+                assertThat(stationExcludedLine.getStations()
+                    .stream()
+                    .map(StationResponse::getName))
+                    .doesNotContain(station0.getName());
+            })
+        );
     }
 
     private void deleteStationFromLine(Long lineId, Long stationId) {
