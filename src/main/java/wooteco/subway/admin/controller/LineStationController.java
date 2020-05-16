79eed680fadc87733347294264f05bd084d221dc
Tak Hyungmin <milkho11@naver.com>
2020-05-16 12:38:03 +0900

    fix: 구간 추가 시 구간 보여주기 기능

--- src/main/java/wooteco/subway/admin/controller/LineStationController.java
+++ src/main/java/wooteco/subway/admin/controller/LineStationController.java
@@ -11,44 +11,44 @@ import wooteco.subway.admin.service.StationService;
 @RestController
 public class LineStationController {
 
-	private final LineService lineService;
-	private final StationService stationService;
+    private final LineService lineService;
+    private final StationService stationService;
 
-	public LineStationController(LineService lineService, StationService stationService) {
-		this.lineService = lineService;
-		this.stationService = stationService;
-	}
+    public LineStationController(LineService lineService, StationService stationService) {
+        this.lineService = lineService;
+        this.stationService = stationService;
+    }
 
-	@PostMapping("/line-stations")
-	public ResponseEntity<Void> createLineStation(@RequestBody LineStationRequest view) {
-		if (view.getPreStationName().isEmpty()) {
-			Station station = stationService.findByName(view.getStationName());
+    @PostMapping("/line-stations")
+    public ResponseEntity<Void> createLineStation(@RequestBody LineStationRequest view) {
+        if (view.getPreStationName().isEmpty()) {
+            Station station = stationService.findByName(view.getStationName());
 
-			lineService.addLineStation(view.getLineId(),
-					new LineStationCreateRequest(null, station.getId(), 0, 0));
+            lineService.addLineStation(view.getLineId(),
+                    new LineStationCreateRequest(null, station.getId(), 0, 0));
 
-			return ResponseEntity
-					.ok()
-					.build();
-		}
-		Station preStation = stationService.findByName(view.getPreStationName());
-		Station station = stationService.findByName(view.getStationName());
+            return ResponseEntity
+                    .ok()
+                    .build();
+        }
+        Station preStation = stationService.findByName(view.getPreStationName());
+        Station station = stationService.findByName(view.getStationName());
 
-		lineService.addLineStation(view.getLineId(),
-				new LineStationCreateRequest(preStation.getId(), station.getId(), 0, 0));
+        lineService.addLineStation(view.getLineId(),
+                new LineStationCreateRequest(preStation.getId(), station.getId(), 0, 0));
 
-		return ResponseEntity
-				.ok()
-				.build();
-	}
+        return ResponseEntity
+                .ok()
+                .build();
+    }
 
-	@DeleteMapping("/line-stations/{lineId}/{stationId}")
-	public ResponseEntity<Void> deleteLineStation(@PathVariable("lineId") Long lineId, @PathVariable("stationId") Long stationId) {
+    @DeleteMapping("/line-stations/{lineId}/{stationId}")
+    public ResponseEntity<Void> deleteLineStation(@PathVariable("lineId") Long lineId, @PathVariable("stationId") Long stationId) {
 
-		lineService.removeLineStation(lineId, stationId);
+        lineService.removeLineStation(lineId, stationId);
 
-		return ResponseEntity
-				.ok()
-				.build();
-	}
+        return ResponseEntity
+                .ok()
+                .build();
+    }
 }
--- src/main/resources/static/js/views/AdminEdge.js
+++ src/main/resources/static/js/views/AdminEdge.js
@@ -6,94 +6,104 @@ import api from "../../api/index.js";
 
 
 function AdminEdge() {
-  const $subwayLinesSlider = document.querySelector(".subway-lines-slider");
-  const $createStationToLineButton = document.querySelector("#submit-button");
-  const $lineIdInput = document.querySelector("#station-select-options");
-  const $preStationInput = document.querySelector("#depart-station-name");
-  const $stationInput = document.querySelector("#arrival-station-name");
+    const $subwayLinesSlider = document.querySelector(".subway-lines-slider");
+    const $createStationToLineButton = document.querySelector("#submit-button");
+    const $lineIdInput = document.querySelector("#station-select-options");
+    const $preStationInput = document.querySelector("#depart-station-name");
+    const $stationInput = document.querySelector("#arrival-station-name");
 
-  const createSubwayEdgeModal = new Modal();
+    const createSubwayEdgeModal = new Modal();
 
-  const initSubwayLinesSlider = async () => {
-    await api.line.get()
-      .then(data => {
-        $subwayLinesSlider.innerHTML = data
-          .map(line => subwayLinesItemTemplate(line))
-          .join("");
-      });
+    const initSubwayLinesSlider = async () => {
+        await api.line.get()
+            .then(data => {
+                $subwayLinesSlider.innerHTML = data
+                    .map(line => subwayLinesItemTemplate(line))
+                    .join("");
+            });
 
-    tns({
-      container: ".subway-lines-slider",
-      loop: true,
-      slideBy: "page",
-      speed: 400,
-      autoplayButtonOutput: false,
-      mouseDrag: true,
-      lazyload: true,
-      controlsContainer: "#slider-controls",
-      items: 1,
-      edgePadding: 25
-    });
-  };
-
-  const initSubwayLineOptions = async () => {
-    const subwayLineOptionTemplate = await api.line.get()
-      .then(data => {
-        return data
-          .map(line => optionTemplate(line))
-          .join("");
-      });
-
-    const $stationSelectOptions = document.querySelector(
-      "#station-select-options"
-    );
-
-    $stationSelectOptions.insertAdjacentHTML("afterbegin", subwayLineOptionTemplate);
-  };
-
-  const onRemoveStationHandler = event => {
-    const $target = event.target;
-    const isDeleteButton = $target.classList.contains("mdi-delete");
-
-    if (isDeleteButton) {
-      const lineId = $target.closest(".line").getAttribute("data-line-id");
-      const targetId = $target.closest("div").dataset.stationId;
-
-      api.lineStation.delete(lineId, targetId);
-
-      $target.closest(".list-item").remove();
-
-    }
-  };
-
-  const onCreateStationToLine = event => {
-    event.preventDefault();
-    const lindId = $lineIdInput.options[$lineIdInput.selectedIndex].dataset.id;
-    const preStationName = $preStationInput.value;
-    const stationName = $stationInput.value;
-
-    const request = {
-      lineId: lindId,
-      preStationName: preStationName,
-      stationName: stationName
+        tns({
+            container: ".subway-lines-slider",
+            loop: true,
+            slideBy: "page",
+            speed: 400,
+            autoplayButtonOutput: false,
+            mouseDrag: true,
+            lazyload: true,
+            controlsContainer: "#slider-controls",
+            items: 1,
+            edgePadding: 25
+        });
     };
-    api.lineStation.create(request)
-      .then(data => console.log(data));
-  };
 
-  const initEventListeners = () => {
-    $subwayLinesSlider.addEventListener(
-      EVENT_TYPE.CLICK,
-      onRemoveStationHandler
-    );
-    $createStationToLineButton.addEventListener(EVENT_TYPE.CLICK, onCreateStationToLine);
-  };
+    const initSubwayLineOptions = async () => {
+        const subwayLineOptionTemplate = await api.line.get()
+            .then(data => {
+                return data
+                    .map(line => optionTemplate(line))
+                    .join("");
+            });
 
-  this.init = () => {
-    initSubwayLinesSlider();
-    initSubwayLineOptions();
-    initEventListeners();
-  };
+        const $stationSelectOptions = document.querySelector(
+            "#station-select-options"
+        );
+
+        $stationSelectOptions.insertAdjacentHTML("afterbegin", subwayLineOptionTemplate);
+    };
+
+    const onRemoveStationHandler = event => {
+        const $target = event.target;
+        const isDeleteButton = $target.classList.contains("mdi-delete");
+
+        if (isDeleteButton) {
+            const lineId = $target.closest(".line").getAttribute("data-line-id");
+            const targetId = $target.closest("div").dataset.stationId;
+
+            api.lineStation.delete(lineId, targetId);
+
+            $target.closest(".list-item").remove();
+
+        }
+    };
+
+    const onCreateStationToLine = event => {
+        event.preventDefault();
+        const lindId = $lineIdInput.options[$lineIdInput.selectedIndex].dataset.id;
+        const preStationName = $preStationInput.value;
+        const stationName = $stationInput.value;
+
+        const request = {
+            lineId: lindId,
+            preStationName: preStationName,
+            stationName: stationName
+        };
+
+        fetch(`/line-stations`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(request)
+        }).then(data => createSubwayEdgeModal.toggle())
+            .then(data => initSubwayLinesSlider());
+
+        // api.lineStation.create(request)
+        //     .then(data => initSubwayLinesSlider);
+    };
+
+    const initEventListeners = () => {
+        $subwayLinesSlider.addEventListener(
+            EVENT_TYPE.CLICK,
+            onRemoveStationHandler
+        );
+        $createStationToLineButton.addEventListener(EVENT_TYPE.CLICK, onCreateStationToLine);
+    };
+
+    this.init = () => {
+        initSubwayLinesSlider();
+        initSubwayLineOptions();
+        initEventListeners();
+    };
 }
 
 const adminEdge = new AdminEdge();
