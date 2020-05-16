4adc88c1849197fbb47cc79c13e3cfc8580cec0d
gunhee lee <dlrjsgml42@naver.com>
2020-05-16 17:03:06 +0900

    feat: 예외 처리를 위한 컨트롤러 추가

---
+++ src/main/java/wooteco/subway/admin/controller/ExceptionController.java
@@ -0,0 +1,13 @@
+package wooteco.subway.admin.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionController {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handler(Exception e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+}
