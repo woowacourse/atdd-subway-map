5a346c4cb5044d65ed66ecf186b6f6d5dbea35dc
soring <jnsorn@gmail.com>
2020-05-17 13:10:43 +0900

    refactor : this를 이용하여 생성자 중복 제거

--- src/main/java/wooteco/subway/admin/domain/line/Line.java
+++ src/main/java/wooteco/subway/admin/domain/line/Line.java
@@ -14,110 +14,100 @@ import wooteco.subway.admin.domain.line.relation.LineStations;
 import wooteco.subway.admin.domain.line.vo.LineTimeTable;
 
 public class Line {
-    @Id
-    private Long id;
-    private String name;
+	@Id
+	private Long id;
+	private String name;
 
-    @Embedded.Nullable
-    private LineTimeTable lineTimeTable;
+	@Embedded.Nullable
+	private LineTimeTable lineTimeTable;
 
-    private String bgColor;
+	private String bgColor;
 
-    @Embedded.Empty
-    private LineStations stations;
+	@Embedded.Empty
+	private LineStations stations;
 
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
+	private LocalDateTime createdAt;
+	private LocalDateTime updatedAt;
 
-    @PersistenceConstructor
-    public Line(Long id, String name, LineTimeTable lineTimeTable,
-        String bgColor, LineStations stations) {
-        this.id = id;
-        this.name = name;
-        this.lineTimeTable = lineTimeTable;
-        this.bgColor = bgColor;
-        this.stations = stations;
-        this.createdAt = LocalDateTime.now();
-        this.updatedAt = LocalDateTime.now();
-    }
+	@PersistenceConstructor
+	public Line(Long id, String name, LineTimeTable lineTimeTable,
+		String bgColor, LineStations stations) {
+		this.id = id;
+		this.name = name;
+		this.lineTimeTable = lineTimeTable;
+		this.bgColor = bgColor;
+		this.stations = stations;
+		this.createdAt = LocalDateTime.now();
+		this.updatedAt = LocalDateTime.now();
+	}
 
-    public Line(Long id, String name, LocalTime start, LocalTime end, int intervalTime,
-        String bgColor, Set<LineStation> stations) {
-        this.id = id;
-        this.name = name;
-        this.lineTimeTable = new LineTimeTable(start, end, intervalTime);
-        this.bgColor = bgColor;
-        this.stations = new LineStations(stations);
-        this.createdAt = LocalDateTime.now();
-        this.updatedAt = LocalDateTime.now();
-    }
+	public Line(Long id, String name, LocalTime start, LocalTime end, int intervalTime,
+		String bgColor, Set<LineStation> stations) {
+		this(id, name, new LineTimeTable(start, end, intervalTime), bgColor, new LineStations(stations));
+	}
 
-    public void update(Line line) {
-        if (line.getName() != null) {
-            this.name = line.getName();
-        }
-        if (line.getLineTimeTable() != null) {
-            this.lineTimeTable = line.getLineTimeTable();
-        }
-        if (line.getBgColor() != null) {
-            this.bgColor = line.getBgColor();
-        }
-        this.updatedAt = LocalDateTime.now();
-    }
+	public void update(Line line) {
+		if (line.getName() != null) {
+			this.name = line.getName();
+		}
+		if (line.lineTimeTable != null) {
+			this.lineTimeTable = line.lineTimeTable;
+		}
+		if (line.getBgColor() != null) {
+			this.bgColor = line.getBgColor();
+		}
+		this.updatedAt = LocalDateTime.now();
+	}
 
-    public void addLineStation(LineStation lineStation) {
-        stations.add(lineStation);
-    }
+	public void addLineStation(LineStation lineStation) {
+		stations.add(lineStation);
+	}
 
-    public void removeLineStationById(Long stationId) {
-        stations.remove(stationId);
-    }
+	public void removeLineStationById(Long stationId) {
+		stations.remove(stationId);
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public LocalTime getStartTime() {
-        return lineTimeTable.getStartTime();
-    }
+	public LocalTime getStartTime() {
+		return lineTimeTable.getStartTime();
+	}
 
-    public LocalTime getEndTime() {
-        return lineTimeTable.getEndTime();
-    }
+	public LocalTime getEndTime() {
+		return lineTimeTable.getEndTime();
+	}
 
-    public int getIntervalTime() {
-        return lineTimeTable.getIntervalTime();
-    }
+	public int getIntervalTime() {
+		return lineTimeTable.getIntervalTime();
+	}
 
-    public String getBgColor() {
-        return bgColor;
-    }
+	public String getBgColor() {
+		return bgColor;
+	}
 
-    public Set<LineStation> getStations() {
-        return stations.getLineStations();
-    }
+	public Set<LineStation> getStations() {
+		return stations.getLineStations();
+	}
 
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
 
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
-    }
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
 
-    public LineTimeTable getLineTimeTable() {
-        return lineTimeTable;
-    }
+	public List<Long> getSortedStationsId() {
+		return stations.getSortedStationsId();
+	}
 
-    public List<Long> getSortedStationsId() {
-        return stations.getSortedStationsId();
-    }
-
-    public List<Long> getStationsId() {
-        return stations.getIds();
-    }
+	public List<Long> getStationsId() {
+		return stations.getIds();
+	}
 }
