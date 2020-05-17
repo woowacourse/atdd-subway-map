301a5b10faffd337e5dd875c63be6973e8b62b36
황준호 <42054054+hwanghe159@users.noreply.github.com>
2020-05-17 17:23:49 +0900

    feat : LineTest 같은 데이터 셋을 공유하지 않도록 수정

--- src/main/java/wooteco/subway/admin/dto/StationCreateRequest.java
+++ src/main/java/wooteco/subway/admin/dto/StationCreateRequest.java
@@ -1,8 +1,5 @@
 package wooteco.subway.admin.dto;
 
-
-import wooteco.subway.admin.domain.Station;
-
 import javax.validation.constraints.NotBlank;
 
 public class StationCreateRequest {
--- src/test/java/wooteco/subway/admin/domain/LineTest.java
+++ src/test/java/wooteco/subway/admin/domain/LineTest.java
@@ -1,6 +1,5 @@
 package wooteco.subway.admin.domain;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalTime;
@@ -11,24 +10,28 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class LineTest {
     private Line line;
 
-    @BeforeEach
-    void setUp() {
+    @Test
+    void getLineStations() {
         line = new Line(1L,"비내리는호남선",  LocalTime.of(05, 30), LocalTime.of(22, 30),5, "bg-yellow-700");
         line.addLineStation(new LineStation(null, 1L, 10, 10));
         line.addLineStation(new LineStation(1L, 2L, 10, 10));
         line.addLineStation(new LineStation(2L,3L,10,10));
-    }
 
-    @Test
-    void getLineStations() {
         List<Long> stationIds = line.getLineStationsId();
+
         assertThat(stationIds.size()).isEqualTo(3);
         assertThat(stationIds.get(0)).isEqualTo(1L);
+        assertThat(stationIds.get(1)).isEqualTo(2L);
         assertThat(stationIds.get(2)).isEqualTo(3L);
     }
 
     @Test
     void removeOneLineStation() {
+        line = new Line(1L,"비내리는호남선",  LocalTime.of(05, 30), LocalTime.of(22, 30),5, "bg-yellow-700");
+        line.addLineStation(new LineStation(null, 1L, 10, 10));
+        line.addLineStation(new LineStation(1L, 2L, 10, 10));
+        line.addLineStation(new LineStation(2L,3L,10,10));
+
         line.removeLineStationById(1L);
 
         assertThat(line.getLineStationsId()).hasSize(2);
