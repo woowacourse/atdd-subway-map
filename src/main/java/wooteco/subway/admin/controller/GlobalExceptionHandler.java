520000606827684f66c9a8ce98b4b6f84453e951
Ramen6315 <ramen6315@gmail.com>
2020-05-24 23:21:58 +0900

    feat: 처리 불가능한 예외 상황을 처리 하는 ExceptionHandler 추가

--- src/main/java/wooteco/subway/admin/controller/GlobalExceptionHandler.java
+++ src/main/java/wooteco/subway/admin/controller/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package wooteco.subway.admin.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -20,6 +21,13 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(NoSuchElementException.class)
     public ResponseEntity<ErrorResponse> noSuchElementException(NoSuchElementException e) {
         ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+        ResponseEntity.status(HttpStatus.FOUND).body(errorResponse);
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> ramainingException(Exception e) {
+        ErrorResponse errorResponse = new ErrorResponse(e.getMessage() + "UnHandleException");
         return ResponseEntity.badRequest().body(errorResponse);
     }
 }
