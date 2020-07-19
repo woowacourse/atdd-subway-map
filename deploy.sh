b266146a7fcccbed80e0d00035e58e32238870e8
hj <a301dks@naver.com>
2020-07-19 20:47:19 +0900

    깃헙 액션 디플로이 테스트55

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 REPOSITORY=/opt/Deploy-App
 cd $REPOSITORY
 
-APP_NAME=action_codedeploy
+APP_NAME=atdd-subway-admin
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
@@ -11,7 +11,7 @@ CURRENT_PID=$(pgrep -f $APP_NAME)
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> 종료할 것 없음."
+  echo "> 종료할것 없음."
 else
   echo "> kill -9 $CURRENT_PID"
   kill -15 $CURRENT_PID
