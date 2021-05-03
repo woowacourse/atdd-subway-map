46f7772e30f5396ea3149aaa45d4f14290dd3403
임동준 <im1@woowahan.com>
2021-05-04 07:33:54 +0900

    refactor: remove login button in Header.vue

---
+++ frontend/src/pages/shared/Header.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-app-bar flat app color="amber" clipped-left :height="65">
+    <v-toolbar-title class="pl-0 mr-12 align-center relative bottom-2">
+      <v-btn to="/" text>
+        <div class="title">
+          <span>RUNNINGMAP</span>
+        </div>
+      </v-btn>
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn
+      v-for="navItem in navItems"
+      :key="navItem._id"
+      :to="navItem.link"
+      text
+      >{{ navItem.text }}</v-btn
+    >
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      navItems: [
+        {
+          _id: 1,
+          link: "/stations",
+          text: "역 관리"
+        },
+        {
+          _id: 2,
+          link: "/lines",
+          text: "노선 관리"
+        },
+        {
+          _id: 3,
+          link: "/sections",
+          text: "구간 관리"
+        },
+        {
+          _id: 4,
+          link: "/path",
+          text: "경로 검색"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.header-logo {
+  width: 25px;
+}
+</style>
