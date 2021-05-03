05e4c20c8d6f643d023924109d0cbf423c40796d
gracefulBrown <brown@woowahan.com>
2021-05-04 01:52:21 +0900

    feat: default code

--- src/main/java/wooteco/subway/admin/repository/StationRepository.java
+++
@@ -1,9 +0,0 @@
-package wooteco.subway.admin.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import wooteco.subway.admin.domain.Station;
-
-import java.util.Set;
-
-public interface StationRepository extends CrudRepository<Station, Long> {
-}
