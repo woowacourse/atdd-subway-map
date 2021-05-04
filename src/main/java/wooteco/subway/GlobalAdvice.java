2e0bf9c4c0c2d3e8bf47c597a69d7169162ef096
pkeugine <pkeugine@gmail.com>
2021-05-05 03:40:59 +0900

    feat : GlobalAdvice에 LineException 추가

--- src/main/java/wooteco/subway/GlobalAdvice.java
+++ src/main/java/wooteco/subway/GlobalAdvice.java
@@ -3,20 +3,25 @@ package wooteco.subway;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import wooteco.subway.line.LineException;
 import wooteco.subway.station.StationException;
 
 @ControllerAdvice
-final public class GlobalAdvice {
+public final class GlobalAdvice {
 
     @ExceptionHandler(RuntimeException.class)
-    final public ResponseEntity<String> handleUnknownException(final RuntimeException e) {
+    public ResponseEntity<String> handleUnknownException(final RuntimeException e) {
         final String message = "unhandled exceptions";
         return ResponseEntity.badRequest().body(message);
     }
 
     @ExceptionHandler(StationException.class)
-    final public ResponseEntity<String> handleStationException(final StationException e) {
+    public ResponseEntity<String> handleStationException(final StationException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
     }
 
+    @ExceptionHandler(LineException.class)
+    public ResponseEntity<String> handleLineException(final LineException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
 }
