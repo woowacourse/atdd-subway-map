2b4f7435641c1a29f970e8231ccac0f01deea9c7
Wannte <T.wannte@gmail.com>
2021-05-04 16:07:47 +0900

    feat(Line): 노선 목록 조회 기능 구현

--- src/main/java/wooteco/subway/line/LineController.java
+++ src/main/java/wooteco/subway/line/LineController.java
@@ -1,13 +1,13 @@
 package wooteco.subway.line;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("/lines")
@@ -25,6 +25,15 @@ public class LineController {
                 newLine.getColor(), Collections.emptyList());
         return ResponseEntity.created(URI.create("/lines/" + lineResponse.getId()))
                              .body(lineResponse);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<LineResponse>> showLines() {
+        List<Line> lines = lineService.findAll();
+        List<LineResponse> lineResponses = lines.stream()
+                                          .map(line -> new LineResponse(line.getId(), line.getName(), line.getColor()))
+                                          .collect(Collectors.toList());
+        return ResponseEntity.ok().body(lineResponses);
     }
 
 }
