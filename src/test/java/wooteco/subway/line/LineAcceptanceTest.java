1261d0b38d587f08870c63514d6d1bb026e98519
cjy17 <cjy1705@naver.com>
2021-05-04 18:03:14 +0900

    test : 노선 생성 테스트 구현

---
+++ src/test/java/wooteco/subway/line/LineAcceptanceTest.java
@@ -0,0 +1,71 @@
+package wooteco.subway.line;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import wooteco.subway.AcceptanceTest;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LineAcceptanceTest extends AcceptanceTest {
+
+    @BeforeEach
+    void setUpStations() {
+        // given
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "강남역");
+        RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post("/stations")
+                .then().log().all()
+                .extract();
+
+        // given
+        Map<String, String> params2 = new HashMap<>();
+        params2.put("name", "성수역");
+        RestAssured.given().log().all()
+                .body(params2)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post("/stations")
+                .then().log().all()
+                .extract();
+    }
+
+    @DisplayName("지하철 노선을 생성한다.")
+    @Test
+    void createLine() {
+        // given
+        Map<String, Object> params = new HashMap<>();
+        params.put("name", "테스트선");
+        params.put("color", "red");
+        params.put("upStationId", 1);
+        params.put("downStationId", 2);
+        params.put("distance", "1000");
+        params.put("extraFare", "100");
+
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post("/lines")
+                .then().log().all()
+                .extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        assertThat(response.header("Location")).isNotBlank();
+    }
+}
