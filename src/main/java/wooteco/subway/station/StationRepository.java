5ce6aeece9a40e0aa4eb26d1ef26e823bba887a3
Kimun Kim <kkm97351@gmail.com>
2021-05-04 16:26:13 +0900

    refactor: Service 분리, StationDao 인터페이스화, AppConfig로 구현체 생성

---
+++ src/main/java/wooteco/subway/station/StationRepository.java
@@ -0,0 +1,12 @@
+package wooteco.subway.station;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface StationRepository {
+    Station save(Station station);
+
+    List<Station> findAll();
+
+    Optional<Station> findByName(String name);
+}
