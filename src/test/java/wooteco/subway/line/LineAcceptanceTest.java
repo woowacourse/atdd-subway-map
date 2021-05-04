8b33785d24bef6f61f91479dc643a1fbf5cd6dfa
JinYoung Park <basejin0810@naver.com>
2021-05-04 21:39:29 +0900

    test(LineAcceptanceTest): 노선 목록 조회 테스트 추가

--- src/test/java/wooteco/subway/line/LineAcceptanceTest.java
+++ src/test/java/wooteco/subway/line/LineAcceptanceTest.java
@@ -6,8 +6,11 @@ import static org.junit.jupiter.api.Assertions.*;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,6 +18,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import wooteco.subway.AcceptanceTest;
 import wooteco.subway.station.StationDao;
+import wooteco.subway.station.StationResponse;
 
 @DisplayName("노선 관련 기능")
 class LineAcceptanceTest extends AcceptanceTest {
@@ -51,5 +55,58 @@ class LineAcceptanceTest extends AcceptanceTest {
         assertThat(response.body().as(LineResponse.class).getId()).isEqualTo(1L);
         assertThat(response.body().as(LineResponse.class).getName()).isEqualTo("2호선");
         assertThat(response.body().as(LineResponse.class).getColor()).isEqualTo("grey darken-1");
+    }
+
+    @DisplayName("노선을 조회한다.")
+    @Test
+    void showLines() {
+        /// given
+        Map<String, Object> params1 = new HashMap<>();
+        params1.put("name", "2호선");
+        params1.put("color", "grey darken-1");
+        params1.put("upStationId", 1);
+        params1.put("downStationId", 2);
+        params1.put("distance", 2);
+        params1.put("extraFare", 500);
+
+        ExtractableResponse<Response> createResponse1 = RestAssured.given().log().all()
+            .body(params1)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .when()
+            .post("/lines")
+            .then().log().all()
+            .extract();
+
+        Map<String, Object> params2 = new HashMap<>();
+        params2.put("name", "3호선");
+        params2.put("color", "grey darken-2");
+        params2.put("upStationId", 5);
+        params2.put("downStationId", 6);
+        params2.put("distance", 12);
+        params2.put("extraFare", 1500);
+        ExtractableResponse<Response> createResponse2 = RestAssured.given().log().all()
+            .body(params2)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .when()
+            .post("/lines")
+            .then().log().all()
+            .extract();
+
+        // when
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+            .when()
+            .get("/lines")
+            .then().log().all()
+            .extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        List<Long> expectedLineIds = Arrays.asList(createResponse1, createResponse2).stream()
+            .map(it -> Long.parseLong(it.header("Location").split("/")[2]))
+            .collect(Collectors.toList());
+        List<Long> resultLineIds = response.jsonPath().getList(".", LineResponse.class).stream()
+            .map(it -> it.getId())
+            .collect(Collectors.toList());
+        assertThat(resultLineIds).containsAll(expectedLineIds);
     }
 }
