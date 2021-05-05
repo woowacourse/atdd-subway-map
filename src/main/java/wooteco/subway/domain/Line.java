fe5f563626fabc3d89faf053ba5a3213ebcc754e
taehee-kim-dev <taehee.kim.dev@gmail.com>
2021-05-05 21:44:25 +0900

    refactor: 도메인 주 생성자 사용 방식 변경 및 모든 필드 final로 수정

--- src/main/java/wooteco/subway/domain/Line.java
+++ src/main/java/wooteco/subway/domain/Line.java
@@ -1,18 +1,18 @@
 package wooteco.subway.domain;
 
 public class Line {
-    private Long id;
+    private final Long id;
     private final String name;
     private final String color;
 
     public Line(Long id, String name, String color) {
-        this(name, color);
         this.id = id;
+        this.name = name;
+        this.color = color;
     }
 
     public Line(String name, String color) {
-        this.name = name;
-        this.color = color;
+        this(null, name, color);
     }
 
     public Line(Long id, Line line) {
