583fad5d55c4e5a309efcc75122497092c3d984f
멍토 <daum7766@naver.com>
2021-05-05 14:37:09 +0900

    feat: 개별 노선 조회api 구현

--- src/main/java/wooteco/subway/line/LineController.java
+++ src/main/java/wooteco/subway/line/LineController.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,11 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class LineController {
 
+    private static final LineDao LINE_DAO = LineDaoFactory.makeLineDao();
+
     @PostMapping("/lines")
     public ResponseEntity<LineResponse> createLine(@RequestBody LineRequest lineRequest) {
         Line line = new Line(lineRequest.getName(), lineRequest.getColor());
-        LineDao lineDao = LineDaoFactory.makeLineDao();
-        Line newLine = lineDao.save(line);
+        Line newLine = LINE_DAO.save(line);
         LineResponse lineResponse = new LineResponse(newLine.getId(), newLine.getName(),
             newLine.getColor());
         return ResponseEntity.created(URI.create("/lines/" + newLine.getId())).body(lineResponse);
@@ -26,13 +28,19 @@ public class LineController {
 
     @GetMapping(value = "/lines", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<LineResponse>> showLines() {
-        LineDao lineDao = LineDaoFactory.makeLineDao();
-        List<Line> lines = lineDao.findAll();
+        List<Line> lines = LINE_DAO.findAll();
 
         List<LineResponse> lineResponses = lines.stream()
             .map(it -> new LineResponse(it.getId(), it.getName(), it.getColor()))
             .collect(Collectors.toList());
         return ResponseEntity.ok().body(lineResponses);
+    }
+
+    @GetMapping(value = "/lines/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<LineResponse> showLine(@PathVariable Long id) {
+        Line line = LINE_DAO.findById(id);
+        LineResponse lineResponse = new LineResponse(line.getId(), line.getName(), line.getColor());
+        return ResponseEntity.ok().body(lineResponse);
     }
 
 }
--- src/test/java/wooteco/subway/line/LineAcceptanceTest.java
+++ src/test/java/wooteco/subway/line/LineAcceptanceTest.java
@@ -10,12 +10,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import wooteco.subway.AcceptanceTest;
+import static org.hamcrest.core.Is.is;
 
 @DisplayName("지하철 노선 관련 기능")
 public class LineAcceptanceTest extends AcceptanceTest {
@@ -72,7 +74,7 @@ public class LineAcceptanceTest extends AcceptanceTest {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 
-    @DisplayName("지하철역을 조회한다.")
+    @DisplayName("지하철역 목록을 조회한다.")
     @Test
     void getStations() {
         /// given
@@ -109,13 +111,68 @@ public class LineAcceptanceTest extends AcceptanceTest {
 
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
-        List<Long> expectedLineIds = Arrays.asList(createResponse1, createResponse2).stream()
+        List<Long> expectedLineIds = Stream.of(createResponse1, createResponse2)
             .map(it -> Long.parseLong(it.header("Location").split("/")[2]))
             .collect(Collectors.toList());
         List<Long> resultLineIds = response.jsonPath().getList(".", LineResponse.class).stream()
-            .map(it -> it.getId())
+            .map(LineResponse::getId)
             .collect(Collectors.toList());
         assertThat(resultLineIds).containsAll(expectedLineIds);
     }
 
+    @DisplayName("id를 이용하여 지하철역을 조회한다.")
+    @Test
+    public void getStation() {
+        /// given
+        Map<String, String> params1 = new HashMap<>();
+        params1.put("name", "2호선");
+        params1.put("color", "bg-green-600");
+        ExtractableResponse<Response> createResponse1 = RestAssured.given().log().all()
+            .body(params1)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .when()
+            .post("/lines")
+            .then().log().all()
+            .extract();
+
+        Map<String, String> params2 = new HashMap<>();
+        params2.put("name", "3호선");
+        params2.put("color", "bg-orange-600");
+        ExtractableResponse<Response> createResponse2 = RestAssured.given().log().all()
+            .body(params2)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .when()
+            .post("/lines")
+            .then().log().all()
+            .extract();
+
+        // when
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .when()
+            .get("/lines/1")
+            .then().log().all()
+            .statusCode(HttpStatus.OK.value())
+            .body("id", is(1))
+            .body("name", is("2호선"))
+            .body("color", is("bg-green-600"))
+            .extract();
+    }
 }
+/*
+
+ RestAssured.given().log().all()
+     .accept(MediaType.APPLICATION_JSON_VALUE)
+     .when().get("/room/1/statistics")
+     .then().log().all()
+     .statusCode(HttpStatus.OK.value())
+     .contentType(MediaType.APPLICATION_JSON_VALUE)
+     .body("whiteName", is("fortune"))
+     .body("whiteWin", is("0"))
+     .body("whiteLose", is("0"))
+     .body("blackName", is("portune"))
+     .body("blackWin", is("0"))
+     .body("blackLose", is("0"));
+
+
+ */
