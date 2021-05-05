231463d14b5fd749be21cb71d0ca0270be70948a
Bepoz <45073750+Be-poz@users.noreply.github.com>
2021-05-05 11:54:15 +0900

    feat: 존재하지 않는 노선 ID 검색 테스트 추가

--- src/test/java/wooteco/subway/line/LineServiceTest.java
+++ src/test/java/wooteco/subway/line/LineServiceTest.java
@@ -1,9 +1,11 @@
 package wooteco.subway.line;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -12,6 +14,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class LineServiceTest {
     @Autowired
     private LineService lineService;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void setUp() {
+        jdbcTemplate.execute("delete from LINE");
+        jdbcTemplate.execute("alter table LINE alter column ID restart with 1");
+    }
 
     @Test
     @DisplayName("노선 정상 생성 테스트")
@@ -25,6 +35,15 @@ class LineServiceTest {
     void createDuplicatedStation() {
         lineService.createLine("2호선", "초록색");
         assertThatThrownBy(() -> lineService.createLine("2호선", "초록색"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 노선 ID 검색")
+    void findNoneExistLineById() {
+        String sql = "insert into Line (name, color) values (?, ?)";
+        jdbcTemplate.update(sql, "2호선", "초록색");
+        assertThatThrownBy(() -> lineService.findById(2L))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
