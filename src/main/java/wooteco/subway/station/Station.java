a727f41299018d0722cb0f677f2b4cffd4a3818b
ROOT <junroot0909@gmail.com>
2021-05-05 16:19:57 +0900

    refactor: 불필요한 빈 생성자 제거

--- src/main/java/wooteco/subway/station/Station.java
+++ src/main/java/wooteco/subway/station/Station.java
@@ -4,9 +4,6 @@ public class Station {
     private Long id;
     private String name;
 
-    public Station() {
-    }
-
     public Station(Long id, String name) {
         this.id = id;
         this.name = name;
