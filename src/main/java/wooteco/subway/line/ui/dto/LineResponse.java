ed9fc6335c23d10aa5b135f5b1bc3a83eac082d5
dwl5 <ssop6403@gmail.com>
2021-05-05 10:15:10 +0900

    refactor: 패키지 분리

--- src/main/java/wooteco/subway/line/ui/dto/LineResponse.java
+++ src/main/java/wooteco/subway/line/ui/dto/LineResponse.java
@@ -1,6 +1,6 @@
-package wooteco.subway.line;
+package wooteco.subway.line.ui.dto;
 
-import wooteco.subway.station.StationResponse;
+import wooteco.subway.station.ui.dto.StationResponse;
 
 import java.util.List;
 
