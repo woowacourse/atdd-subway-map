dcc552fcdb75cc292deae66900f99552e7a9c992
Kimun Kim <kkm97351@gmail.com>
2021-05-05 17:51:22 +0900

    feat: H2 데이터베이스를 이용한 역 생성 기능 구현

---
+++ src/main/java/wooteco/subway/station/StationH2Dao.java
@@ -0,0 +1,53 @@
+package wooteco.subway.station;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+import java.sql.PreparedStatement;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+@Repository
+public class StationH2Dao implements StationRepository {
+    JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public StationH2Dao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public Station save(Station station) {
+        String query = "INSERT INTO STATION (name) VALUES (?)";
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection
+                    .prepareStatement(query, new String[]{"id"});
+            ps.setString(1, station.getName());
+            return ps;
+        }, keyHolder);
+
+        return new Station(Objects.requireNonNull(keyHolder.getKey()).longValue(), station.getName());
+    }
+
+    @Override
+    public List<Station> findAll() {
+        return null;
+    }
+
+    @Override
+    public Optional<Station> findByName(String name) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
