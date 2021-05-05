2a1b572db4c8418d43ea9b7587f03aa4f2ce94d3
Solji Choi <bssj9307@gmail.com>
2021-05-05 13:20:19 +0900

    feat: ExceptionController 추가

---
+++ src/main/java/wooteco/subway/controller/ExceptionController.java
@@ -0,0 +1,14 @@
+package wooteco.subway.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionController {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> illegalArgumentException(IllegalArgumentException exception) {
+        return ResponseEntity.badRequest().body(exception.getMessage());
+    }
+}
