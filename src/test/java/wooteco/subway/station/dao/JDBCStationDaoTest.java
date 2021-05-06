b61f9d5d1d17fa03e121f56bff0323c82535eeaf
pika96 <52442949+pika96@users.noreply.github.com>
2021-05-06 10:29:58 +0900

    feat: JDBCStationDao 테스트 추가

---
+++ src/test/java/wooteco/subway/station/dao/JDBCStationDaoTest.java
@@ -0,0 +1,69 @@
+package wooteco.subway.station.dao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.TestConstructor;
+import org.springframework.transaction.annotation.Transactional;
+import wooteco.subway.station.domain.Station;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJdbcTest
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+class JDBCStationDaoTest {
+
+    private JDBCStationDao jdbcStationDao;
+
+    public JDBCStationDaoTest(JdbcTemplate jdbcTemplate) {
+        this.jdbcStationDao = new JDBCStationDao(jdbcTemplate);
+    }
+
+    @Test
+    void save() {
+        Station station = new Station("강남역");
+
+        Station savedStation = jdbcStationDao.save(station);
+
+        assertThat(savedStation.getName()).isEqualTo(station.getName());
+    }
+
+    @Test
+    void findAll() {
+        Station station1 = new Station("강남역");
+        Station station2 = new Station("역삼역");
+        jdbcStationDao.save(station1);
+        jdbcStationDao.save(station2);
+
+        List<Station> stations = jdbcStationDao.findAll();
+
+        assertThat(stations).hasSize(2);
+    }
+
+    @Test
+    void findById() {
+        Station station1 = new Station("강남역");
+        Station savedStation = jdbcStationDao.save(station1);
+
+        Station findByIdStation = jdbcStationDao.findById(savedStation.getId());
+        assertThat(savedStation.getName()).isEqualTo(findByIdStation.getName());
+    }
+
+    @Test
+    void delete() {
+        Station station1 = new Station("강남역");
+        Station station2 = new Station("역삼역");
+        jdbcStationDao.save(station1);
+        Station savedStation = jdbcStationDao.save(station2);
+
+        jdbcStationDao.delete(savedStation.getId());
+        List<Station> findStations = jdbcStationDao.findAll();
+
+        assertThat(findStations).hasSize(1);
+    }
+}
