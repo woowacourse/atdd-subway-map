afd285b03a4835e3688c5f2ee40a85130c569764
bomin <bomin_93@naver.com>
2021-05-06 18:01:28 +0900

    add: 도메인 기본 틀 완성

--- src/main/java/wooteco/subway/domain/Section.java
+++ src/main/java/wooteco/subway/domain/Section.java
@@ -1,6 +1,9 @@
 package wooteco.subway.domain;
 
 public class Section {
-
-
+    private Long id;
+    private Line line;
+    private Station upStation;
+    private Station downStation;
+    private int distance;
 }
---
+++ src/main/java/wooteco/subway/domain/Sections.java
@@ -0,0 +1,8 @@
+package wooteco.subway.domain;
+
+import java.util.List;
+
+public class Sections {
+    private List<Section> sections;
+
+}
