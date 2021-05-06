fbaaf4c651dbd1fbed1237d02a397950642db20e
dwl5 <ssop6403@gmail.com>
2021-05-06 15:57:44 +0900

    refactor: station list 추가

--- src/main/java/wooteco/subway/line/ui/dto/LineResponse.java
+++ src/main/java/wooteco/subway/line/ui/dto/LineResponse.java
@@ -1,9 +1,15 @@
 package wooteco.subway.line.ui.dto;
 
+import wooteco.subway.station.ui.dto.StationResponse;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LineResponse {
     private final Long id;
     private final String name;
     private final String color;
+    private final List<StationResponse> stations = new ArrayList<>();
 
     public LineResponse(final Long id, final String name, final String color) {
         this.id = id;
@@ -23,4 +29,7 @@ public class LineResponse {
         return color;
     }
 
+    public List<StationResponse> getStations() {
+        return stations;
+    }
 }
