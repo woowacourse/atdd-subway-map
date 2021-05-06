44d395e8772fa8f6c414f544fd7248f32555addc
임동준 <im1@woowahan.com>
2021-05-06 14:35:16 +0900

    feat: add line delete button

---
+++ frontend/src/pages/line/components/LineDeleteButton.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-btn @click="onDeleteLine" icon>
+    <v-icon color="grey lighten-1">mdi-delete</v-icon>
+  </v-btn>
+</template>
+
+<script>
+import { mapMutations } from "vuex";
+import { SHOW_SNACKBAR } from "../../../store/shared/mutationTypes";
+import { SNACKBAR_MESSAGES } from "../../../utils/constants";
+
+export default {
+  name: "LineDeleteButton",
+  props: {
+    line: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    ...mapMutations([SHOW_SNACKBAR]),
+    onDeleteLine() {
+      try {
+        // TODO Line을 삭제하는 API를 추가해주세요.
+        // TODO 전체 Line 데이터를 불러오는 API를 추가해주세요.
+        // this.setLines([전체 라인 데이터])
+        this.showSnackbar(SNACKBAR_MESSAGES.LINE.DELETE.SUCCESS);
+      } catch (e) {
+        this.showSnackbar(SNACKBAR_MESSAGES.LINE.DELETE.FAIL);
+      }
+    },
+  },
+};
+</script>
+
+<style scoped></style>
