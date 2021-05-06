a1f0854374190eb864dcbcbbd702bf78cc3c01ff
Minsung <sns2831@gmail.com>
2021-05-06 18:05:30 +0900

    refactor: dto 기본생성자를 삭제

--- src/main/java/wooteco/subway/station/ui/dto/StationResponse.java
+++ src/main/java/wooteco/subway/station/ui/dto/StationResponse.java
@@ -4,9 +4,6 @@ public class StationResponse {
     private Long id;
     private String name;
 
-    public StationResponse() {
-    }
-
     public StationResponse(Long id, String name) {
         this.id = id;
         this.name = name;
