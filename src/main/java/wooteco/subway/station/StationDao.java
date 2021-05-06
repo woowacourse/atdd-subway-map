ea9774481ee0f9a164ff5c12baca01bf02060228
Seed <hsik0225@gmail.com>
2021-05-06 13:11:51 +0900

    feat(station-dao):같은 이름 지하철역 생성 불가 기능 추가

--- src/main/java/wooteco/subway/station/StationDao.java
+++ src/main/java/wooteco/subway/station/StationDao.java
@@ -1,20 +1,29 @@
 package wooteco.subway.station;
 
-import org.springframework.util.ReflectionUtils;
-
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+
+import org.springframework.util.ReflectionUtils;
 
 public class StationDao {
     private static Long seq = 0L;
     private static List<Station> stations = new ArrayList<>();
 
     public static Station save(Station station) {
+        if (alreadyStoredStationName(station)) {
+            throw new IllegalArgumentException("이미 저장된 역 이름입니다.");
+        }
+
         Station persistStation = createNewObject(station);
         stations.add(persistStation);
         return persistStation;
+    }
+
+    private static boolean alreadyStoredStationName(Station station) {
+        final String stationName = station.getName();
+        return stations.stream()
+                       .anyMatch(storedStation -> storedStation.getName().equals(stationName));
     }
 
     public static List<Station> findAll() {
