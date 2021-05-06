70baabf9243a6b49c79e72174127d7b2db77f409
Jung-ChanYoung <38939015+MyaGya@users.noreply.github.com>
2021-05-06 15:17:31 +0900

    test: LineDao 노선 전체 조회 테스트

--- src/test/java/wooteco/subway/line/LineDaoTest.java
+++ src/test/java/wooteco/subway/line/LineDaoTest.java
@@ -1,8 +1,10 @@
 package wooteco.subway.line;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -45,5 +47,23 @@ public class LineDaoTest {
         }).isInstanceOf(DuplicateKeyException.class);
     }
 
+    @DisplayName("line 비어있는 리스트 전체 조회 테스트")
+    @Test
+    void findAllTestWhenEmpty() {
+        List<Line> lines = lineDao.findAll();
+
+        assertThat(lines.isEmpty()).isTrue();
+    }
+
+    @DisplayName("line 전체 조회 테스트")
+    @Test
+    void findAllLineTest() {
+        lineDao.save(new Line("신분당선", "black"));
+        lineDao.save(new Line("2호선", "white"));
+        List<Line> lines = lineDao.findAll();
+        assertThat(lines.size()).isEqualTo(2);
+        assertThat(lines.get(0).getId()).isEqualTo(1L);
+        assertThat(lines.get(1).getId()).isEqualTo(2L);
+    }
 
 }
