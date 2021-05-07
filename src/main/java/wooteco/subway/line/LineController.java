3487c39d53a1315aafc0a0eccf9bb7c5a6f071c4
Seed <hsik0225@gmail.com>
2021-05-07 13:48:26 +0900

    feat(controller):노선 목록 조회 컨트롤러 기능 구현

--- src/main/java/wooteco/subway/line/LineController.java
+++ src/main/java/wooteco/subway/line/LineController.java
@@ -1,21 +1,37 @@
 package wooteco.subway.line;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/lines")
 @RestController
 public class LineController {
 
-    @PostMapping("/lines")
+    @PostMapping
     public ResponseEntity<LineResponse> createLine(@RequestBody LineRequest lineRequest) {
-        Line line = new Line(lineRequest.getName(), lineRequest.getColor());
-        Line newLine = LineDao.save(line);
-        LineResponse lineResponse = new LineResponse(newLine);
+        final Line line = new Line(lineRequest.getName(), lineRequest.getColor());
+        final Line newLine = LineDao.save(line);
+        final LineResponse lineResponse = new LineResponse(newLine);
         return ResponseEntity.created(URI.create("/stations/" + newLine.getId()))
                              .body(lineResponse);
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<LineResponse>> showLines() {
+        final List<Line> lines = LineDao.findAll();
+        final List<LineResponse> lineResponses = lines.stream()
+                                                      .map(line -> new LineResponse(line.getId(),
+                                                              line.getName(), line.getColor()))
+                                                      .collect(Collectors.toList());
+        return ResponseEntity.ok().body(lineResponses);
     }
 }
