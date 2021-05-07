aae8ec0378fb0a1baeb1002ce2669f5f29debb0b
Kimun Kim <kkm97351@gmail.com>
2021-05-07 14:25:07 +0900

    refactor: 메서드 체이닝 내려 쓰기, URI 변수로 분리

--- src/main/java/wooteco/subway/line/LineController.java
+++ src/main/java/wooteco/subway/line/LineController.java
@@ -20,30 +20,42 @@ public class LineController {
     @PostMapping
     public ResponseEntity<LineResponse> createLines(@RequestBody LineRequest lineRequest) {
         LineResponse lineResponse = lineService.createLine(lineRequest.getName(), lineRequest.getColor());
-        return ResponseEntity.created(URI.create("/lines/" + lineResponse.getId())).body(lineResponse);
+        URI location = URI.create("/lines/" + lineResponse.getId());
+        return ResponseEntity
+                .created(location)
+                .body(lineResponse);
     }
 
     @GetMapping
     public ResponseEntity<List<LineResponse>> showLines() {
         List<LineResponse> lineResponses = lineService.findAll();
-        return ResponseEntity.ok().body(lineResponses);
+        return ResponseEntity
+                .ok()
+                .body(lineResponses);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<LineResponse> getLine(@PathVariable long id) {
-        return ResponseEntity.ok().body(lineService.findById(id));
+        return ResponseEntity
+                .ok()
+                .body(lineService.findById(id));
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<LineResponse> updateLine(@PathVariable long id, @RequestBody LineRequest lineRequest) {
         LineResponse lineResponse = lineService.update(id, lineRequest.getName(), lineRequest.getColor());
-        return ResponseEntity.created(URI.create("/lines/" + lineResponse.getId())).body(lineResponse);
+        URI location = URI.create("/lines/" + lineResponse.getId());
+        return ResponseEntity
+                .created(location)
+                .body(lineResponse);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<LineResponse> deleteLine(@PathVariable long id) {
         lineService.deleteLine(id);
-        return ResponseEntity.noContent().build();
+        return ResponseEntity
+                .noContent()
+                .build();
     }
 
 }
