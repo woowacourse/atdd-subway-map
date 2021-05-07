172247986b513eca487fbb8818dcb7cf2c57260c
taehee-kim-dev <taehee.kim.dev@gmail.com>
2021-05-07 16:50:41 +0900

    refactor: @Transactional 부모 클래스로 이동

--- src/test/java/wooteco/subway/line/LineAcceptanceTest.java
+++ src/test/java/wooteco/subway/line/LineAcceptanceTest.java
@@ -14,12 +14,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.transaction.annotation.Transactional;
 import wooteco.subway.AcceptanceTest;
 import wooteco.subway.controller.dto.response.line.LineResponseDto;
 
 @DisplayName("지하철 노선 관련 기능")
-@Transactional
 class LineAcceptanceTest extends AcceptanceTest {
 
     @DisplayName("노선을 생성한다.")
--- src/test/java/wooteco/subway/station/StationAcceptanceTest.java
+++ src/test/java/wooteco/subway/station/StationAcceptanceTest.java
@@ -14,12 +14,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.transaction.annotation.Transactional;
 import wooteco.subway.AcceptanceTest;
 import wooteco.subway.controller.dto.response.station.StationResponseDto;
 
 @DisplayName("지하철역 관련 기능")
-@Transactional
 class StationAcceptanceTest extends AcceptanceTest {
 
     @DisplayName("지하철역을 생성한다.")
