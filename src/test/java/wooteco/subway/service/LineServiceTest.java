5fb6f95ca5c79247e149d63b3de1c18ff5bf1301
Jinhong <xlffm3@gmail.com>
2021-05-07 11:14:47 +0900

    refactor: 불필요한 Service test 삭제

--- src/test/java/wooteco/subway/service/LineServiceTest.java
+++ src/test/java/wooteco/subway/service/LineServiceTest.java
@@ -9,11 +9,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import wooteco.subway.dao.LineDao;
 import wooteco.subway.domain.line.Line;
 
-import java.util.Arrays;
-import java.util.Collections;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -35,30 +31,14 @@ class LineServiceTest {
         Line line = new Line(name, color);
         Line retrivedLine = new Line(1L, name, color);
 
-        given(lineDao.findAll()).willReturn(Collections.emptyList());
         given(lineDao.save(line)).willReturn(1L);
         given(lineDao.findById(1L)).willReturn(retrivedLine);
 
         Line savedLine = lineService.createLine(name, color);
 
         assertThat(savedLine).isEqualTo(retrivedLine);
-        verify(lineDao, times(1)).findAll();
         verify(lineDao, times(1)).save(line);
         verify(lineDao, times(1)).findById(1L);
-    }
-
-    @DisplayName("중복된 이름의 노선을 생성할 수 없다.")
-    @Test
-    void cannotDuplicatedLine() {
-        String name = "testLine";
-        String color = "black";
-        given(lineDao.findAll()).willReturn(Arrays.asList(new Line(1L, name, color)));
-
-        assertThatCode(() -> lineService.createLine(name, color))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("중복된 이름의 노선이 존재합니다.");
-
-        verify(lineDao, times(1)).findAll();
     }
 
     @DisplayName("노선의 이름을 수정한다.")
@@ -66,25 +46,9 @@ class LineServiceTest {
     void editLine() {
         String name = "changedName";
         String color = "black";
-        given(lineDao.findAll()).willReturn(Arrays.asList(new Line("dummy", color)));
 
         lineService.editLine(1L, name, color);
 
-        verify(lineDao, times(1)).findAll();
         verify(lineDao, times(1)).update(1L, name, color);
-    }
-
-    @DisplayName("수정하려는 이름의 노선이 존재하면 노선을 수정할 수 없다.")
-    @Test
-    void cannotEditLine() {
-        String name = "changedName";
-        String color = "black";
-        given(lineDao.findAll()).willReturn(Arrays.asList(new Line(name, color)));
-
-        assertThatCode(() -> lineService.editLine(1L, name, color))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("중복된 이름의 노선이 존재합니다.");
-
-        verify(lineDao, times(1)).findAll();
     }
 }
--- src/test/java/wooteco/subway/service/StationServiceTest.java
+++ src/test/java/wooteco/subway/service/StationServiceTest.java
@@ -9,11 +9,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import wooteco.subway.dao.StationDao;
 import wooteco.subway.domain.station.Station;
 
-import java.util.Arrays;
-import java.util.Collections;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -34,27 +30,13 @@ class StationServiceTest {
         Station station = new Station(name);
         Station retrivedStation = new Station(1L, name);
 
-        given(stationDao.findAll()).willReturn(Collections.emptyList());
         given(stationDao.save(station)).willReturn(1L);
         given(stationDao.findById(1L)).willReturn(retrivedStation);
 
         Station savedStation = stationService.createStation(name);
 
         assertThat(savedStation).isEqualTo(retrivedStation);
-        verify(stationDao, times(1)).findAll();
         verify(stationDao, times(1)).save(station);
         verify(stationDao, times(1)).findById(1L);
-    }
-
-    @DisplayName("중복된 이름의 역을 생성할 수 없다.")
-    @Test
-    void cannotCreateStation() {
-        String name = "testStation";
-        given(stationDao.findAll()).willReturn(Arrays.asList(new Station(name)));
-
-        assertThatCode(() -> stationService.createStation(name))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("중복된 이름의 역이 존재합니다.");
-        verify(stationDao, times(1)).findAll();
     }
 }
