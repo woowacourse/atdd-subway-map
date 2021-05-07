df606a4c6b6ed3cf6f5242fb743cdca3d6549f5a
jaeseongDev <qkrwotjd1445@naver.com>
2021-05-07 18:50:43 +0900

    refactor : 테스트 코드에서 SQL문을 DAO 코드로 대체

--- src/main/java/wooteco/subway/repository/LineRepository.java
+++ src/main/java/wooteco/subway/repository/LineRepository.java
@@ -1,14 +1,12 @@
 package wooteco.subway.repository;
 
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 import wooteco.subway.dao.LineDao;
+import wooteco.subway.dao.SectionDao;
 import wooteco.subway.domain.Line;
 import wooteco.subway.domain.Section;
 
-import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,41 +14,27 @@ import java.util.List;
 public class LineRepository {
     private final JdbcTemplate jdbcTemplate;
     private final LineDao lineDao;
+    private final SectionDao sectionDao;
 
-    public LineRepository(JdbcTemplate jdbcTemplate, LineDao lineDao) {
+    public LineRepository(JdbcTemplate jdbcTemplate, LineDao lineDao, SectionDao sectionDao) {
         this.jdbcTemplate = jdbcTemplate;
         this.lineDao = lineDao;
+        this.sectionDao = sectionDao;
     }
 
     public Line saveLineWithSection(String name, String color, Long upStationId, Long downStationId, int distance) {
         Long createdLineId = lineDao.create(name, color);
-        Long createdSectionId = insertSection(createdLineId, upStationId, downStationId, distance);
+        Long createdSectionId = sectionDao.create(createdLineId, upStationId, downStationId, distance);
         List<Section> sections = new ArrayList<>();
         Section section = new Section(createdSectionId, createdLineId, upStationId, downStationId, distance);
         sections.add(section);
         return new Line(createdLineId, name, color, sections);
     }
 
-    private Long insertSection(Long lineId, Long upStationId, Long downStationId, int distance) {
-        String insertSectionSql = "INSERT INTO SECTION " +
-                "(line_id, up_station_id, down_station_id, distance) " +
-                "VALUES (?, ?, ?, ?)";
-        KeyHolder sectionKeyHolder = new GeneratedKeyHolder();
-        jdbcTemplate.update(connection -> {
-            PreparedStatement ps = connection.prepareStatement(insertSectionSql, new String[]{"id"});
-            ps.setLong(1, lineId);
-            ps.setLong(2, upStationId);
-            ps.setLong(3, downStationId);
-            ps.setInt(4, distance);
-            return ps;
-        }, sectionKeyHolder);
-        return sectionKeyHolder.getKey().longValue();
-    }
-
     public List<Line> findAllLine() {
         return lineDao.findAll();
     }
-//
+
 //    public Optional<Line> findById(Long lineId) {
 //        String query = "SELECT * FROM LINE WHERE id = ?";
 //        Line result = DataAccessUtils.singleResult(
@@ -90,8 +74,6 @@ public class LineRepository {
             String deleteSectionQuery = "DELETE FROM section WHERE id = ?";
             jdbcTemplate.update(deleteSectionQuery, sectionId);
         }
-
-
 
         String deleteLineQuery = "DELETE FROM LINE WHERE id = ?";
         return (long) jdbcTemplate.update(deleteLineQuery, lineId);
