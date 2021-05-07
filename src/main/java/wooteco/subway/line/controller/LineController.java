632c99dae23323b07ecae869fc011e831124a1bc
yoonsung.jung <ybook2012@gmail.com>
2021-05-07 13:25:59 +0900

    refactor(LineController) : DTO 컨버팅 과정을 Service로 위임

--- src/main/java/wooteco/subway/line/controller/LineController.java
+++ src/main/java/wooteco/subway/line/controller/LineController.java
@@ -2,14 +2,12 @@ package wooteco.subway.line.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import wooteco.subway.line.domain.Line;
 import wooteco.subway.line.dto.LineRequest;
 import wooteco.subway.line.dto.LineResponse;
 import wooteco.subway.line.dto.LinesResponse;
 import wooteco.subway.line.service.LineService;
 
 import java.net.URI;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -23,33 +21,23 @@ public class LineController {
 
     @PostMapping
     public ResponseEntity<LineResponse> createLine(@RequestBody LineRequest lineRequest) {
-        Line line = new Line(lineRequest.getColor(), lineRequest.getName());
-        Line newLine = lineService.save(line);
-        LineResponse lineResponse = new LineResponse(newLine.getId(), newLine.getName(), newLine.getColor());
-        return ResponseEntity.created(URI.create("/lines/" + newLine.getId())).body(lineResponse);
+        LineResponse lineResponse = lineService.save(lineRequest);
+        return ResponseEntity.created(URI.create("/lines/" + lineResponse.getId())).body(lineResponse);
     }
 
     @GetMapping
     public ResponseEntity<List<LinesResponse>> getLines() {
-        List<Line> lines = lineService.getLines();
-        List<LinesResponse> linesResponses = new ArrayList<>();
-        for (Line line : lines) {
-            linesResponses.add(new LinesResponse(line.getId(), line.getName(), line.getColor()));
-        }
-        return ResponseEntity.ok().body(linesResponses);
+        return ResponseEntity.ok().body(lineService.getLines());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<LineResponse> getLine(@PathVariable final Long id) {
-
-        Line line = lineService.getLine(id);
-        LineResponse lineResponse = new LineResponse(line.getId(), line.getName(), line.getColor());
-        return ResponseEntity.ok().body(lineResponse);
+        return ResponseEntity.ok().body(lineService.getLineById(id));
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Void> updateLine(@PathVariable final Long id, @RequestBody LineRequest lineRequest) {
-        lineService.updateLine(new Line(id, lineRequest.getColor(), lineRequest.getName()));
+        lineService.updateLine(id, lineRequest);
         return ResponseEntity.ok().build();
     }
 
--- src/main/java/wooteco/subway/line/service/LineService.java
+++ src/main/java/wooteco/subway/line/service/LineService.java
@@ -3,8 +3,12 @@ package wooteco.subway.line.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import wooteco.subway.line.domain.Line;
+import wooteco.subway.line.dto.LineRequest;
+import wooteco.subway.line.dto.LineResponse;
+import wooteco.subway.line.dto.LinesResponse;
 import wooteco.subway.line.repository.LineRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -16,23 +20,30 @@ public class LineService {
         this.lineRepository = lineRepository;
     }
 
-    public List<Line> getLines() {
-        return lineRepository.getLines();
+    public List<LinesResponse> getLines() {
+        List<LinesResponse> linesResponses = new ArrayList<>();
+        for (Line line : lineRepository.getLines()) {
+            linesResponses.add(new LinesResponse(line.getId(), line.getName(), line.getColor()));
+        }
+        return linesResponses;
     }
 
-    public Line save(final Line line) {
-        if (lineRepository.isExistName(line)) {
+    public LineResponse save(final LineRequest lineRequest) {
+        Line newLine = new Line(lineRequest.getColor(), lineRequest.getName());
+        if (lineRepository.isExistName(newLine)) {
             throw new IllegalArgumentException("이미 존재하는 Line 입니다.");
         }
-        return lineRepository.save(line);
+        Line savedLine = lineRepository.save(newLine);
+        return new LineResponse(savedLine.getId(), savedLine.getName(), savedLine.getColor());
     }
 
-    public Line getLine(final Long id) {
-        return lineRepository.getLine(id);
+    public LineResponse getLineById(final Long id) {
+        Line line = lineRepository.getLine(id);
+        return new LineResponse(line.getId(), line.getName(), line.getColor());
     }
 
-    public void updateLine(final Line line) {
-        lineRepository.update(line);
+    public void updateLine(final Long id, final LineRequest lineRequest) {
+        lineRepository.update(new Line(id, lineRequest.getColor(), lineRequest.getName()));
     }
 
     public void deleteById(final Long id) {
