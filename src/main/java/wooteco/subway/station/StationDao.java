8a320f71cde211ff0f7f7efb1c8effb9212bd772
Seed <hsik0225@gmail.com>
2021-05-07 18:58:31 +0900

    feat(station-dao):Jdbc를 사용한 DAO 작성

---
+++ src/main/java/wooteco/subway/station/StationDao.java
@@ -0,0 +1,14 @@
+package wooteco.subway.station;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface StationDao {
+    Station save(StationRequest stationRequest);
+
+    List<Station> findAll();
+
+    void delete(Long id);
+
+    Optional<Station> findByName(String name);
+}
