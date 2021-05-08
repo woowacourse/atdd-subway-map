48cb10735e21bdcf3d73f6de90ded1f5e9b77f79
Sung Si Hyung <fjzjqhdl@gmail.com>
2021-05-08 12:36:16 +0900

    feat : 구간 거리 객체인 SectionDistance 구현 및 구간 더하는 기능, 음수일 때 예외발생하는 기능 구현

---
+++ src/main/java/wooteco/subway/line/section/domain/SectionDistance.java
@@ -0,0 +1,25 @@
+package wooteco.subway.line.section.domain;
+
+public class SectionDistance {
+    private static final int ZERO = 0;
+    private final long distance;
+
+    public SectionDistance(long distance) {
+        whenDistanceIsNegative(distance);
+        this.distance = distance;
+    }
+
+    private void whenDistanceIsNegative(long distance) {
+        if (distance < ZERO) {
+            throw new IllegalArgumentException(String.format("구간 거리는 음수일 수 없습니다. 입력된 거리 : %d", distance));
+        }
+    }
+
+    public long getDistance() {
+        return distance;
+    }
+
+    public SectionDistance sum(SectionDistance that) {
+        return new SectionDistance(this.distance + that.getDistance());
+    }
+}
---
+++ src/test/java/wooteco/subway/line/section/domain/SectionDistanceTest.java
@@ -0,0 +1,38 @@
+package wooteco.subway.line.section.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+@DisplayName("구간 거리 기능 확인")
+class SectionDistanceTest {
+    @DisplayName("거리가 음수일 때 예외가 발생하는지 확인")
+    @Test
+    void whenDistanceIsNegative() {
+        //given
+        long negativeDistance = -1;
+        //when
+        //then
+        assertThatThrownBy(() -> new SectionDistance(negativeDistance))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("구간 거리는 음수일 수 없습니다. 입력된 거리");
+    }
+
+    @DisplayName("거리를 합하는 기능 확인")
+    @Test
+    void sum() {
+        //given
+        long aDistanceValue = 10;
+        long anotherDistanceValue = 20;
+
+        SectionDistance aSectionDistance = new SectionDistance(aDistanceValue);
+        SectionDistance anotherSectionDistance = new SectionDistance(anotherDistanceValue);
+
+        //when
+        SectionDistance result = aSectionDistance.sum(anotherSectionDistance);
+        //then
+        assertThat(result.getDistance()).isEqualTo(aDistanceValue + anotherDistanceValue);
+    }
+}
