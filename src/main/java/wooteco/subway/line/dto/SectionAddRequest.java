80bb04d0d4920ee5d6f50167ef7dbfb714a03389
NewWisdom <wlgp2500@gmail.com>
2021-05-08 11:10:53 +0900

    feat(): 구간 추가 서비스 로직 정리

---
+++ src/main/java/wooteco/subway/line/dto/SectionAddRequest.java
@@ -0,0 +1,46 @@
+package wooteco.subway.line.dto;
+
+import wooteco.subway.line.domain.Section;
+
+public class SectionAddRequest {
+    private Long upStationId;
+    private Long downStationId;
+    private int distance;
+
+    public SectionAddRequest() {
+    }
+
+    public SectionAddRequest(Long upStationId, Long downStationId, int distance) {
+        this.upStationId = upStationId;
+        this.downStationId = downStationId;
+        this.distance = distance;
+    }
+
+    public Long getUpStationId() {
+        return upStationId;
+    }
+
+    public void setUpStationId(final Long upStationId) {
+        this.upStationId = upStationId;
+    }
+
+    public Long getDownStationId() {
+        return downStationId;
+    }
+
+    public void setDownStationId(final Long downStationId) {
+        this.downStationId = downStationId;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setDistance(final int distance) {
+        this.distance = distance;
+    }
+
+    public Section toEntity(Long lineId) {
+        return new Section(lineId, upStationId, downStationId, distance);
+    }
+}
