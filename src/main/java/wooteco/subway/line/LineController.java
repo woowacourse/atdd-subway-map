c2674729c0152d5d6a1e1a01f9a8254a378eafa8
naeunkim <knae11@naver.com>
2021-05-08 19:28:47 +0900

    refactor: LineResponse 생성 정적팩토리메소드로 변경

--- src/main/java/wooteco/subway/line/LineController.java
+++ src/main/java/wooteco/subway/line/LineController.java
@@ -1,21 +1,16 @@
 package wooteco.subway.line;
 
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import wooteco.subway.exception.LineNameDuplicatedException;
+import wooteco.subway.exception.LineNotFoundException;
+import wooteco.subway.line.dao.LineDao;
+
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import wooteco.subway.exception.LineNameDuplicatedException;
-import wooteco.subway.exception.LineNotFoundException;
-import wooteco.subway.line.dao.LineDao;
 
 @RestController
 public class LineController {
@@ -35,26 +30,24 @@ public class LineController {
         }
         Line line = new Line(name, color);
         Line createdLine = lineDao.save(line);
-        LineResponse lineResponse = new LineResponse(createdLine.getId(), createdLine.getName(),
-            createdLine.getColor());
+        LineResponse lineResponse = LineResponse.of(createdLine);
         return ResponseEntity.created(URI.create("/lines/" + createdLine.getId()))
-            .body(lineResponse);
+                .body(lineResponse);
     }
 
     @GetMapping(value = "/lines", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<LineResponse>> showLines() {
         List<Line> lines = lineDao.findAll();
         List<LineResponse> lineResponses = lines.stream()
-            .map(it -> new LineResponse(it.getId(), it.getName(), it.getColor()))
-            .collect(Collectors.toList());
+                .map(LineResponse::of)
+                .collect(Collectors.toList());
         return ResponseEntity.ok().body(lineResponses);
     }
 
     @GetMapping(value = "/lines/{id}")
     public ResponseEntity<LineResponse> findLineById(@PathVariable Long id) {
         Line line = lineDao.findLineById(id).orElseThrow(LineNotFoundException::new);
-        return ResponseEntity.ok().body(new LineResponse(line.getId(), line.getName(),
-            line.getColor()));
+        return ResponseEntity.ok().body(LineResponse.of(line));
     }
 
     @PutMapping(value = "/lines/{id}")
--- src/main/java/wooteco/subway/line/LineResponse.java
+++ src/main/java/wooteco/subway/line/LineResponse.java
@@ -1,29 +1,22 @@
 package wooteco.subway.line;
 
-import java.util.List;
-import wooteco.subway.station.StationResponse;
-
 public class LineResponse {
 
     private Long id;
     private String name;
     private String color;
-    private List<StationResponse> stations;
 
-    public LineResponse() {
+    private LineResponse() {
     }
 
-    public LineResponse(Long id, String name, String color) {
+    private LineResponse(Long id, String name, String color) {
         this.id = id;
         this.name = name;
         this.color = color;
     }
 
-    public LineResponse(Long id, String name, String color, List<StationResponse> stations) {
-        this.id = id;
-        this.name = name;
-        this.color = color;
-        this.stations = stations;
+    public static LineResponse of(Line line) {
+        return new LineResponse(line.getId(), line.getName(), line.getColor());
     }
 
     public Long getId() {
