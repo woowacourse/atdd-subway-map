fa57a05a2fe789b081a092d5fbedacf13cb6ef12
lns13301 <rkdrbgks22@ks.ac.kr>
2021-05-08 12:16:14 +0900

    refactor: 예외 처리 메세지 추가

--- src/main/java/wooteco/subway/exception/LineNotFoundException.java
+++ src/main/java/wooteco/subway/exception/LineNotFoundException.java
@@ -2,4 +2,9 @@ package wooteco.subway.exception;
 
 public class LineNotFoundException extends RuntimeException {
 
+    private static final String MESSAGE = "노선을 찾을 수 없습니다.";
+
+    public LineNotFoundException() {
+        super(MESSAGE);
+    }
 }
