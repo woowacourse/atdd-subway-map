5152910d8cc8f4fc3c1661691854cfe209faa7b4
Minsung <sns2831@gmail.com>
2021-05-08 10:31:28 +0900

    refactor: ddl 변경
    auto_increment to UUID

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,24 +1,22 @@
 create table if not exists STATION
 (
-    id bigint auto_increment not null,
+    id varchar(36) not null,
     name varchar(255) not null unique,
     primary key(id)
-    );
+);
 
 create table if not exists LINE
 (
-    id bigint auto_increment not null,
+    id varchar(36) not null,
     name varchar(255) not null unique,
     color varchar(20) not null,
     primary key(id)
-    );
+);
 
 create table if not exists SECTION
 (
-    id bigint auto_increment not null,
-    line_id bigint not null,
+    line_id varchar(36) not null,
     up_station_id bigint not null,
     down_station_id bigint not null,
     distance int,
-    primary key(id)
-    );
+);
