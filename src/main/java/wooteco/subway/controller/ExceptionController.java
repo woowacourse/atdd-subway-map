447d8f1d1e6f9f81552cabd7e2d015832439a40b
Sol ji Choi <bssj9307@gmail.com>
2021-05-08 17:24:19 +0900

    refactor: new responseentiry 수정

--- src/main/java/wooteco/subway/controller/ExceptionController.java
+++ src/main/java/wooteco/subway/controller/ExceptionController.java
@@ -14,16 +14,16 @@ public class ExceptionController {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Void> exception(Exception exception) {
-        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
     }
 
     @ExceptionHandler({LineDuplicationException.class, StationDuplicationException.class})
     public ResponseEntity<String> nameDuplicationException(RuntimeException exception) {
-        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+        return ResponseEntity.badRequest().body(exception.getMessage());
     }
 
     @ExceptionHandler({LineNotFoundException.class, StationNotFoundException.class})
     public ResponseEntity<String> notFoundException(RuntimeException exception) {
-        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+        return ResponseEntity.badRequest().body(exception.getMessage());
     }
 }
