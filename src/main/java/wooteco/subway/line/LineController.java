193e53dd64cc001d6572d00f412d4a99453f1e07
Seed <hsik0225@gmail.com>
2021-05-08 19:09:54 +0900

    feat(line-controller):노선 이름 변경 시 이름 중복 검사 추가

--- src/main/java/wooteco/subway/line/LineController.java
+++ src/main/java/wooteco/subway/line/LineController.java
@@ -1,73 +1,68 @@
 package wooteco.subway.line;
 
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import wooteco.subway.exception.DuplicateNameException;
+
 import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import wooteco.subway.exception.DuplicateNameException;
 
 @RequestMapping("/lines")
 @RestController
 public class LineController {
 
-    private final LineDao lineDao;
+	private final LineDao lineDao;
 
-    public LineController(LineDao lineDao) {
-        this.lineDao = lineDao;
-    }
+	public LineController(LineDao lineDao) {
+		this.lineDao = lineDao;
+	}
 
-    @PostMapping
-    public ResponseEntity<LineResponse> createLine(@RequestBody LineRequest lineRequest) {
-        boolean existsName = lineDao.findByName(lineRequest.getName()).isPresent();
-        if (existsName) {
-            throw new DuplicateNameException("이미 저장된 노선 이름입니다.");
-        }
+	@PostMapping
+	public ResponseEntity<LineResponse> createLine(@RequestBody LineRequest lineRequest) {
+		boolean existsName = lineDao.findByName(lineRequest.getName()).isPresent();
+		if (existsName) {
+			throw new DuplicateNameException("이미 존재하는 노선 이름입니다.");
+		}
 
-        final Line newLine = lineDao.save(lineRequest);
-        final LineResponse lineResponse = new LineResponse(newLine);
-        return ResponseEntity.created(URI.create("/lines/" + newLine.getId()))
-                             .body(lineResponse);
-    }
+		final Line newLine = lineDao.save(lineRequest);
+		final LineResponse lineResponse = new LineResponse(newLine);
+		final URI uri = URI.create("/lines/" + newLine.getId());
+		return ResponseEntity.created(uri).body(lineResponse);
+	}
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<LineResponse>> showLines() {
-        final List<Line> lines = lineDao.findAll();
-        final List<LineResponse> lineResponses = lines.stream()
-                                                      .map(line -> new LineResponse(line.getId(),
-                                                              line.getName(), line.getColor()))
-                                                      .collect(Collectors.toList());
-        return ResponseEntity.ok().body(lineResponses);
-    }
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<LineResponse>> showLines() {
+		final List<LineResponse> lineResponses = lineDao.findAll()
+														.stream()
+														.map(LineResponse::new)
+														.collect(Collectors.toList());
+		return ResponseEntity.ok().body(lineResponses);
+	}
 
-    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<LineResponse> showLine(@PathVariable Long id) {
-        final Line line = lineDao.findById(id).orElseThrow(
-                () -> new IllegalArgumentException("해당 ID에 해당하는 노선이 존재하지 않습니다."));
-        LineResponse lineResponse = new LineResponse(line.getId(), line.getName(), line.getColor());
-        return ResponseEntity.ok().body(lineResponse);
-    }
+	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<LineResponse> showLine(@PathVariable Long id) {
+		final Line line = lineDao.findById(id)
+								 .orElseThrow(() -> new IllegalArgumentException("해당 ID에 해당하는 노선이 존재하지 않습니다."));
+		LineResponse lineResponse = new LineResponse(line);
+		return ResponseEntity.ok().body(lineResponse);
+	}
 
-    @PutMapping(value = "/{id}")
-    public ResponseEntity<Void> editLine(@PathVariable Long id,
-                                         @RequestBody LineRequest lineRequest) {
-        lineDao.update(id, lineRequest);
-        return ResponseEntity.ok().build();
-    }
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Void> editLine(@PathVariable Long id, @RequestBody LineRequest lineRequest) {
+		boolean existsName = lineDao.findByName(lineRequest.getName()).isPresent();
+		if (existsName) {
+			throw new DuplicateNameException("이미 존재하는 노선 이름입니다.");
+		}
 
-    @DeleteMapping(value = "/{id}")
-    public ResponseEntity<Void> deleteLine(@PathVariable Long id) {
-        lineDao.delete(id);
-        return ResponseEntity.noContent().build();
-    }
+		lineDao.update(id, lineRequest);
+		return ResponseEntity.ok().build();
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> deleteLine(@PathVariable Long id) {
+		lineDao.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 }
