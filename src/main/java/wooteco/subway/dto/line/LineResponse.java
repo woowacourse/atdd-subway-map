02af39d35e82e8374b76ec107f963650ac0df90d
NewWisdom <wlgp2500@gmail.com>
2021-05-08 20:11:15 +0900

    refactor(line-response): dto 내부에서 필요한 값을 세팅하도록 변경

--- src/main/java/wooteco/subway/dto/line/LineResponse.java
+++ src/main/java/wooteco/subway/dto/line/LineResponse.java
@@ -1,8 +1,10 @@
 package wooteco.subway.dto.line;
 
+import wooteco.subway.domain.Station;
 import wooteco.subway.dto.station.StationResponse;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LineResponse {
     private Long id;
@@ -13,11 +15,13 @@ public class LineResponse {
     public LineResponse() {
     }
 
-    public LineResponse(Long id, String name, String color, List<StationResponse> stations) {
+    public LineResponse(Long id, String name, String color, List<Station> stations) {
         this.id = id;
         this.name = name;
         this.color = color;
-        this.stations = stations;
+        this.stations = stations.stream()
+                .map(station -> new StationResponse(station.getId(), station.getName()))
+                .collect(Collectors.toList());
     }
 
     public Long getId() {
