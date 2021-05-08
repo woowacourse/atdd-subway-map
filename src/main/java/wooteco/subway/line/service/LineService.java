49e09f3fc2fce0b8eb587c03c57c860858ac13c6
Jinho <jh8579@gmail.com>
2021-05-08 17:06:08 +0900

    refactor : 사용하지 않는 메소드 제거

--- src/main/java/wooteco/subway/line/service/LineService.java
+++ src/main/java/wooteco/subway/line/service/LineService.java
@@ -86,11 +86,6 @@ public class LineService {
         }
     }
 
-    private Station findStationById(Long id) {
-        return stationDao.findById(id)
-            .orElseThrow(() -> new IllegalArgumentException("해당 지하철역이 존재하지 않습니다"));
-    }
-
     private void validateIsExistLineById(Long id) {
         if(lineDao.findById(id).isPresent()) {
             throw new IllegalArgumentException("해당 노선이 존재하지 않습니다");
