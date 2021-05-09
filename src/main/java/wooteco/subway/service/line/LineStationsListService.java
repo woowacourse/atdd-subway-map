f7502847985523eb5e4ee3d7cc8e1ac76ecef602
taehee-kim-dev <taehee.kim.dev@gmail.com>
2021-05-09 18:01:03 +0900

    refactor: LineStationsListService 리팩토링

--- src/main/java/wooteco/subway/service/line/LineStationsListService.java
+++ src/main/java/wooteco/subway/service/line/LineStationsListService.java
@@ -2,6 +2,7 @@ package wooteco.subway.service.line;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -31,42 +32,67 @@ public class LineStationsListService {
         this.lineDao = lineDao;
     }
 
-
     public LineStationsListResponseDto getAllStationsInOrderListByLineId(Long lineId) {
+        Line line = getLine(lineId);
         List<Section> sectionsOfLine = sectionDao.findAllByLineId(lineId);
-        List<Long> stationIdsInOrder = new LinkedList<>();
         Section firstSection = sectionsOfLine.get(0);
-        stationIdsInOrder.add(firstSection.getUpStationId());
-        stationIdsInOrder.add(firstSection.getDownStationId());
+        List<Long> stationIdsInOrder = new LinkedList<>(Arrays.asList(firstSection.getUpStationId(), firstSection.getDownStationId()));
+        setStationIdsInOrder(sectionsOfLine, stationIdsInOrder);
+        List<Station> stationsInOrder = getStationsInOrder(stationIdsInOrder);
+        List<StationResponseDto> stationResponseDtosInOrder = getStationResponseDtosFromStationsInOrder(stationsInOrder);
+        return new LineStationsListResponseDto(line.getId(), line.getName(), line.getColor(), stationResponseDtosInOrder);
+    }
 
-        Long upStationId = firstSection.getUpStationId();
-        Long downStationId = firstSection.getDownStationId();
-        while (stationIdsInOrder.size() < sectionsOfLine.size() + 1) {
-            for (Section section : sectionsOfLine) {
-                if (section.getDownStationId().equals(upStationId)) {
-                    stationIdsInOrder.add(0, section.getUpStationId());
-                    upStationId = section.getUpStationId();
-                }
-                if (section.getUpStationId().equals(downStationId)) {
-                    stationIdsInOrder.add(section.getDownStationId());
-                    downStationId = section.getDownStationId();
-                }
-            }
+    private Line getLine(Long lineId) {
+        return lineDao.findById(lineId)
+            .orElseThrow(() -> new HttpException(HttpStatus.BAD_REQUEST, "존재하지 않는 노선 id 입니다."));
+    }
+
+    private void setStationIdsInOrder(List<Section> sectionsOfLine, List<Long> stationIdsInOrder) {
+        int numberOfStationsInLine = sectionsOfLine.size() + 1;
+        while (stationIdsInOrder.size() < numberOfStationsInLine) {
+            checkStationsOfLineAndSetIdsInOrder(sectionsOfLine, stationIdsInOrder);
         }
+    }
 
+    private void checkStationsOfLineAndSetIdsInOrder(List<Section> sectionsOfLine, List<Long> stationIdsInOrder) {
+        Long firstId = stationIdsInOrder.get(0);
+        Long lastId = stationIdsInOrder.get(stationIdsInOrder.size() - 1);
+        for (Section section : sectionsOfLine) {
+            firstId = addStationIdToFirst(stationIdsInOrder, firstId, section);
+            lastId = addStationIdToLast(stationIdsInOrder, lastId, section);
+        }
+    }
+
+    private Long addStationIdToFirst(List<Long> stationIdsInOrder, Long firstId, Section section) {
+        if (section.getDownStationId().equals(firstId)) {
+            stationIdsInOrder.add(0, section.getUpStationId());
+            firstId = section.getUpStationId();
+        }
+        return firstId;
+    }
+
+    private Long addStationIdToLast(List<Long> stationIdsInOrder, Long lastId, Section section) {
+        if (section.getUpStationId().equals(lastId)) {
+            stationIdsInOrder.add(section.getDownStationId());
+            lastId = section.getDownStationId();
+        }
+        return lastId;
+    }
+
+    private List<Station> getStationsInOrder(List<Long> stationIdsInOrder) {
         List<Station> stationsInOrder = new ArrayList<>();
         for (Long stationId : stationIdsInOrder) {
             Station station = stationDao.findById(stationId)
                 .orElseThrow(() -> new HttpException(HttpStatus.INTERNAL_SERVER_ERROR, "노선 역 목록 조회 에러"));
             stationsInOrder.add(station);
         }
+        return stationsInOrder;
+    }
 
-        List<StationResponseDto> stationResponseDtosInOrder = stationsInOrder.stream()
+    private List<StationResponseDto> getStationResponseDtosFromStationsInOrder(List<Station> stationsInOrder) {
+        return stationsInOrder.stream()
             .map(StationResponseDto::new)
             .collect(Collectors.toList());
-
-        Line line = lineDao.findById(lineId)
-            .orElseThrow(() -> new HttpException(HttpStatus.BAD_REQUEST, "존재하지 않는 노선 id 입니다."));
-        return new LineStationsListResponseDto(line.getId(), line.getName(), line.getColor(), stationResponseDtosInOrder);
     }
 }
