b5855530e0c55780c5c837f7647c49c37044c292
pomo <bosl95@naver.com>
2021-05-09 09:27:14 +0900

    refactor: 부생성자가 주생성자를 사용하도록 수정

--- src/main/java/wooteco/subway/line/controller/LineResponse.java
+++ src/main/java/wooteco/subway/line/controller/LineResponse.java
@@ -3,6 +3,7 @@ package wooteco.subway.line.controller;
 import wooteco.subway.line.domain.Line;
 import wooteco.subway.station.controller.StationResponse;
 
+import java.util.Collections;
 import java.util.List;
 
 public class LineResponse {
@@ -12,7 +13,14 @@ public class LineResponse {
     private List<StationResponse> stations;
 
     public LineResponse() {
+    }
 
+    public LineResponse(Line line) {
+        this(line.getId(), line.getName(), line.getColor());
+    }
+
+    public LineResponse(Long id, String name, String color) {
+        this(id, name, color, Collections.emptyList());
     }
 
     public LineResponse(Long id, String name, String color, List<StationResponse> stations) {
@@ -20,16 +28,6 @@ public class LineResponse {
         this.name = name;
         this.color = color;
         this.stations = stations;
-    }
-
-    public LineResponse(Long id, String name, String color) {
-        this.id = id;
-        this.name = name;
-        this.color = color;
-    }
-
-    public LineResponse(Line line) {
-        this(line.getId(), line.getName(), line.getColor());
     }
 
     public Long getId() {
