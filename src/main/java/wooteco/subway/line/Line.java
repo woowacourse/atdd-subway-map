71b99761c2ed5cf5d2081396baf2f1d186605a21
YeongBeom Heo <hybeom@gmail.com>
2021-05-10 03:25:01 +0900

    feat(Line) : Line Entity 동등성 추가

--- src/main/java/wooteco/subway/line/Line.java
+++ src/main/java/wooteco/subway/line/Line.java
@@ -1,5 +1,7 @@
 package wooteco.subway.line;
 
+import java.util.Objects;
+
 public class Line {
 
     private Long id;
@@ -34,5 +36,22 @@ public class Line {
 
     public String getColor() {
         return color;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Line line = (Line) o;
+        return Objects.equals(id, line.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
