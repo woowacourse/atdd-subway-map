f186acca77a9ebf3134fe8af6daf1d6a8070b96b
taehee-kim-dev <taehee.kim.dev@gmail.com>
2021-05-09 18:01:15 +0900

    refactor: SectionDeleteService 리팩토링

--- src/main/java/wooteco/subway/service/section/SectionDeleteService.java
+++ src/main/java/wooteco/subway/service/section/SectionDeleteService.java
@@ -21,37 +21,58 @@ public class SectionDeleteService {
 
     public void deleteSectionById(Long lineId, Long stationIdToDelete) {
         List<Section> sections = sectionDao.findAllByLineId(lineId);
-        List<Section> sectionsWithStationToDelete = sections.stream()
+        List<Section> sectionsWithStationIdToDelete = getSectionsWithStationIdToDelete(stationIdToDelete, sections);
+        validateDeleteCondition(sections, sectionsWithStationIdToDelete);
+        if (sectionsWithStationIdToDelete.size() == 1) {
+            deleteFirstOrLastStation(sectionsWithStationIdToDelete);
+            return;
+        }
+        deleteStationFromMiddleOfLine(lineId, stationIdToDelete, sectionsWithStationIdToDelete);
+    }
+
+    private List<Section> getSectionsWithStationIdToDelete(Long stationIdToDelete, List<Section> sections) {
+        return sections.stream()
             .filter(section ->
                 section.getUpStationId().equals(stationIdToDelete)
                     || section.getDownStationId().equals(stationIdToDelete))
             .collect(Collectors.toList());
-        if (sectionsWithStationToDelete.size() == 0) {
+    }
+
+    private void validateDeleteCondition(List<Section> sections, List<Section> sectionsWithStationIdToDelete) {
+        if (sectionsWithStationIdToDelete.size() == 0) {
             throw new HttpException(HttpStatus.BAD_REQUEST, "노선에 삭제할 역이 존재하지 않습니다.");
         }
         if (sections.size() == 1) {
             throw new HttpException(HttpStatus.BAD_REQUEST, "한 개의 구간만 존재하는 노선은 구간을 삭제할 수 없습니다.");
         }
-        if (sectionsWithStationToDelete.size() == 1) {
-            Section sectionToDelete = sectionsWithStationToDelete.get(0);
-            sectionDao.deleteById(sectionToDelete.getId());
-        }
-        if (sectionsWithStationToDelete.size() == 2) {
-            Section newUpStationSection = sectionsWithStationToDelete.stream()
-                .filter(section -> section.getDownStationId().equals(stationIdToDelete))
-                .findFirst()
-                .orElseThrow(() -> new IllegalStateException(SECTION_DELETE_ERROR_MESSAGE));
-            Section newDownStationSection = sectionsWithStationToDelete.stream()
-                .filter(section -> section.getUpStationId().equals(stationIdToDelete))
-                .findFirst()
-                .orElseThrow(() -> new IllegalStateException(SECTION_DELETE_ERROR_MESSAGE));
+    }
 
-            int newDistance = newUpStationSection.getDistance() + newDownStationSection.getDistance();
-            Section newSection
-                = new Section(lineId, newUpStationSection.getUpStationId(), newDownStationSection.getDownStationId(), newDistance);
-            sectionDao.deleteById(newUpStationSection.getId());
-            sectionDao.deleteById(newDownStationSection.getId());
-            sectionDao.save(newSection);
-        }
+    private void deleteFirstOrLastStation(List<Section> sectionsWithStationIdToDelete) {
+        Section sectionToDelete = sectionsWithStationIdToDelete.get(0);
+        sectionDao.deleteById(sectionToDelete.getId());
+    }
+
+    private void deleteStationFromMiddleOfLine(Long lineId, Long stationIdToDelete, List<Section> sectionsWithStationIdToDelete) {
+        Section newUpStationSection = getNewUpStationSection(stationIdToDelete, sectionsWithStationIdToDelete);
+        Section newDownStationSection = getNewDownStationSection(stationIdToDelete, sectionsWithStationIdToDelete);
+        int newDistance = newUpStationSection.getDistance() + newDownStationSection.getDistance();
+        Section newSection = new Section(lineId, newUpStationSection.getUpStationId(), newDownStationSection.getDownStationId(), newDistance);
+        sectionDao.deleteById(newUpStationSection.getId());
+        sectionDao.deleteById(newDownStationSection.getId());
+        sectionDao.save(newSection);
+    }
+
+    private Section getNewUpStationSection(Long stationIdToDelete, List<Section> sectionsWithStationIdToDelete) {
+        return sectionsWithStationIdToDelete.stream()
+            .filter(section -> section.getDownStationId().equals(stationIdToDelete))
+            .findFirst()
+            .orElseThrow(() -> new IllegalStateException(SECTION_DELETE_ERROR_MESSAGE));
+    }
+
+    private Section getNewDownStationSection(Long stationIdToDelete, List<Section> sectionsWithStationIdToDelete) {
+        return sectionsWithStationIdToDelete.stream()
+            .filter(section -> section.getUpStationId().equals(stationIdToDelete))
+            .findFirst()
+            .orElseThrow(() -> new IllegalStateException(SECTION_DELETE_ERROR_MESSAGE));
     }
 }
