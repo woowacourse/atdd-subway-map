b6d6e98ba0dd5f711bf1e58cd7d79593da8e0078
jaeseongDev <qkrwotjd1445@naver.com>
2021-05-10 22:34:32 +0900

    refactor : Optional을 장점을 살리도록 수정

--- src/main/java/wooteco/subway/service/LineService.java
+++ src/main/java/wooteco/subway/service/LineService.java
@@ -1,5 +1,9 @@
 package wooteco.subway.service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 import wooteco.subway.controller.dto.SectionDto;
@@ -16,93 +20,90 @@ import wooteco.subway.exception.SubwayException;
 import wooteco.subway.repository.LineRepository;
 import wooteco.subway.repository.StationRepository;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Service
 public class LineService {
-    private static final int FIRST_INDEX = 0;
 
-    private final LineRepository lineRepository;
-    private final StationRepository stationRepository;
+  private static final int FIRST_INDEX = 0;
 
-    public LineService(LineRepository lineRepository, StationRepository stationRepository) {
-        this.lineRepository = lineRepository;
-        this.stationRepository = stationRepository;
+  private final LineRepository lineRepository;
+  private final StationRepository stationRepository;
+
+  public LineService(LineRepository lineRepository, StationRepository stationRepository) {
+    this.lineRepository = lineRepository;
+    this.stationRepository = stationRepository;
+  }
+
+  public LineCreateResponseDto createLine(LineCreateRequestDto lineRequest) {
+    Line newLine;
+    try {
+      newLine = lineRepository.saveLineWithSection(
+          lineRequest.getName(),
+          lineRequest.getColor(),
+          lineRequest.getUpStationId(),
+          lineRequest.getDownStationId(),
+          lineRequest.getDistance()
+      );
+    } catch (DuplicateKeyException e) {
+      throw new SubwayException("중복된 이름으로 Line을 등록할 수 없습니다.");
     }
+    Section section = newLine.getSections().get(FIRST_INDEX);
+    List<SectionDto> sectionDtos = Arrays.asList(
+        new SectionDto(
+            section.getId(),
+            section.getUpStationId(),
+            section.getDownStationId(),
+            section.getDistance()
+        )
+    );
+    return new LineCreateResponseDto(
+        newLine.getId(),
+        newLine.getName(),
+        newLine.getColor(),
+        sectionDtos
+    );
+  }
 
-    public LineCreateResponseDto createLine(LineCreateRequestDto lineRequest) {
-        Line newLine;
-        try {
-             newLine = lineRepository.saveLineWithSection(
-                    lineRequest.getName(),
-                    lineRequest.getColor(),
-                    lineRequest.getUpStationId(),
-                    lineRequest.getDownStationId(),
-                    lineRequest.getDistance()
-            );
-        } catch (DuplicateKeyException e) {
-            throw new SubwayException("중복된 이름으로 Line을 등록할 수 없습니다.");
-        }
-        Section section = newLine.getSections().get(FIRST_INDEX);
-        List<SectionDto> sectionDtos = Arrays.asList(
-                new SectionDto(
-                        section.getId(),
-                        section.getUpStationId(),
-                        section.getDownStationId(),
-                        section.getDistance()
-                )
-        );
-        return new LineCreateResponseDto(
-                newLine.getId(),
-                newLine.getName(),
-                newLine.getColor(),
-                sectionDtos
-        );
-    }
+  public List<LineFindAllResponseDto> showLines() {
+    List<Line> lines = lineRepository.findAllLine();
+    return lines.stream()
+        .map(it -> new LineFindAllResponseDto(it.getId(), it.getName(), it.getColor()))
+        .collect(Collectors.toList());
+  }
 
-    public List<LineFindAllResponseDto> showLines() {
-        List<Line> lines = lineRepository.findAllLine();
-        return lines.stream()
-                .map(it -> new LineFindAllResponseDto(it.getId(), it.getName(), it.getColor()))
-                .collect(Collectors.toList());
-    }
+  public LineFindResponseDto showLine(Long lineId) {
+    Line line = lineRepository.findLineWithSectionsById(lineId);
+    List<Section> sections = line.sortSections();
+    List<Station> stations = new ArrayList<>();
+    List<Long> stationIds = sections.stream().map(section -> section.getDownStationId())
+        .collect(Collectors.toList());
+    List<StationDto> stationDtos = stations.stream()
+        .map(station -> new StationDto(station.getId(), station.getName()))
+        .collect(Collectors.toList());
 
-    public LineFindResponseDto showLine(Long lineId) {
-        Line line = lineRepository.findLineWithSectionsById(lineId);
-        List<Section> sections = line.sortSections();
-        List<Station> stations = new ArrayList<>();
-        Station firstStation = stationRepository.findStationById(sections.get(0).getUpStationId()).get();
-        stations.add(firstStation);
+    Station firstStation = stationRepository.findStationById(sections.get(0).getUpStationId())
+        .orElseThrow(() -> new SubwayException("해당 라인에 역이 존재하지 않습니다."));
+    stations.add(firstStation);
+    List<Station> foundStations = stationRepository.findStationsByIds(stationIds);
+    stations.addAll(foundStations);
 
-        List<Long> stationIds = sections.stream().map(section -> section.getDownStationId()).collect(Collectors.toList());
-        List<Station> foundStations = stationRepository.findStationsByIds(stationIds);
-        stations.addAll(foundStations);
+    return new LineFindResponseDto(
+        line, stationDtos
+    );
+  }
 
-        List<StationDto> stationDtos = stations.stream()
-                .map(station -> new StationDto(station.getId(), station.getName()))
-                .collect(Collectors.toList());
+  public long editLine(Long lineId, LineEditRequestDto request) {
+    return lineRepository.edit(lineId, request.getName(), request.getColor());
+  }
 
-        return new LineFindResponseDto(
-                line, stationDtos
-        );
-    }
+  public int deleteLine(Long lineId) {
+    return lineRepository.deleteLineWithSectionByLineId(lineId);
+  }
 
-    public long editLine(Long lineId, LineEditRequestDto request) {
-        return lineRepository.edit(lineId, request.getName(), request.getColor());
-    }
+  public void createSectionInLine(Long lineId, Long upStationId, Long downStationId, int distance) {
+    lineRepository.createSectionInLine(lineId, upStationId, downStationId, distance);
+  }
 
-    public int deleteLine(Long lineId) {
-        return lineRepository.deleteLineWithSectionByLineId(lineId);
-    }
-
-    public void createSectionInLine(Long lineId, Long upStationId, Long downStationId, int distance) {
-        lineRepository.createSectionInLine(lineId, upStationId, downStationId, distance);
-    }
-
-    public void deleteSectionInLine(Long lineId, Long stationId) {
-        lineRepository.deleteSectionInLine(lineId, stationId);
-    }
+  public void deleteSectionInLine(Long lineId, Long stationId) {
+    lineRepository.deleteSectionInLine(lineId, stationId);
+  }
 }
