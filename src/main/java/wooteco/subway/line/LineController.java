f636ec3a0fd3817af83173bba1bd11cbebb77f8a
멍토 <daum7766@naver.com>
2021-05-10 17:35:12 +0900

    refactor: Service와 통신하도록 변경

--- src/main/java/wooteco/subway/line/LineController.java
+++ src/main/java/wooteco/subway/line/LineController.java
@@ -2,7 +2,6 @@ package wooteco.subway.line;
 
 import java.net.URI;
 import java.util.List;
-import java.util.stream.Collectors;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,50 +16,40 @@ import wooteco.subway.line.dto.LineRequest;
 import wooteco.subway.line.dto.LineResponse;
 
 @RestController
-@RequestMapping("/lines")
+@RequestMapping(value = "/lines", produces = MediaType.APPLICATION_JSON_VALUE)
 public class LineController {
 
-    private final LineDao lineDao;
+    private final LineService lineService;
 
-    public LineController(LineDao lineDao) {
-        this.lineDao = lineDao;
+    public LineController(LineService lineService) {
+        this.lineService = lineService;
     }
 
     @PostMapping
     public ResponseEntity<LineResponse> createLine(@RequestBody LineRequest lineRequest) {
-        Line line = new Line(lineRequest);
-        Line newLine = lineDao.save(line);
-        LineResponse lineResponse = new LineResponse(newLine);
-        return ResponseEntity.created(URI.create("/lines/" + newLine.getId())).body(lineResponse);
+        LineResponse lineResponse = lineService.create(lineRequest);
+        return ResponseEntity.created(URI.create("/lines/" + lineResponse.getId())).body(lineResponse);
     }
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping()
     public ResponseEntity<List<LineResponse>> showLines() {
-        List<Line> lines = lineDao.findAll();
-
-        List<LineResponse> lineResponses = lines.stream()
-            .map(LineResponse::new)
-            .collect(Collectors.toList());
-        return ResponseEntity.ok(lineResponses);
+        return ResponseEntity.ok(lineService.findAll());
     }
 
-    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping("/{id}")
     public ResponseEntity<LineResponse> showLine(@PathVariable Long id) {
-        Line line = lineDao.findById(id);
-        LineResponse lineResponse = new LineResponse(line);
-        return ResponseEntity.ok(lineResponse);
+        return ResponseEntity.ok(lineService.findById(id));
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Void> updateLine(@PathVariable Long id, @RequestBody LineRequest lineRequest) {
-        Line line = new Line(id, lineRequest);
-        lineDao.update(line);
+        lineService.update(id, lineRequest);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteLine(@PathVariable Long id) {
-        lineDao.delete(id);
+        lineService.delete(id);
         return ResponseEntity.noContent().build();
     }
 
