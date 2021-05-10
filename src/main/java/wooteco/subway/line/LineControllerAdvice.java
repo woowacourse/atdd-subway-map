6f8da897bfac6e20cb79085a740913b1fc907a32
YeongBeom Heo <hybeom@gmail.com>
2021-05-11 01:54:10 +0900

    polish(LineControllerAdvice) : 불필요한 의존성 제거

--- src/main/java/wooteco/subway/line/LineControllerAdvice.java
+++ src/main/java/wooteco/subway/line/LineControllerAdvice.java
@@ -1,7 +1,5 @@
 package wooteco.subway.line;
 
-import java.sql.SQLNonTransientException;
-import org.springframework.dao.DuplicateKeyException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
