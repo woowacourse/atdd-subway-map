fa0c9f84e799eb9fd37422654dc67359e9448f0f
JinYoung Park <basejin0810@naver.com>
2021-05-10 14:56:34 +0900

    feat: 구간 정렬 기능 구현

--- src/main/java/wooteco/subway/station/Stations.java
+++ src/main/java/wooteco/subway/station/Stations.java
@@ -2,6 +2,7 @@ package wooteco.subway.station;
 
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -13,12 +14,13 @@ public class Stations {
     private final Map<Long, Station> stationGroup;
 
     public Stations(final Map<Long, Station> stationGroup) {
-        this.stationGroup = new HashMap<>(stationGroup);
+        this.stationGroup = new LinkedHashMap<>(stationGroup);
     }
 
     public void addStations(final List<Station> stations) {
         stationGroup.putAll(stations.stream()
-            .collect(Collectors.toMap(Station::getId, Function.identity())));
+            .collect(Collectors.toMap(Station::getId, Function.identity(),
+                (existing, replacement) -> existing, LinkedHashMap::new)));
     }
 
     public Map<Long, Station> toMap() {
