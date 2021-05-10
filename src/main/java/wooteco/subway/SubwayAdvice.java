bd217fc98c78c5e4c0377807ebe5aaa8aecb5a24
멍토 <daum7766@naver.com>
2021-05-10 13:48:11 +0900

    feat: 예외처리 상황 추가

--- src/main/java/wooteco/subway/SubwayAdvice.java
+++ src/main/java/wooteco/subway/SubwayAdvice.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import wooteco.subway.exception.DuplicateException;
+import wooteco.subway.exception.NotExistItemException;
 
 @ControllerAdvice
 public class SubwayAdvice {
@@ -13,4 +14,14 @@ public class SubwayAdvice {
         return ResponseEntity.badRequest().body(e.getMessage());
     }
 
+    @ExceptionHandler(NotExistItemException.class)
+    public ResponseEntity<String> handleNotExistItemException(Exception e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception e) {
+        System.out.println(e.getMessage());
+        return ResponseEntity.badRequest().body("알 수 없는 에러가 발상했습니다.");
+    }
 }
