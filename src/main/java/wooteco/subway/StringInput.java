7af92b0ccce159c8ce0b6af47fdc8ca66863f47b
멍토 <daum7766@naver.com>
2021-05-10 22:44:40 +0900

    refactor: 검증하는 객체를 분리

---
+++ src/main/java/wooteco/subway/StringInput.java
@@ -0,0 +1,41 @@
+package wooteco.subway;
+
+import java.util.Objects;
+import wooteco.subway.exception.NotInputDataException;
+
+public class StringInput {
+
+    private final String item;
+
+    public StringInput(String item) {
+        validate(item);
+        this.item = item;
+    }
+
+    private void validate(String item) {
+        if (Objects.isNull(item) || item.trim().length() == 0) {
+            throw new NotInputDataException();
+        }
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        StringInput that = (StringInput) o;
+        return Objects.equals(item, that.item);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(item);
+    }
+}
--- src/test/java/wooteco/subway/StringInputTest.java
+++ src/test/java/wooteco/subway/StringInputTest.java
@@ -1,25 +1,26 @@
-package wooteco.subway.line;
+package wooteco.subway;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import wooteco.subway.exception.NotInputDataException;
+import wooteco.subway.station.Station;
 
-@DisplayName("Line객체 테스트")
-class LineTest {
+@DisplayName("문자열 입력 객체 테스츠")
+class StringInputTest {
 
     @Test
     @DisplayName("입력을 null 상태로 객체를 만들면 에러가 발생한다.")
     public void createWithDataNull() {
-        assertThatThrownBy(() -> new Line(0L, null, null))
+        assertThatThrownBy(() -> new StringInput(null))
             .isInstanceOf(NotInputDataException.class);
     }
 
     @Test
     @DisplayName("입력을 공백 상태로 객체를 만들면 에러가 발생한다.")
     public void createWithDataSpace() {
-        assertThatThrownBy(() -> new Line(0L, " ", " "))
+        assertThatThrownBy(() -> new StringInput(" "))
             .isInstanceOf(NotInputDataException.class);
     }
 }
