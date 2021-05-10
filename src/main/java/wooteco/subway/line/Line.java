5e3e271be8e43a2cd0b49509547b482dba7c3fbf
seunghyun <ggyool@naver.com>
2021-05-10 14:44:00 +0900

    feat: Sections, Stations 일급컬렉션 추가

--- src/main/java/wooteco/subway/line/Line.java
+++ src/main/java/wooteco/subway/line/Line.java
@@ -1,30 +1,41 @@
 package wooteco.subway.line;
 
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import wooteco.subway.line.section.Section;
+import wooteco.subway.line.section.Sections;
 import wooteco.subway.station.Station;
+import wooteco.subway.station.Stations;
 
 public class Line {
 
     private final Long id;
     private final String name;
     private final String color;
-    private final List<Station> stations;
+    private final Sections sections;
+    private final Stations stations;
 
     public Line(String name, String color) {
-        this(null, name, color, new ArrayList<>());
+        this(null, name, color, Collections.emptyList(), Collections.emptyMap());
     }
 
     public Line(Long id, String name, String color) {
-        this(id, name, color, new ArrayList<>());
+        this(id, name, color, Collections.emptyList(), Collections.emptyMap());
     }
 
-    public Line(Long id, String name, String color, List<Station> stations) {
+    public Line(Long id, String name, String color, List<Section> sectionGroup, Map<Long, Station> stationGroup) {
+        this(id, name, color, new Sections(sectionGroup), new Stations(stationGroup));
+    }
+
+    public Line(final Long id, final String name, final String color, final Sections sections,
+        final Stations stations) {
         this.id = id;
         this.name = name;
         this.color = color;
+        this.sections = sections;
         this.stations = stations;
     }
 
@@ -40,7 +51,11 @@ public class Line {
         return color;
     }
 
-    public List<Station> getStations() {
+    public Sections getSections() {
+        return sections;
+    }
+
+    public Stations getStations() {
         return stations;
     }
 
