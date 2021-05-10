619fad7152771461862360a8aa98c44e2317b385
Jinhong <xlffm3@gmail.com>
2021-05-10 11:56:37 +0900

    refaxtor: StationDao simpleJdbcInsert 사용

--- src/main/java/wooteco/subway/dao/StationDao.java
+++ src/main/java/wooteco/subway/dao/StationDao.java
@@ -3,63 +3,56 @@ package wooteco.subway.dao;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import wooteco.subway.domain.station.Station;
 import wooteco.subway.exception.ExceptionStatus;
 import wooteco.subway.exception.SubwayException;
 
-import java.sql.PreparedStatement;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public class StationDao {
-    private static final RowMapper<Station> ROW_MAPPER = (resultSet, rowNumber) -> {
-        long id = resultSet.getLong("id");
-        String name = resultSet.getString("name");
+    private static final RowMapper<Station> BASIC_STATION_ROW_MAPPER = (resultSet, rowNumber) -> {
+        long id = resultSet.getLong("ID");
+        String name = resultSet.getString("NAME");
         return new Station(id, name);
     };
     private static final int ROW_COUNTS_FOR_ID_NOT_FOUND = 0;
 
     private final JdbcTemplate jdbcTemplate;
+    private final SimpleJdbcInsert simpleJdbcInsert;
 
     public StationDao(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+        this.simpleJdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("STATION")
+                .usingGeneratedKeyColumns("ID");
     }
 
     public long save(Station station) {
-        String query = "INSERT INTO Station (name) VALUES (?)";
-        KeyHolder keyHolder = new GeneratedKeyHolder();
-        PreparedStatementCreator preparedStatementCreator = getPreparedStatementCreator(station, query);
+        SqlParameterSource sqlParameterSource = new BeanPropertySqlParameterSource(station);
         try {
-            jdbcTemplate.update(preparedStatementCreator, keyHolder);
-            return keyHolder.getKey().longValue();
+            return simpleJdbcInsert.executeAndReturnKey(sqlParameterSource)
+                    .longValue();
         } catch (DuplicateKeyException duplicateKeyException) {
             throw new SubwayException(ExceptionStatus.DUPLICATED_NAME);
         }
     }
 
-    private PreparedStatementCreator getPreparedStatementCreator(Station station, String query) {
-        return (connection) -> {
-            PreparedStatement prepareStatement = connection.prepareStatement(query, new String[]{"id"});
-            prepareStatement.setString(1, station.getName());
-            return prepareStatement;
-        };
-    }
-
     public List<Station> findAll() {
         String query = "SELECT ID, NAME FROM STATION";
-        return jdbcTemplate.query(query, ROW_MAPPER);
+        return jdbcTemplate.query(query, BASIC_STATION_ROW_MAPPER);
     }
 
     public Optional<Station> findById(long id) {
         String query = "SELECT ID, NAME FROM STATION WHERE ID = ?";
         try {
-            Station station = jdbcTemplate.queryForObject(query, ROW_MAPPER, id);
+            Station station = jdbcTemplate.queryForObject(query, BASIC_STATION_ROW_MAPPER, id);
             return Optional.ofNullable(station);
         } catch (EmptyResultDataAccessException emptyResultDataAccessException) {
             return Optional.empty();
--- src/main/java/wooteco/subway/domain/station/Station.java
+++ src/main/java/wooteco/subway/domain/station/Station.java
@@ -16,10 +16,6 @@ public class Station {
         this(null, name);
     }
 
-    public boolean hasSameName(String name) {
-        return this.name.equals(name);
-    }
-
     public Long getId() {
         return id;
     }
