fb995a3299fcfb1c54a597217008432d3e5c394e
멍토 <daum7766@naver.com>
2021-05-12 01:12:36 +0900

    refactor: 변수명 대문자로 변경

--- src/main/java/wooteco/subway/exception/DuplicateException.java
+++ src/main/java/wooteco/subway/exception/DuplicateException.java
@@ -2,9 +2,9 @@ package wooteco.subway.exception;
 
 public class DuplicateException extends RuntimeException {
 
-    private static final String message = "[ERROR] 중복된 이름입니다.";
+    private static final String MESSAGE = "[ERROR] 중복된 이름입니다.";
 
     public DuplicateException() {
-        super(message);
+        super(MESSAGE);
     }
 }
--- src/main/java/wooteco/subway/exception/NotExistItemException.java
+++ src/main/java/wooteco/subway/exception/NotExistItemException.java
@@ -2,9 +2,9 @@ package wooteco.subway.exception;
 
 public class NotExistItemException extends RuntimeException {
 
-    private static final String message = "[ERROR] 해당 아이템이 존재하지 않습니다.";
+    private static final String MESSAGE = "[ERROR] 해당 아이템이 존재하지 않습니다.";
 
     public NotExistItemException() {
-        super(message);
+        super(MESSAGE);
     }
 }
