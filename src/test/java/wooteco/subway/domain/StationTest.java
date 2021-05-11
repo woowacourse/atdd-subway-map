a8a910786dea8b29ee036a46dbecb92c2f1f7962
ROOT <junroot0909@gmail.com>
2021-05-11 12:33:45 +0900

    test: 역, 구간 도메인 TC 작성

---
+++ src/test/java/wooteco/subway/domain/StationTest.java
@@ -0,0 +1,22 @@
+package wooteco.subway.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("역 도메인 테스트")
+public class StationTest {
+
+    @DisplayName("ID가 같으면 같은 역으로 취급한다.")
+    @Test
+    void equals() {
+        assertThat(new Station(1L, "왕십리역")).isEqualTo(new Station(1L, "답십리역"));
+    }
+
+    @DisplayName("ID가 다르면 다른 역으로 취급한다.")
+    @Test
+    void notEquals() {
+        assertThat(new Station(1L, "왕십리역")).isNotEqualTo(new Station(2L, "왕십리역"));
+    }
+}
