93f7ac129c9ba32c595a491f1e438bbd2730bab5
Jinhong <xlffm3@gmail.com>
2021-05-11 19:32:04 +0900

    feat: BindingResult 관련 예외처리 추가

--- src/main/java/wooteco/subway/controller/ExceptionController.java
+++ src/main/java/wooteco/subway/controller/ExceptionController.java
@@ -1,12 +1,18 @@
 package wooteco.subway.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import wooteco.subway.exception.SubwayException;
 
 @ControllerAdvice
 public class ExceptionController {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleBindingException(MethodArgumentNotValidException methodArgumentNotValidException) {
+        return ResponseEntity.badRequest().body(methodArgumentNotValidException.getMessage());
+    }
 
     @ExceptionHandler(SubwayException.class)
     public ResponseEntity<String> handleSubwayException(SubwayException subwayException) {
