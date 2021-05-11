d538871226a3896d863de20acb58e556e90b2c70
yoonsung.jung <ybook2012@gmail.com>
2021-05-11 10:26:47 +0900

    feat : 구간 삽입시, 기존 구간을 삭제하는 기능 구현

--- src/main/java/wooteco/subway/line/repository/SectionRepository.java
+++ src/main/java/wooteco/subway/line/repository/SectionRepository.java
@@ -6,6 +6,7 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
+import wooteco.subway.exception.NotFoundException;
 import wooteco.subway.line.dto.SectionRequest;
 
 import java.sql.PreparedStatement;
@@ -13,6 +14,7 @@ import java.util.*;
 
 @Repository
 public class SectionRepository {
+    private static final int NO_EXIST_COUNT = 0;
     private final JdbcTemplate jdbcTemplate;
 
     private final RowMapper<Long> stationIdRowMapperByUpStationId = (resultSet, rowNum) -> resultSet.getLong("up_station_id");
@@ -47,41 +49,51 @@ public class SectionRepository {
 
     public void saveBaseOnUpStation(final Long lineId, final SectionRequest sectionRequest) {
         try {
-            String query = "SELECT down_station_id FROM section WHERE line_id = ? AND up_station_id = ? ORDER BY distance DESC LIMIT 1";
-            Long closestStationId = Objects.requireNonNull(jdbcTemplate.queryForObject(query, Long.class, lineId, sectionRequest.getUpStationId()));
-            saveSectionBetweenStationsBaseOnUpStation(lineId, sectionRequest, closestStationId);
+            String query = "SELECT down_station_id FROM section WHERE line_id = ? AND up_station_id = ?";
+            Long beforeConnectedStationId = Objects.requireNonNull(jdbcTemplate.queryForObject(query, Long.class, lineId, sectionRequest.getUpStationId()));
+            saveSectionBetweenStationsBaseOnUpStation(lineId, sectionRequest, beforeConnectedStationId);
         } catch (EmptyResultDataAccessException e) {
             save(lineId, sectionRequest.getUpStationId(), sectionRequest.getDownStationId(), sectionRequest.getDistance());
         }
     }
 
-    private void saveSectionBetweenStationsBaseOnUpStation(final Long lineId, final SectionRequest sectionRequest, final Long closestStationId) {
-        String query = "SELECT distance FROM section WHERE line_id = ? AND up_station_id = ? ORDER BY distance DESC LIMIT 1";
+    private void saveSectionBetweenStationsBaseOnUpStation(final Long lineId, final SectionRequest sectionRequest, final Long beforeConnectedStationId) {
+        String query = "SELECT distance FROM section WHERE line_id = ? AND up_station_id = ?";
         int currentShortestDistance = Objects.requireNonNull(jdbcTemplate.queryForObject(query, Integer.class, lineId, sectionRequest.getUpStationId()));
         if (currentShortestDistance <= sectionRequest.getDistance()) {
             throw new IllegalArgumentException("기존에 존재하는 구간의 길이가 더 짧습니다.");
         }
         save(lineId, sectionRequest.getUpStationId(), sectionRequest.getDownStationId(), sectionRequest.getDistance());
-        save(lineId, sectionRequest.getDownStationId(), closestStationId, currentShortestDistance - sectionRequest.getDistance());
+        save(lineId, sectionRequest.getDownStationId(), beforeConnectedStationId, currentShortestDistance - sectionRequest.getDistance());
+        deleteSection(lineId, sectionRequest.getUpStationId(), beforeConnectedStationId);
     }
 
     public void saveBaseOnDownStation(final Long lineId, final SectionRequest sectionRequest) {
         try {
-            String query = "SELECT up_station_id FROM section WHERE line_id = ? AND down_station_id = ? ORDER BY distance DESC LIMIT 1";
-            Long closestStationId = Objects.requireNonNull(jdbcTemplate.queryForObject(query, Long.class, lineId, sectionRequest.getDownStationId()));
-            saveSectionBetweenStationsBaseOnDownStation(lineId, sectionRequest, closestStationId);
+            String query = "SELECT up_station_id FROM section WHERE line_id = ? AND down_station_id = ?";
+            Long beforeConnectedStationId = Objects.requireNonNull(jdbcTemplate.queryForObject(query, Long.class, lineId, sectionRequest.getDownStationId()));
+            saveSectionBetweenStationsBaseOnDownStation(lineId, sectionRequest, beforeConnectedStationId);
         } catch (EmptyResultDataAccessException e) {
             save(lineId, sectionRequest.getUpStationId(), sectionRequest.getDownStationId(), sectionRequest.getDistance());
         }
     }
 
-    private void saveSectionBetweenStationsBaseOnDownStation(final Long lineId, final SectionRequest sectionRequest, final Long closestStationId) {
-        String query = "SELECT distance FROM section WHERE line_id = ? AND down_station_id = ? ORDER BY distance DESC LIMIT 1";
+    private void saveSectionBetweenStationsBaseOnDownStation(final Long lineId, final SectionRequest sectionRequest, final Long beforeConnectedStationId) {
+        String query = "SELECT distance FROM section WHERE line_id = ? AND down_station_id = ?";
         int currentShortestDistance = Objects.requireNonNull(jdbcTemplate.queryForObject(query, Integer.class, lineId, sectionRequest.getDownStationId()));
         if (currentShortestDistance <= sectionRequest.getDistance()) {
             throw new IllegalArgumentException("기존에 존재하는 구간의 길이가 더 짧습니다.");
         }
         save(lineId, sectionRequest.getUpStationId(), sectionRequest.getDownStationId(), sectionRequest.getDistance());
-        save(lineId, closestStationId, sectionRequest.getUpStationId(), currentShortestDistance - sectionRequest.getDistance());
+        deleteSection(lineId, beforeConnectedStationId, sectionRequest.getDownStationId());
+        save(lineId, beforeConnectedStationId, sectionRequest.getUpStationId(), currentShortestDistance - sectionRequest.getDistance());
+    }
+
+    private void deleteSection(final Long lineId, final Long upStationId, final Long downStationId) {
+        String query = "DELETE FROM section WHERE line_id = ? AND up_station_id = ? AND down_station_id = ?";
+        int affectedRowCount = jdbcTemplate.update(query, lineId, upStationId, downStationId);
+        if (affectedRowCount == NO_EXIST_COUNT) {
+            throw new NotFoundException("존재하지 않는 구간을 삭제하려 했습니다.");
+        }
     }
 }
