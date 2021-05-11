beb0724d5b7f382f56292d7b535080aa65547d67
pika96 <52442949+pika96@users.noreply.github.com>
2021-05-12 02:32:32 +0900

    feat: 발생 시킨 에러를 잡는 코드 구현

--- src/main/java/wooteco/subway/SubwayControllerAdvice.java
+++ src/main/java/wooteco/subway/SubwayControllerAdvice.java
@@ -4,13 +4,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import wooteco.subway.exception.DuplicatedNameException;
-import wooteco.subway.exception.IllegalLineArgumentException;
+import wooteco.subway.exception.*;
 import wooteco.subway.exception.notfoundexception.NotFoundException;
 import wooteco.subway.exception.notfoundexception.NotFoundStationException;
 
 @ControllerAdvice
 public class SubwayControllerAdvice {
+
+    @ExceptionHandler(SubwayException.class)
+    public ResponseEntity<String> subWayException(SubwayException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
 
     @ExceptionHandler(DuplicatedNameException.class)
     public ResponseEntity<String> duplicatedException(DuplicatedNameException e) {
@@ -23,7 +27,17 @@ public class SubwayControllerAdvice {
     }
 
     @ExceptionHandler(IllegalLineArgumentException.class)
-    public ResponseEntity<String> notFoundException(IllegalLineArgumentException e) {
+    public ResponseEntity<String> lineArgumentException(IllegalLineArgumentException e) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+    }
+
+    @ExceptionHandler(IllegalSectionStatusException.class)
+    public ResponseEntity<String> sectionStatusException(IllegalSectionStatusException e) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+    }
+
+    @ExceptionHandler(SectionUpdateException.class)
+    public ResponseEntity<String> sectionUpdateException(SectionUpdateException e) {
         return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
     }
 }
