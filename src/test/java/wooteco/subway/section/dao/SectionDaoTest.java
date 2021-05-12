870cbe20fc93af212627585e4b7d30a0f0116f43
rok93 <goodboy302@naver.com>
2021-05-13 03:53:51 +0900

    refactor(SectionDao) : SectionDao test 코드 Dao 초기화를 위한 파라미터 주입방식 변경

--- src/test/java/wooteco/subway/section/dao/SectionDaoTest.java
+++ src/test/java/wooteco/subway/section/dao/SectionDaoTest.java
@@ -1,6 +1,7 @@
 package wooteco.subway.section.dao;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
@@ -15,18 +16,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 @JdbcTest
 class SectionDaoTest {
 
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
-
     private SectionDao sectionDao;
 
     @BeforeEach
-    void setUp() {
+    void setUp(@Autowired JdbcTemplate jdbcTemplate) {
         sectionDao = new SectionDao(jdbcTemplate);
     }
 
+    @DisplayName("특정 노선에 포함되는 section들을 조회하는 기능")
     @Test
-    void name() {
+    void findSectionsByLineId() {
         //given
         LineRequest request = new LineRequest("2호선", "green", 1L, 2L, 10);
         LineRequest request2 = new LineRequest("2호선", "green", 2L, 3L, 10);
