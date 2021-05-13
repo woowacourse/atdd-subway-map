0dd62f77874533c3cdcd150dbdd791de9fb3aaf7
Gomding <backchida@gmail.com>
2021-05-14 00:06:15 +0900

    refactor(LineService.java) : Optional 의 값을 받고 값이 없을 시 예외를 던지는 방법 변경

--- src/main/java/wooteco/subway/line/service/LineService.java
+++ src/main/java/wooteco/subway/line/service/LineService.java
@@ -2,10 +2,13 @@ package wooteco.subway.line.service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.yaml.snakeyaml.constructor.DuplicateKeyException;
+import wooteco.subway.exception.DuplicatedNameException;
 import wooteco.subway.exception.LineNotFoundException;
 import wooteco.subway.line.domain.Line;
 import wooteco.subway.line.repository.LineRepository;
 import wooteco.subway.line.repository.dto.LineDto;
+import wooteco.subway.line.service.dto.LineSaveDto;
 
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -22,9 +25,13 @@ public class LineService {
     }
 
     @Transactional
-    public LineDto save(final LineDto linedto) {
-        Line line = new Line(linedto.getName(), linedto.getColor());
-        return LineDto.from(lineRepository.save(line));
+    public LineDto save(final LineSaveDto lineSaveDto) {
+        try {
+            Line line = new Line(lineSaveDto.getName(), lineSaveDto.getColor());
+            return LineDto.from(lineRepository.save(line));
+        } catch (DuplicateKeyException e) {
+            throw new DuplicatedNameException("이미 존재하는 지하철 노선 이름 입니다.");
+        }
     }
 
     @Transactional(readOnly = true)
@@ -37,26 +44,21 @@ public class LineService {
     @Transactional(readOnly = true)
     public LineDto findById(final Long id) {
         Optional<Line> optionalLine = lineRepository.findById(id);
-        if (optionalLine.isPresent()) {
-            return LineDto.from(optionalLine.get());
-        }
-        throw new LineNotFoundException("해당 Id의 지하철 노선은 존재하지 않습니다.");
+        Line line = optionalLine.orElseThrow(LineNotFoundException::new);
+        return LineDto.from(line);
     }
 
     @Transactional
     public void delete(final Long id) {
         Optional<Line> optionalLine = lineRepository.findById(id);
-        if (optionalLine.isPresent()) {
-            lineRepository.delete(id);
-            return;
-        }
-        throw new LineNotFoundException("해당 Id의 지하철 노선은 존재하지 않습니다.");
+        optionalLine.orElseThrow(LineNotFoundException::new);
+        lineRepository.delete(id);
     }
 
     @Transactional
     public void update(final LineDto lineDto) {
         try {
-            Line line = lineRepository.findById(lineDto.getId()).get();
+            Line line = lineRepository.findById(lineDto.getId()).orElseThrow(LineNotFoundException::new);
             Line updatedLine = line.update(lineDto.getName(), lineDto.getColor());
             lineRepository.update(updatedLine);
         } catch (NoSuchElementException e) {
