6a74c8fb1d9468e021cd1b620318a1fd2c7b132f
pika96 <52442949+pika96@users.noreply.github.com>
2021-05-14 03:08:50 +0900

    fix: SectionDao 테스트 수정

--- src/test/java/wooteco/subway/section/dao/JDBCSectionDaoTest.java
+++ src/test/java/wooteco/subway/section/dao/JDBCSectionDaoTest.java
@@ -8,9 +8,9 @@ import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import wooteco.subway.section.domain.Section;
 import wooteco.subway.section.domain.Sections;
-
-import java.util.ArrayList;
-import java.util.List;
+import wooteco.subway.station.dao.JDBCStationDao;
+import wooteco.subway.station.dao.StationDao;
+import wooteco.subway.station.domain.Station;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -20,57 +20,65 @@ class JDBCSectionDaoTest {
     @Autowired
     private JdbcTemplate jdbcTemplate;
     private JDBCSectionDao jdbcSectionDao;
+    private StationDao stationDao;
+    private Station ganamStation;
+    private Station yeoksamStation;
+    private Station jamsilStation;
+    private Section section1;
+    private Section section2;
 
     @BeforeEach
     void setUp() {
+
         jdbcSectionDao = new JDBCSectionDao(jdbcTemplate);
+        stationDao = new JDBCStationDao(jdbcTemplate);
+        ganamStation = new Station(1L, "강남역");
+        yeoksamStation = new Station(2L, "역삼역");
+        jamsilStation = new Station(3L, "잠실역");
+
+        stationDao.save(ganamStation);
+        stationDao.save(yeoksamStation);
+        stationDao.save(jamsilStation);
+
+        section1 = new Section(1L, 1L, ganamStation, yeoksamStation, 3);
+        section2 = new Section(2L, 1L, yeoksamStation, jamsilStation, 3);
     }
 
     @Test
     @DisplayName("구간 저장 테스트")
     void save() {
-        Section section = new Section(1L, 1L, 1L, 2L, 3);
-        Section savedSection = jdbcSectionDao.save(section);
+        Section savedSection = jdbcSectionDao.save(section1);
 
-        assertThat(savedSection.getUpStationId()).isEqualTo(section.getUpStationId());
-        assertThat(savedSection.getDownStationId()).isEqualTo(section.getDownStationId());
-        assertThat(savedSection.getDistance()).isEqualTo(section.getDistance());
+        assertThat(savedSection.getUpStation()).isEqualTo(section1.getUpStation());
+        assertThat(savedSection.getDownStation()).isEqualTo(section1.getDownStation());
+        assertThat(savedSection.getDistance()).isEqualTo(section1.getDistance());
     }
 
     @Test
     @DisplayName("노선 번호로 구간 찾기 테스트")
     void findByLineId() {
-        Section section1 = new Section(1L, 1L, 1L, 2L, 3);
-        Section section2 = new Section(2L, 1L, 2L, 3L, 3);
 
-        List<Section> sectionList = new ArrayList<>();
-        sectionList.add(section1);
-        sectionList.add(section2);
-
-        Sections sections = new Sections(sectionList);
-        jdbcSectionDao.save(section1);
+        Section savedSection = jdbcSectionDao.save(section1);
         jdbcSectionDao.save(section2);
 
-        Sections findByLineIdSections = jdbcSectionDao.findByLineId(1L);
+        Sections findByLineIdSections = jdbcSectionDao.findByLineId(savedSection.getLineId());
 
         Section findByLineIdSection = findByLineIdSections.getSections().get(0);
 
-        assertThat(findByLineIdSection.getUpStationId()).isEqualTo(section1.getUpStationId());
-        assertThat(findByLineIdSection.getDownStationId()).isEqualTo(section1.getDownStationId());
+        assertThat(findByLineIdSection.getUpStation()).isEqualTo(section1.getUpStation());
+        assertThat(findByLineIdSection.getDownStation()).isEqualTo(section1.getDownStation());
         assertThat(findByLineIdSection.getDistance()).isEqualTo(section1.getDistance());
     }
 
     @Test
     @DisplayName("구간 업데이트 테스트")
     void update() {
-        Section section = new Section(1L, 1L, 1L, 2L, 3);
-
-        Section savedSection = jdbcSectionDao.save(section);
-        Section updateSection = new Section(savedSection.getId(), 1L, 2L, 3L, 5);
+        Section savedSection = jdbcSectionDao.save(section1);
+        Section updateSection = new Section(savedSection.getId(), savedSection.getLineId(), yeoksamStation, jamsilStation, 5);
 
         jdbcSectionDao.update(updateSection);
 
-        Sections findByLineIdSections = jdbcSectionDao.findByLineId(1L);
+        Sections findByLineIdSections = jdbcSectionDao.findByLineId(savedSection.getLineId());
 
         assertThat(findByLineIdSections.getSections().get(0)).isEqualTo(updateSection);
     }
@@ -78,14 +86,12 @@ class JDBCSectionDaoTest {
     @Test
     @DisplayName("구간 삭제 테스트")
     void delete() {
-        Section section1 = new Section(1L, 1L, 1L, 2L, 3);
-        Section section2 = new Section(2L, 1L, 2L, 3L, 3);
 
-        jdbcSectionDao.save(section1);
-        jdbcSectionDao.save(section2);
-        jdbcSectionDao.delete(1L, 3L);
+        Section savedSection1 = jdbcSectionDao.save(section1);
+        Section savedSection2 = jdbcSectionDao.save(section2);
+        jdbcSectionDao.delete(savedSection2.getLineId(), savedSection2.getDownStation());
 
-        Sections findByLineIdSections = jdbcSectionDao.findByLineId(1L);
+        Sections findByLineIdSections = jdbcSectionDao.findByLineId(savedSection1.getLineId());
 
         assertThat(findByLineIdSections.getSections().get(0)).isEqualTo(section1);
         assertThat(findByLineIdSections.getSections().size()).isEqualTo(1);
