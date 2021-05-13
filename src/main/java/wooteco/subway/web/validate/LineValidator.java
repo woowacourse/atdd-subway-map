e3ad39d3a2c286562d5cff3e4953bf20fb5e8486
lns13301 <rkdrbgks22@ks.ac.kr>
2021-05-13 23:31:29 +0900

    refactor: Line valid 예외 처리 추가

--- src/main/java/wooteco/subway/web/validate/LineValidator.java
+++ src/main/java/wooteco/subway/web/validate/LineValidator.java
@@ -2,7 +2,10 @@ package wooteco.subway.web.validate;
 
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
+import wooteco.subway.exception.line.InsufficientLineInformationException;
 import wooteco.subway.web.request.LineRequest;
+
+import java.util.Objects;
 
 public class LineValidator implements Validator {
     @Override
@@ -13,6 +16,11 @@ public class LineValidator implements Validator {
     @Override
     public void validate(Object target, Errors errors) {
         LineRequest lineRequest = (LineRequest) target;
+
+        if (Objects.isNull(lineRequest.getUpStationId()) || Objects.isNull(lineRequest.getDownStationId())) {
+            throw new InsufficientLineInformationException();
+        }
+
         if (lineRequest.getDownStationId().equals(lineRequest.getUpStationId())) {
             errors.rejectValue("downStationId", "duplicatedStation", "중복된 역입니다.");
         }
