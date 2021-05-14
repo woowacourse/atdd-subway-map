e5c203abec280bc09bfdb8526fc97dca86c6d708
Bepoz <45073750+Be-poz@users.noreply.github.com>
2021-05-14 12:07:27 +0900

    refactor: 도메인 테스트 코드변경

--- src/test/java/wooteco/subway/section/SectionsTest.java
+++ src/test/java/wooteco/subway/section/SectionsTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import wooteco.subway.line.LineDao;
+import wooteco.subway.station.Station;
 import wooteco.subway.station.StationDao;
 import wooteco.subway.station.StationResponse;
 
@@ -13,26 +14,18 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
 public class SectionsTest {
-    @Autowired
-    private StationDao stationDao;
-    @Autowired
-    private SectionDao sectionDao;
-    @Autowired
-    private LineDao lineDao;
 
     @Test
     @DisplayName("Sections 내 Station 정렬 확인")
     public void sortedStations() {
-        lineDao.save("9호선", "황토색");
-        stationDao.save("염창역");   //1L
-        stationDao.save("가양역");   //2L
-        stationDao.save("등촌역");   //3L
-        stationDao.save("증미역");   //4L
-        Section section1 = sectionDao.save(1L, 2L, 4L, 10);
-        Section section2 = sectionDao.save(1L, 4L, 3L, 10);
-        Section section3 = sectionDao.save(1L, 3L, 1L, 10);
+        Station station1 = new Station(1L, "염창역");
+        Station station2 = new Station(2L, "가양역");
+        Station station3 = new Station(3L, "등촌역");
+        Station station4 = new Station(4L, "증미역");
+        Section section1 = new Section(1L, 1L, station2, station4, 10);
+        Section section2 = new Section(2L, 1L, station4, station3, 10);
+        Section section3 = new Section(3L, 1L, station3, station1, 10);
 
         List<Section> sectionList = Arrays.asList(section3, section1, section2);
 
