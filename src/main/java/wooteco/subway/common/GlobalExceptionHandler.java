0702973f5f98dcac33c4ac1cc20f609f1189d6f2
TaeJeong Kim <xowjd41@naver.com>
2021-05-15 00:35:14 +0900

    refactor: ExceptionHandler 예외 처리 로직 변경

--- src/main/java/wooteco/subway/common/GlobalExceptionHandler.java
+++ src/main/java/wooteco/subway/common/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package wooteco.subway.common;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,9 +13,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
     private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
-    @ExceptionHandler(value = {IllegalArgumentException.class, IllegalStateException.class, DuplicateKeyException.class})
+    @ExceptionHandler(value = {IllegalArgumentException.class, IllegalStateException.class})
     public ResponseEntity<ResponseError> handleException(RuntimeException e) {
         logger.info(e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseError(e.getMessage()));
+    }
+
+    @ExceptionHandler(value = DataAccessException.class)
+    public ResponseEntity<ResponseError> dataBaseError(RuntimeException e) {
+        logger.info(e.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ResponseError(e.getMessage()));
     }
 }
