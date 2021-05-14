683dcc76ed309840405abaf39a5755bf6d20bd60
xrabcde <xrabcde@gmail.com>
2021-05-15 00:51:20 +0900

    refactor(StationController): 컨트롤러에서 repository를 필드로 들고있는 것 수정

--- src/main/java/wooteco/subway/controller/StationController.java
+++ src/main/java/wooteco/subway/controller/StationController.java
@@ -3,51 +3,35 @@ package wooteco.subway.controller;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import wooteco.subway.dao.StationRepository;
-import wooteco.subway.domain.Station;
 import wooteco.subway.dto.StationRequest;
 import wooteco.subway.dto.StationResponse;
-import wooteco.subway.exception.DuplicateStationNameException;
+import wooteco.subway.service.StationService;
 
 import java.net.URI;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/stations")
 public class StationController {
-    private final StationRepository stationRepository;
+    private final StationService stationService;
 
-    public StationController(StationRepository stationRepository) {
-        this.stationRepository = stationRepository;
+    public StationController(StationService stationService) {
+        this.stationService = stationService;
     }
 
     @PostMapping
     public ResponseEntity<StationResponse> createStation(@RequestBody StationRequest stationRequest) {
         String name = stationRequest.getName();
-        Station station = new Station(name);
-        validateDuplicateStationName(name);
-
-        Station newStation = stationRepository.save(station);
-        StationResponse stationResponse = new StationResponse(newStation.getId(), newStation.getName());
-
+        StationResponse stationResponse = stationService.createStation(name);
         URI location = URI.create("/stations/" + stationResponse.getId());
         return ResponseEntity
                 .created(location)
                 .body(stationResponse);
     }
 
-    private void validateDuplicateStationName(String name) {
-        this.stationRepository.findByName(name).ifPresent(line -> {
-            throw new DuplicateStationNameException(name);
-        });
-    }
-
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<StationResponse>> showStations() {
-        List<StationResponse> stationResponses = stationRepository.findAll().stream()
-                .map(it -> new StationResponse(it.getId(), it.getName()))
-                .collect(Collectors.toList());
+        List<StationResponse> stationResponses = stationService.findAllStations();
 
         return ResponseEntity
                 .ok()
@@ -56,7 +40,7 @@ public class StationController {
 
     @DeleteMapping("/{id}")
     public ResponseEntity deleteStation(@PathVariable Long id) {
-        stationRepository.delete(id);
+        stationService.deleteStation(id);
 
         return ResponseEntity
                 .noContent()
---
+++ src/main/java/wooteco/subway/service/StationService.java
@@ -0,0 +1,44 @@
+package wooteco.subway.service;
+
+import org.springframework.stereotype.Service;
+import wooteco.subway.dao.StationRepository;
+import wooteco.subway.domain.Station;
+import wooteco.subway.dto.StationResponse;
+import wooteco.subway.exception.DuplicateStationNameException;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class StationService {
+    private final StationRepository stationRepository;
+
+    public StationService(StationRepository stationRepository) {
+        this.stationRepository = stationRepository;
+    }
+
+    public StationResponse createStation(String name) {
+        Station station = new Station(name);
+        validateDuplicateStationName(name);
+
+        Station newStation = stationRepository.save(station);
+        return new StationResponse(newStation.getId(), newStation.getName());
+    }
+
+    private void validateDuplicateStationName(String name) {
+        this.stationRepository.findByName(name).ifPresent(line -> {
+            throw new DuplicateStationNameException(name);
+        });
+    }
+
+    public List<StationResponse> findAllStations() {
+        List<Station> all = stationRepository.findAll();
+        return all.stream()
+                .map(it -> new StationResponse(it.getId(), it.getName()))
+                .collect(Collectors.toList());
+    }
+
+    public void deleteStation(Long id) {
+        stationRepository.delete(id);
+    }
+}
