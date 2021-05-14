2f3e6c6b7bf88991ab9f73f3949d8483bdd34e19
Jinho <jh8579@gmail.com>
2021-05-14 11:24:41 +0900

    refactor: console에 찍던 로그를 파일에 쓰도록 변경

--- src/main/java/wooteco/subway/ExceptionControllerAdvice.java
+++ src/main/java/wooteco/subway/ExceptionControllerAdvice.java
@@ -1,5 +1,7 @@
 package wooteco.subway;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -11,38 +13,38 @@ import wooteco.subway.station.exception.StationIllegalArgumentException;
 
 @ControllerAdvice
 public class ExceptionControllerAdvice {
+    private static final Logger LOG = LoggerFactory.getLogger(ExceptionControllerAdvice.class);
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Void> unpredictableException(Exception error) {
-        System.out.println("[ERROR] " +  error.getMessage());
+        LOG.warn(error.getMessage());
         return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<String> methodArgumentNotValidExceptionHandler(
-        MethodArgumentNotValidException error) {
-        System.out.println("[ERROR] " +  error.getMessage());
+    public ResponseEntity<String> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException error) {
+        LOG.warn(error.getMessage());
         return ResponseEntity.badRequest().body(error.getMessage());
     }
 
     @ExceptionHandler(StationIllegalArgumentException.class)
     public ResponseEntity<String> StationIllegalArgumentExceptionHandler(
         StationIllegalArgumentException error) {
-        System.out.println("[ERROR] " +  error.getMessage());
+        LOG.warn(error.getMessage());
         return ResponseEntity.badRequest().body(error.getMessage());
     }
 
     @ExceptionHandler(LineIllegalArgumentException.class)
     public ResponseEntity<String> LineIllegalArgumentExceptionHandler(
         LineIllegalArgumentException error) {
-        System.out.println("[ERROR] " +  error.getMessage());
+        LOG.warn(error.getMessage());
         return ResponseEntity.badRequest().body(error.getMessage());
     }
 
     @ExceptionHandler(SectionIllegalArgumentException.class)
     public ResponseEntity<String> SectionIllegalArgumentExceptionHandler(
         SectionIllegalArgumentException error) {
-        System.out.println("[ERROR] " +  error.getMessage());
+        LOG.warn(error.getMessage());
         return ResponseEntity.badRequest().body(error.getMessage());
     }
 }
