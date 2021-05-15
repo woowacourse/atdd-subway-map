05dd046f5754c69a7054f8fb9e07bc70c3d814b3
Kimun Kim <kkm97351@gmail.com>
2021-05-15 22:18:08 +0900

    refactor: StationDao NamedParameterJdbcTemplate를 이용하여 리팩토링

--- src/main/java/wooteco/subway/dao/StationDao.java
+++ src/main/java/wooteco/subway/dao/StationDao.java
@@ -1,21 +1,24 @@
 package wooteco.subway.dao;
 
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 import wooteco.subway.domain.Station;
 
-import java.sql.PreparedStatement;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Repository
 public class StationDao implements StationRepository {
-    private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate jdbcTemplate;
 
-    public StationDao(JdbcTemplate jdbcTemplate) {
+    public StationDao(NamedParameterJdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
@@ -27,24 +30,29 @@ public class StationDao implements StationRepository {
 
     @Override
     public Station save(Station station) {
-        String query = "INSERT INTO STATION (name) VALUES (?)";
+        String query = "INSERT INTO STATION (name) VALUES (:name)";
+
+        Map<String, Object> param = new HashMap<>();
+        param.put("name", station.getName());
+
+        SqlParameterSource mapSqlParameterSource = new MapSqlParameterSource(param);
 
         KeyHolder keyHolder = new GeneratedKeyHolder();
 
-        this.jdbcTemplate.update(connection -> {
-            PreparedStatement ps = connection
-                    .prepareStatement(query, new String[]{"id"});
-            ps.setString(1, station.getName());
-            return ps;
-        }, keyHolder);
+        jdbcTemplate.update(query, mapSqlParameterSource, keyHolder);
 
         return this.findById(keyHolder.getKey().longValue());
     }
 
     @Override
     public Station findById(long id) {
-        String query = "SELECT * FROM STATION WHERE id = ?";
-        return this.jdbcTemplate.queryForObject(query, stationRowMapper, id);
+        String query = "SELECT * FROM STATION WHERE id = :id";
+        Map<String, Object> param = new HashMap<>();
+        param.put("id", id);
+
+        SqlParameterSource mapSqlParameterSource = new MapSqlParameterSource(param);
+
+        return this.jdbcTemplate.queryForObject(query, mapSqlParameterSource, stationRowMapper);
     }
 
     @Override
@@ -55,21 +63,32 @@ public class StationDao implements StationRepository {
 
     @Override
     public Optional<Station> findByName(String name) {
-        String query = "SELECT * FROM STATION WHERE name = ?";
+        String query = "SELECT * FROM STATION WHERE name = :name";
 
-        return this.jdbcTemplate.query(query, (rs) -> {
+        Map<String, Object> param = new HashMap<>();
+        param.put("name", name);
+
+        SqlParameterSource mapSqlParameterSource = new MapSqlParameterSource(param);
+
+        return this.jdbcTemplate.query(query, mapSqlParameterSource, (rs) -> {
             if (rs.next()) {
                 long id = rs.getLong("id");
                 String stationName = rs.getString("name");
                 return Optional.of(new Station(id, stationName));
             }
             return Optional.empty();
-        }, name);
+        });
     }
 
     @Override
     public void delete(Long id) {
-        String query = "DELETE FROM STATION WHERE id = ?";
-        jdbcTemplate.update(query, id);
+        String query = "DELETE FROM STATION WHERE id = :id";
+
+        Map<String, Object> param = new HashMap<>();
+        param.put("id", id);
+
+        SqlParameterSource mapSqlParameterSource = new MapSqlParameterSource(param);
+
+        jdbcTemplate.update(query, mapSqlParameterSource);
     }
 }
