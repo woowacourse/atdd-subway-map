274929158b848b819e5528ebcd3e285c674fe9be
joanne <mathmjseo@naver.com>
2021-05-15 23:05:56 +0900

    feat: Exception.class를 담당하는 예외처리핸들러 추가

--- src/main/java/wooteco/subway/controller/GlobalExceptionHandler.java
+++ src/main/java/wooteco/subway/controller/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package wooteco.subway.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -15,5 +16,11 @@ public class GlobalExceptionHandler {
     public ResponseEntity<Object> handle(SubwayException e) {
         logger.error(String.valueOf(e.getBody()));
         return ResponseEntity.status(e.getHttpStatus()).body(e.getBody());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleUnchecked(Exception e) {
+        logger.error(String.valueOf(e.getMessage()));
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
     }
 }
