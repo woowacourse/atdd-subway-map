d6e9f819c0b450a337f11960ab2e17081eb6b689
Kimun Kim <kkm97351@gmail.com>
2021-05-16 00:55:11 +0900

    refactor: Service 레이어에서 예상 가능한 예외에 대해 500을 응답하지 않도록 수정

--- src/main/java/wooteco/subway/controller/ControllerAdvice.java
+++ src/main/java/wooteco/subway/controller/ControllerAdvice.java
@@ -5,12 +5,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import wooteco.subway.domain.exception.DomainException;
+import wooteco.subway.service.exception.ServiceException;
 
 @RestControllerAdvice
 public class ControllerAdvice {
 
     @ExceptionHandler(DomainException.class)
     public ResponseEntity<String> handleDomainException(DomainException exception) {
+        return ResponseEntity.badRequest()
+                .body(exception.getMessage());
+    }
+
+    @ExceptionHandler(ServiceException.class)
+    public ResponseEntity<String> handleDomainException(ServiceException exception) {
         return ResponseEntity.badRequest()
                 .body(exception.getMessage());
     }
