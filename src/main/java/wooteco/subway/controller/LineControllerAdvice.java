d304f3a2597ceb9610d30df1ac2ac2b5cebfe972
Kimun Kim <kkm97351@gmail.com>
2021-05-15 22:17:28 +0900

    fix: ControllerAdvice의 핸들링 우선 순위 중복으로 인해 잘못된 응답 코드가 발생하는 문제 해결

--- src/main/java/wooteco/subway/controller/LineControllerAdvice.java
+++ src/main/java/wooteco/subway/controller/LineControllerAdvice.java
@@ -1,6 +1,7 @@
 package wooteco.subway.controller;
 
 import org.springframework.dao.DuplicateKeyException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,5 +13,11 @@ public class LineControllerAdvice {
     public ResponseEntity<String> handleDuplicateUniqueColumnException(DuplicateKeyException exception) {
         return ResponseEntity.badRequest()
                 .body("이미 존재하는 노선 이름입니다.");
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleInternalServerException(Exception exception) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(exception.getMessage());
     }
 }
