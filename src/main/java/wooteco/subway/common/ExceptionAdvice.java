988e518b43bff56a24d35d3ac933ed50ac7ef4f7
NewWisdom <wlgp2500@gmail.com>
2021-05-16 22:59:21 +0900

    refactor(): dto 관련 exception handler 변경

--- src/main/java/wooteco/subway/common/ExceptionAdvice.java
+++ src/main/java/wooteco/subway/common/ExceptionAdvice.java
@@ -4,20 +4,26 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.stream.Collectors;
+
 @RestControllerAdvice
 public class ExceptionAdvice {
+    private static final int FILED_NAME_INDEX = 1;
     private final Logger logger = LoggerFactory.getLogger(ExceptionAdvice.class);
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponse> handle(final MethodArgumentNotValidException e) {
-        BindingResult bindingResult = e.getBindingResult();
-        String message = bindingResult.getFieldError().getDefaultMessage();
-        logger.info(e.getMessage());
+    public ResponseEntity<ErrorResponse> handleBindException(MethodArgumentNotValidException e) {
+        logger.error("method argument not valid exception occurred. message=[{}]", e.getMessage(), e);
+
+        String message = e.getBindingResult().getAllErrors().stream()
+                .filter(err -> err != null && err.getCodes() != null)
+                .filter(err -> err.getCodes().length > 1)
+                .map(error -> error.getCodes()[FILED_NAME_INDEX] + " : " + error.getDefaultMessage())
+                .collect(Collectors.joining(", "));
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(message));
     }
 
