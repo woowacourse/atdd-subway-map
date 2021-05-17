5763ab4effbfc23af59a03a896e39567584edd89
Minsung <sns2831@gmail.com>
2021-05-17 16:29:55 +0900

    test: 실패하는 테스트 수정

--- src/test/java/wooteco/subway/application/line/LineServiceTest.java
+++ src/test/java/wooteco/subway/application/line/LineServiceTest.java
@@ -18,6 +18,7 @@ import java.util.Collections;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.lenient;
 
 @ExtendWith(MockitoExtension.class)
 class LineServiceTest {
@@ -34,16 +35,16 @@ class LineServiceTest {
     @DisplayName("존재하지 않는 역 사이의 구간이 입력됐을 경우 예외")
     @Test
     void save_exceptionTestWhenNoExistingStationsIsInputted() {
-        given(stationRepository.findById(1L))
-                .willReturn(StationFactory.create(1L, "a"));
-        given(stationRepository.findById(2L))
-                .willThrow(new IllegalArgumentException("test"));
+        lenient().when(stationRepository.findById(1L))
+                .thenReturn(StationFactory.create(1L, "a"));
+        lenient().when(stationRepository.findById(2L))
+                .thenThrow(new IllegalStateException("stationId가 존재하지 않습니다."));
 
         assertThatThrownBy(() -> lineService.save(
                 LineFactory.create(1L, "a", "a", Collections.singletonList(
                         SectionFactory.create(1L, 1L, 2L, 10L)
                 ))
-        )).isInstanceOf(IllegalArgumentException.class).hasMessage("test");
+        )).isInstanceOf(IllegalStateException.class).hasMessage("stationId가 존재하지 않습니다.");
     }
 
     @DisplayName("이미 존재하는 라인 이름을 입력받으면 예외")
