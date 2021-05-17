da46f0040aa6a08cbcdf9a3913effe9f5d47f9ca
pkeugine <pkeugine@gmail.com>
2021-05-17 21:53:03 +0900

    feat: 요청 정보 검증 후 에러 처리

--- src/main/java/wooteco/subway/GlobalAdvice.java
+++ src/main/java/wooteco/subway/GlobalAdvice.java
@@ -2,6 +2,7 @@ package wooteco.subway;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import wooteco.subway.exception.BusinessException;
@@ -12,13 +13,18 @@ import wooteco.subway.exception.RequestException;
 public final class GlobalAdvice {
 
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<String> handleUnknownException(final RuntimeException e) {
+    public ResponseEntity<String> handleUnknownException() {
         final String message = "unhandled exceptions";
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(message);
     }
 
     @ExceptionHandler({RequestException.class, DatabaseException.class})
-    public ResponseEntity<String> handleIllegalArgumentException(final BusinessException e) {
+    public ResponseEntity<String> handleBusinessException(final BusinessException e) {
         return ResponseEntity.status(e.status()).body(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
     }
 }
