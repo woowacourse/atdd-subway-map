4d5345f06f68fc8002e434abecad7f501e40f255
TaewanKimmmm <ktww88@gmail.com>
2021-05-19 01:52:41 +0900

    refactor: StationRepository 제거

--- src/main/java/wooteco/subway/service/StationService.java
+++ src/main/java/wooteco/subway/service/StationService.java
@@ -2,7 +2,7 @@ package wooteco.subway.service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import wooteco.subway.domain.repository.StationRepository;
+import wooteco.subway.dao.StationDao;
 import wooteco.subway.domain.station.Station;
 import wooteco.subway.dto.StationRequest;
 import wooteco.subway.dto.StationResponse;
@@ -13,21 +13,21 @@ import java.util.stream.Collectors;
 @Service
 @Transactional(readOnly = true)
 public class StationService {
-    private final StationRepository stationRepository;
+    private final StationDao stationDao;
 
-    public StationService(StationRepository stationRepository) {
-        this.stationRepository = stationRepository;
+    public StationService(StationDao stationDao) {
+        this.stationDao = stationDao;
     }
 
     @Transactional
     public StationResponse createStation(StationRequest stationRequest) {
         Station station = stationRequest.createStation();
-        long stationId = stationRepository.insertStation(station);
+        long stationId = stationDao.insert(station);
         return new StationResponse(stationId, station);
     }
 
     public List<StationResponse> findStations() {
-        List<Station> stations = stationRepository.findStations();
+        List<Station> stations = stationDao.selectAll();
         return stations.stream()
                 .map(StationResponse::new)
                 .collect(Collectors.toList());
@@ -35,6 +35,6 @@ public class StationService {
 
     @Transactional
     public void deleteStation(Long id) {
-        stationRepository.deleteStation(id);
+        stationDao.delete(id);
     }
 }
