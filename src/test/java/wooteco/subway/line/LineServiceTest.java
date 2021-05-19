e80fd34eb9a07dae3369516c9fa53a5e50030d3e
YeongBeom Heo <hybeom@gmail.com>
2021-05-19 21:39:15 +0900

    test(LineServiceTest) : 단위 테스트를 위한 mock 객체 도입

--- src/test/java/wooteco/subway/line/LineServiceTest.java
+++ src/test/java/wooteco/subway/line/LineServiceTest.java
@@ -2,52 +2,122 @@ package wooteco.subway.line;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
-import java.util.Deque;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.jdbc.Sql;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import wooteco.subway.domain.Id;
+import wooteco.subway.exception.line.NotFoundLineException;
 import wooteco.subway.exception.section.InvalidDistanceException;
 import wooteco.subway.exception.station.NotFoundStationException;
 import wooteco.subway.line.dao.LineDao;
+import wooteco.subway.line.domain.Color;
+import wooteco.subway.line.domain.Line;
+import wooteco.subway.line.domain.Name;
+import wooteco.subway.line.dto.CreateLineDto;
+import wooteco.subway.line.dto.LineServiceDto;
+import wooteco.subway.line.service.LineService;
 import wooteco.subway.section.dao.SectionDao;
+import wooteco.subway.section.domain.Distance;
 import wooteco.subway.section.domain.Section;
-import wooteco.subway.section.domain.Sections;
 import wooteco.subway.section.dto.DeleteStationDto;
 import wooteco.subway.section.dto.SectionServiceDto;
-import wooteco.subway.line.service.LineService;
-import wooteco.subway.station.domain.Station;
 import wooteco.subway.station.dao.StationDao;
+import wooteco.subway.station.domain.Station;
 
-@SpringBootTest
-@Sql("classpath:initializeData.sql")
+@ExtendWith(MockitoExtension.class)
 public class LineServiceTest {
 
+    private final Line oneLine = new Line(1L, "OneLine", "BLUE");
+    private final Station sinSeolStation = new Station(1L, "신설동역");
+    private final Station dongMyoStation = new Station(2L, "동묘앞역");
+    private final Station dongDaeMoonStation = new Station(3L, "동대문역");
+    private final Station seoulStation = new Station(4L, "서울역");
+    private final Distance distance = new Distance(10);
+    private final Section firstSection = new Section(new Id(1L), oneLine, sinSeolStation, dongMyoStation, distance);
+    private final Section secondSection = new Section(new Id(2L), oneLine, dongMyoStation, dongDaeMoonStation, distance);
     private final Long lineId = 1L;
     private final Long stationSinSeolId = 1L;
     private final Long stationDongMyoId = 2L;
     private final Long stationDongDaeMoonId = 3L;
-    private final int distance = 10;
+    private final int intDistance = 10;
+    private final List<Section> sections = Arrays.asList(firstSection,secondSection);
 
-    @Autowired
-    private SectionDao sectionDao;
-    @Autowired
-    private StationDao stationDao;
-    @Autowired
-    private LineDao lineDao;
-    @Autowired
+    @Mock
+    private SectionDao mockSectionDao;
+    @Mock
+    private StationDao mockStationDao;
+    @Mock
+    private LineDao mockLineDao;
+    @InjectMocks
     private LineService lineService;
 
     @BeforeEach
     void setUp() {
         SectionServiceDto sinSeolAndDongMyoDto = new SectionServiceDto(lineId, stationSinSeolId,
-            stationDongMyoId, distance);
+            stationDongMyoId, intDistance);
         SectionServiceDto dongMyoAndDongDaeMoonDto = new SectionServiceDto(lineId, stationDongMyoId,
-            stationDongDaeMoonId, distance);
+            stationDongDaeMoonId, intDistance);
+    }
+
+    @Test
+    @DisplayName("Line 추가")
+    void createLine() {
+        // given
+        long newLineId = 2L;
+        String lineName = "2호선";
+        String lineColor = "초록색";
+        long sectionId = 1L;
+        long downStationId = 4L;
+        long upStationId = 3L;
+        int distance = 10;
+        Line line = new Line(new Id(newLineId), new Name(lineName), new Color(lineColor));
+        when(mockLineDao.save(any())).thenReturn(line);
+        when(mockStationDao.show(upStationId)).thenReturn(Optional.of(dongDaeMoonStation));
+        when(mockStationDao.show(downStationId)).thenReturn(Optional.of(seoulStation));
+        when(mockStationDao.showAll()).thenReturn(Arrays.asList(dongDaeMoonStation, seoulStation));
+        when(mockLineDao.show(newLineId)).thenReturn(Optional.of(line));
+        when(mockSectionDao.save(any())).thenReturn(new Section(new Id(1L), line,
+            dongDaeMoonStation,seoulStation, new Distance(distance)));
+        when(mockSectionDao.findAllByLineId(2L)).thenReturn(Arrays.asList());
+
+
+        // when
+        CreateLineDto createLineDto = new CreateLineDto(lineName, lineColor, upStationId, downStationId, distance);
+        LineServiceDto lineServiceDto = lineService.createLine(createLineDto);
+
+        // then
+        assertThat(lineServiceDto.getId()).isEqualTo(newLineId);
+        assertThat(lineServiceDto.getName()).isEqualTo(lineName);
+        assertThat(lineServiceDto.getColor()).isEqualTo(lineColor);
+    }
+
+    @Test
+    @DisplayName("Line 업데이트시에 해당 라인 없을시 오류")
+    void updateLineException() {
+        // given
+        long newLineId = 2L;
+        String updateLineName = "3호선";
+        String updateLineColor = "보라색";
+        long sectionId = 1L;
+        when(mockLineDao.show(2L)).thenReturn(Optional.empty());
+
+        // when
+        LineServiceDto lineServiceDto = new LineServiceDto(newLineId, updateLineName, updateLineColor);
+
+
+        //then
+        assertThatThrownBy(()-> lineService.update(lineServiceDto))
+            .isInstanceOf(NotFoundLineException.class);
     }
 
     @Test
@@ -59,7 +129,13 @@ public class LineServiceTest {
         long upStationId = 3L;
         int distance = 10;
         String name = "회기역";
-        stationDao.save(new Station(name));
+        when(mockStationDao.show(3L)).thenReturn(Optional.of(dongDaeMoonStation));
+        when(mockStationDao.show(4L)).thenReturn(Optional.of(seoulStation));
+        when(mockLineDao.show(1L)).thenReturn(Optional.of(oneLine));
+        when(mockSectionDao.save(any())).thenReturn(new Section(new Id(3L), oneLine,
+            dongDaeMoonStation,seoulStation, new Distance(distance)));
+        when(mockSectionDao.findAllByLineId(1L)).thenReturn(sections);
+
 
         // when
         SectionServiceDto dongDaeMoonAndHaegiDto = new SectionServiceDto(lineId, upStationId,
@@ -82,18 +158,20 @@ public class LineServiceTest {
         long targetDownStationId = 4L;
         int targetDistance = 5;
         String name = "회기역";
-        stationDao.save(new Station(name));
 
-        List<Section> allByLineId = sectionDao.findAllByLineId(1L);
+        List<Section> allByLineId = sections;
+        when(mockStationDao.show(2L)).thenReturn(Optional.of(dongMyoStation));
+        when(mockStationDao.show(4L)).thenReturn(Optional.of(seoulStation));
+        when(mockLineDao.show(1L)).thenReturn(Optional.of(oneLine));
+        when(mockSectionDao.findAllByLineId(1L)).thenReturn(sections);
+        when(mockSectionDao.save(any())).thenReturn(new Section(new Id(3L), oneLine,
+            dongMyoStation,seoulStation, new Distance(targetDistance)));
 
         // when
         SectionServiceDto dongDaeMoonAndHaegiDto = new SectionServiceDto(lineId, targetUpStationId,
             targetDownStationId, targetDistance);
         SectionServiceDto savedDongDaeMoonAndHaegiDto = lineService.saveSection(dongDaeMoonAndHaegiDto);
         assertThat(savedDongDaeMoonAndHaegiDto).isNotNull();
-        Section changedSection = sectionDao.findByLineIdAndDownStationId(lineId, 3L).get();
-
-        allByLineId = sectionDao.findAllByLineId(1L);
 
 
         // then
@@ -101,9 +179,6 @@ public class LineServiceTest {
         assertThat(savedDongDaeMoonAndHaegiDto.getUpStationId()).isEqualTo(targetUpStationId);
         assertThat(savedDongDaeMoonAndHaegiDto.getDownStationId()).isEqualTo(targetDownStationId);
         assertThat(savedDongDaeMoonAndHaegiDto.getDistance()).isEqualTo(targetDistance);
-
-        assertThat(changedSection.getDistance().value()).isEqualTo(10 - targetDistance);
-        assertThat(changedSection.upStation().getId()).isEqualTo(4L);
     }
 
     @Test
@@ -112,13 +187,15 @@ public class LineServiceTest {
         // given
         long targetUpStationId = 2L;
         long targetDownStationId = 4L;
-        int distance = 15;
-        String name = "회기역";
-        stationDao.save(new Station(name));
+        Distance targetDistance = new Distance(15);
+        when(mockStationDao.show(2L)).thenReturn(Optional.of(dongMyoStation));
+        when(mockStationDao.show(4L)).thenReturn(Optional.of(seoulStation));
+        when(mockLineDao.show(1L)).thenReturn(Optional.of(oneLine));
+        when(mockSectionDao.findAllByLineId(1L)).thenReturn(sections);
 
         // when
         SectionServiceDto dongDaeMoonAndHaegiDto =
-            new SectionServiceDto(lineId, targetUpStationId, targetDownStationId, distance);
+            new SectionServiceDto(lineId, targetUpStationId, targetDownStationId, targetDistance.value());
 
         // then
         assertThatThrownBy(() -> lineService.saveSection(dongDaeMoonAndHaegiDto))
@@ -126,51 +203,29 @@ public class LineServiceTest {
     }
 
     @Test
-    @DisplayName("지하철 역이 3개 이상일 때 노선의 중간역 삭제")
+    @DisplayName("지하철 역이 삭제 시 없는 노선 확인")
     void deleteSection() {
         // given
-        DeleteStationDto deleteStationDto = new DeleteStationDto(lineId, stationDongMyoId);
+        DeleteStationDto deleteStationDto = new DeleteStationDto(lineId, 5L);
 
         // when
-        lineService.deleteSection(deleteStationDto);
-        Section section = sectionDao.findByLineIdAndUpStationId(lineId, stationSinSeolId)
-            .orElseThrow(NotFoundStationException::new);
 
         // then
-        assertThat(section.upStation().getId()).isEqualTo(stationSinSeolId);
-        assertThat(section.downStation().getId()).isEqualTo(stationDongDaeMoonId);
-        assertThat(section.getDistance().value()).isEqualTo(20);
-    }
-
-    @Test
-    @DisplayName("지하철 역이 3개 이상일 때 노선 끝에 존재하는 역(구간) 삭제")
-    void deleteSectionAtEnd() {
-        // given
-        DeleteStationDto deleteStationDto = new DeleteStationDto(lineId, stationDongDaeMoonId);
-
-        // when
-        Sections beforeSections = new Sections(sectionDao.findAllByLineId(lineId));
-        Deque<Long> beforeStationIds = beforeSections.sortedStationIds();
-        lineService.deleteSection(deleteStationDto);
-        Sections afterSections = new Sections(sectionDao.findAllByLineId(lineId));
-        Deque<Long> afterStationIds = afterSections.sortedStationIds();
-
-        // then
-        assertThat(beforeStationIds.peekFirst()).isEqualTo(stationSinSeolId);
-        assertThat(beforeStationIds.peekLast()).isEqualTo(stationDongDaeMoonId);
-        assertThat(afterStationIds.peekFirst()).isEqualTo(stationSinSeolId);
-        assertThat(afterStationIds.peekLast()).isEqualTo(stationDongMyoId);
+        assertThatThrownBy(() ->lineService.deleteSection(deleteStationDto))
+            .isInstanceOf(NotFoundStationException.class);
     }
 
     @Test
     @DisplayName("지하철 역이 2개만 있을 때(구간이 1개일 때)의 삭제")
     void deleteSectionWithTwoStations() {
         // given
+        List<Section> sections = Arrays.asList(firstSection);
         DeleteStationDto preparedDeleteStationDto = new DeleteStationDto(lineId, stationDongMyoId);
         DeleteStationDto targetDeleteStationDto = new DeleteStationDto(lineId, stationSinSeolId);
+        when(mockSectionDao.findAllByLineId(any())).thenReturn(sections);
+        when(mockStationDao.show(stationSinSeolId)).thenReturn(Optional.of(sinSeolStation));
 
         // when
-        lineService.deleteSection(preparedDeleteStationDto);
 
         // then
         assertThatThrownBy(() -> lineService.deleteSection(targetDeleteStationDto))
