7356fdc576c0db7b27dbc1d975e62c06fa724f5f
Seed <hsik0225@gmail.com>
2021-05-19 17:05:08 +0900

    refactor(line-controller) : converLineToLineResponse를 LineResponse 안에서 하도록 수정

--- src/main/java/wooteco/subway/controller/LineController.java
+++ src/main/java/wooteco/subway/controller/LineController.java
@@ -1,20 +1,20 @@
 package wooteco.subway.controller;
 
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.validation.Valid;
+
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
 import wooteco.subway.controller.dto.LineEditRequest;
 import wooteco.subway.controller.dto.LineRequest;
 import wooteco.subway.controller.dto.LineResponse;
-import wooteco.subway.controller.dto.StationResponse;
 import wooteco.subway.domain.Line;
 import wooteco.subway.service.LineService;
-
-import javax.validation.Valid;
-import java.net.URI;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Validated
 @RequestMapping("/lines")
@@ -29,42 +29,29 @@ public class LineController {
 
     @PostMapping
     public ResponseEntity<LineResponse> createLine(@Valid @RequestBody LineRequest lineRequest) {
-        final Line newLine = lineService.save(lineRequest.toLineEntity(), lineRequest.toSectionEntity());
-        final LineResponse lineResponse = convertLineToLineResponse(newLine);
+        final Line newLine = lineService.save(lineRequest);
         final URI uri = URI.create("/lines/" + newLine.getId());
-        return ResponseEntity.created(uri).body(lineResponse);
-    }
-
-    private LineResponse convertLineToLineResponse(final Line line) {
-        final List<StationResponse> stationResponses = lineService.findStationsByLineId(line.getId())
-                                                                  .stream()
-                                                                  .map(StationResponse::new)
-                                                                  .collect(Collectors.toList());
-
-        return new LineResponse(line, stationResponses);
+        return ResponseEntity.created(uri).body(LineResponse.from(newLine));
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<LineResponse>> showLines() {
-        final List<Line> lines = lineService.findAll();
-        final List<LineResponse> lineResponses = lines.stream()
-                                                      .map(this::convertLineToLineResponse)
-                                                      .collect(Collectors.toList());
-
+        final List<LineResponse> lineResponses = lineService.findAll()
+                                                            .stream()
+                                                            .map(LineResponse::from)
+                                                            .collect(Collectors.toList());
         return ResponseEntity.ok().body(lineResponses);
     }
 
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<LineResponse> showLine(@PathVariable Long id) {
         final Line line = lineService.findById(id);
-        final LineResponse lineResponse = convertLineToLineResponse(line);
-        return ResponseEntity.ok().body(lineResponse);
+        return ResponseEntity.ok().body(LineResponse.from(line));
     }
 
     @PutMapping(value = "/{id}")
     public ResponseEntity<Void> editLine(@PathVariable Long id, @Valid @RequestBody LineEditRequest lineEditRequest) {
-        final Line line = lineEditRequest.toEntity();
-        lineService.update(id, line);
+        lineService.update(id, lineEditRequest);
         return ResponseEntity.ok().build();
     }
 
