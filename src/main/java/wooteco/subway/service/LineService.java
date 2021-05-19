72f4af1b903934042afda8243b14ffea90139e9d
Seed <hsik0225@gmail.com>
2021-05-19 17:04:22 +0900

    refactor(line-service) : Line을 테이블과 매칭되지 않게 함에 따른 코드 수정

--- src/main/java/wooteco/subway/service/LineService.java
+++ src/main/java/wooteco/subway/service/LineService.java
@@ -1,20 +1,19 @@
 package wooteco.subway.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import wooteco.subway.controller.dto.LineEditRequest;
+import wooteco.subway.controller.dto.LineRequest;
 import wooteco.subway.dao.LineDao;
 import wooteco.subway.dao.SectionDao;
-import wooteco.subway.dao.StationDao;
 import wooteco.subway.domain.Line;
-import wooteco.subway.domain.OrderedStationIds;
 import wooteco.subway.domain.Section;
 import wooteco.subway.domain.Station;
 import wooteco.subway.exception.DuplicateNameException;
 import wooteco.subway.exception.EntityNotFoundException;
-
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.List;
 
 @Transactional
 @Service
@@ -22,37 +21,32 @@ public class LineService {
 
     private final LineDao lineDao;
     private final SectionDao sectionDao;
-    private final StationDao stationDao;
+    private final StationService stationService;
 
-    public LineService(final LineDao lineDao, final SectionDao sectionDao, final StationDao stationDao) {
+    public LineService(LineDao lineDao, SectionDao sectionDao, StationService stationService) {
         this.lineDao = lineDao;
         this.sectionDao = sectionDao;
-        this.stationDao = stationDao;
+        this.stationService = stationService;
     }
 
-    public Line save(final Line line, final Section section) {
-        checkDuplicateLineName(line);
-        checkStationIsExist(section);
+    public Line save(final LineRequest lineRequest) {
+        checkDuplicateLineName(lineRequest.getName());
+
+        final Station upStation = stationService.findById(lineRequest.getUpStationId());
+        final Station downStation = stationService.findById(lineRequest.getDownStationId());
+        final Section section = new Section(upStation, downStation, lineRequest.getDistance());
+        final Line line = lineRequest.toEntity(section);
+
         final Line newLine = lineDao.save(line);
-        final Section sectionWithNewLineId = Section.ofLineId(newLine.getId(), section);
-        sectionDao.save(sectionWithNewLineId);
+        sectionDao.save(newLine.getId(), section);
+
         return newLine;
     }
 
-    @Transactional(readOnly = true)
-    void checkDuplicateLineName(final Line line) {
-        boolean existsName = lineDao.findByName(line.getName()).isPresent();
+    public void checkDuplicateLineName(final String name) {
+        boolean existsName = lineDao.findByName(name).isPresent();
         if (existsName) {
             throw new DuplicateNameException("이미 존재하는 노선 이름입니다.");
-        }
-    }
-
-    @Transactional(readOnly = true)
-    void checkStationIsExist(final Section section) {
-        final boolean existsUpStation = stationDao.findById(section.getUpStationId()).isPresent();
-        final boolean existsDownStation = stationDao.findById(section.getDownStationId()).isPresent();
-        if (!(existsUpStation && existsDownStation)) {
-            throw new EntityNotFoundException("해당 ID와 일치하는 역이 존재하지 않습니다.");
         }
     }
 
@@ -63,38 +57,27 @@ public class LineService {
 
     @Transactional(readOnly = true)
     public Line findById(final Long id) {
-        return lineDao.findById(id).orElseThrow(() -> new EntityNotFoundException("해당 ID와 일치하는 노선이 존재하지 않습니다."));
+        return lineDao.findById(id)
+                      .orElseThrow(() -> new EntityNotFoundException("해당 ID와 일치하는 노선이 존재하지 않습니다."));
     }
 
-    public void update(final Long id, final Line line) {
-        final boolean onlyChangeContent = findById(id).isNameEquals(line);
-        final boolean isPresentName = lineDao.findByName(line.getName()).isPresent();
+    public void update(final Long id, final LineEditRequest lineEditRequest) {
+        final Line beforeLine = findById(id);
+        validateUpdateName(beforeLine, lineEditRequest.getName());
+        final Line afterLine = lineEditRequest.toEntity(beforeLine);
+        lineDao.updateTo(afterLine);
+    }
+
+    @Transactional(readOnly = true)
+    void validateUpdateName(Line beforeLine, String editName) {
+        final boolean onlyChangeContent = beforeLine.isNameEquals(editName);
+        final boolean isPresentName = lineDao.findByName(editName).isPresent();
         if (!onlyChangeContent && isPresentName) {
             throw new DuplicateNameException("이미 존재하는 노선 이름입니다.");
         }
-        lineDao.update(id, line);
     }
 
     public void delete(final Long id) {
         lineDao.delete(id);
-    }
-
-    @Transactional(readOnly = true)
-    public List<Station> findStationsByLineId(final Long id) {
-        final Line line = findById(id);
-        final List<Section> sections = sectionDao.findAllByLineId(id);
-        final Deque<Long> orderedStationIds = OrderedStationIds.of(line, sections).getOrderedStationIds();
-
-        List<Station> orderedStations = new ArrayList<>();
-        for (int i = 0, size = orderedStationIds.size(); i < size; i++) {
-            orderedStations.add(findStationByStationId(orderedStationIds.pollFirst()));
-        }
-        return orderedStations;
-    }
-
-    @Transactional(readOnly = true)
-    public Station findStationByStationId(final Long stationId) {
-        return stationDao.findById(stationId)
-                         .orElseThrow(() -> new EntityNotFoundException("해당 ID와 일치하는 역이 존재하지 않습니다."));
     }
 }
