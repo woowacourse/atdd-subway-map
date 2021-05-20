f6d69b35004aaea5d31de296e0347bfd59844455
TaeJeong Kim <xowjd41@naver.com>
2021-05-20 21:34:59 +0900

    refactor(LineServiceTest.class): 테스트 수정

--- src/test/java/wooteco/subway/line/LineFactory.java
+++ src/test/java/wooteco/subway/line/LineFactory.java
@@ -9,6 +9,7 @@ import static wooteco.subway.line.SectionFactory.인천2호선_구간;
 
 @DisplayName("기본 노선 생성")
 public class LineFactory {
-    public static final Line 인천1호선 = new Line(1L, new LineName("인천1호선"), "bg-red-600", 인천1호선_구간);
-    public static final Line 인천2호선 = new Line(2L, new LineName("인천2호선"), "bg-red-500", 인천2호선_구간);
+    public static final Line 구간없는_인천1호선 = new Line(1L, new LineName("인천1호선"), "bg-red-600");
+    public static final Line 인천1호선 = new Line(2L, new LineName("인천1호선"), "bg-red-600", 인천1호선_구간);
+    public static final Line 인천2호선 = new Line(3L, new LineName("인천2호선"), "bg-red-500", 인천2호선_구간);
 }
--- src/test/java/wooteco/subway/line/application/LineServiceTest.java
+++ src/test/java/wooteco/subway/line/application/LineServiceTest.java
@@ -7,6 +7,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import wooteco.subway.common.exception.AlreadyExistsException;
+import wooteco.subway.common.exception.InvalidInputException;
 import wooteco.subway.common.exception.NotFoundException;
 import wooteco.subway.line.domain.Line;
 import wooteco.subway.line.domain.LineDao;
@@ -17,18 +18,21 @@ import wooteco.subway.line.dto.LineResponse;
 import wooteco.subway.line.dto.SectionRequest;
 import wooteco.subway.station.domain.Station;
 import wooteco.subway.station.domain.StationDao;
-import wooteco.subway.station.dto.StationResponse;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.Collections;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static wooteco.subway.line.LineFactory.구간없는_인천1호선;
+import static wooteco.subway.line.LineFactory.인천1호선;
+import static wooteco.subway.line.SectionFactory.인천1호선_흑기백기구간;
+import static wooteco.subway.station.StationFactory.백기역;
+import static wooteco.subway.station.StationFactory.흑기역;
 
+@DisplayName("노선 서비스로직 테스트")
 class LineServiceTest {
     @InjectMocks
     private LineService lineService;
@@ -42,92 +46,65 @@ class LineServiceTest {
     @Mock
     private StationDao stationDao;
 
-    private Station station1;
-    private Station station2;
-    private Station station3;
-    private Station station4;
     private Line line;
-    private Section section1;
-    private Section section2;
-    private int distance1;
-    private int distance2;
+    private Line noSectionsLine;
+    private LineRequest 인천1호선_Request;
 
     @BeforeEach
     public void init() {
         MockitoAnnotations.openMocks(this);
 
-        station1 = new Station(1L, "1역");
-        station2 = new Station(2L, "2역");
-        station3 = new Station(3L, "3역");
-        station4 = new Station(4L, "4역");
-        line = new Line(1L, "백기선", "bg-red-600");
-        distance1 = 10;
-        distance2 = 5;
-        section1 = new Section(1L, line, station1, station2, distance1);
-        section2 = new Section(2L, line, station2, station3, distance2);
+        인천1호선_Request = new LineRequest(인천1호선.name(), 인천1호선.color(), 흑기역.id(), 백기역.id(), 7);
+        line = new Line(인천1호선.id(), 인천1호선.name(), 인천1호선.color(), 인천1호선.sortedSections());
+        noSectionsLine = new Line(인천1호선.id(), 인천1호선.name(), 인천1호선.color());
     }
 
     @Test
     @DisplayName("노선 정상 저장된다")
     void save() {
         //given
-        when(lineDao.save(any(Line.class))).thenReturn(line);
-        when(sectionDao.save(any(Section.class))).thenReturn(section1);
-        when(stationDao.findById(1L)).thenReturn(Optional.of(station1));
-        when(stationDao.findById(2L)).thenReturn(Optional.of(station2));
-
         //when
-        LineResponse lineResponse = lineService.save(new LineRequest(line.name(), line.color(), station1.id(), station2.id(), distance1));
+        LineResponse lineResponse = 노선_저장_요청(인천1호선_Request);
 
         //then
-        assertThat(lineResponse.getId()).isEqualTo(line.id());
-        assertThat(lineResponse.getStations()).hasSize(2);
-        assertThat(lineResponse.getStations().get(0).getId()).isEqualTo(station1.id());
+        노선_정상_저장됨(인천1호선_Request, lineResponse);
     }
 
     @Test
     @DisplayName("구건 저장 시 상행역과 하행역이 같으면 예와가 발생한다")
     void saveException() {
         //given
-        when(lineDao.save(any(Line.class))).thenReturn(line);
+        LineRequest request = new LineRequest(인천1호선_Request.getName(), 인천1호선_Request.getColor(), 흑기역.id(), 흑기역.id(), 인천1호선_흑기백기구간.distance());
 
         //then
-        assertThatThrownBy(() -> lineService.save(new LineRequest(line.name(), line.color(), station1.id(), station1.id(), distance1)))
-                .isInstanceOf(NotFoundException.class);
+        assertThatThrownBy(() -> 노선_저장_요청(request))
+                .isInstanceOf(InvalidInputException.class)
+                .hasMessage("상행역과 하행역은 같을 수 없음! ");
     }
 
     @Test
     @DisplayName("노선에 포함된 구간을 찾는다")
     void findBy() {
         //given
-        line.addSection(section1);
-        line.addSection(section2);
-
-        baseLine();
-
-        when(lineDao.findById(line.id())).thenReturn(Optional.of(line));
+        LineResponse saveResponse = 노선_저장_요청(인천1호선_Request);
 
         //when
-        LineResponse response = lineService.findLine(line.id());
-
+        LineResponse findResponse = 노선단일_조회_요청(line);
 
         //then
-        assertThat(response.getName()).isEqualTo(line.name());
-        assertThat(stationResponsesToString(response.getStations())).containsExactly(station1.name(), station2.name(), station3.name());
+        노선_정상_조회됨(saveResponse, findResponse);
     }
 
     @Test
-    @DisplayName("구간 등록시 상행역과 하행역이 이미 등록 되어있다면 에러가 발생한다. ")
+    @DisplayName("구간 등록시 상행역과 하행역이 이미 등록 되어있다면 예외가 발생한다. ")
     void registrationDuplicateException() {
         //given
-        baseLine();
-        line.addSection(section1);
-        line.addSection(section2);
+        LineResponse saveResponse = 노선_저장_요청(인천1호선_Request);
+        SectionRequest sectionRequest = new SectionRequest(인천1호선_흑기백기구간.upStationId(), 인천1호선_흑기백기구간.downStationId(), 5);
 
         //when
-
         //then
-        assertThatThrownBy(() -> lineService.addSection(line.id(), new SectionRequest(section1.upStationId(), section1.downStationId(), distance1)))
+        assertThatThrownBy(() ->노선_구간_추가_요청(line, sectionRequest))
                 .isInstanceOf(AlreadyExistsException.class);
     }
 
@@ -135,31 +112,49 @@ class LineServiceTest {
     @DisplayName("초기 구간 등록시 상행역과 하행역의 아이디가 없으면 예외가 발생한다. ")
     void registrationNotExistException() {
         //given
-        Long stationId = null;
-        baseLine();
-
-        //when
+        LineRequest request = new LineRequest(인천1호선_Request.getName(), 인천1호선_Request.getColor(), 흑기역.id(), 흑기역.id(), 인천1호선_흑기백기구간.distance());
 
         //then
-        assertThatThrownBy(() -> lineService.save(new LineRequest(line.name(), line.color(), stationId, stationId, distance1)))
-                .isInstanceOf(NotFoundException.class);
+        assertThatThrownBy(() -> 노선_저장_요청(request))
+                .isInstanceOf(InvalidInputException.class)
+                .hasMessage("상행역과 하행역은 같을 수 없음! ");
     }
 
-    private void baseLine() {
-        when(sectionDao.save(any(Section.class))).thenReturn(section1);
-        when(lineDao.save(any(Line.class))).thenReturn(line);
-        when(sectionDao.findByLineId(line.id())).thenReturn(Arrays.asList(section1, section2));
-
+    private void 노선_구간_추가_요청(Line line, SectionRequest sectionRequest) {
+        노선_저장되어_있음();
+        when(sectionDao.findByLineId(line.id())).thenReturn(Collections.singletonList(인천1호선_흑기백기구간));
         when(lineDao.findById(line.id())).thenReturn(Optional.of(line));
-        when(stationDao.findById(station1.id())).thenReturn(Optional.of(station1));
-        when(stationDao.findById(station2.id())).thenReturn(Optional.of(station2));
-        when(stationDao.findById(station3.id())).thenReturn(Optional.of(station3));
-        when(stationDao.findById(station4.id())).thenReturn(Optional.of(station4));
+        lineService.addSection(line.id(), sectionRequest);
     }
 
-    private List<String> stationResponsesToString(List<StationResponse> response) {
-        return response.stream()
-                .map(StationResponse::getName)
-                .collect(Collectors.toList());
+    private LineResponse 노선_저장_요청(LineRequest request) {
+        노선_저장되어_있음();
+        return lineService.save(request);
+    }
+
+    private void 노선_저장되어_있음() {
+        when(lineDao.save(any(Line.class))).thenReturn(noSectionsLine);
+        when(sectionDao.save(any(Section.class))).thenReturn(인천1호선_흑기백기구간);
+        when(stationDao.findById(흑기역.id())).thenReturn(Optional.of(흑기역));
+        when(stationDao.findById(백기역.id())).thenReturn(Optional.of(백기역));
+    }
+
+    private LineResponse 노선단일_조회_요청(Line line) {
+        when(lineDao.findById(line.id())).thenReturn(Optional.of(line));
+        return lineService.findLine(line.id());
+    }
+
+    private void 노선_정상_저장됨(LineRequest lineRequest, LineResponse lineResponse) {
+        assertThat(lineRequest.getName()).isEqualTo(lineResponse.getName());
+        assertThat(lineRequest.getColor()).isEqualTo(lineResponse.getColor());
+    }
+
+    private void 노선_정상_조회됨(LineResponse saveResponse, LineResponse findResponse) {
+        assertThat(saveResponse.getName()).isEqualTo(findResponse.getName());
+        노선_정상_포함됨(saveResponse, findResponse);
+    }
+
+    private void 노선_정상_포함됨(LineResponse saveResponse, LineResponse findResponse) {
+        assertThat(saveResponse.getStations().containsAll(findResponse.getStations())).isEqualTo(true);
     }
 }
