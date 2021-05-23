df979ea3ea1a5f2ae15d5db9f463f22cd40c68ed
Gomding <backchida@gmail.com>
2021-05-23 13:53:45 +0900

    refactor : Service 테스트 수정

--- src/test/java/wooteco/subway/line/service/LineServiceTest.java
+++ src/test/java/wooteco/subway/line/service/LineServiceTest.java
@@ -2,55 +2,40 @@ package wooteco.subway.line.service;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import wooteco.subway.exception.badrequest.DuplicatedNameException;
+import wooteco.subway.exception.badrequest.LineNotFoundException;
 import wooteco.subway.line.controller.dto.LineNameColorResponse;
 import wooteco.subway.line.controller.dto.LineRequest;
 import wooteco.subway.line.controller.dto.LineResponse;
 import wooteco.subway.line.controller.dto.SectionRequest;
-import wooteco.subway.line.domain.Line;
-import wooteco.subway.line.domain.repository.LineRepository;
-import wooteco.subway.line.domain.section.Distance;
-import wooteco.subway.line.domain.section.Section;
-import wooteco.subway.line.domain.section.Sections;
 import wooteco.subway.station.domain.Station;
-import wooteco.subway.station.domain.Stations;
-import wooteco.subway.station.service.StationService;
+import wooteco.subway.station.infra.StationDao;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.BDDMockito.*;
-import static org.mockito.Mockito.mock;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+@Transactional
 class LineServiceTest {
-    private List<Section> 생선된_구간들 = Collections.singletonList(new Section(1L, 1L, 2L, new Distance(10)));
-    private LineRequest 노선_저장요청 = new LineRequest("1호선", "빨간색", 1L, 2L, 20);
-    private LineRequest 노선_수정요청 = new LineRequest("222호선", "노란색", 1L, 2L, 20);
-    private Line 노선_저장_후_응답 = new Line(1L, "1호선", "빨간색");
-    private Line 노선_조회 = new Line(1L, "2호선", "파란색", new Sections(new ArrayList<>(생선된_구간들)));
-    private Stations 지하철역들 = new Stations(Arrays.asList(new Station(1L, "강남역"), new Station(2L, "구의역")));
-
-    @Mock
-    LineRepository lineRepository = mock(LineRepository.class);
-
-    @Mock
-    StationService stationService = mock(StationService.class);
-
-    @InjectMocks
     private LineService lineService;
+    private StationDao stationDao;
 
-    @DisplayName("지하철 노선 저장")
+    @Autowired
+    public LineServiceTest(LineService lineService, StationDao stationDao) {
+        this.lineService = lineService;
+        this.stationDao = stationDao;
+    }
+
+    @DisplayName("지하철 노선 추가한다.")
     @Test
     void saveLine() {
         //given
-        given(lineRepository.saveLine(any(Line.class))).willReturn(노선_저장_후_응답);
+        LineRequest 노선_저장요청 = new LineRequest("1호선", "빨간색", 1L, 2L, 20);
 
         //when
         LineNameColorResponse 저장_후_응답 = lineService.saveLine(노선_저장요청);
@@ -58,93 +43,172 @@ class LineServiceTest {
         //then
         assertThat(저장_후_응답.getName()).isEqualTo(노선_저장요청.getName());
         assertThat(저장_후_응답.getColor()).isEqualTo(노선_저장요청.getColor());
+    }
 
-        then(lineRepository).should().saveLine(any(Line.class));
+    @DisplayName("중복된 노선 이름을 저장할 경우 예외 처리 한다.")
+    @Test
+    void saveLine2() {
+        //given
+        LineRequest 노선_저장요청 = new LineRequest("1호선", "빨간색", 1L, 2L, 20);
+        lineService.saveLine(노선_저장요청);
+
+        //when then
+        assertThatThrownBy(() -> lineService.saveLine(노선_저장요청)).isInstanceOf(DuplicatedNameException.class);
     }
 
     @DisplayName("지하철 노선 전체 조회")
     @Test
     void findAll() {
         //given
-        given((lineRepository.findAll())).willReturn(new ArrayList<>());
+        LineRequest 일호선_저장요청 = new LineRequest("1호선", "빨간색", 1L, 2L, 20);
+        LineRequest 삼호선_저장요청 = new LineRequest("3호선", "빨간색", 1L, 2L, 20);
+        LineNameColorResponse 일호선_저장응답 = lineService.saveLine(일호선_저장요청);
+        LineNameColorResponse 삼호선_저장응답 = lineService.saveLine(삼호선_저장요청);
 
         //when
         List<LineNameColorResponse> 라인_전체_조회 = lineService.findAll();
 
         //then
         assertThat(라인_전체_조회).isNotNull();
-        then(lineRepository).should().findAll();
+        assertThat(라인_전체_조회).hasSize(2);
     }
 
-    @SuppressWarnings("unchecked")
     @DisplayName("지하철 노선 1개 조회")
     @Test
     void findById() {
         //given
-        given(lineRepository.findLineSectionById(1L)).willReturn(노선_조회);
-        given(stationService.findSortStationsByIds(any(List.class))).willReturn(지하철역들);
+        LineNameColorResponse 일호선_저장응답 = 일호선_저장응답();
+        Long lineId = 일호선_저장응답.getId();
 
         //when
-        LineResponse 조회한_노선 = lineService.findById(1L);
+        LineResponse 조회한_노선 = lineService.findById(lineId);
 
         //then
-        assertThat(조회한_노선.getId()).isEqualTo(노선_조회.getId());
-        assertThat(조회한_노선.getName()).isEqualTo(노선_조회.getName());
-        assertThat(조회한_노선.getColor()).isEqualTo(노선_조회.getColor());
-        then(lineRepository).should().findLineSectionById(1L);
+        assertThat(조회한_노선.getId()).isEqualTo(일호선_저장응답.getId());
+        assertThat(조회한_노선.getName()).isEqualTo(일호선_저장응답.getName());
+        assertThat(조회한_노선.getColor()).isEqualTo(일호선_저장응답.getColor());
+    }
+
+    @DisplayName("존재하지 않는 지하철 노선 조회 시 예외 발생한다.")
+    @Test
+    void findByNotExistId() {
+        //given
+        LineNameColorResponse 일호선_저장응답 = 일호선_저장응답();
+
+        //when
+        assertThatThrownBy(() -> lineService.findById(999L))
+                .isInstanceOf(LineNotFoundException.class);
     }
 
     @DisplayName("지하철 노선 삭제")
     @Test
     void delete() {
         //given
-        willDoNothing().given(lineRepository).delete(1L);
+        LineNameColorResponse 일호선_저장응답 = 일호선_저장응답();
+        Long targetId = 일호선_저장응답.getId();
 
         //when
-        lineService.delete(1L);
+        lineService.delete(targetId);
 
         //then
-        then(lineRepository).should().delete(1L);
+        assertThatThrownBy(() -> lineService.findById(targetId)).isInstanceOf(LineNotFoundException.class);
     }
 
     @DisplayName("지하철 노선 수정")
     @Test
     void update() {
         //given
-        given(lineRepository.findLineSectionById(1L)).willReturn(노선_조회);
-        willDoNothing().given(lineRepository).update(any(Line.class));
+        LineNameColorResponse 일호선_저장응답 = 일호선_저장응답();
+        Long targetId = 일호선_저장응답.getId();
+        LineRequest 노선_수정요청 = new LineRequest("222호선", "노란색");
 
         //when
-        lineService.update(1L, 노선_수정요청);
+        lineService.update(targetId, 노선_수정요청);
+        LineResponse 수정_후_노선 = lineService.findById(targetId);
 
         //then
-        then(lineRepository).should().findLineSectionById(1L);
-        then(lineRepository).should().update(any(Line.class));
+        assertThat(수정_후_노선.getName()).isEqualTo(노선_수정요청.getName());
+        assertThat(수정_후_노선.getColor()).isEqualTo(노선_수정요청.getColor());
     }
 
-    @DisplayName("지하철 구간 추가")
+    @DisplayName("지하철 구간 추가한다.")
     @Test
     void addSection() {
         //given
-        willDoNothing().given(lineRepository).addSection(any(Section.class));
+        Station 강남역 = stationDao.save(new Station(1L, "강남역"));
+        Station 잠실역 = stationDao.save(new Station(2L, "잠실역"));
+        Station 잠실나루역 = stationDao.save(new Station(3L, "잠실나루역"));
+        LineRequest 일호선_저장요청 = new LineRequest("1호선", "빨간색", 강남역.getId(), 잠실역.getId(), 20);
+        LineNameColorResponse 일호선_저장응답 = lineService.saveLine(일호선_저장요청);
+        Long targetId = 일호선_저장응답.getId();
 
         //when
-        lineService.addSection(1L, new SectionRequest(1L, 2L, 20));
+        lineService.addSection(targetId, new SectionRequest(잠실나루역.getId(), 잠실역.getId(), 10));
+        LineResponse 구간_추가된_일호선 = lineService.findById(targetId);
 
         //then
-        then(lineRepository).should().addSection(any(Section.class));
+        assertThat(구간_추가된_일호선.getStations()).hasSize(3);
     }
 
-    @DisplayName("지하철 구간 삭제")
+    @DisplayName("지하철 구간 중간 추가 시 거리가 같으면 예외 처리 한다.")
+    @Test
+    void addSectionSameDistance() {
+        //given
+        Station 강남역 = stationDao.save(new Station(1L, "강남역"));
+        Station 잠실역 = stationDao.save(new Station(2L, "잠실역"));
+        Station 잠실나루역 = stationDao.save(new Station(3L, "잠실나루역"));
+        LineRequest 일호선_저장요청 = new LineRequest("1호선", "빨간색", 강남역.getId(), 잠실역.getId(), 20);
+        LineNameColorResponse 일호선_저장응답 = lineService.saveLine(일호선_저장요청);
+        Long targetId = 일호선_저장응답.getId();
+
+        //when then
+        assertThatThrownBy(() -> lineService.addSection(targetId, new SectionRequest(잠실나루역.getId(), 잠실역.getId(), 20)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("역과 역 사이 새로운 역을 추가할 때 기존 역 사이의 길이보다 크거나 같으면 등록할 수 없습니다.");
+    }
+
+    @DisplayName("존재하는 지하철 구간 삭제한다.")
     @Test
     void deleteSection() {
         //given
-        willDoNothing().given(lineRepository).deleteSection(1L, 2L);
+        Station 강남역 = stationDao.save(new Station(1L, "강남역"));
+        Station 잠실역 = stationDao.save(new Station(2L, "잠실역"));
+        Station 잠실나루역 = stationDao.save(new Station(3L, "잠실나루역"));
+        LineRequest 일호선_저장요청 = new LineRequest("1호선", "빨간색", 강남역.getId(), 잠실역.getId(), 20);
+        LineNameColorResponse 일호선_저장응답 = lineService.saveLine(일호선_저장요청);
+        Long targetId = 일호선_저장응답.getId();
+
+        lineService.addSection(targetId, new SectionRequest(잠실나루역.getId(), 잠실역.getId(), 10));
 
         //when
-        lineService.deleteSection(1L, 2L);
+        lineService.deleteSection(targetId, 잠실역.getId());
+        LineResponse 구간_추가된_노선 = lineService.findById(targetId);
 
         //then
-        then(lineRepository).should().deleteSection(1L, 2L);
+        assertThat(구간_추가된_노선.getStations()).hasSize(2);
+    }
+
+    @DisplayName("존재하지 않는 지하철 구간 삭제하면 예외 처리한다.")
+    @Test
+    void notFoundLine() {
+        //given
+        Station 강남역 = stationDao.save(new Station(1L, "강남역"));
+        Station 잠실역 = stationDao.save(new Station(2L, "잠실역"));
+        Station 잠실나루역 = stationDao.save(new Station(3L, "잠실나루역"));
+        LineRequest 일호선_저장요청 = new LineRequest("1호선", "빨간색", 강남역.getId(), 잠실역.getId(), 20);
+        LineNameColorResponse 일호선_저장응답 = lineService.saveLine(일호선_저장요청);
+        Long targetId = 일호선_저장응답.getId();
+
+        lineService.addSection(targetId, new SectionRequest(잠실나루역.getId(), 잠실역.getId(), 10));
+        assertThatThrownBy(() -> lineService.deleteSection(targetId, 999L))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("삭제하려는 역을 포함하는 구간이 존재하지 않습니다.");
+    }
+
+    private LineNameColorResponse 일호선_저장응답() {
+        Station 강남역 = stationDao.save(new Station(1L, "강남역"));
+        Station 잠실역 = stationDao.save(new Station(2L, "잠실역"));
+        LineRequest 일호선_저장요청 = new LineRequest("1호선", "빨간색", 강남역.getId(), 잠실역.getId(), 20);
+        return lineService.saveLine(일호선_저장요청);
     }
 }
--- src/test/java/wooteco/subway/station/service/StationServiceTest.java
+++ src/test/java/wooteco/subway/station/service/StationServiceTest.java
@@ -7,6 +7,10 @@ import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import wooteco.subway.exception.badrequest.DuplicatedNameException;
 import wooteco.subway.station.controller.dto.StationRequest;
 import wooteco.subway.station.controller.dto.StationResponse;
 import wooteco.subway.station.domain.Station;
@@ -18,75 +22,93 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 import static org.mockito.Mockito.mock;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+@Transactional
 class StationServiceTest {
-    private Station 저장_후_반환된_역 = new Station(1L, "강남역");
-    private StationRequest 저장_요청_역 = new StationRequest("강남역");
-    private List<Station> 지하철_역들 = Arrays.asList(new Station(1L, "강남역"),
-            new Station(2L, "역삼역"), new Station(3L, "잠실역"));
+    private StationService stationService;
 
-    @Mock
-    StationRepository stationRepository = mock(StationRepository.class);
+    @Autowired
+    public StationServiceTest(StationService stationService) {
+        this.stationService = stationService;
+    }
 
-    @InjectMocks
-    StationService stationService;
-
-    @DisplayName("지하철 역 저장")
+    @DisplayName("지하철 역 저장한다.")
     @Test
     void save() {
         //given
-        given(stationRepository.save(any(Station.class))).willReturn(저장_후_반환된_역);
+        StationRequest 지하철_역_저장_요청 = new StationRequest("강남역");
 
         //when
-        StationResponse 응답되는_역 = stationService.save(저장_요청_역);
+        StationResponse 응답되는_역 = stationService.save(지하철_역_저장_요청);
 
         //then
-        assertThat(응답되는_역.getName()).isEqualTo(저장_요청_역.getName());
-        then(stationRepository).should().save(any(Station.class));
+        assertThat(응답되는_역.getName()).isEqualTo(지하철_역_저장_요청.getName());
+    }
+
+    @DisplayName("중복된 지하철 역 저장하면 예외 처리 한다.")
+    void duplicateNameSave() {
+        //given
+        StationRequest 지하철_역_저장_요청 = new StationRequest("강남역");
+        stationService.save(지하철_역_저장_요청);
+
+        //when then
+        assertThatThrownBy(() -> stationService.save(지하철_역_저장_요청)).isInstanceOf(DuplicatedNameException.class);
     }
 
     @DisplayName("지하철 역 전체 조회")
     @Test
     void findAll() {
         //given
-        given(stationRepository.findAll()).willReturn(지하철_역들);
+        StationRequest 강남역_저장_요청 = new StationRequest("강남역");
+        StationRequest 잠실역_저장_요청 = new StationRequest("잠실역");
+        stationService.save(강남역_저장_요청);
+        stationService.save(잠실역_저장_요청);
 
         //when
         List<StationResponse> 지하철역들 = stationService.findAll();
 
         //then
-        assertThat(지하철_역들).hasSize(지하철역들.size());
-        then(stationRepository).should().findAll();
+        assertThat(지하철역들).hasSize(2);
     }
 
     @DisplayName("지하철 역 삭제")
     @Test
     void delete() {
         //given
-        willDoNothing().given(stationRepository).delete(1L);
+        StationRequest 지하철_역_저장_요청 = new StationRequest("강남역");
+        StationResponse 응답되는_역 = stationService.save(지하철_역_저장_요청);
+
 
         //when
-        stationService.delete(1L);
+        stationService.delete(응답되는_역.getId());
+        List<StationResponse> 저장된_지하철_역들 = stationService.findAll();
 
         //then
-        then(stationRepository).should().delete(1L);
+        assertThat(저장된_지하철_역들).hasSize(0);
     }
 
     @DisplayName("순서가 정렬된 지하철 역 조회할 수 있다.")
-    @SuppressWarnings("unchecked")
     @Test
     void findSortStationsByIds() {
         //given
-        given(stationRepository.findByIds(any(List.class))).willReturn(지하철_역들);
+        Station 강남역 = 저장한_지하철역(new StationRequest("강남역"));
+        Station 잠실역 = 저장한_지하철역(new StationRequest("잠실역"));
+        Station 잠실나루역 = 저장한_지하철역(new StationRequest("잠실나루역"));
 
         //when
-        Stations 조회한_역들 = stationService.findSortStationsByIds(Arrays.asList(3L, 2L, 1L));
+        Stations 조회한_역들 = stationService.findSortStationsByIds(Arrays.asList(강남역.getId(), 잠실나루역.getId(), 잠실역.getId()));
 
         //then
-        then(stationRepository).should().findByIds(any(List.class));
+        assertThat(조회한_역들.toList()).containsExactly(강남역, 잠실나루역, 잠실역);
+    }
+
+    private Station 저장한_지하철역(StationRequest stationRequest) {
+        StationResponse 저장된_지하철역_응답 = stationService.save(stationRequest);
+        return new Station(저장된_지하철역_응답.getId(), 저장된_지하철역_응답.getName());
     }
 }
