7a2c7d00d0310b675c89eca566d461cbb12383cb
Wannte <T.wannte@gmail.com>
2021-05-29 00:16:39 +0900

    test(StationService): Mock 을 이용하여 test 진행

--- src/test/java/wooteco/subway/station/StationServiceTest.java
+++ src/test/java/wooteco/subway/station/StationServiceTest.java
@@ -2,39 +2,47 @@ package wooteco.subway.station;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.transaction.annotation.Transactional;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import wooteco.subway.station.dao.StationDao;
 import wooteco.subway.station.domain.Station;
 import wooteco.subway.station.dto.StationRequest;
 import wooteco.subway.station.exception.StationError;
 import wooteco.subway.station.exception.StationException;
 import wooteco.subway.station.service.StationService;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 
-@Transactional
-@Sql("/init-station.sql")
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
 class StationServiceTest {
-    private static final StationRequest stationRequest = new StationRequest("잠실역");
+    private static final String stationName = "잠실역";
+    private static final StationRequest stationRequest = new StationRequest(stationName);
 
-    @Autowired
+    @InjectMocks
     private StationService stationService;
+
+    @Mock
+    private StationDao stationDao;
 
     @Test
     @DisplayName("역 정상 생성 테스트")
     void createStation() {
-        Station savedStation = stationService.createStation(stationRequest);
-        assertEquals(stationRequest.getName(), savedStation.getName());
+        stationService.createStation(stationRequest);
+
+        verify(stationDao).save(stationName);
     }
 
     @Test
     @DisplayName("역 이름 중복 생성 테스트")
     void createDuplicatedStation() {
-        stationService.createStation(stationRequest);
+        given(stationDao.findByName(stationName)).willReturn(Optional.of(new Station(stationName)));
+
         assertThatThrownBy(() -> stationService.createStation(stationRequest))
                 .isInstanceOf(StationException.class)
                 .hasMessage(StationError.ALREADY_EXIST_STATION_NAME.getMessage());
