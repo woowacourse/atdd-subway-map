28525fb9ede09d88e4679c2c750dffcfd2df2ddc
브리 <buyong.jo@woowahan.com>
2022-04-14 16:52:26 +0900

    chore: adjust new specification (#174)
    * refactor: remove frontend code
    
    * refactor: flatten project structure
    
    * docs: remove frontend guide
    
    * refactor: remove unusable resources
    
    * chore: Update versions

---
+++ src/main/java/wooteco/subway/domain/Station.java
@@ -0,0 +1,27 @@
+package wooteco.subway.domain;
+
+public class Station {
+    private Long id;
+    private String name;
+
+    public Station() {
+    }
+
+    public Station(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Station(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
---
+++ src/main/java/wooteco/subway/ui/StationController.java
@@ -0,0 +1,39 @@
+package wooteco.subway.ui;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import wooteco.subway.dao.StationDao;
+import wooteco.subway.domain.Station;
+import wooteco.subway.dto.StationRequest;
+import wooteco.subway.dto.StationResponse;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+public class StationController {
+
+    @PostMapping("/stations")
+    public ResponseEntity<StationResponse> createStation(@RequestBody StationRequest stationRequest) {
+        Station station = new Station(stationRequest.getName());
+        Station newStation = StationDao.save(station);
+        StationResponse stationResponse = new StationResponse(newStation.getId(), newStation.getName());
+        return ResponseEntity.created(URI.create("/stations/" + newStation.getId())).body(stationResponse);
+    }
+
+    @GetMapping(value = "/stations", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<StationResponse>> showStations() {
+        List<Station> stations = StationDao.findAll();
+        List<StationResponse> stationResponses = stations.stream()
+                .map(it -> new StationResponse(it.getId(), it.getName()))
+                .collect(Collectors.toList());
+        return ResponseEntity.ok().body(stationResponses);
+    }
+
+    @DeleteMapping("/stations/{id}")
+    public ResponseEntity deleteStation(@PathVariable Long id) {
+        return ResponseEntity.noContent().build();
+    }
+}
