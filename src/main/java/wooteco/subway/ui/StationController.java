d5ed4a340291baa398ac9b0bd2fd65cd2cdf6763
woowabrie <buyong.jo@woowahan.com>
2022-05-02 10:51:23 +0900

    refactor: add generic type to ResponseEntity

--- src/main/java/wooteco/subway/ui/StationController.java
+++ src/main/java/wooteco/subway/ui/StationController.java
@@ -33,7 +33,7 @@ public class StationController {
     }
 
     @DeleteMapping("/stations/{id}")
-    public ResponseEntity deleteStation(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteStation(@PathVariable Long id) {
         return ResponseEntity.noContent().build();
     }
 }
