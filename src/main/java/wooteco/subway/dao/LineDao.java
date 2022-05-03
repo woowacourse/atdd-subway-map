54792f57876225774100aa386191e4ab120e8eb3
Jeong Jinwoo <jwjeong96@gmail.com>
2022-05-03 16:30:16 +0900

    feat(Line): implement Line domain and LineDao

---
+++ src/main/java/wooteco/subway/dao/LineDao.java
@@ -0,0 +1,66 @@
+package wooteco.subway.dao;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.util.ReflectionUtils;
+import wooteco.subway.domain.Line;
+import wooteco.subway.exception.NotFoundException;
+
+public class LineDao {
+
+    private static Long seq = 0L;
+    private static final List<Line> lines = new ArrayList<>();
+
+    public static List<Line> findAll() {
+        return lines;
+    }
+
+    public static Line findById(Long id) {
+        return lines.stream()
+                .filter(it -> it.hasIdOf(id))
+                .findFirst()
+                .orElseThrow(() -> new NotFoundException("해당되는 노선은 존재하지 않습니다."));
+    }
+
+    public static Line save(Line line) {
+        validateUniqueName(line);
+        Line persistLine = createNewObject(line);
+        lines.add(persistLine);
+        return persistLine;
+    }
+
+    public static void update(Line line) {
+        validateUniqueName(line);
+       Line currentLine = lines.stream()
+                .filter(it -> it.hasIdOf(line.getId()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("해당되는 노선은 존재하지 않습니다."));
+
+        currentLine.setName(line.getName());
+        currentLine.setColor(line.getColor());
+    }
+
+    public static void deleteById(Long id) {
+        boolean removed = lines.removeIf(it -> it.getId().equals(id));
+        if (!removed) {
+            throw new IllegalArgumentException("해당되는 노선은 존재하지 않습니다.");
+        }
+    }
+
+    private static void validateUniqueName(Line newLine) {
+        boolean hasDuplicate = lines.stream()
+                .filter(it -> !it.hasIdOf(newLine.getId()))
+                .anyMatch(it -> it.hasNameOf(newLine.getName()));
+        if (hasDuplicate) {
+            throw new IllegalArgumentException("중복되는 이름의 지하철 노선이 존재합니다.");
+        }
+    }
+
+    private static Line createNewObject(Line station) {
+        Field field = ReflectionUtils.findField(Line.class, "id");
+        field.setAccessible(true);
+        ReflectionUtils.setField(field, station, ++seq);
+        return station;
+    }
+}
---
+++ src/main/java/wooteco/subway/domain/Line.java
@@ -0,0 +1,47 @@
+package wooteco.subway.domain;
+
+public class Line {
+
+    private Long id;
+    private String name;
+    private String color;
+
+    public Line(Long id, String name, String color) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+    }
+
+    public Line(String name, String color) {
+        this(null, name, color);
+    }
+
+    public boolean hasIdOf(Long id) {
+        return this.id.equals(id);
+    }
+
+    public boolean hasNameOf(String name) {
+        return this.name.equals(name);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
+
