c020a6484b6948421d52dcdd7d4fe01d641fe907
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2022-05-03 18:00:05 +0900

    test(StationService): 모든 Stations 검색 기능 테스트 추가

--- src/test/java/wooteco/subway/service/StationServiceTest.java
+++ src/test/java/wooteco/subway/service/StationServiceTest.java
@@ -1,6 +1,9 @@
 package wooteco.subway.service;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -29,5 +32,24 @@ public class StationServiceTest {
 
         //then
         assertThat(actual.getName()).isEqualTo(station.getName());
+    }
+
+    @Test
+    @DisplayName("모든 station 을 검색한다.")
+    void findAll() {
+        //given
+        Station station1 = new Station("lala");
+        Station station2 = new Station("sojukang");
+        stationService.save(station1);
+        stationService.save(station2);
+
+        //when
+        List<Station> actual = stationService.findAll();
+
+        //then
+        assertAll(
+            () -> assertThat(actual.get(0).getName()).isEqualTo(station1.getName()),
+            () -> assertThat(actual.get(1).getName()).isEqualTo(station2.getName())
+        );
     }
 }
