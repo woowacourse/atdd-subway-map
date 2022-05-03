ebae9a592051fd617e53f59796127dffb4334e7c
yh20studio(tiki) <yh20studio@gmail.com>
2022-05-03 16:34:35 +0900

    feat: 노선을 등록하는 api 추가

---
+++ src/test/java/wooteco/subway/acceptance/LineAcceptanceTest.java
@@ -0,0 +1,44 @@
+package wooteco.subway.acceptance;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+class LineAcceptanceTest extends AcceptanceTest {
+
+    @DisplayName("라인을 등록한다.")
+    @Test
+    void createLine() {
+        // given
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "신분당선");
+        params.put("color", "rgb-red-600");
+
+        // when
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post("/lines")
+                .then().log().all()
+                .extract();
+
+        // then
+        assertAll(
+                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value()),
+                () -> assertThat(response.header("Location")).isEqualTo("/lines/1"),
+                () -> assertThat(response.body().jsonPath().getLong("id")).isEqualTo(1L),
+                () -> assertThat(response.body().jsonPath().getString("name")).isEqualTo("신분당선"),
+                () -> assertThat(response.body().jsonPath().getString("color")).isEqualTo("rgb-red-600")
+        );
+    }
+}
