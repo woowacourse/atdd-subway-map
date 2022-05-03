eb2c2e027305e38f6a751d25b87cfcc6bf2168d9
EastHShin <xldk78@gmail.com>
2022-05-03 18:42:55 +0900

    feat: 전역적으로 예외 핸들링하는 Controller Advice 생성

---
+++ src/main/java/wooteco/subway/ui/SubwayExceptionController.java
@@ -0,0 +1,19 @@
+package wooteco.subway.ui;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class SubwayExceptionController {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<?> handleIllegalArgumentException(RuntimeException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> handleOtherException(RuntimeException e) {
+        return ResponseEntity.badRequest().body("서버에 오류가 있으니 잠시 후 다시 시도해주세요.");
+    }
+}
