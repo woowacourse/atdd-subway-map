9f42472e018a27bbd98467e7adf06578b902fe8e
devHudi <devhudi@gmail.com>
2022-05-03 16:45:46 +0900

    feat: 지하철역 추가 시 중복 이름 검증

--- src/main/java/wooteco/subway/dao/StationDao.java
+++ src/main/java/wooteco/subway/dao/StationDao.java
@@ -12,9 +12,19 @@ public class StationDao {
     private static List<Station> stations = new ArrayList<>();
 
     public static Station save(Station station) {
+        validateDuplicateName(station);
         Station persistStation = createNewObject(station);
         stations.add(persistStation);
         return persistStation;
+    }
+
+    private static void validateDuplicateName(Station station) {
+        stations.stream()
+                .filter(it -> it.isSameStation(station))
+                .findAny()
+                .ifPresent(it -> {
+                    throw new IllegalArgumentException(String.format("%s은 이미 존재하는 지하철 역입니다.", it.getName()));
+                });
     }
 
     public static List<Station> findAll() {
--- src/main/java/wooteco/subway/domain/Station.java
+++ src/main/java/wooteco/subway/domain/Station.java
@@ -16,6 +16,10 @@ public class Station {
         this.name = name;
     }
 
+    public boolean isSameStation(Station other) {
+        return this.name.equals(other.name);
+    }
+
     public Long getId() {
         return id;
     }
