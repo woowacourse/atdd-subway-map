5120c07a7ecf566191d834de59e0e5b99b9610bc
DOES <ldk980130@gmail.com>
2022-05-03 14:25:31 +0900

    refactor: StationDao static 제거

--- src/main/java/wooteco/subway/dao/StationDao.java
+++ src/main/java/wooteco/subway/dao/StationDao.java
@@ -6,25 +6,26 @@ import wooteco.subway.domain.Station;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class StationDao {
-    private static Long seq = 0L;
-    private static List<Station> stations = new ArrayList<>();
+	private Long seq = 0L;
+	private final List<Station> stations = new ArrayList<>();
 
-    public static Station save(Station station) {
-        Station persistStation = createNewObject(station);
-        stations.add(persistStation);
-        return persistStation;
-    }
+	public Station save(Station station) {
+		Station persistStation = createNewObject(station);
+		stations.add(persistStation);
+		return persistStation;
+	}
 
-    public static List<Station> findAll() {
-        return stations;
-    }
+	private Station createNewObject(Station station) {
+		Field field = ReflectionUtils.findField(Station.class, "id");
+		Objects.requireNonNull(field).setAccessible(true);
+		ReflectionUtils.setField(field, station, ++seq);
+		return station;
+	}
 
-    private static Station createNewObject(Station station) {
-        Field field = ReflectionUtils.findField(Station.class, "id");
-        field.setAccessible(true);
-        ReflectionUtils.setField(field, station, ++seq);
-        return station;
-    }
+	public List<Station> findAll() {
+		return stations;
+	}
 }
