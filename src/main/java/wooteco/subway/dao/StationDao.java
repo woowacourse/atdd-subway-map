97ebfdcdfaf8a67d1f6a1ff52615fdb9cb0f2fe9
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2022-05-03 17:26:37 +0900

    refactor(StationDao): createNewObject 내부 로직 수정

--- src/main/java/wooteco/subway/dao/StationDao.java
+++ src/main/java/wooteco/subway/dao/StationDao.java
@@ -1,17 +1,14 @@
 package wooteco.subway.dao;
 
-import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
-
-import org.springframework.util.ReflectionUtils;
 
 import wooteco.subway.domain.Station;
 
 public class StationDao {
 
-    private static Long seq = 0L;
     private static final List<Station> stations = new ArrayList<>();
+    private static Long seq = 0L;
 
     public Station save(Station station) {
         Station persistStation = createNewObject(station);
@@ -24,9 +21,6 @@ public class StationDao {
     }
 
     private Station createNewObject(Station station) {
-        Field field = ReflectionUtils.findField(Station.class, "id");
-        field.setAccessible(true);
-        ReflectionUtils.setField(field, station, ++seq);
-        return station;
+        return new Station(++seq, station.getName());
     }
 }
