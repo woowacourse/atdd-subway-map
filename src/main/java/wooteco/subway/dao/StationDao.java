d2659b5a2fb32517b7a85030ec1a08136f1498b2
nailseong <nnis9857@gmail.com>
2022-05-03 15:20:16 +0900

    feat(StationDao): 중복된 이름을 검증하는 기능

--- src/main/java/wooteco/subway/dao/StationDao.java
+++ src/main/java/wooteco/subway/dao/StationDao.java
@@ -1,20 +1,29 @@
 package wooteco.subway.dao;
 
-import org.springframework.util.ReflectionUtils;
-import wooteco.subway.domain.Station;
-
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.util.ReflectionUtils;
+import wooteco.subway.domain.Station;
 
 public class StationDao {
+
     private static Long seq = 0L;
     private static List<Station> stations = new ArrayList<>();
 
     public static Station save(Station station) {
+        if (isDuplicateName(station)) {
+            throw new IllegalArgumentException("중복된 이름의 역은 저장할 수 없습니다.");
+        }
+
         Station persistStation = createNewObject(station);
         stations.add(persistStation);
         return persistStation;
+    }
+
+    private static boolean isDuplicateName(final Station station) {
+        return stations.stream()
+                .anyMatch(it -> it.getName().equals(station.getName()));
     }
 
     public static List<Station> findAll() {
---
+++ src/test/java/wooteco/subway/dao/StationDaoTest.java
@@ -0,0 +1,27 @@
+package wooteco.subway.dao;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static wooteco.subway.dao.StationDao.save;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import wooteco.subway.domain.Station;
+
+class StationDaoTest {
+
+    @Test
+    @DisplayName("역 이름이 중복되면 예외가 발생한다.")
+    void save_inValidName() {
+        // given
+        final Station station = new Station("선릉");
+        save(station);
+
+        // when
+        final Station newStation = new Station("선릉");
+
+        // then
+        assertThatThrownBy(() -> StationDao.save(newStation))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("중복된 이름의 역은 저장할 수 없습니다.");
+    }
+}
