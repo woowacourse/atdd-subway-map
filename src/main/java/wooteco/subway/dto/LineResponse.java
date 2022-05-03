1bd29383cd03110b83253497739e34dff0bba36e
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2022-05-03 19:25:52 +0900

    style: formatter 적용

--- src/main/java/wooteco/subway/dto/LineResponse.java
+++ src/main/java/wooteco/subway/dto/LineResponse.java
@@ -1,14 +1,12 @@
 package wooteco.subway.dto;
 
-import wooteco.subway.dto.StationResponse;
-
 import java.util.List;
 
 public class LineResponse {
-    private Long id;
-    private String name;
-    private String color;
-    private List<StationResponse> stations;
+    private final Long id;
+    private final String name;
+    private final String color;
+    private final List<StationResponse> stations;
 
     public LineResponse(Long id, String name, String color, List<StationResponse> stations) {
         this.id = id;
