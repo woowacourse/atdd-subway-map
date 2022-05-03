cd36640d5b174772bd376ade55e475b4318a7527
Jeong Jinwoo <jwjeong96@gmail.com>
2022-05-03 15:38:39 +0900

    feat(station): implement API for station
    - implement StationDao and StationsController
    - add test cases

--- src/main/java/wooteco/subway/dao/StationDao.java
+++ src/main/java/wooteco/subway/dao/StationDao.java
@@ -1,24 +1,44 @@
 package wooteco.subway.dao;
 
-import org.springframework.util.ReflectionUtils;
-import wooteco.subway.domain.Station;
-
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.util.ReflectionUtils;
+import wooteco.subway.domain.Station;
 
 public class StationDao {
+
     private static Long seq = 0L;
-    private static List<Station> stations = new ArrayList<>();
+    private static final List<Station> stations = new ArrayList<>();
+
+    public static List<Station> findAll() {
+        return stations;
+    }
 
     public static Station save(Station station) {
+        validateUniqueName(station);
         Station persistStation = createNewObject(station);
         stations.add(persistStation);
         return persistStation;
     }
 
-    public static List<Station> findAll() {
-        return stations;
+    public static void deleteById(Long id) {
+        boolean removed = stations.removeIf(it -> it.getId().equals(id));
+        if (!removed) {
+            throw new IllegalArgumentException("해당되는 역은 존재하지 않습니다.");
+        }
+    }
+
+    public static void clear() {
+        stations.clear();
+    }
+
+    private static void validateUniqueName(Station station) {
+        boolean hasDuplicate = stations.stream()
+                .anyMatch(it -> it.getName().equals(station.getName()));
+        if (hasDuplicate) {
+            throw new IllegalArgumentException("중복되는 이름의 지하철역이 존재합니다.");
+        }
     }
 
     private static Station createNewObject(Station station) {
--- src/main/java/wooteco/subway/ui/StationController.java
+++ src/main/java/wooteco/subway/ui/StationController.java
@@ -1,5 +1,6 @@
 package wooteco.subway.ui;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,7 +21,8 @@ public class StationController {
         Station station = new Station(stationRequest.getName());
         Station newStation = StationDao.save(station);
         StationResponse stationResponse = new StationResponse(newStation.getId(), newStation.getName());
-        return ResponseEntity.created(URI.create("/stations/" + newStation.getId())).body(stationResponse);
+        URI location = URI.create("/stations/" + newStation.getId());
+        return ResponseEntity.created(location).body(stationResponse);
     }
 
     @GetMapping(value = "/stations", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -34,6 +36,18 @@ public class StationController {
 
     @DeleteMapping("/stations/{id}")
     public ResponseEntity<Void> deleteStation(@PathVariable Long id) {
+        StationDao.deleteById(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<String> handleException(Exception e) {
+        e.printStackTrace();
+        return new ResponseEntity<>("예상치 못한 문제가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<String> handleException(IllegalArgumentException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 }
