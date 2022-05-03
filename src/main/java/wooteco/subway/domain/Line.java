7c9e28fe9c6866833015924c204aa006389edd38
Jeong Jinwoo <jwjeong96@gmail.com>
2022-05-03 23:17:39 +0900

    refactor(subway): minor cleansing

--- src/main/java/wooteco/subway/domain/Line.java
+++ src/main/java/wooteco/subway/domain/Line.java
@@ -5,8 +5,8 @@ import java.util.Objects;
 public class Line {
 
     private final Long id;
-    private String name;
-    private String color;
+    private final String name;
+    private final String color;
 
     public Line(Long id, String name, String color) {
         this.id = id;
@@ -16,14 +16,6 @@ public class Line {
 
     public Line(String name, String color) {
         this(null, name, color);
-    }
-
-    public boolean hasIdOf(Long id) {
-        return this.id.equals(id);
-    }
-
-    public boolean hasNameOf(String name) {
-        return this.name.equals(name);
     }
 
     public Long getId() {
@@ -38,14 +30,6 @@ public class Line {
         return color;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -55,7 +39,8 @@ public class Line {
             return false;
         }
         Line line = (Line) o;
-        return Objects.equals(id, line.id) && Objects.equals(name, line.name)
+        return Objects.equals(id, line.id)
+                && Objects.equals(name, line.name)
                 && Objects.equals(color, line.color);
     }
 
