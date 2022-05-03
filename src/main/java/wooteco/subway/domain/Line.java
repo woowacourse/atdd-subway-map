e4fb874744c79f140ac279bcf70a340216fdef64
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-05-03 17:21:28 +0900

    feat: Line도메인 추가

---
+++ src/main/java/wooteco/subway/domain/Line.java
@@ -0,0 +1,26 @@
+package wooteco.subway.domain;
+
+public class Line {
+
+    private final Long id;
+    private final String name;
+    private final String color;
+
+    public Line(final Long id, final String name, final String color) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
