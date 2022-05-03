30a807c486e43d88a37474a1696f13d6133c02c7
EastHShin <xldk78@gmail.com>
2022-05-03 19:13:17 +0900

    refactor(service): `@Transactional` 을 붙여 원자성 보장

--- src/main/java/wooteco/subway/application/LineService.java
+++ src/main/java/wooteco/subway/application/LineService.java
@@ -2,6 +2,7 @@ package wooteco.subway.application;
 
 import java.util.List;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import wooteco.subway.dao.LineDao;
 import wooteco.subway.domain.Line;
 
@@ -14,6 +15,7 @@ public class LineService {
         this.lineDao = lineDao;
     }
 
+    @Transactional
     public Line save(String name, String color) {
         if (lineDao.existsByName(name)){
             throw new IllegalArgumentException("중복된 노선 이름입니다.");
@@ -22,10 +24,12 @@ public class LineService {
         return lineDao.save(new Line(name, color));
     }
 
+    @Transactional(readOnly = true)
     public List<Line> showLines() {
         return lineDao.findAll();
     }
 
+    @Transactional(readOnly = true)
     public Line showLine(Long id) {
         if (lineDao.notExistsById(id)) {
             throw new IllegalArgumentException("존재하지 않는 id입니다.");
@@ -34,6 +38,7 @@ public class LineService {
         return lineDao.findById(id);
     }
 
+    @Transactional
     public void updateLine(Long id, String name, String color) {
         if (lineDao.notExistsById(id)) {
             throw new IllegalArgumentException("존재하지 않는 id입니다.");
@@ -41,6 +46,7 @@ public class LineService {
         lineDao.updateLineById(id, name, color);
     }
 
+    @Transactional
     public void deleteLine(Long id) {
         if (lineDao.notExistsById(id)) {
             throw new IllegalArgumentException("존재하지 않는 id입니다.");
