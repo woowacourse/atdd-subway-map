f90873acb8910a25de2644be15edf2b1afca8b35
pup-paw <wldusdl0310@gmail.com>
2022-05-03 15:37:43 +0900

    feat: line 생성 기능 구현

---
+++ src/main/java/wooteco/subway/domain/Line.java
@@ -0,0 +1,49 @@
+package wooteco.subway.domain;
+
+import java.util.Objects;
+
+public class Line {
+    private Long id;
+    private String name;
+    private String  color;
+
+    public Line() {}
+
+    public Line(Long id, String name, String color) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+    }
+
+    public Line(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Line line = (Line)o;
+        return id.equals(line.id) && name.equals(line.name) && color.equals(line.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, color);
+    }
+}
