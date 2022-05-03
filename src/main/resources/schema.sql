ea1ce3e4eedea0c61e47176b10f91ca72fcae708
jayjaehunchoi <wogns0108@naver.com>
2022-05-03 18:44:59 +0900

    refactor : schema 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,14 @@
-create table if not exists STATION
+drop table if exists STATION;
+drop table if exists LINE;
+
+create table STATION
 (
     id bigint auto_increment not null,
     name varchar(255) not null unique,
     primary key(id)
 );
 
-create table if not exists LINE
+create table LINE
 (
     id bigint auto_increment not null,
     name varchar(255) not null unique,
