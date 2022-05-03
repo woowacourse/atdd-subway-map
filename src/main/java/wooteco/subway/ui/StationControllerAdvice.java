8ccaa50f17c8b141ed78f874379ef3c4c1f03cb7
Richard JEON <ztzy1907@gmail.com>
2022-05-03 20:57:20 +0900

    feat(controller-advice): 맵핑 예외 추가

--- src/main/java/wooteco/subway/ui/StationControllerAdvice.java
+++ src/main/java/wooteco/subway/ui/StationControllerAdvice.java
@@ -4,12 +4,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import wooteco.subway.exception.LineDuplicateException;
+import wooteco.subway.exception.NoLineFoundException;
+import wooteco.subway.exception.NoStationFoundException;
 import wooteco.subway.exception.StationDuplicateException;
 
 @RestControllerAdvice
 public class StationControllerAdvice {
 
-    @ExceptionHandler(StationDuplicateException.class)
+    @ExceptionHandler({StationDuplicateException.class, LineDuplicateException.class, NoLineFoundException.class,
+            NoStationFoundException.class})
     public ResponseEntity<Void> duplicateStation() {
         return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
