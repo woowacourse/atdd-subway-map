ab84ee8762c06fca54e9b1ac87b47c05273c910c
yangdongjue5510 <djwhy5510@gmail.com>
2022-05-03 16:25:13 +0900

    feat(ExceptionAdvice): 예외 처리 기능

---
+++ src/main/java/wooteco/subway/ui/ExceptionAdvice.java
@@ -0,0 +1,15 @@
+package wooteco.subway.ui;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import wooteco.subway.dto.ErrorResponse;
+
+@ControllerAdvice(assignableTypes = {StationController.class})
+public class ExceptionAdvice {
+
+    @ExceptionHandler(value = {IllegalArgumentException.class})
+    public ResponseEntity<ErrorResponse> handleException(Exception exception) {
+        return ResponseEntity.badRequest().body(new ErrorResponse(exception.getMessage()));
+    }
+}
