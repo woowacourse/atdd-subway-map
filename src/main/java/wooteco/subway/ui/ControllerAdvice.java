a2d160dab437e773c59461b4d15348a95ca8d4f3
devHudi <devhudi@gmail.com>
2022-05-03 17:26:30 +0900

    feat: 중복된 역 이름 전달 시 Bad Request 응답
    - 별개 테스트 전 생성된 역을 모두 지우는 로직 추가

---
+++ src/main/java/wooteco/subway/ui/ControllerAdvice.java
@@ -0,0 +1,14 @@
+package wooteco.subway.ui;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerAdvice {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handle(Exception e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+}
