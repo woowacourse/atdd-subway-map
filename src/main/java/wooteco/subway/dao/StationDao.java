998c1b1c4318b1114620f768e4037eb65ec24c15
Richard JEON <ztzy1907@gmail.com>
2022-05-03 15:15:37 +0900

    feat(station-dao): 지하철역 추가시 이름 중복 검증 로직 추가

--- src/main/java/wooteco/subway/dao/StationDao.java
+++ src/main/java/wooteco/subway/dao/StationDao.java
@@ -1,20 +1,27 @@
 package wooteco.subway.dao;
 
-import org.springframework.util.ReflectionUtils;
-import wooteco.subway.domain.Station;
-
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.util.ReflectionUtils;
+import wooteco.subway.domain.Station;
+import wooteco.subway.exception.StationDuplicateException;
 
 public class StationDao {
     private static Long seq = 0L;
-    private static List<Station> stations = new ArrayList<>();
+    private static final List<Station> stations = new ArrayList<>();
 
     public static Station save(Station station) {
+        validateDuplicatedStation(station);
         Station persistStation = createNewObject(station);
         stations.add(persistStation);
         return persistStation;
+    }
+
+    private static void validateDuplicatedStation(Station station) {
+        if (stations.contains(station)) {
+            throw new StationDuplicateException();
+        }
     }
 
     public static List<Station> findAll() {
