0c5385cb526b8a9fbdc53f33e272e401a500070b
Juhyung990122 <harukawa99@naver.com>
2022-05-03 15:37:43 +0900

    feat: line 생성 기능 구현

---
+++ src/main/java/wooteco/subway/ui/LineController.java
@@ -0,0 +1,28 @@
+package wooteco.subway.ui;
+
+import java.net.URI;
+import java.util.ArrayList;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import wooteco.subway.dao.LineDao;
+import wooteco.subway.domain.Line;
+import wooteco.subway.dto.LineRequest;
+import wooteco.subway.dto.LineResponse;
+
+@RestController
+public class LineController {
+
+    @PostMapping("/lines")
+    public ResponseEntity<LineResponse> createStation(@RequestBody LineRequest lineRequest) {
+        Line line = new Line(lineRequest.getName(), lineRequest.getColor());
+        Line newLine = LineDao.save(line);
+        LineResponse lineResponse = new LineResponse(newLine.getId(), newLine.getName(), newLine.getColor(),
+                new ArrayList<>());
+        return ResponseEntity.created(URI.create("/lines/" + newLine.getId()))
+                .body(lineResponse);
+    }
+}
---
+++ src/test/java/wooteco/subway/acceptance/LineAcceptanceTest.java
@@ -0,0 +1,38 @@
+package wooteco.subway.acceptance;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+
+class LineAcceptanceTest extends AcceptanceTest {
+
+    @Test
+    void createLine() {
+        // given
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "신분당선");
+        params.put("color", "bg-red-600");
+
+        // when
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post("/lines")
+                .then().log().all()
+                .extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        assertThat(response.header("Location")).isNotBlank();
+    }
+}
