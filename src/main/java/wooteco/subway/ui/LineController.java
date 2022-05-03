d5089dd763bb583010edd9230659c32dc881c966
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-05-03 18:15:00 +0900

    feat: 노선 추가 api 생성 및 테스트

---
+++ src/main/java/wooteco/subway/ui/LineController.java
@@ -0,0 +1,24 @@
+package wooteco.subway.ui;
+
+import java.net.URI;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import wooteco.subway.dao.LineDao;
+import wooteco.subway.domain.Line;
+import wooteco.subway.dto.LineRequest;
+import wooteco.subway.dto.LineResponse;
+import wooteco.subway.service.LineService;
+
+@RestController
+public class LineController {
+
+    private final LineService lineService = new LineService(new LineDao());
+
+    @PostMapping("/lines")
+    public ResponseEntity<LineResponse> createLine(@RequestBody LineRequest lineRequest) {
+        Line line = lineService.save(lineRequest.toLine());
+        return ResponseEntity.created(URI.create("/lines/" + line.getId())).body(LineResponse.from(line));
+    }
+}
---
+++ src/test/java/wooteco/subway/acceptance/LineAcceptanceTest.java
@@ -0,0 +1,38 @@
+package wooteco.subway.acceptance;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+class LineAcceptanceTest extends AcceptanceTest {
+
+    @Test
+    @DisplayName("노선을 추가한다.")
+    void save() {
+        //given
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "신분당선");
+        params.put("color", "bg-red-600");
+
+        //when
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post("/lines")
+                .then().log().all()
+                .extract();
+
+        //then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        assertThat(response.header("Location")).isNotBlank();
+    }
+}
