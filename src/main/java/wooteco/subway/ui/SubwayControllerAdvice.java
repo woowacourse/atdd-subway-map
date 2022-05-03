e438e66240683ceea782c09b7492fbb28bf0eba3
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-05-03 21:01:59 +0900

    feat: 지원하지 않는 url 요청 핸들링 및 테스트

--- src/main/java/wooteco/subway/ui/SubwayControllerAdvice.java
+++ src/main/java/wooteco/subway/ui/SubwayControllerAdvice.java
@@ -3,6 +3,7 @@ package wooteco.subway.ui;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import wooteco.subway.dto.SubwayErrorResponse;
 
 @RestControllerAdvice
@@ -11,5 +12,10 @@ public class SubwayControllerAdvice {
     @ExceptionHandler(IllegalStateException.class)
     public ResponseEntity<SubwayErrorResponse> handleBusinessException(RuntimeException exception) {
         return ResponseEntity.badRequest().body(SubwayErrorResponse.from(exception));
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<Void> handleNoHandlerFoundExceptoin() {
+        return ResponseEntity.notFound().build();
     }
 }
