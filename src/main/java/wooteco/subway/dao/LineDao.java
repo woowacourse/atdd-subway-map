4bde17ac36a60842a322f128dee4a156c5080f3f
beomWhale <cndqjacndqja@gmail.com>
2022-05-03 17:37:27 +0900

    feat: 노선 목록 조회 기능 추가

--- src/main/java/wooteco/subway/dao/LineDao.java
+++ src/main/java/wooteco/subway/dao/LineDao.java
@@ -1,6 +1,8 @@
 package wooteco.subway.dao;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import wooteco.subway.domain.Line;
 
@@ -12,5 +14,9 @@ public class LineDao {
         Line newLine = new Line(++seq, line.getName(), line.getColor(), line.getStations());
         lines.put(seq, newLine);
         return seq;
+    }
+
+    public static List<Line> findAll() {
+         return new ArrayList<>(lines.values());
     }
 }
--- src/test/java/wooteco/subway/dao/LineDaoTest.java
+++ src/test/java/wooteco/subway/dao/LineDaoTest.java
@@ -3,23 +3,45 @@ package wooteco.subway.dao;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 import wooteco.subway.domain.Line;
 import wooteco.subway.domain.Station;
 
 class LineDaoTest {
+
     @Test
     void save() {
         // given
-        String name = "1호선";
-        String color = "빨강";
         List<Station> stations = List.of(new Station("교대역"), new Station("강남역"));
-        Line line = new Line(name, color, stations);
+        Line line = new Line("1호선", "빨강", stations);
 
         // when
         Long savedId = LineDao.save(line);
 
         // then
         assertThat(savedId).isEqualTo(1L);
+    }
+
+    @Test
+    void findAll() {
+        // given
+        List<Station> stations = List.of(new Station("교대역"), new Station("강남역"));
+        Line line1 = new Line("1호선", "빨강", stations);
+        Line line2 = new Line("2호선", "초록", stations);
+
+        // when
+        LineDao.save(line1);
+        LineDao.save(line2);
+
+        // then
+        List<String> names = LineDao.findAll()
+            .stream()
+            .map(Line::getName)
+            .collect(Collectors.toList());
+
+        assertThat(names)
+            .hasSize(2)
+            .contains(line1.getName(), line2.getName());
     }
 }
