cb00e7073649f6b3aa53bb4c73d0e9b19f9289c0
Juhyung990122 <harukawa99@naver.com>
2022-05-03 17:03:08 +0900

    feat: line 개별 조회 기능 구현

--- src/main/java/wooteco/subway/ui/LineController.java
+++ src/main/java/wooteco/subway/ui/LineController.java
@@ -1,19 +1,18 @@
 package wooteco.subway.ui;
 
 import java.net.URI;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import wooteco.subway.dao.LineDao;
 import wooteco.subway.domain.Line;
-import wooteco.subway.dto.LineAndStationsResponse;
 import wooteco.subway.dto.LineRequest;
 import wooteco.subway.dto.LineResponse;
 
@@ -21,14 +20,12 @@ import wooteco.subway.dto.LineResponse;
 public class LineController {
 
     @PostMapping("/lines")
-    public ResponseEntity<LineAndStationsResponse> createLine(@RequestBody LineRequest lineRequest) {
+    public ResponseEntity<LineResponse> createLine(@RequestBody LineRequest lineRequest) {
         Line line = new Line(lineRequest.getName(), lineRequest.getColor());
         Line newLine = LineDao.save(line);
-        LineAndStationsResponse lineAndStationsResponse = new LineAndStationsResponse(newLine.getId(),
-                newLine.getName(), newLine.getColor(),
-                new ArrayList<>());
+        LineResponse lineResponse = new LineResponse(line);
         return ResponseEntity.created(URI.create("/lines/" + newLine.getId()))
-                .body(lineAndStationsResponse);
+                .body(lineResponse);
     }
 
     @GetMapping("/lines")
@@ -39,4 +36,13 @@ public class LineController {
                 .collect(Collectors.toList());
         return ResponseEntity.ok(lineResponses);
     }
+
+    @GetMapping("/lines/{id}")
+    public ResponseEntity<LineResponse> findById(@PathVariable Long id) {
+        Line line = LineDao.findById(id);
+        LineResponse lineResponse = new LineResponse(line);
+        return ResponseEntity.ok(lineResponse);
+    }
+
+
 }
