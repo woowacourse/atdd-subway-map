87a73564445c2e7818ab715ff2bced8bb5ff0eac
hyeonic <dev.hyeonic@gmail.com>
2022-05-04 15:17:45 +0900

    test: 노선 중복 예외 검증 테스트 추가

--- src/test/java/wooteco/subway/dao/JdbcLineDaoTest.java
+++ src/test/java/wooteco/subway/dao/JdbcLineDaoTest.java
@@ -1,12 +1,14 @@
 package wooteco.subway.dao;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import wooteco.subway.domain.Line;
 
@@ -32,4 +34,16 @@ public class JdbcLineDaoTest {
                 () -> assertThat(savedLine.getColor()).isEqualTo(line.getColor())
         );
     }
+
+    @DisplayName("중복된 노선을 저장할 경우 예외가 발생한다.")
+    @Test
+    void 중복된_노선_예외발생() {
+        Line line = new Line("2호선", "bg-green-600");
+
+        lineDao.save(line);
+
+        assertThatThrownBy(() -> lineDao.save(line))
+                .isInstanceOf(DuplicateKeyException.class);
+    }
+
 }
