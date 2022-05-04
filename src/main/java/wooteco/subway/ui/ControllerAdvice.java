322c3d7a00347697f9fd5d97e8d593df01f8479a
yh20studio(tiki) <yh20studio@gmail.com>
2022-05-04 19:59:57 +0900

    feat: Exception.class 핸들러 추가

--- src/main/java/wooteco/subway/ui/ControllerAdvice.java
+++ src/main/java/wooteco/subway/ui/ControllerAdvice.java
@@ -8,9 +8,17 @@ import wooteco.subway.dto.response.ErrorMessageResponse;
 @RestControllerAdvice
 public class ControllerAdvice {
 
+    private static final String INTERNAL_EXCEPTION_MESSAGE = "서버 내부 에러입니다.";
+
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<ErrorMessageResponse> exceptionHandler(RuntimeException e) {
+    public ResponseEntity<ErrorMessageResponse> handleIllegalArgumentException(RuntimeException e) {
         ErrorMessageResponse errorMessageResponse = new ErrorMessageResponse(e.getMessage());
         return ResponseEntity.badRequest().body(errorMessageResponse);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorMessageResponse> handleException() {
+        ErrorMessageResponse errorMessageResponse = new ErrorMessageResponse(INTERNAL_EXCEPTION_MESSAGE);
+        return ResponseEntity.internalServerError().body(errorMessageResponse);
     }
 }
