5ef76fa7f9272e808152392fe16152e64832a765
yhh1056 <yhh1056@naver.com>
2022-05-04 16:50:31 +0900

    feat: 노선 이름 범위 예외 처리

--- src/main/java/wooteco/subway/domain/Line.java
+++ src/main/java/wooteco/subway/domain/Line.java
@@ -1,16 +1,28 @@
 package wooteco.subway.domain;
 
-import java.util.Objects;
-
 public class Line {
+
+    private static final int MAX_RANGE = 10;
+    private static final int MIN_RANGE = 3;
 
     private Long id;
     private String name;
     private String color;
 
     public Line(String name, String color) {
+        validateNameRange(name);
         this.name = name;
         this.color = color;
+    }
+
+    private void validateNameRange(String name) {
+        if (name.length() >= MAX_RANGE) {
+            throw new IllegalArgumentException("노선 이름은 10글자를 초과할 수 없습니다.");
+        }
+
+        if (name.length() < MIN_RANGE) {
+            throw new IllegalArgumentException("노선 이름은 3글자 이상이어야 합니다.");
+        }
     }
 
     public Long getId() {
--- src/test/java/wooteco/subway/domain/LineTest.java
+++ src/test/java/wooteco/subway/domain/LineTest.java
@@ -1,8 +1,11 @@
 package wooteco.subway.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class LineTest {
 
@@ -10,5 +13,14 @@ class LineTest {
     @DisplayName("이름과 색깔로 노선을 생성한다.")
     void create() {
         assertThat(new Line("2호선", "bg-red-600")).isNotNull();
+    }
+
+    @ParameterizedTest(name = "이름 : {0}, 메시지 : {1}")
+    @CsvSource({"12345678910, 노선 이름은 10글자를 초과할 수 없습니다.", "노선, 노선 이름은 3글자 이상이어야 합니다."})
+    @DisplayName("노선 이름이 3글자 미만 10글자 초과일 경우 예외를 발생한다.")
+    void inValidName(String value, String message) {
+        assertThatIllegalArgumentException()
+            .isThrownBy(() -> new Line(value, "blue"))
+            .withMessage(message);
     }
 }
