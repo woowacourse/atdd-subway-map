e4dc153ce38e7dc8a83534253b7009df62cbb2ae
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-05-04 13:11:37 +0900

    test: Invalid Dto 테스트 추가

--- src/test/java/wooteco/subway/ui/SubwayControllerAdviceTest.java
+++ src/test/java/wooteco/subway/ui/SubwayControllerAdviceTest.java
@@ -1,12 +1,15 @@
 package wooteco.subway.ui;
 
 import io.restassured.RestAssured;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SubwayControllerAdviceTest {
@@ -27,5 +30,21 @@ class SubwayControllerAdviceTest {
                 .post("/test/not_found")
                 .then().log().all()
                 .statusCode(HttpStatus.NOT_FOUND.value());
+    }
+
+    @Test
+    @DisplayName("invalid request dto 요청")
+    void invalidDto() {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", null);
+        params.put("color", "bg-red-600");
+
+        RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post("/lines")
+                .then().log().all()
+                .statusCode(HttpStatus.BAD_REQUEST.value());
     }
 }
