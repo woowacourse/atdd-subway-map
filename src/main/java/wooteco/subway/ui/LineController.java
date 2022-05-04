200abf436f6375462b7cc3f85d5a0deec993ba8f
yeon-06 <rnjstldus2@gmail.com>
2022-05-04 13:50:12 +0900

    refactor: 반환값 ResponseEntity -> DTO 변환

--- src/main/java/wooteco/subway/ui/LineController.java
+++ src/main/java/wooteco/subway/ui/LineController.java
@@ -3,8 +3,8 @@ package wooteco.subway.ui;
 import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,12 +12,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import wooteco.subway.domain.Line;
 import wooteco.subway.dto.LineDto;
 import wooteco.subway.dto.LineRequest;
 import wooteco.subway.service.LineService;
 
-@Controller
+@RestController
 @RequestMapping("/lines")
 public class LineController {
 
@@ -35,29 +37,26 @@ public class LineController {
     }
 
     @GetMapping
-    public ResponseEntity<List<LineDto>> showLines() {
+    public List<LineDto> showLines() {
         List<Line> lines = lineService.findAll();
-        List<LineDto> lineResponses = lines.stream()
+        return lines.stream()
                 .map(it -> new LineDto(it.getId(), it.getName(), it.getColor()))
                 .collect(Collectors.toList());
-        return ResponseEntity.ok().body(lineResponses);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<LineDto> findLine(@PathVariable Long id) {
-        LineDto lineDto = lineService.findById(id);
-        return ResponseEntity.ok().body(lineDto);
+    public LineDto findLine(@PathVariable Long id) {
+        return lineService.findById(id);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Void> updateLine(@PathVariable Long id, @RequestBody LineRequest lineRequest) {
+    public void updateLine(@PathVariable Long id, @RequestBody LineRequest lineRequest) {
         lineService.update(id, lineRequest);
-        return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id) {
         lineService.deleteById(id);
-        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/wooteco/subway/ui/StationController.java
+++ src/main/java/wooteco/subway/ui/StationController.java
@@ -3,6 +3,7 @@ package wooteco.subway.ui;
 import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import wooteco.subway.domain.Station;
 import wooteco.subway.dto.StationRequest;
@@ -35,17 +37,16 @@ public class StationController {
     }
 
     @GetMapping
-    public ResponseEntity<List<StationResponse>> showStations() {
+    public List<StationResponse> showStations() {
         List<Station> stations = stationService.findAll();
-        List<StationResponse> stationResponses = stations.stream()
+        return stations.stream()
                 .map(it -> new StationResponse(it.getId(), it.getName()))
                 .collect(Collectors.toList());
-        return ResponseEntity.ok().body(stationResponses);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteStation(@PathVariable Long id) {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteStation(@PathVariable Long id) {
         stationService.deleteById(id);
-        return ResponseEntity.noContent().build();
     }
 }
