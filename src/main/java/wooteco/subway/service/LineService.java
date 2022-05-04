afb119100000eec9c9d861546da00073c438665c
yeongunheo <gjduddns@gmail.com>
2022-05-04 15:20:17 +0900

    feat: 컨트롤러로부터 서비스 로직 분리

---
+++ src/main/java/wooteco/subway/service/LineService.java
@@ -0,0 +1,61 @@
+package wooteco.subway.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import wooteco.subway.dao.LineDao;
+import wooteco.subway.domain.Line;
+import wooteco.subway.dto.LineRequest;
+import wooteco.subway.dto.LineResponse;
+
+@Transactional
+@Service
+public class LineService {
+    private final LineDao lineDao;
+
+    public LineService(LineDao lineDao) {
+        this.lineDao = lineDao;
+    }
+
+    public LineResponse saveLine(LineRequest lineRequest) {
+        final Line line = new Line(lineRequest.getName(), lineRequest.getColor());
+        if (lineDao.hasLine(lineRequest.getName())) {
+            throw new IllegalArgumentException("같은 이름의 노선이 존재합니다.");
+        }
+        final Long newLineId = lineDao.save(line);
+        return new LineResponse(newLineId, line.getName(), line.getColor());
+    }
+
+    @Transactional(readOnly = true)
+    public List<LineResponse> findAllLines() {
+        final List<Line> lines = lineDao.findAll();
+        return lines.stream()
+                .map(line -> new LineResponse(line.getId(), line.getName(), line.getColor()))
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    @Transactional(readOnly = true)
+    public LineResponse findLine(Long id) {
+        final Line line = checkExistLine(id);
+        return new LineResponse(line.getId(), line.getName(), line.getColor());
+    }
+
+    public void updateLine(Long id, String name, String color) {
+        checkExistLine(id);
+        lineDao.updateById(id, name, color);
+    }
+
+    public void deleteLine(Long id) {
+        checkExistLine(id);
+        lineDao.deleteById(id);
+    }
+
+    private Line checkExistLine(Long id) {
+        final Line line = lineDao.findById(id);
+        if (line == null) {
+            throw new IllegalArgumentException("해당하는 노선이 존재하지 않습니다.");
+        }
+        return line;
+    }
+}
--- src/main/java/wooteco/subway/ui/LineController.java
+++ src/main/java/wooteco/subway/ui/LineController.java
@@ -2,7 +2,6 @@ package wooteco.subway.ui;
 
 import java.net.URI;
 import java.util.List;
-import java.util.stream.Collectors;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,48 +12,46 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import wooteco.subway.dao.LineDao;
-import wooteco.subway.domain.Line;
 import wooteco.subway.dto.ErrorResponse;
 import wooteco.subway.dto.LineRequest;
 import wooteco.subway.dto.LineResponse;
+import wooteco.subway.service.LineService;
 
 @RestController
 public class LineController {
+    private final LineService lineService;
+
+    public LineController(LineService lineService) {
+        this.lineService = lineService;
+    }
+
     @PostMapping("/lines")
     public ResponseEntity<LineResponse> createLine(@RequestBody LineRequest lineRequest) {
-        Line line = new Line(lineRequest.getName(), lineRequest.getColor());
-        Line newLine = LineDao.save(line);
-        LineResponse lineResponse = new LineResponse(newLine.getId(), newLine.getName(), newLine.getColor());
-        return ResponseEntity.created(URI.create("/lines/" + newLine.getId())).body(lineResponse);
+        final LineResponse lineResponse = lineService.saveLine(lineRequest);
+        return ResponseEntity.created(URI.create("/lines/" + lineResponse.getId())).body(lineResponse);
     }
 
     @GetMapping(value = "/lines", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<LineResponse>> showLines() {
-        List<Line> lines = LineDao.findAll();
-        List<LineResponse> lineResponses = lines.stream()
-                .map(it -> new LineResponse(it.getId(), it.getName(), it.getColor()))
-                .collect(Collectors.toList());
+        final List<LineResponse> lineResponses = lineService.findAllLines();
         return ResponseEntity.ok().body(lineResponses);
     }
 
     @GetMapping("/lines/{id}")
     public ResponseEntity<LineResponse> showLine(@PathVariable Long id) {
-        Line line = LineDao.findById(id);
-        LineResponse lineResponse = new LineResponse(line.getId(), line.getName(), line.getColor());
+        final LineResponse lineResponse = lineService.findLine(id);
         return ResponseEntity.ok().body(lineResponse);
     }
 
     @PutMapping("/lines/{id}")
-    public ResponseEntity<LineResponse> updateLine(@PathVariable Long id, @RequestBody LineRequest lineRequest) {
-        Line line = LineDao.updateById(id, lineRequest.getName(), lineRequest.getColor());
-        LineResponse lineResponse = new LineResponse(line.getId(), line.getName(), line.getColor());
-        return ResponseEntity.ok().body(lineResponse);
+    public ResponseEntity<Void> updateLine(@PathVariable Long id, @RequestBody LineRequest lineRequest) {
+        lineService.updateLine(id, lineRequest.getName(), lineRequest.getColor());
+        return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/lines/{id}")
     public ResponseEntity<Void> deleteLine(@PathVariable Long id) {
-        LineDao.deleteById(id);
+        lineService.deleteLine(id);
         return ResponseEntity.noContent().build();
     }
 
