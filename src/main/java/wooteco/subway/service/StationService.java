a6929de0ad158ded3c4ee64e695eadb8928fb863
yeongunheo <gjduddns@gmail.com>
2022-05-04 17:23:05 +0900

    test: Service 테스트 작성

--- src/main/java/wooteco/subway/service/StationService.java
+++ src/main/java/wooteco/subway/service/StationService.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import wooteco.subway.dao.JdbcStationDao;
+import wooteco.subway.dao.StationDao;
 import wooteco.subway.domain.Station;
 import wooteco.subway.dto.StationRequest;
 import wooteco.subway.dto.StationResponse;
@@ -12,9 +12,9 @@ import wooteco.subway.dto.StationResponse;
 @Transactional
 @Service
 public class StationService {
-    private final JdbcStationDao stationDao;
+    private final StationDao stationDao;
 
-    public StationService(JdbcStationDao stationDao) {
+    public StationService(StationDao stationDao) {
         this.stationDao = stationDao;
     }
 
