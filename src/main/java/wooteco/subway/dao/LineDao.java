a746b1b91ee24b4b336c9590a6bc8e73b3c23f93
Juhyung990122 <harukawa99@naver.com>
2022-05-04 15:58:13 +0900

    refactor: line 저장 기능 h2 적용

--- src/main/java/wooteco/subway/dao/LineDao.java
+++ src/main/java/wooteco/subway/dao/LineDao.java
@@ -1,56 +1,79 @@
 package wooteco.subway.dao;
 
-import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Optional;
 
-import org.springframework.util.ReflectionUtils;
+import javax.sql.DataSource;
+
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RestController;
 
 import wooteco.subway.domain.Line;
-import wooteco.subway.dto.LineRequest;
 
+@Repository
 public class LineDao {
-    private static Long seq = 0L;
-    private static List<Line> lines = new ArrayList<>();
 
-    public static Line save(Line line) {
-        if (lines.contains(line)) {
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    private final SimpleJdbcInsert simpleJdbcInsert;
+
+    public LineDao(final DataSource dataSource) {
+        this.namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+        this.simpleJdbcInsert = new SimpleJdbcInsert(dataSource)
+                .withTableName("line")
+                .usingGeneratedKeyColumns("id");
+    }
+
+    public Line save(final String name, final String color) {
+        checkDuplicateName(name);
+        SqlParameterSource parameters = new MapSqlParameterSource("name", name)
+                .addValue("color", color);
+        Long id = simpleJdbcInsert.executeAndReturnKey(parameters).longValue();
+        return new Line(id, name, color);
+    }
+
+    private void checkDuplicateName(String name) {
+        if (findByName(name).isPresent()) {
             throw new IllegalArgumentException("이미 존재하는 노선입니다.");
         }
-        Line persistLine = createNewObject(line);
-        lines.add(persistLine);
-        return persistLine;
     }
 
-    private static Line createNewObject(Line line) {
-        Field field = ReflectionUtils.findField(Line.class, "id");
-        field.setAccessible(true);
-        ReflectionUtils.setField(field, line, ++seq);
-        return line;
+    private Optional<Line> findByName(String name) {
+        String sql = "SELECT name FROM line WHERE name = :name";
+        MapSqlParameterSource parameters = new MapSqlParameterSource("name", name);
+        try {
+            return Optional.ofNullable(namedParameterJdbcTemplate.queryForObject(sql, parameters, Line.class));
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
     }
 
-    public static List<Line> findAll() {
-        return lines;
+    public void deleteAll() {
+        String sql = "truncate table line";
+        namedParameterJdbcTemplate.update(sql, new MapSqlParameterSource());
     }
 
-    public static Line findById(final Long id) {
-         return lines.stream()
-                .filter(it -> it.getId() == id)
-                .findFirst()
-                .orElseThrow(()-> new IllegalArgumentException("해당 노선이 존재하지 않습니다."));
-    }
-
-    public static void deleteAll(){
-        seq = 0L;
-        lines = new ArrayList<>();
-    }
-
-    public static void deleteById(Long id) {
-        lines.remove(findById(id));
-    }
-
-    public static void update(Long id, LineRequest lineRequest) {
-        Line targetLine = LineDao.findById(id);
-        targetLine.update(lineRequest);
-    }
+    //
+    // public static List<Line> findAll() {
+    //     return lines;
+    // }
+    //
+    // public static Line findById(final Long id) {
+    //      return lines.stream()
+    //             .filter(it -> it.getId() == id)
+    //             .findFirst()
+    //             .orElseThrow(()-> new IllegalArgumentException("해당 노선이 존재하지 않습니다."));
+    // }
+    //
+    // public static void deleteById(Long id) {
+    //     lines.remove(findById(id));
+    // }
+    //
+    // public static void update(Long id, LineRequest lineRequest) {
+    //     Line targetLine = LineDao.findById(id);
+    //     targetLine.update(lineRequest);
+    // }
 }
--- src/main/java/wooteco/subway/ui/LineController.java
+++ src/main/java/wooteco/subway/ui/LineController.java
@@ -1,60 +1,58 @@
 package wooteco.subway.ui;
 
 import java.net.URI;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import wooteco.subway.dao.LineDao;
 import wooteco.subway.domain.Line;
-import wooteco.subway.dto.LineAndStationRequest;
 import wooteco.subway.dto.LineRequest;
 import wooteco.subway.dto.LineResponse;
 
 @RestController
 public class LineController {
 
+    private final LineDao lineDao;
+
+    public LineController(LineDao lineDao) {
+        this.lineDao = lineDao;
+    }
+
     @PostMapping("/lines")
-    public ResponseEntity<LineResponse> createLine(@RequestBody LineAndStationRequest lineRequest) {
-        Line line = new Line(lineRequest.getName(), lineRequest.getColor());
-        Line newLine = LineDao.save(line);
+    public ResponseEntity<LineResponse> createLine(@RequestBody LineRequest lineRequest) {
+        Line line = lineDao.save(lineRequest.getName(), lineRequest.getColor());
         LineResponse lineResponse = new LineResponse(line);
-        return ResponseEntity.created(URI.create("/lines/" + newLine.getId()))
+        return ResponseEntity.created(URI.create("/lines/" + line.getId()))
                 .body(lineResponse);
     }
 
-    @GetMapping("/lines")
-    public ResponseEntity<List<LineResponse>> findAllLine() {
-        List<Line> lines = LineDao.findAll();
-        List<LineResponse> lineResponses = lines.stream()
-                .map(LineResponse::new)
-                .collect(Collectors.toList());
-        return ResponseEntity.ok(lineResponses);
-    }
-
-    @GetMapping("/lines/{id}")
-    public ResponseEntity<LineResponse> findById(@PathVariable Long id) {
-        Line line = LineDao.findById(id);
-        LineResponse lineResponse = new LineResponse(line);
-        return ResponseEntity.ok(lineResponse);
-    }
-
-    @PutMapping("/lines/{id}")
-    public void update(@PathVariable Long id, @RequestBody LineRequest lineRequest) {
-        LineDao.update(id, lineRequest);
-    }
-
-    @DeleteMapping("/lines/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id) {
-        LineDao.deleteById(id);
-        return ResponseEntity.noContent().build();
-    }
+    // @GetMapping("/lines")
+    // public ResponseEntity<List<LineResponse>> findAllLine() {
+    //     List<Line> lines = LineDao.findAll();
+    //     List<LineResponse> lineResponses = lines.stream()
+    //             .map(LineResponse::new)
+    //             .collect(Collectors.toList());
+    //     return ResponseEntity.ok(lineResponses);
+    // }
+    //
+    // @GetMapping("/lines/{id}")
+    // public ResponseEntity<LineResponse> findById(@PathVariable Long id) {
+    //     Line line = LineDao.findById(id);
+    //     LineResponse lineResponse = new LineResponse(line);
+    //     return ResponseEntity.ok(lineResponse);
+    // }
+    //
+    // @PutMapping("/lines/{id}")
+    // public void update(@PathVariable Long id, @RequestBody LineRequest lineRequest) {
+    //     LineDao.update(id, lineRequest);
+    // }
+    //
+    // @DeleteMapping("/lines/{id}")
+    // public ResponseEntity<Void> delete(@PathVariable Long id) {
+    //     LineDao.deleteById(id);
+    //     return ResponseEntity.noContent().build();
+    // }
 }
