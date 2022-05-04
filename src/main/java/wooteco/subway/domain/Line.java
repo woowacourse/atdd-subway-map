1b6f84d46f38d775fab10cc247da85cfb4d57b30
Soomin Sohn <soomin.sohn.dev@gmail.com>
2022-05-04 16:00:00 +0900

    refactor: Line toString 제거

--- src/main/java/wooteco/subway/domain/Line.java
+++ src/main/java/wooteco/subway/domain/Line.java
@@ -48,13 +48,4 @@ public class Line {
     public int hashCode() {
         return Objects.hash(id, name, color);
     }
-
-    @Override
-    public String toString() {
-        return "Line{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", color='" + color + '\'' +
-                '}';
-    }
 }
