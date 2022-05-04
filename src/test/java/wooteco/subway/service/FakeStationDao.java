edd8b06e0b2d1089b77f06ae5489df236316a377
shindong96 <2006peter@naver.com>
2022-05-04 21:57:24 +0900

    test: StationServiceTest 생성 및 save 테스트 생성

---
+++ src/test/java/wooteco/subway/service/FakeStationDao.java
@@ -0,0 +1,53 @@
+package wooteco.subway.service;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.util.ReflectionUtils;
+
+import wooteco.subway.dao.StationDao;
+import wooteco.subway.domain.Station;
+
+public class FakeStationDao implements StationDao {
+    private Long seq = 0L;
+    private Map<Long, Station> stations = new LinkedHashMap<>();
+
+    @Override
+    public Station save(Station station) {
+        Station persistStation = createNewObject(station);
+        stations.put(persistStation.getId(), persistStation);
+        return persistStation;
+    }
+
+    private Station createNewObject(Station station) {
+        Field field = ReflectionUtils.findField(Station.class, "id");
+        field.setAccessible(true);
+        ReflectionUtils.setField(field, station, ++seq);
+        return station;
+    }
+
+    @Override
+    public boolean existByName(String name) {
+        return stations.values()
+            .stream()
+            .anyMatch(station -> station.getName().equals(name));
+    }
+
+    @Override
+    public List<Station> findAll() {
+        return new ArrayList<>(stations.values());
+    }
+
+    @Override
+    public void delete(Long id) {
+        stations.remove(id);
+    }
+
+    @Override
+    public boolean existById(Long id) {
+        return stations.containsKey(id);
+    }
+}
---
+++ src/test/java/wooteco/subway/service/StationServiceTest.java
@@ -0,0 +1,36 @@
+package wooteco.subway.service;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import wooteco.subway.dto.StationRequest;
+import wooteco.subway.dto.StationResponse;
+
+public class StationServiceTest {
+    private StationService stationService;
+
+    @BeforeEach
+    void setUp() {
+        stationService = new StationService(new FakeStationDao());
+    }
+
+    @DisplayName("지하철역을 생성한다.")
+    @Test
+    void createStation() {
+        StationRequest stationRequest = new StationRequest("강남역");
+        StationResponse stationResponse = stationService.save(stationRequest);
+        assertThat(stationResponse.getName()).isEqualTo(stationRequest.getName());
+    }
+
+    @DisplayName("중복된 이름의 지하철역을 생성하면 에러를 반환한다.")
+    @Test
+    void createStationWithDuplicateName() {
+        StationRequest stationRequest = new StationRequest("강남역");
+        stationService.save(stationRequest);
+        assertThatThrownBy(() -> stationService.save(stationRequest)).isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("중복된 지하철 역 이름입니다.");
+    }
+}
