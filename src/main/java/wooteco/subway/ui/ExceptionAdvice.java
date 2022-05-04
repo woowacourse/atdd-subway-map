f8f11a14f4ef6156b418b51d3ec5ff485125fe5f
nailseong <nnis9857@gmail.com>
2022-05-04 23:39:12 +0900

    feat(ExceptionAdvice): 로그 기록 기능

--- src/main/java/wooteco/subway/ui/ExceptionAdvice.java
+++ src/main/java/wooteco/subway/ui/ExceptionAdvice.java
@@ -1,5 +1,9 @@
 package wooteco.subway.ui;
 
+import java.time.LocalDateTime;
+import javax.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,6 +14,8 @@ import wooteco.subway.exception.NotFoundException;
 
 @ControllerAdvice(assignableTypes = {StationController.class, LineController.class})
 public class ExceptionAdvice {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @ExceptionHandler(value = {IllegalArgumentException.class})
     public ResponseEntity<ErrorResponse> handleException(final Exception e) {
@@ -22,7 +28,27 @@ public class ExceptionAdvice {
     }
 
     @ExceptionHandler(value = {InternalServerException.class})
-    public ResponseEntity<ErrorResponse> handleInternalServerException(final Exception e) {
+    public ResponseEntity<ErrorResponse> handleInternalServerException(final Exception e,
+                                                                       final HttpServletRequest request) {
+        log(e, request);
         return ResponseEntity.internalServerError().body(new ErrorResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler(value = {Exception.class})
+    public ResponseEntity<ErrorResponse> handleUnexpectedException(final Exception e,
+                                                                   final HttpServletRequest request) {
+        log(e, request);
+        return ResponseEntity.internalServerError().body(new ErrorResponse("서버가 요청을 처리할 수 없습니다."));
+    }
+
+    private void log(final Exception e, final HttpServletRequest request) {
+        String messageFormat = "[{}] {} {}";
+        logger.error(
+                messageFormat,
+                LocalDateTime.now(),
+                request.getMethod(),
+                request.getRequestURI(),
+                e
+        );
     }
 }
