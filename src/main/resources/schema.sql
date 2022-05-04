be6adc36950efc901011f719dd3e725b8ce77187
sinb57 <sinb57@naver.com>
2022-05-05 03:37:06 +0900

    chore: application.properties datasource.url 변경 및 h2 ddl 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,11 @@
-CREATE TABLE Line (
+CREATE TABLE IF NOT EXISTS Line (
     id integer not null auto_increment,
     name varchar(30) not null,
     color varchar(30) not null,
     primary key (id)
 );
 
-CREATE TABLE Station (
+CREATE TABLE IF NOT EXISTS Station (
     id integer not null auto_increment,
     name varchar(30) not null,
     primary key (id)
