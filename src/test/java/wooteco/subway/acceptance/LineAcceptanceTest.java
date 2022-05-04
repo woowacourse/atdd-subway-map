6f16a5da80e6c45ac0ecf1560e6fbb9dad919bc0
shindong96 <2006peter@naver.com>
2022-05-04 11:20:11 +0900

    feat: 이미 등록된 이름으로 지하철 노선 등록 요청 시 에러를 응답하는 기능 구현

--- src/test/java/wooteco/subway/acceptance/LineAcceptanceTest.java
+++ src/test/java/wooteco/subway/acceptance/LineAcceptanceTest.java
@@ -38,4 +38,33 @@ public class LineAcceptanceTest extends AcceptanceTest {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
         assertThat(response.header("Location")).isNotBlank();
     }
+
+    @DisplayName("기존에 존재하는 지하철 노선 이름으로 지하철역을 생성한다.")
+    @Test
+    void createStationWithDuplicateName() {
+        // given
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "2호선");
+        params.put("color", "green");
+        RestAssured.given().log().all()
+            .body(params)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .when()
+            .post("/lines")
+            .then().log().all()
+            .extract();
+
+        // when
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+            .body(params)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .when()
+            .post("/lines")
+            .then()
+            .log().all()
+            .extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
 }
