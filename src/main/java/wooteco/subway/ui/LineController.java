75a2020408eb7762e54c51197baba15002a596bb
asebn1 <asebn1@khu.ac.kr>
2022-05-04 11:50:35 +0900

    feat: 특정 조회 ATDD 및 기능 구현

--- src/main/java/wooteco/subway/ui/LineController.java
+++ src/main/java/wooteco/subway/ui/LineController.java
@@ -1,11 +1,9 @@
 package wooteco.subway.ui;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import wooteco.subway.dao.LineDao;
 import wooteco.subway.dao.StationDao;
 import wooteco.subway.domain.Station;
@@ -26,7 +24,16 @@ public class LineController {
         LineResponse lineResponse = LineService.createLine(lineRequest);
         return ResponseEntity.created(URI.create("/lines/" + lineResponse.getId())).body(lineResponse);
     }
-//
+
+    @GetMapping("/lines/{id}")
+    public ResponseEntity<LineResponse> showline(@PathVariable Long id) {
+        LineResponse lineInfos = LineService.findLineInfos(id);
+        return new ResponseEntity<>(
+                lineInfos,
+                HttpStatus.OK
+        );
+    }
+
 //    @GetMapping(value = "/lines", produces = MediaType.APPLICATION_JSON_VALUE)
 //    public ResponseEntity<List<LineResponse>> showStations() {
 //        List<Line> lines = LineDao.findAll();
--- src/test/java/wooteco/subway/acceptance/LineAcceptance.java
+++ src/test/java/wooteco/subway/acceptance/LineAcceptance.java
@@ -7,13 +7,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import wooteco.subway.dto.LineResponse;
-
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -69,6 +64,35 @@ public class LineAcceptance extends AcceptanceTest {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 
+    @DisplayName("특정 노선을 조회한다.")
+    @Test
+    void getLines() {
+        /// given
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "30호선");
+        params.put("color", "녹색");
+        ExtractableResponse<Response> createResponse = RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post("/lines")
+                .then().log().all()
+                .extract();
+        String id = createResponse.header("Location").split("/")[2];
+
+        // when
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .when()
+                .get("/lines/" + id)
+                .then().log().all()
+                .extract();
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.body().jsonPath().get("id").toString()).isEqualTo(id);
+        assertThat(response.body().jsonPath().get("name").toString()).isEqualTo("30호선");
+        assertThat(response.body().jsonPath().get("color").toString()).isEqualTo("녹색");
+    }
+
 //    @DisplayName("노선을 조회한다.")
 //    @Test
 //    void getLines() {
@@ -96,7 +120,7 @@ public class LineAcceptance extends AcceptanceTest {
 //        // when
 //        ExtractableResponse<Response> response = RestAssured.given().log().all()
 //                .when()
-//                .get("/lines/")
+//                .get("/lines")
 //                .then().log().all()
 //                .extract();
 //
