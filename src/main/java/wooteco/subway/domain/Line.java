f1765d6ed01131a5041104c9e7d000aa0251c6b8
cjlee38 <cjlee38@github.com>
2022-05-04 15:00:52 +0900

    feat(Line): 노선 객체 추가

---
+++ src/main/java/wooteco/subway/domain/Line.java
@@ -0,0 +1,34 @@
+package wooteco.subway.domain;
+
+public class Line {
+
+    private Long id;
+    private final String name;
+    private final String color;
+
+    public Line(Long id, String name, String color) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+    }
+
+    public Line(String name, String color) {
+        this(null, name, color);
+    }
+
+    public boolean hasSameNameWith(Line otherLine) {
+        return this.name.equals(otherLine.name);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
---
+++ src/test/java/wooteco/subway/domain/LineTest.java
@@ -0,0 +1,20 @@
+package wooteco.subway.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LineTest {
+
+    @Test
+    @DisplayName("이름이 같은지 확인한다.")
+    public void hasSameNameWith() {
+        // given
+        final Line line = new Line("신분당선", "bg-red-600");
+        // when
+        final boolean hasSameName = line.hasSameNameWith(new Line("신분당선", "bg-red-600"));
+        // then
+        assertThat(hasSameName).isTrue();
+    }
+}
