975ee41554d30a36f5a92890fa81b1fa3be9ff4b
pup-paw <wldusdl0310@gmail.com>
2022-05-04 15:37:49 +0900

    refactor: station 전체 조회 기능 h2 적용

--- src/test/java/wooteco/subway/dao/StationDaoTest.java
+++ src/test/java/wooteco/subway/dao/StationDaoTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestConstructor;
-import org.springframework.transaction.annotation.Transactional;
 
 import wooteco.subway.domain.Station;
 
