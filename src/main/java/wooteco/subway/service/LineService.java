f588dc55c4c18962fd7d2c87ef7ed887b1c57c38
pup-paw <wldusdl0310@gmail.com>
2022-05-04 18:45:40 +0900

    refactor: controller와 service 분리

---
+++ src/main/java/wooteco/subway/service/LineService.java
@@ -0,0 +1,47 @@
+package wooteco.subway.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import wooteco.subway.dao.LineDao;
+import wooteco.subway.domain.Line;
+import wooteco.subway.dto.LineRequest;
+import wooteco.subway.dto.LineResponse;
+
+@Service
+public class LineService {
+
+    private final LineDao lineDao;
+
+    public LineService(LineDao lineDao) {
+        this.lineDao = lineDao;
+    }
+
+    public LineResponse create(LineRequest lineRequest) {
+        Line line = lineDao.save(lineRequest.getName(), lineRequest.getColor());
+        return new LineResponse(line);
+    }
+
+    public List<LineResponse> findAll() {
+        List<Line> lines = lineDao.findAll();
+        return lines.stream()
+                .map(LineResponse::new)
+                .collect(Collectors.toList());
+    }
+
+    public LineResponse findById(Long id) {
+        Line line = lineDao.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 노선이 없습니다."));
+        return new LineResponse(line);
+    }
+
+    public void update(Long id, LineRequest lineRequest) {
+        lineDao.update(new Line(id, lineRequest.getName(), lineRequest.getColor()));
+    }
+
+    public void delete(Long id) {
+        lineDao.deleteById(id);
+    }
+}
