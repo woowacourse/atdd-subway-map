b2c3487d2166c20b5bfbf334e5093594d2b5ed12
sinb57 <sinb57@naver.com>
2022-05-05 04:26:01 +0900

    chore: ddl unique 속성 추가

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS Line (
     id integer not null auto_increment,
-    name varchar(30) not null,
-    color varchar(30) not null,
+    name varchar(30) not null unique,
+    color varchar(30) not null unique,
     primary key (id)
 );
 
 CREATE TABLE IF NOT EXISTS Station (
     id integer not null auto_increment,
-    name varchar(30) not null,
+    name varchar(30) not null unique,
     primary key (id)
 )
