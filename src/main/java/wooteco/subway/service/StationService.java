64f99d14f4118a767b49fabdce70619412076fa8
Hongdonggeon <ghd700@naver.com>
2022-05-04 16:28:23 +0900

    feat: Station에 관한 서비스 계층 추가

---
+++ src/main/java/wooteco/subway/service/StationService.java
@@ -0,0 +1,39 @@
+package wooteco.subway.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Service;
+import wooteco.subway.dao.JdbcLineDao;
+import wooteco.subway.dao.JdbcStationDao;
+import wooteco.subway.domain.Station;
+import wooteco.subway.dto.StationRequest;
+import wooteco.subway.dto.StationResponse;
+
+@Service
+public class StationService {
+
+    private final JdbcStationDao stationDao;
+    private final JdbcLineDao lineDao;
+
+    public StationService(JdbcStationDao stationDao, JdbcLineDao lineDao) {
+        this.stationDao = stationDao;
+        this.lineDao = lineDao;
+    }
+
+    public StationResponse createStation(StationRequest stationRequest) {
+        String name = stationRequest.getName();
+        return new StationResponse(stationDao.save(name), name);
+    }
+
+    public List<StationResponse> getStations() {
+        List<Station> stations = stationDao.findAll();
+        List<StationResponse> stationResponses = stations.stream()
+                .map(it -> new StationResponse(it.getId(), it.getName()))
+                .collect(Collectors.toList());
+        return stationResponses;
+    }
+
+    public void deleteStation(Long id) {
+        stationDao.deleteById(id);
+    }
+}
