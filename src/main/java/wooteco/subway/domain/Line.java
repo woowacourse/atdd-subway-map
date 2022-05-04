a9b39df086093776389088d00b82dde11037cbde
DOES <ldk980130@gmail.com>
2022-05-04 14:24:48 +0900

    refactor: 도메인 객체에 검증 메서드 추가

--- src/main/java/wooteco/subway/domain/Line.java
+++ src/main/java/wooteco/subway/domain/Line.java
@@ -1,23 +1,35 @@
 package wooteco.subway.domain;
 
+import java.util.List;
+import java.util.Objects;
+
 public class Line {
 
+	private static final String NULL_ERROR_MESSAGE = "빈 값이 들어올 수 없습니다.";
 	private static final long TEMPORARY_ID = 0L;
 
 	private final Long id;
 	private final String name;
 	private final String color;
 
+	public Line(String name, String color) {
+		this(TEMPORARY_ID, name, color);
+	}
+
 	public Line(Long id, String name, String color) {
+		List.of(id, name, color)
+			.forEach(this::validateNotNull);
 		this.id = id;
 		this.name = name;
 		this.color = color;
 	}
 
-	public Line(String name, String color) {
-		this.id = TEMPORARY_ID;
-		this.name = name;
-		this.color = color;
+	private <T> void validateNotNull(T object) {
+		try {
+			Objects.requireNonNull(object);
+		} catch (NullPointerException exception) {
+			throw new IllegalArgumentException(NULL_ERROR_MESSAGE);
+		}
 	}
 
 	public Long getId() {
--- src/main/java/wooteco/subway/domain/Station.java
+++ src/main/java/wooteco/subway/domain/Station.java
@@ -1,19 +1,32 @@
 package wooteco.subway.domain;
 
+import java.util.Objects;
+
 public class Station {
+
     private static final long TEMPORARY_ID = 0L;
+    private static final String NULL_ERROR_MESSAGE = "빈 값이 들어올 수 없습니다.";
 
     private final Long id;
     private final String name;
 
+    public Station(String name) {
+        this(TEMPORARY_ID, name);
+    }
+
     public Station(Long id, String name) {
+        validateNotNull(id);
+        validateNotNull(name);
         this.id = id;
         this.name = name;
     }
 
-    public Station(String name) {
-        this.id = TEMPORARY_ID;
-        this.name = name;
+    private <T> void validateNotNull(T object) {
+        try {
+            Objects.requireNonNull(object);
+        } catch (NullPointerException exception) {
+            throw new IllegalArgumentException(NULL_ERROR_MESSAGE);
+        }
     }
 
     public Long getId() {
