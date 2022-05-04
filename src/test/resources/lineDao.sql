719b9e8d06c3b1d0a0f71f3aa2bb6e868d929aa5
Chris <bjuuuu98@gmail.com>
2022-05-04 16:16:51 +0900

    style: sql 코드 포맷 수정

--- src/test/resources/lineDao.sql
+++ src/test/resources/lineDao.sql
@@ -2,22 +2,8 @@ drop table line if exists;
 
 create table if not exists LINE
 (
-    id
-    bigint
-    auto_increment
-    not
-    null,
-    name
-    varchar
-(
-    255
-) not null unique,
-    color varchar
-(
-    20
-) not null,
-    primary key
-(
-    id
-)
-    );
+    id bigint auto_increment not null,
+    name varchar(255) not null unique,
+    color varchar(20) not null,
+    primary key(id)
+);
--- src/test/resources/stationDao.sql
+++ src/test/resources/stationDao.sql
@@ -2,18 +2,7 @@ drop table station if exists;
 
 create table if not exists STATION
 (
-    id
-    bigint
-    auto_increment
-    not
-    null,
-    name
-    varchar
-(
-    255
-) not null unique,
-    primary key
-(
-    id
-)
-    );
+    id bigint auto_increment not null,
+    name varchar(255) not null unique,
+    primary key(id)
+);
