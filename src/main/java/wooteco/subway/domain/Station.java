4a858092f53cdcb823fbe649a737b5ce6985fbdb
injoon2019 <ison@sfu.ca>
2022-05-05 17:30:49 +0900

    refactor: equals/hashCode 재정의 삭제

--- src/main/java/wooteco/subway/domain/Station.java
+++ src/main/java/wooteco/subway/domain/Station.java
@@ -25,22 +25,5 @@ public class Station {
     public String getName() {
         return name;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Station station = (Station) o;
-        return Objects.equals(getId(), station.getId()) && Objects.equals(getName(), station.getName());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId(), getName());
-    }
 }
 
