025336e45b6853c2b47143f2076d00a6035c23b7
김찬 <gmelddl514@gmail.com>
2022-05-05 11:34:13 +0900

    style : schema.sql 컨벤션 수정

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,70 +1,24 @@
-DROP TABLE STATION IF EXISTS;
-DROP TABLE LINE IF EXISTS;
-
 create table if not exists STATION
 (
-    id
-    bigint
-    auto_increment
-    not
-    null,
-    name
-    varchar
-(
-    255
-) not null unique,
-    primary key
-(
-    id
-)
-    );
+    id   bigint auto_increment not null,
+    name varchar(255)          not null unique,
+    primary key (id)
+);
 
 create table if not exists LINE
 (
-    id
-    bigint
-    auto_increment
-    not
-    null,
-    name
-    varchar
-(
-    255
-) not null unique,
-    color varchar
-(
-    20
-) not null unique,
-    primary key
-(
-    id
-)
-    );
+    id    bigint auto_increment not null,
+    name  varchar(255)          not null unique,
+    color varchar(20)           not null unique,
+    primary key (id)
+);
 
 create table if not exists SECTION
 (
-    id
-    bigint
-    auto_increment
-    not
-    null,
-    line_id
-    bigint
-    not
-    null,
-    up_station_id
-    bigint
-    not
-    null,
-    down_station_id
-    bigint
-    not
-    null,
-    distance
-    int,
-    primary
-    key
-(
-    id
-)
-    );
+    id              bigint auto_increment not null,
+    line_id         bigint                not null,
+    up_station_id   bigint                not null,
+    down_station_id bigint                not null,
+    distance        int,
+    primary key (id)
+);
