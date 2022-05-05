bff58a3a45bdc6b468e30440e04f97f9277b7018
DOES <ldk980130@gmail.com>
2022-05-05 22:54:28 +0900

    fix: 없는 id로 조회시 500에러 대신 404 notFound 처리

--- src/main/java/wooteco/subway/controller/ExceptionManager.java
+++ src/main/java/wooteco/subway/controller/ExceptionManager.java
@@ -1,14 +1,21 @@
 package wooteco.subway.controller;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+import java.util.NoSuchElementException;
 
-@ControllerAdvice
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class ExceptionManager {
 
 	@ExceptionHandler(IllegalArgumentException.class)
-	public ResponseEntity<Void> handle() {
+	public ResponseEntity<Void> handleIllegalInput() {
 		return ResponseEntity.badRequest().build();
+	}
+
+	@ExceptionHandler(NoSuchElementException.class)
+	public ResponseEntity<Void> handleNoData() {
+		return ResponseEntity.notFound().build();
 	}
 }
