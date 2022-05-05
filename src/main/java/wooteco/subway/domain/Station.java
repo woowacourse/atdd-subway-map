be0f74a943535368200574a2e601e478f15a4684
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2022-05-05 16:53:38 +0900

    feat: Station 이름 검증 기능 구현

--- src/main/java/wooteco/subway/domain/Station.java
+++ src/main/java/wooteco/subway/domain/Station.java
@@ -1,5 +1,7 @@
 package wooteco.subway.domain;
 
+import java.util.Objects;
+
 public class Station {
 
     private Long id;
@@ -9,8 +11,21 @@ public class Station {
     }
 
     public Station(Long id, String name) {
+        validateName(name);
         this.id = id;
         this.name = name;
+    }
+
+    private void validateName(String name) {
+        Objects.requireNonNull(name, "이름은 Null 일 수 없습니다.");
+        validateNameLength(name);
+    }
+
+    private void validateNameLength(String name) {
+        int length = name.length();
+        if (length < 1 || length > 30) {
+            throw new IllegalArgumentException("이름은 1~30 자 이내여야 합니다.");
+        }
     }
 
     public Station(String name) {
