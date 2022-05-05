0b2888bdef9c9b756c8e927b4ebc42dd1b68df28
cjlee38 <cjlee38@github.com>
2022-05-05 11:28:19 +0900

    resource: 지하철 스키마 파일 추가

---
+++ src/test/resources/schema.sql
@@ -0,0 +1,31 @@
+DROP TABLE IF EXISTS section;
+DROP TABLE IF EXISTS station;
+DROP TABLE IF EXISTS line;
+
+CREATE TABLE station
+(
+    id   BIGINT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(255)          NOT NULL UNIQUE,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE line
+(
+    id    BIGINT AUTO_INCREMENT NOT NULL,
+    name  VARCHAR(255)          NOT NULL UNIQUE,
+    color VARCHAR(20)           NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE section
+(
+    id              BIGINT AUTO_INCREMENT NOT NULL,
+    line_id         BIGINT                NOT NULL,
+    up_station_id   BIGINT                NOT NULL,
+    down_station_id BIGINT                NOT NULL,
+    distance        INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (line_id) REFERENCES line (id),
+    FOREIGN KEY (up_station_id) REFERENCES station (id),
+    FOREIGN KEY (down_station_id) REFERENCES station (id)
+);
