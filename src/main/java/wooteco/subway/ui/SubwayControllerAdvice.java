bfeff56d8c39115e0ce1990374062324d95f2e18
summerlunaa <summerlunaa9@gmail.com>
2022-05-05 16:41:19 +0900

    refactor: errorHandle 메서드 반환 방식 변경

--- src/main/java/wooteco/subway/ui/SubwayControllerAdvice.java
+++ src/main/java/wooteco/subway/ui/SubwayControllerAdvice.java
@@ -4,7 +4,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.NoHandlerFoundException;
 import wooteco.subway.dto.ErrorResponse;
 
@@ -14,18 +13,18 @@ public class SubwayControllerAdvice {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleException() {
         ErrorResponse errorResponse = new ErrorResponse("잘못된 접근입니다.");
-        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+        return ResponseEntity.internalServerError().body(errorResponse);
     }
 
     @ExceptionHandler(NoHandlerFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ResponseEntity<String> handleNoHandlerFoundException() {
-        return new ResponseEntity<>("잘못된 주소입니다.", HttpStatus.NOT_FOUND);
+    public ResponseEntity<ErrorResponse> handleNoHandlerFoundException() {
+        ErrorResponse errorResponse = new ErrorResponse("잘못된 주소입니다.");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ErrorResponse> handleIllegalArgumentException(Exception exception) {
         ErrorResponse errorResponse = new ErrorResponse(exception.getMessage());
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+        return ResponseEntity.badRequest().body(errorResponse);
     }
 }
