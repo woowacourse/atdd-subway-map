57286825de73bc5e2967d3cca524e0b2613e5b49
Wishoon <heonga2@gmail.com>
2022-05-05 12:01:48 +0900

    fix: 사용되지 않는 생성자 제거

--- src/main/java/wooteco/subway/domain/Station.java
+++ src/main/java/wooteco/subway/domain/Station.java
@@ -6,9 +6,6 @@ public class Station {
     private Long id;
     private String name;
 
-    public Station() {
-    }
-
     public Station(Long id, String name) {
         this.id = id;
         this.name = name;
