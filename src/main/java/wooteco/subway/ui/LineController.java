a41817c4f0931631bc4ed584642e44581fe1fab9
YJGwon <yj970125@gmail.com>
2022-05-06 15:13:48 +0900

    refactor(subway/ui): LineService 분리
    - Controller로부터 domain과 dao에 대한 의존 제거
    - 요청 처리 및 응답 생성 과정 캡슐화

--- src/main/java/wooteco/subway/ui/LineController.java
+++ src/main/java/wooteco/subway/ui/LineController.java
@@ -2,10 +2,10 @@ package wooteco.subway.ui;
 
 import java.net.URI;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,60 +14,42 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import wooteco.subway.dao.LineDao;
-import wooteco.subway.domain.Line;
 import wooteco.subway.dto.LineRequest;
 import wooteco.subway.dto.LineResponse;
 
 @RestController
 public class LineController {
+    private final LineService lineService;
 
-    private final LineDao lineDao;
-
-    public LineController(LineDao lineDao) {
-        this.lineDao = lineDao;
+    public LineController(LineService lineService) {
+        this.lineService = lineService;
     }
 
     @PostMapping("/lines")
     public ResponseEntity<LineResponse> createLine(@RequestBody LineRequest lineRequest) {
-        Line line = new Line(lineRequest.getName(), lineRequest.getColor());
-        Line createdLine = lineDao.save(line);
-        LineResponse lineResponse = new LineResponse(
-                createdLine.getId(),
-                createdLine.getName(),
-                createdLine.getColor()
-        );
-        return ResponseEntity.created(URI.create("/lines/" + createdLine.getId())).body(lineResponse);
+        LineResponse lineResponse = lineService.create(lineRequest);
+        return ResponseEntity.created(URI.create("/lines/" + lineResponse.getId())).body(lineResponse);
     }
 
     @GetMapping(value = "/lines", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<LineResponse>> showLines() {
-        final List<Line> lines = lineDao.findAll();
-        return ResponseEntity.ok().body(
-                lines.stream()
-                        .map(line -> new LineResponse(line.getId(), line.getName(), line.getColor()))
-                        .collect(Collectors.toList())
-        );
+        return ResponseEntity.ok().body(lineService.findAll());
     }
 
     @GetMapping("/lines/{id}")
     public ResponseEntity<LineResponse> showLine(@PathVariable Long id) {
-        final Line line = lineDao.findById(id);
-        return ResponseEntity.ok().body(
-                new LineResponse(line.getId(), line.getName(), line.getColor())
-        );
+        return ResponseEntity.ok().body(lineService.findById(id));
     }
 
     @PutMapping("/lines/{id}")
     public ResponseEntity<Void> modifyLine(@PathVariable Long id, @RequestBody LineRequest lineRequest) {
-        final Line line = new Line(id, lineRequest.getName(), lineRequest.getColor());
-        lineDao.update(line);
+        lineService.modify(id, lineRequest);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/lines/{id}")
     public ResponseEntity<Void> deleteLine(@PathVariable Long id) {
-        lineDao.delete(id);
+        lineService.delete(id);
         return ResponseEntity.noContent().build();
     }
 }
---
+++ src/main/java/wooteco/subway/ui/LineService.java
@@ -0,0 +1,49 @@
+package wooteco.subway.ui;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import wooteco.subway.dao.LineDao;
+import wooteco.subway.domain.Line;
+import wooteco.subway.dto.LineRequest;
+import wooteco.subway.dto.LineResponse;
+
+@Service
+public class LineService {
+    private final LineDao lineDao;
+
+    public LineService(LineDao lineDao) {
+        this.lineDao = lineDao;
+    }
+
+    public LineResponse create(LineRequest lineRequest) {
+        String name = lineRequest.getName();
+        String color = lineRequest.getColor();
+        Line line = new Line(name, color);
+        Line createdLine = lineDao.save(line);
+        return new LineResponse(createdLine.getId(), name, color);
+    }
+
+    public List<LineResponse> findAll() {
+        final List<Line> lines = lineDao.findAll();
+        return lines.stream()
+                .map(line -> new LineResponse(line.getId(), line.getName(), line.getColor()))
+                .collect(Collectors.toList());
+    }
+
+    public LineResponse findById(Long id) {
+        final Line line = lineDao.findById(id);
+        return new LineResponse(line.getId(), line.getName(), line.getColor());
+    }
+
+    public void modify(Long id, LineRequest lineRequest) {
+        final Line line = new Line(id, lineRequest.getName(), lineRequest.getColor());
+        lineDao.update(line);
+    }
+
+    public void delete(Long id) {
+        lineDao.delete(id);
+    }
+}
