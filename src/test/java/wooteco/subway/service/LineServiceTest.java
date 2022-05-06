5c0a821bf81025d06db701cde5c01f23bd29ba60
seong-wooo <seongwoo0513@uos.ac.kr>
2022-05-06 17:55:03 +0900

    refactor: serviceTest의 dao를 jdbcDao로 변경

--- src/test/java/wooteco/subway/service/LineServiceTest.java
+++ src/test/java/wooteco/subway/service/LineServiceTest.java
@@ -2,26 +2,32 @@ package wooteco.subway.service;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import wooteco.subway.dao.InmemoryLineDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import wooteco.subway.dao.JdbcLineDao;
 import wooteco.subway.domain.Line;
 
+@JdbcTest
 class LineServiceTest {
 
-    private final InmemoryLineDao inmemoryLineDao = InmemoryLineDao.getInstance();
-    private final LineService lineService = new LineService(inmemoryLineDao);
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
-    @AfterEach
-    void afterEach() {
-        inmemoryLineDao.clear();
+    private LineService lineService;
+    
+    @BeforeEach
+    void setUp() {
+        this.lineService = new LineService(new JdbcLineDao(jdbcTemplate));
     }
 
     @Test
     @DisplayName("이미 존재하는 노선의 이름이 있을 때 예외가 발생한다.")
     void saveExceptionByExistName() {
-        inmemoryLineDao.save(new Line("신분당선", "bg-red-600"));
+        lineService.save(new Line("신분당선", "bg-red-600"));
         assertThatThrownBy(() -> lineService.save(new Line("신분당선", "bg-green-600")))
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessage("이미 존재하는 노선 이름입니다.");
--- src/test/java/wooteco/subway/service/StationServiceTest.java
+++ src/test/java/wooteco/subway/service/StationServiceTest.java
@@ -2,26 +2,32 @@ package wooteco.subway.service;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import wooteco.subway.dao.InmemoryStationDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import wooteco.subway.dao.JdbcStationDao;
 import wooteco.subway.domain.Station;
 
+@JdbcTest
 class StationServiceTest {
 
-    private final InmemoryStationDao inmemoryStationDao = InmemoryStationDao.getInstance();
-    private final StationService stationService = new StationService(inmemoryStationDao);
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
-    @AfterEach
-    void afterEach() {
-        inmemoryStationDao.clear();
+    private StationService stationService;
+
+    @BeforeEach
+    void setUp() {
+        this.stationService = new StationService(new JdbcStationDao(jdbcTemplate));
     }
 
     @Test
     @DisplayName("이미 존재하는 역 이름이 있을 때 예외가 발생한다.")
     void saveExceptionByDuplicatedName() {
-        inmemoryStationDao.save(new Station("오리"));
+        stationService.save(new Station("오리"));
         assertThatThrownBy(() -> stationService.save(new Station("오리")))
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessage("이미 존재하는 역 이름입니다.");
