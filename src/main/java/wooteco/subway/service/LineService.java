24b86270f37aee6b690f37695ec8822181b41f04
is2js <tingstyle1@gmail.com>
2022-05-06 21:57:15 +0900

    feat: service에서 2개 이상의 로직 수행시 transactional 애노테이션 적용, 전체 조회에 대해서는 readonly 적용하여 성능 최적화

--- src/main/java/wooteco/subway/service/LineService.java
+++ src/main/java/wooteco/subway/service/LineService.java
@@ -2,6 +2,7 @@ package wooteco.subway.service;
 
 import java.util.List;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import wooteco.subway.dao.LineDao;
 import wooteco.subway.domain.Line;
 import wooteco.subway.dto.LineRequest;
@@ -21,6 +22,7 @@ public class LineService {
         return lineDao.save(line);
     }
 
+    @Transactional(readOnly = true)
     public List<Line> findAll() {
         return lineDao.findAll();
     }
@@ -30,12 +32,14 @@ public class LineService {
             .orElseThrow(() -> new LineNotFoundException("해당 노선이 없습니다."));
     }
 
+    @Transactional
     public void update(final Long id, final LineRequest lineRequest) {
         findById(id);
         final Line updatedLine = lineRequest.toEntity(id);
         lineDao.update(updatedLine);
     }
 
+    @Transactional
     public void delete(final Long id) {
         final Line targetLine = findById(id);
         lineDao.delete(targetLine);
--- src/main/java/wooteco/subway/service/StationService.java
+++ src/main/java/wooteco/subway/service/StationService.java
@@ -2,6 +2,7 @@ package wooteco.subway.service;
 
 import java.util.List;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import wooteco.subway.dao.StationDao;
 import wooteco.subway.domain.Station;
 import wooteco.subway.dto.StationRequest;
@@ -21,10 +22,12 @@ public class StationService {
         return stationDao.save(station);
     }
 
+    @Transactional(readOnly = true)
     public List<Station> show() {
         return stationDao.findAll();
     }
 
+    @Transactional
     public void delete(final Long id) {
         final Station targetLine = stationDao.findById(id)
             .orElseThrow(() -> new StationNotFoundException("이미 존재하는 지하철역 이름입니다."));
