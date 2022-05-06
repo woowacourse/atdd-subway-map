e78dbb49d63c1dd50ee64b177b8843f64a1c3e5e
seong-wooo <seongwoo0513@uos.ac.kr>
2022-05-06 22:58:08 +0900

    refactor: serviceTest에 @SpringBootTest 사용

--- src/test/java/wooteco/subway/service/LineServiceTest.java
+++ src/test/java/wooteco/subway/service/LineServiceTest.java
@@ -2,27 +2,17 @@ package wooteco.subway.service;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
-import org.springframework.jdbc.core.JdbcTemplate;
-import wooteco.subway.dao.JdbcLineDao;
+import org.springframework.boot.test.context.SpringBootTest;
 import wooteco.subway.domain.Line;
 
-@JdbcTest
+@SpringBootTest
 class LineServiceTest {
 
     @Autowired
-    private JdbcTemplate jdbcTemplate;
-
     private LineService lineService;
-    
-    @BeforeEach
-    void setUp() {
-        this.lineService = new LineService(new JdbcLineDao(jdbcTemplate));
-    }
 
     @Test
     @DisplayName("이미 존재하는 노선의 이름이 있을 때 예외가 발생한다.")
--- src/test/java/wooteco/subway/service/StationServiceTest.java
+++ src/test/java/wooteco/subway/service/StationServiceTest.java
@@ -2,28 +2,18 @@ package wooteco.subway.service;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
-import org.springframework.jdbc.core.JdbcTemplate;
-import wooteco.subway.dao.JdbcStationDao;
+import org.springframework.boot.test.context.SpringBootTest;
 import wooteco.subway.domain.Station;
 
-@JdbcTest
+@SpringBootTest
 class StationServiceTest {
 
     @Autowired
-    private JdbcTemplate jdbcTemplate;
-
     private StationService stationService;
-
-    @BeforeEach
-    void setUp() {
-        this.stationService = new StationService(new JdbcStationDao(jdbcTemplate));
-    }
-
+    
     @Test
     @DisplayName("이미 존재하는 역 이름이 있을 때 예외가 발생한다.")
     void saveExceptionByDuplicatedName() {
