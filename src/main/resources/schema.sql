13f7d518569308415e23f53f08682ee39a1a500a
DOES <ldk980130@gmail.com>
2022-05-07 00:19:05 +0900

    feat: SECTION 테이블 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -12,3 +12,13 @@ create table if not exists LINE
     color varchar(20) not null,
     primary key(id)
 );
+
+create table if not exists SECTION
+(
+    id bigint auto_increment not null,
+    line_id bigint not null,
+    up_station_id bigint not null,
+    down_station_id bigint not null,
+    distance int,
+    primary key(id)
+);
