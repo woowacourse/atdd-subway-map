5c32f98c20ea46207aa3aa1b6300aec52d4ce236
awesomeo184 <awesomeo184@gmail.com>
2022-05-06 11:32:06 +0900

    refactor: URI 상수화

--- src/main/java/wooteco/subway/ui/LineController.java
+++ src/main/java/wooteco/subway/ui/LineController.java
@@ -1,5 +1,7 @@
 package wooteco.subway.ui;
 
+import static wooteco.subway.ui.LineController.LINES_API_URI;
+
 import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import wooteco.subway.dao.LineDao;
 import wooteco.subway.domain.Line;
@@ -17,7 +20,10 @@ import wooteco.subway.dto.LineRequest;
 import wooteco.subway.dto.LineResponse;
 
 @RestController
+@RequestMapping(LINES_API_URI)
 public class LineController {
+
+    public static final String LINES_API_URI = "/lines";
 
     private final LineDao lineDao;
 
@@ -25,15 +31,15 @@ public class LineController {
         this.lineDao = lineDao;
     }
 
-    @PostMapping("/lines")
+    @PostMapping
     public ResponseEntity<LineResponse> createLine(@RequestBody LineRequest lineRequest) {
         Line line = new Line(lineRequest.getName(), lineRequest.getColor());
         Long id = lineDao.save(line);
         LineResponse lineResponse = new LineResponse(id, line.getName(), line.getColor());
-        return ResponseEntity.created(URI.create("/lines/" + id)).body(lineResponse);
+        return ResponseEntity.created(URI.create(LINES_API_URI + "/" + id)).body(lineResponse);
     }
 
-    @GetMapping("/lines")
+    @GetMapping
     public ResponseEntity<List<LineResponse>> showLines() {
         List<Line> lines = lineDao.findAll();
         List<LineResponse> lineResponses = lines.stream()
@@ -42,20 +48,20 @@ public class LineController {
         return ResponseEntity.ok().body(lineResponses);
     }
 
-    @GetMapping("/lines/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<LineResponse> showLine(@PathVariable Long id) {
         Line line = lineDao.findById(id);
         LineResponse lineResponse = new LineResponse(id, line.getName(), line.getColor());
         return ResponseEntity.ok().body(lineResponse);
     }
 
-    @PutMapping(value = "/lines/{id}")
+    @PutMapping(value = "/{id}")
     public ResponseEntity<Void> updateLine(@PathVariable Long id, @RequestBody LineRequest lineRequest) {
         lineDao.update(id, lineRequest.getName(), lineRequest.getColor());
         return ResponseEntity.ok().build();
     }
 
-    @DeleteMapping("/lines/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteById(@PathVariable Long id) {
         lineDao.deleteById(id);
         return ResponseEntity.noContent().build();
--- src/main/java/wooteco/subway/ui/StationController.java
+++ src/main/java/wooteco/subway/ui/StationController.java
@@ -1,5 +1,7 @@
 package wooteco.subway.ui;
 
+import static wooteco.subway.ui.StationController.STATIONS_API_URI;
+
 import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import wooteco.subway.dao.StationDao;
 import wooteco.subway.domain.Station;
@@ -16,7 +19,10 @@ import wooteco.subway.dto.StationRequest;
 import wooteco.subway.dto.StationResponse;
 
 @RestController
+@RequestMapping(STATIONS_API_URI)
 public class StationController {
+
+    public static final String STATIONS_API_URI = "/stations";
 
     private final StationDao stationDao;
 
@@ -24,15 +30,15 @@ public class StationController {
         this.stationDao = stationDao;
     }
 
-    @PostMapping("/stations")
+    @PostMapping
     public ResponseEntity<StationResponse> createStation(@RequestBody StationRequest stationRequest) {
         Station station = new Station(stationRequest.getName());
         Long id = stationDao.save(station);
         StationResponse stationResponse = new StationResponse(id, station.getName());
-        return ResponseEntity.created(URI.create("/stations/" + id)).body(stationResponse);
+        return ResponseEntity.created(URI.create(STATIONS_API_URI + "/" + id)).body(stationResponse);
     }
 
-    @GetMapping("/stations")
+    @GetMapping
     public ResponseEntity<List<StationResponse>> showStations() {
         List<Station> stations = stationDao.findAll();
         List<StationResponse> stationResponses = stations.stream()
@@ -41,7 +47,7 @@ public class StationController {
         return ResponseEntity.ok().body(stationResponses);
     }
 
-    @DeleteMapping("/stations/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteStation(@PathVariable Long id) {
         stationDao.deleteById(id);
         return ResponseEntity.noContent().build();
