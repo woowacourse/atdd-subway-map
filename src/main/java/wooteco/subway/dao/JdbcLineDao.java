16b66e3285f74be0dc36110e5df89efaa9b55c2c
nailseong <nnis9857@gmail.com>
2022-05-06 18:27:51 +0900

    refactor(JdbcLineDao): INSERT 쿼리를 SimpleJdbcInsert를 활용하도록 변경

--- src/main/java/wooteco/subway/dao/JdbcLineDao.java
+++ src/main/java/wooteco/subway/dao/JdbcLineDao.java
@@ -1,16 +1,16 @@
 package wooteco.subway.dao;
 
 import java.lang.reflect.Field;
-import java.sql.PreparedStatement;
 import java.util.List;
 import java.util.Objects;
+import javax.sql.DataSource;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.ReflectionUtils;
 import wooteco.subway.domain.Line;
@@ -28,8 +28,13 @@ public class JdbcLineDao implements LineDao {
         return setId(line, resultSet.getLong("id"));
     };
 
-    public JdbcLineDao(final JdbcTemplate jdbcTemplate) {
+    private SimpleJdbcInsert simpleJdbcInsert;
+
+    public JdbcLineDao(final JdbcTemplate jdbcTemplate, final DataSource dataSource) {
         this.jdbcTemplate = jdbcTemplate;
+        this.simpleJdbcInsert = new SimpleJdbcInsert(dataSource)
+                .withTableName("LINE")
+                .usingGeneratedKeyColumns("id");
     }
 
     private Line setId(final Line line, final long id) {
@@ -42,18 +47,9 @@ public class JdbcLineDao implements LineDao {
     @Override
     public Line save(final Line line) {
         try {
-            final String sql = "INSERT INTO line SET name = ?, color = ?";
+            final SqlParameterSource parameters = new BeanPropertySqlParameterSource(line);
+            final long id = simpleJdbcInsert.executeAndReturnKey(parameters).longValue();
 
-            final KeyHolder keyHolder = new GeneratedKeyHolder();
-            final PreparedStatementCreator statementCreator = con -> {
-                final PreparedStatement prepareStatement = con.prepareStatement(sql, new String[]{"id"});
-                prepareStatement.setString(1, line.getName());
-                prepareStatement.setString(2, line.getColor());
-                return prepareStatement;
-            };
-
-            jdbcTemplate.update(statementCreator, keyHolder);
-            final long id = Objects.requireNonNull(keyHolder.getKey()).longValue();
             return setId(line, id);
         } catch (final DuplicateKeyException e) {
             throw new IllegalArgumentException("중복된 이름의 노선은 저장할 수 없습니다.");
--- src/test/java/wooteco/subway/dao/DaoTest.java
+++ src/test/java/wooteco/subway/dao/DaoTest.java
@@ -5,15 +5,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class DaoTest {
-
-    @Autowired
-    protected JdbcTemplate jdbcTemplate;
 
     @Autowired
     protected StationDao stationDao;
