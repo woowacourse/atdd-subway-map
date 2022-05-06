8333ec20947445edac988d823be5de392e4e7cdd
awesomeo184 <awesomeo184@gmail.com>
2022-05-06 15:33:03 +0900

    refactor(test): 불필요한 애너테이션 제거

--- src/test/java/wooteco/subway/dao/LineJdbcDaoTest.java
+++ src/test/java/wooteco/subway/dao/LineJdbcDaoTest.java
@@ -10,13 +10,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.transaction.annotation.Transactional;
 import wooteco.subway.dao.jdbc.LineJdbcDao;
 import wooteco.subway.domain.Line;
 import wooteco.subway.exception.DuplicateLineException;
 import wooteco.subway.exception.NoSuchLineException;
 
-@Transactional
 @JdbcTest
 class LineJdbcDaoTest {
 
--- src/test/java/wooteco/subway/dao/StationJdbcDaoTest.java
+++ src/test/java/wooteco/subway/dao/StationJdbcDaoTest.java
@@ -10,12 +10,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.transaction.annotation.Transactional;
 import wooteco.subway.dao.jdbc.StationJdbcDao;
 import wooteco.subway.domain.Station;
 import wooteco.subway.exception.DuplicateStationNameException;
 
-@Transactional
 @JdbcTest
 class StationJdbcDaoTest {
 
