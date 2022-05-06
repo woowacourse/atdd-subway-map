a343906b85e933bef0008188916a5be69532a995
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-05-06 15:48:22 +0900

    feat: Section 스키마 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS STATION;
 DROP TABLE IF EXISTS LINE;
+DROP TABLE IF EXISTS SECTION;
 
 CREATE TABLE STATION
 (
@@ -13,5 +14,15 @@ CREATE TABLE LINE
     id    bigint auto_increment not null,
     name  varchar(255)          not null unique,
     color varchar(20)           not null,
+    primary key (id)
+);
+
+create table SECTION
+(
+    id              bigint auto_increment not null,
+    line_id         bigint                not null,
+    up_station_id   bigint                not null,
+    down_station_id bigint                not null,
+    distance        int,
     primary key (id)
 );
