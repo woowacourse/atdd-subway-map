ef6f91274fad4e4991883bf65be076e9bafd3f2b
brorae <jhr3239@gmail.com>
2022-05-06 16:49:29 +0900

    refactor: ExceptionHandler 추가

--- src/main/java/wooteco/subway/ui/ControllerExceptionHandler.java
+++ src/main/java/wooteco/subway/ui/ControllerExceptionHandler.java
@@ -1,5 +1,7 @@
 package wooteco.subway.ui;
 
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -7,8 +9,18 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
+    @ExceptionHandler(DuplicateKeyException.class)
+    public ResponseEntity<String> handleDuplicateKey() {
+        return ResponseEntity.badRequest().body("이름 혹은 색은 중복될 수 없습니다.");
+    }
+
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public ResponseEntity<String> handleDataNotFound() {
+        return ResponseEntity.badRequest().body("해당 데이터를 조회할 수 없습니다.");
+    }
+
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> errorResponse(Exception e) {
+    public ResponseEntity<String> handleInvalidArgument(Exception e) {
         return ResponseEntity.badRequest().body(e.getMessage());
     }
 }
