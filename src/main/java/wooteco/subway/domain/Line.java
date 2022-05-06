d2713351dbd71210c7b885b6a0ffe9366974bdcb
shindong96 <2006peter@naver.com>
2022-05-06 12:19:09 +0900

    feat: Line 이름과 색상의 길이 관련 정보 검증

--- src/main/java/wooteco/subway/domain/Line.java
+++ src/main/java/wooteco/subway/domain/Line.java
@@ -6,14 +6,30 @@ public class Line {
     private String color;
 
     public Line(Long id, String name, String color) {
+        validateNameSize(name);
+        validateColorSize(color);
         this.id = id;
         this.name = name;
         this.color = color;
     }
 
     public Line(String name, String color) {
+        validateNameSize(name);
+        validateColorSize(color);
         this.name = name;
         this.color = color;
+    }
+
+    private void validateNameSize(String name) {
+        if (name == null || name.isBlank() || name.length() > 255) {
+            throw new IllegalArgumentException("존재할 수 없는 이름입니다.");
+        }
+    }
+
+    private void validateColorSize(String color) {
+        if (color == null || color.isBlank() || color.length() > 20) {
+            throw new IllegalArgumentException("존재할 수 없는 색상입니다.");
+        }
     }
 
     public Long getId() {
