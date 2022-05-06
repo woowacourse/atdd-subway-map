7c82b8ecf7375ffa973c57fd0cc6bbcda1204b6e
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-05-07 02:20:35 +0900

    feat: Sections에서 정렬된 Stations 반환 기능 추가 및 테스트

--- src/main/java/wooteco/subway/domain/Sections.java
+++ src/main/java/wooteco/subway/domain/Sections.java
@@ -2,6 +2,7 @@ package wooteco.subway.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class Sections {
 
@@ -16,5 +17,40 @@ public class Sections {
         if (this.sections.isEmpty()) {
             throw new IllegalArgumentException("sections는 크기가 0으로는 생성할 수 없습니다.");
         }
+    }
+
+    public List<Station> calculateSortedStations() {
+        Section startSection = sections.stream()
+                .findAny()
+                .orElseThrow(() -> new NoSuchElementException("section을 찾을 수 없습니다."));
+
+        Section firstSection = calculateFirstSection(startSection);
+        List<Station> stations = new ArrayList<>();
+        stations.add(firstSection.getUpStation());
+
+        while (sections.stream().anyMatch(firstSection::isDownSection)) {
+            stations.add(firstSection.getDownStation());
+            firstSection = nextSection(firstSection);
+        }
+        stations.add(firstSection.getDownStation());
+        return stations;
+    }
+
+    private Section calculateFirstSection(final Section section) {
+        if (sections.stream()
+                .noneMatch(section::isUpSection)) {
+            return section;
+        }
+        return calculateFirstSection(sections.stream()
+                .filter(section::isUpSection)
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException("section을 찾을 수 없습니다.")));
+    }
+
+    private Section nextSection(final Section section) {
+        return sections.stream()
+                .filter(section::isDownSection)
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException("section을 찾을 수 없습니다."));
     }
 }
--- src/test/java/wooteco/subway/domain/SectionsTest.java
+++ src/test/java/wooteco/subway/domain/SectionsTest.java
@@ -1,8 +1,10 @@
 package wooteco.subway.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,5 +16,25 @@ class SectionsTest {
         assertThatThrownBy(() -> new Sections(new ArrayList<>()))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("sections는 크기가 0으로는 생성할 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("정렬된 Station을 반환할 수 있다.")
+    void calculateSortedStations() {
+        // given
+        Line line = new Line(1L, "신분당선", "bg-red-600");
+        Station station1 = new Station(1L, "오리");
+        Station station2 = new Station(2L, "배카라");
+        Station station3 = new Station(3L, "오카라");
+        Station station4 = new Station(4L, "배리");
+        Sections sections = new Sections(List.of(new Section(3L, line, station3, station4, 4),
+                new Section(1L, line, station1, station2, 1),
+                new Section(2L, line, station2, station3, 2)));
+
+        // when
+        List<Station> stations = sections.calculateSortedStations();
+
+        // then
+        assertThat(stations).containsExactly(station1, station2, station3, station4);
     }
 }
