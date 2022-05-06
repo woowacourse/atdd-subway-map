0068d5df498f714bf1bdc22ca2d16c0a23502999
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-05-06 20:34:42 +0900

    feat: SectionService @Service 어노테이션 추가

--- src/main/java/wooteco/subway/service/SectionService.java
+++ src/main/java/wooteco/subway/service/SectionService.java
@@ -1,8 +1,10 @@
 package wooteco.subway.service;
 
+import org.springframework.stereotype.Service;
 import wooteco.subway.dao.SectionDao;
 import wooteco.subway.domain.Section;
 
+@Service
 public class SectionService {
 
     private final SectionDao sectionDao;
