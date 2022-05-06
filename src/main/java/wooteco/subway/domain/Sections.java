d06b90831bdaec66a0742ae1ed471709647047b7
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-05-07 02:26:27 +0900

    refactor: calculateSortedStations 메소드 분리

--- src/main/java/wooteco/subway/domain/Sections.java
+++ src/main/java/wooteco/subway/domain/Sections.java
@@ -20,31 +20,50 @@ public class Sections {
     }
 
     public List<Station> calculateSortedStations() {
-        Section startSection = sections.stream()
+        Section section = calculateFirstSection(findAnySection());
+        return createSortedStations(section);
+    }
+
+    private Section findAnySection() {
+        return sections.stream()
                 .findAny()
                 .orElseThrow(() -> new NoSuchElementException("section을 찾을 수 없습니다."));
-
-        Section firstSection = calculateFirstSection(startSection);
-        List<Station> stations = new ArrayList<>();
-        stations.add(firstSection.getUpStation());
-
-        while (sections.stream().anyMatch(firstSection::isDownSection)) {
-            stations.add(firstSection.getDownStation());
-            firstSection = nextSection(firstSection);
-        }
-        stations.add(firstSection.getDownStation());
-        return stations;
     }
 
     private Section calculateFirstSection(final Section section) {
-        if (sections.stream()
-                .noneMatch(section::isUpSection)) {
+        if (!hasUpSection(section)) {
             return section;
         }
-        return calculateFirstSection(sections.stream()
+        return calculateFirstSection(calculateUpSection(section));
+    }
+
+    private boolean hasUpSection(final Section section) {
+        return sections.stream()
+                .anyMatch(section::isUpSection);
+    }
+
+    private Section calculateUpSection(final Section section) {
+        return sections.stream()
                 .filter(section::isUpSection)
                 .findFirst()
-                .orElseThrow(() -> new NoSuchElementException("section을 찾을 수 없습니다.")));
+                .orElseThrow(() -> new NoSuchElementException("section을 찾을 수 없습니다."));
+    }
+
+    private List<Station> createSortedStations(Section section) {
+        List<Station> stations = new ArrayList<>();
+        stations.add(section.getUpStation());
+
+        while (hasDownSection(section)) {
+            stations.add(section.getDownStation());
+            section = nextSection(section);
+        }
+        stations.add(section.getDownStation());
+        return stations;
+    }
+
+    private boolean hasDownSection(final Section section) {
+        return sections.stream()
+                .anyMatch(section::isDownSection);
     }
 
     private Section nextSection(final Section section) {
