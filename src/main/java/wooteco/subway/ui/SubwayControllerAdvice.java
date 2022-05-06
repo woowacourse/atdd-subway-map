773f67b71d5865aed316c2db8ece25c2b2249264
yhh1056 <yhh1056@naver.com>
2022-05-07 01:35:10 +0900

    refactor: 런타임 에러에 대한 클라이언트 응답 수정

--- src/main/java/wooteco/subway/ui/SubwayControllerAdvice.java
+++ src/main/java/wooteco/subway/ui/SubwayControllerAdvice.java
@@ -1,5 +1,8 @@
 package wooteco.subway.ui;
 
+import java.util.Arrays;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,23 +14,29 @@ import wooteco.subway.exception.NotFoundException;
 @ControllerAdvice
 public class SubwayControllerAdvice {
 
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<ErrorResponse> badRequestExceptionHandler(BadRequestException e) {
+        log.debug(e.getMessage());
         return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
     }
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ErrorResponse> notFoundExceptionHandler(NotFoundException e) {
+        log.debug(e.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(e.getMessage()));
     }
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<ErrorResponse> runtimeExceptionHandler(RuntimeException e) {
-        return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
+        log.warn(e.getMessage());
+        return ResponseEntity.badRequest().body(new ErrorResponse("서버 에러가 발생했습니다."));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> exceptionHandler(Exception e) {
+        log.error(e.getMessage());
         return ResponseEntity.internalServerError().body(new ErrorResponse("서버 에러가 발생했습니다."));
     }
 }
