a24aab60588f441e0b200862bb0e08a9ccc00a16
wilgur513 <wilgur513@naver.com>
2022-05-06 15:15:14 +0900

    chore: schema.sql 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,39 +1,17 @@
-create table if not exists STATION
-(
-    id
-    bigint
-    auto_increment
-    not
-    null,
-    name
-    varchar
-(
-    255
-) not null unique,
-    primary key
-(
-    id
-)
-    );
+drop table if exists STATION;
+drop table if exists LINE;
 
-create table if not exists LINE
+create table STATION
 (
-    id
-    bigint
-    auto_increment
-    not
-    null,
-    name
-    varchar
+    id   bigint auto_increment not null,
+    name varchar(255) not null unique,
+    primary key (id)
+);
+
+create table LINE
 (
-    255
-) not null unique,
-    color varchar
-(
-    20
-) not null,
-    primary key
-(
-    id
-)
-    );
+    id    bigint auto_increment not null,
+    name  varchar(255) not null unique,
+    color varchar(20)  not null,
+    primary key (id)
+);
