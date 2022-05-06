f4a1c70c6775786577c3c9dd375ad1b390af7d31
seong-wooo <seongwoo0513@uos.ac.kr>
2022-05-06 17:36:39 +0900

    refactor: 변수명 변경

--- src/test/java/wooteco/subway/dao/InmemoryLineDaoTest.java
+++ src/test/java/wooteco/subway/dao/InmemoryLineDaoTest.java
@@ -11,18 +11,18 @@ import wooteco.subway.domain.Line;
 
 class InmemoryLineDaoTest {
 
-    private final InmemoryLineDao inmemoryLineDao = InmemoryLineDao.getInstance();
+    private final InmemoryLineDao lineDao = InmemoryLineDao.getInstance();
 
     @AfterEach
     void afterEach() {
-        inmemoryLineDao.clear();
+        lineDao.clear();
     }
 
     @Test
     @DisplayName("Line을 등록할 수 있다.")
     void save() {
         Line line = new Line("신분당선", "bg-red-600");
-        Line savedLine = inmemoryLineDao.save(line);
+        Line savedLine = lineDao.save(line);
 
         assertThat(savedLine.getId()).isNotNull();
     }
@@ -30,8 +30,8 @@ class InmemoryLineDaoTest {
     @Test
     @DisplayName("Line을 id로 조회할 수 있다.")
     void findById() {
-        Line line = inmemoryLineDao.save(new Line("신분당선", "bg-red-600"));
-        Line findLine = inmemoryLineDao.findById(line.getId());
+        Line line = lineDao.save(new Line("신분당선", "bg-red-600"));
+        Line findLine = lineDao.findById(line.getId());
 
         assertThat(findLine).isEqualTo(line);
     }
@@ -39,25 +39,25 @@ class InmemoryLineDaoTest {
     @Test
     @DisplayName("Line 전체 조회할 수 있다.")
     void findAll() {
-        inmemoryLineDao.save(new Line("신분당선", "bg-red-600"));
-        inmemoryLineDao.save(new Line("분당선", "bg-green-600"));
+        lineDao.save(new Line("신분당선", "bg-red-600"));
+        lineDao.save(new Line("분당선", "bg-green-600"));
 
-        assertThat(inmemoryLineDao.findAll()).hasSize(2);
+        assertThat(lineDao.findAll()).hasSize(2);
     }
 
     @Test
     @DisplayName("Line 이름이 존재하는지 확인할 수 있다.")
     void existByName() {
-        inmemoryLineDao.save(new Line("신분당선", "bg-red-600"));
+        lineDao.save(new Line("신분당선", "bg-red-600"));
 
-        assertThat(inmemoryLineDao.existByName("신분당선")).isTrue();
+        assertThat(lineDao.existByName("신분당선")).isTrue();
     }
 
     @Test
     @DisplayName("Line을 수정할 수 있다.")
     void update() {
-        Line line = inmemoryLineDao.save(new Line("신분당선", "bg-red-600"));
-        int result = inmemoryLineDao.update(new Line(line.getId(), "분당선", line.getColor()));
+        Line line = lineDao.save(new Line("신분당선", "bg-red-600"));
+        int result = lineDao.update(new Line(line.getId(), "분당선", line.getColor()));
 
         assertThat(result).isEqualTo(1);
     }
@@ -65,18 +65,18 @@ class InmemoryLineDaoTest {
     @Test
     @DisplayName("Line을 삭제할 수 있다.")
     void delete() {
-        Line line = inmemoryLineDao.save(new Line("신분당선", "bg-red-600"));
+        Line line = lineDao.save(new Line("신분당선", "bg-red-600"));
 
-        assertThatCode(() -> inmemoryLineDao.delete(line.getId())).doesNotThrowAnyException();
+        assertThatCode(() -> lineDao.delete(line.getId())).doesNotThrowAnyException();
     }
 
     @Test
     @DisplayName("없는 id의 Line을 삭제할 수 없다.")
     void deleteByInvalidId() {
-        Line line = inmemoryLineDao.save(new Line("신분당선", "bg-red-600"));
+        Line line = lineDao.save(new Line("신분당선", "bg-red-600"));
         Long lineId = line.getId() + 1;
 
-        assertThatThrownBy(() -> inmemoryLineDao.delete(lineId))
+        assertThatThrownBy(() -> lineDao.delete(lineId))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("없는 line 입니다.");
     }
@@ -84,11 +84,11 @@ class InmemoryLineDaoTest {
     @Test
     @DisplayName("이미 삭제한 id의 Line 삭제할 수 없다.")
     void deleteByDuplicatedId() {
-        Line line = inmemoryLineDao.save(new Line("신분당선", "bg-red-600"));
+        Line line = lineDao.save(new Line("신분당선", "bg-red-600"));
         Long lineId = line.getId();
-        inmemoryLineDao.delete(lineId);
+        lineDao.delete(lineId);
 
-        assertThatThrownBy(() -> inmemoryLineDao.delete(lineId))
+        assertThatThrownBy(() -> lineDao.delete(lineId))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("없는 line 입니다.");
     }
