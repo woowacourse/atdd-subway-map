a4af30f37595df56332e8f4743b474fa69b78b95
Seongwon97 <osw970325@naver.com>
2022-05-06 15:28:46 +0900

    refactor: 불필요한 메서드 제거

--- src/main/java/wooteco/subway/domain/Line.java
+++ src/main/java/wooteco/subway/domain/Line.java
@@ -27,16 +27,8 @@ public class Line {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getColor() {
         return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
     }
 
     @Override
