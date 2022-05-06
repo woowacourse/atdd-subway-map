67df4112ae537f932317c080bc02b0d438c59e31
nailseong <nnis9857@gmail.com>
2022-05-06 17:30:41 +0900

    refactor(NameTest): 이름 관련 테스트 이동

--- src/test/java/wooteco/subway/domain/NameTest.java
+++ src/test/java/wooteco/subway/domain/NameTest.java
@@ -7,20 +7,20 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-class StationTest {
+class NameTest {
 
     @ParameterizedTest
-    @DisplayName("역 이름이 공백이면 예외가 발생한다")
+    @DisplayName("이름이 공백이면 예외가 발생한다")
     @ValueSource(strings = {"", " ", "    "})
-    void newStation_blankName(final String name) {
-        assertThatThrownBy(() -> new Station(name))
+    void NewStation_BlankName_ExceptionThrown(final String value) {
+        assertThatThrownBy(() -> new Station(value))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("이름이 공백이 되어서는 안됩니다.");
     }
 
     @Test
-    @DisplayName("역 이름이 허용된 길이를 넘으면 예외가 발생한다.")
-    void newStation_nameLength() {
+    @DisplayName("이름이 허용된 길이를 넘으면 예외가 발생한다.")
+    void NewStation_NameLength_ExceptionThrown() {
         // given
         final String name = "1234567890123456";
 
@@ -28,6 +28,5 @@ class StationTest {
         assertThatThrownBy(() -> new Station(name))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("이름이 15자를 넘어서는 안됩니다.");
-
     }
 }
