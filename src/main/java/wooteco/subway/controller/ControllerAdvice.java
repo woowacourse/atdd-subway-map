6b59f587074c4a9e52ce6c5275d12bf083390ba5
asebn1 <asebn1@khu.ac.kr>
2022-05-07 08:41:14 +0900

    refactor: StationAdvice를 ControllerAdvice로 네이밍 변경

--- src/main/java/wooteco/subway/controller/ControllerAdvice.java
+++ src/main/java/wooteco/subway/controller/ControllerAdvice.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.NoSuchElementException;
 
 @RestControllerAdvice
-public class StationAdvice {
+public class ControllerAdvice {
 
     @ExceptionHandler({IllegalArgumentException.class, NoSuchElementException.class})
     public ResponseEntity<String> handle(Exception e){
