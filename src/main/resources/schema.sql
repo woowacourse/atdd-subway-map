df60505a12f52045e98e55d1204ea84befb259e7
kth990303 <kth990303@naver.com>
2022-05-08 01:46:08 +0900

    refactor: `section` 테이블에서 외래키를 가지도록 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -20,5 +20,6 @@ create table if not exists SECTION
     up_station_id   bigint                not null,
     down_station_id bigint                not null,
     distance        int,
-    primary key (id)
+    primary key (id),
+    constraint line_id foreign key (line_id) references LINE (id) on delete cascade
 );
