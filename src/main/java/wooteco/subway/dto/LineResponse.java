96eb1ab193faf86e84b2bf1db25928e354686e77
hyeonic <dev.hyeonic@gmail.com>
2022-05-07 17:16:20 +0900

    feat: dto 불필요 생성자 제거

--- src/main/java/wooteco/subway/dto/LineResponse.java
+++ src/main/java/wooteco/subway/dto/LineResponse.java
@@ -6,20 +6,10 @@ import wooteco.subway.domain.Line;
 
 public class LineResponse {
 
-    private Long id;
-    private String name;
-    private String color;
-    private List<StationResponse> stations;
-
-    private LineResponse() {
-    }
-
-    public LineResponse(Long id, String name, String color, List<StationResponse> stations) {
-        this.id = id;
-        this.name = name;
-        this.color = color;
-        this.stations = stations;
-    }
+    private final Long id;
+    private final String name;
+    private final String color;
+    private final List<StationResponse> stations;
 
     public LineResponse(Line line) {
         this.id = line.getId();
--- src/main/java/wooteco/subway/dto/StationResponse.java
+++ src/main/java/wooteco/subway/dto/StationResponse.java
@@ -4,11 +4,8 @@ import wooteco.subway.domain.Station;
 
 public class StationResponse {
 
-    private Long id;
-    private String name;
-
-    private StationResponse() {
-    }
+    private final Long id;
+    private final String name;
 
     public StationResponse(Station station) {
         this.id = station.getId();
