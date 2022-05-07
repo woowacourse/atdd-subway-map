f8bbe4641b1ba6ec0eee78414d3063be08a8b81f
BeomsooKim <kbsat@naver.com>
2022-05-08 01:41:44 +0900

    feat: SECTION 테이블에 외래키 추가 및 not null 추가

--- src/test/resources/schema-test.sql
+++ src/test/resources/schema-test.sql
@@ -23,6 +23,9 @@ create table if not exists SECTION
     line_id bigint not null,
     up_station_id bigint not null,
     down_station_id bigint not null,
-    distance int,
-    primary key(id)
+    distance int not null,
+    primary key(id),
+    foreign key (line_id) references LINE (id) on delete cascade,
+    foreign key (up_station_id) references STATION (id),
+    foreign key (down_station_id) references STATION (id)
 );
