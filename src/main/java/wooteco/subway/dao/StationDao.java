6bb736ab0576f2b8c002d073b48f91e52ef0dde0
skullKim <kyk990328@gmail.com>
2022-05-07 20:22:41 +0900

    feat(StationDao.java): 예외 추가
    핸들링이 필요한 예외 추가
    
    BREAKING CHANGE: 역 삭제와 중복된 역 저장에 대한 예외 추가

--- src/main/java/wooteco/subway/dao/StationDao.java
+++ src/main/java/wooteco/subway/dao/StationDao.java
@@ -4,14 +4,21 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import javax.sql.DataSource;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import wooteco.subway.domain.Station;
+import wooteco.subway.exception.LineDuplicateException;
+import wooteco.subway.exception.NoLineFoundException;
+import wooteco.subway.exception.NoStationFoundException;
+import wooteco.subway.exception.StationDuplicateException;
 
 @Repository
 public class StationDao {
+
+    private static final int NO_ROW_AFFECTED = 0;
 
     private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
     private final SimpleJdbcInsert simpleInsert;
@@ -26,7 +33,12 @@ public class StationDao {
     public Station save(Station station) {
         final Map<String, Object> params = new HashMap<>();
         params.put("name", station.getName());
-        final Long id = simpleInsert.executeAndReturnKey(params).longValue();
+        final Long id;
+        try {
+            id = simpleInsert.executeAndReturnKey(params).longValue();
+        } catch (DuplicateKeyException e) {
+            throw new StationDuplicateException(station.toString());
+        }
         return new Station(id, station.getName());
     }
 
@@ -39,6 +51,9 @@ public class StationDao {
 
     public void deleteById(Long id) {
         final String sql = "delete from STATION where id = :id";
-        namedParameterJdbcTemplate.update(sql, Map.of("id", id));
+        final int theNumberOfAffectedRow = namedParameterJdbcTemplate.update(sql, Map.of("id", id));
+        if (theNumberOfAffectedRow == NO_ROW_AFFECTED) {
+            throw new NoStationFoundException("id=" + id);
+        }
     }
 }
