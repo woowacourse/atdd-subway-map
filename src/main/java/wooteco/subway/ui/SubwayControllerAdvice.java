71c4763d1d5cc678704906fc9ddc47a974c54ceb
김찬 <gmelddl514@gmail.com>
2022-05-07 10:22:34 +0900

    feat : 예외 처리 추가

--- src/main/java/wooteco/subway/ui/SubwayControllerAdvice.java
+++ src/main/java/wooteco/subway/ui/SubwayControllerAdvice.java
@@ -2,11 +2,28 @@ package wooteco.subway.ui;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
 public class SubwayControllerAdvice {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleUnexpected(Exception e) {
+        return new ResponseEntity<>("예상하지 못한 에러가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<String> handle(Exception e) {
+        return new ResponseEntity<>("페이지를 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleInvalidArgument() {
+        return new ResponseEntity<>("이름이나 색깔이 공백입니다.", HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleBadRequest(Exception e) {
