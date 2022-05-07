d0a988ff23316c06868cc1a15ed5d45c95bf5eb8
hyewoncc <hyewoncc@gmail.com>
2022-05-07 18:16:57 +0900

    feat: 도메인 기본 검증(필드값 존재여부) 추가

--- src/main/java/wooteco/subway/domain/Line.java
+++ src/main/java/wooteco/subway/domain/Line.java
@@ -10,14 +10,30 @@ public class Line {
     }
 
     public Line(Long id, String name, String color) {
+        validateNameNotEmpty(name);
+        validateColorNotEmpty(color);
         this.id = id;
         this.name = name;
         this.color = color;
     }
 
     public Line(String name, String color) {
+        validateNameNotEmpty(name);
+        validateColorNotEmpty(color);
         this.name = name;
         this.color = color;
+    }
+
+    private void validateNameNotEmpty(String name) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("이름은 비워둘 수 없습니다.");
+        }
+    }
+
+    private void validateColorNotEmpty(String color) {
+        if (color.isBlank()) {
+            throw new IllegalArgumentException("색상은 비워둘 수 없습니다.");
+        }
     }
 
     public Long getId() {
--- src/main/java/wooteco/subway/domain/Station.java
+++ src/main/java/wooteco/subway/domain/Station.java
@@ -8,16 +8,24 @@ public class Station {
     }
 
     public Station(Long id, String name) {
+        validateNameNotEmpty(name);
         this.id = id;
         this.name = name;
     }
 
     public Station(String name) {
+        validateNameNotEmpty(name);
         this.name = name;
     }
 
     public boolean isSameName(Station station) {
         return this.name.equals(station.name);
+    }
+
+    private void validateNameNotEmpty(String name) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("이름은 비워둘 수 없습니다.");
+        }
     }
 
     public Long getId() {
