d92edea319a47c6072bb72526e89083e909adedf
김찬 <gmelddl514@gmail.com>
2022-05-07 18:13:23 +0900

    feat : 중복 제거를 위한 fixture 생성

---
+++ src/test/java/wooteco/subway/acceptance/TestFixtures.java
@@ -0,0 +1,46 @@
+package wooteco.subway.acceptance;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.springframework.http.MediaType;
+import wooteco.subway.dto.LineRequest;
+
+public class TestFixtures {
+
+    public static ExtractableResponse<Response> extractPostResponse(LineRequest lineRequest) {
+        return RestAssured.given().log().all()
+                .body(lineRequest)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post("/lines")
+                .then().log().all()
+                .extract();
+    }
+
+    public static ExtractableResponse<Response> extractGetResponse(String uri) {
+        return RestAssured.given().log().all()
+                .when()
+                .get(uri)
+                .then().log().all()
+                .extract();
+    }
+
+    public static ExtractableResponse<Response> extractDeleteResponse(String uri) {
+        return RestAssured.given().log().all()
+                .when()
+                .delete(uri)
+                .then().log().all()
+                .extract();
+    }
+
+    public static ExtractableResponse<Response> extractPutResponse(LineRequest updateLineRequest, String uri) {
+        return RestAssured.given().log().all()
+                .body(updateLineRequest)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .put(uri)
+                .then().log().all()
+                .extract();
+    }
+}
