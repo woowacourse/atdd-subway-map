cde93bfd7cfda63246be0d4b03a48dda32d7f600
nailseong <nnis9857@gmail.com>
2022-05-07 16:34:42 +0900

    refactor(ExceptionAdvice): 불필요한 코드 삭제

--- src/main/java/wooteco/subway/ui/ExceptionAdvice.java
+++ src/main/java/wooteco/subway/ui/ExceptionAdvice.java
@@ -1,54 +1,44 @@
 package wooteco.subway.ui;
 
-import java.time.LocalDateTime;
-import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import wooteco.subway.dto.ErrorResponse;
 import wooteco.subway.exception.SubwayMapException;
 
-@ControllerAdvice(assignableTypes = {StationController.class, LineController.class})
+@RestControllerAdvice
 public class ExceptionAdvice {
 
-    private static final String LOG_MESSAGE_FORMAT = "[{}] {} {}";
     private static final String INTERNAL_SERVER_ERROR_MESSAGE = "서버가 요청을 처리할 수 없습니다.";
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @ExceptionHandler(value = {IllegalArgumentException.class})
+    @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ErrorResponse> handleException(final Exception e) {
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(ErrorResponse.from(e));
     }
 
-    @ExceptionHandler(value = {SubwayMapException.class})
+    @ExceptionHandler(SubwayMapException.class)
     public ResponseEntity<ErrorResponse> handleSubWayMapException(final SubwayMapException e) {
         return ResponseEntity
                 .status(e.getHttpStatus())
                 .body(ErrorResponse.from(e));
     }
 
-    @ExceptionHandler(value = {Exception.class})
-    public ResponseEntity<ErrorResponse> handleUnexpectedException(final Exception e,
-                                                                   final HttpServletRequest request) {
-        log(e, request);
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleUnexpectedException(final Exception e) {
+        logger.error(
+                "{}",
+                e.getMessage(),
+                e
+        );
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(ErrorResponse.from(INTERNAL_SERVER_ERROR_MESSAGE));
-    }
-
-    private void log(final Exception e, final HttpServletRequest request) {
-        logger.error(
-                LOG_MESSAGE_FORMAT,
-                LocalDateTime.now(),
-                request.getMethod(),
-                request.getRequestURI(),
-                e
-        );
     }
 }
