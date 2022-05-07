c26685637947b6da4ee12ee47761715915961630
Richard JEON <ztzy1907@gmail.com>
2022-05-07 14:37:20 +0900

    fix(test): @JdbcTest 와 함께 중복으로 선언된 @Transactional 애너테이션 제거

--- src/test/java/wooteco/subway/dao/LineRepositoryTest.java
+++ src/test/java/wooteco/subway/dao/LineRepositoryTest.java
@@ -14,11 +14,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.transaction.annotation.Transactional;
 import wooteco.subway.dao.entity.LineEntity;
 
 @JdbcTest
-@Transactional
 class LineRepositoryTest {
 
     @Autowired
