3060c0db5b1876998c37735737b22da9aad03536
김찬 <gmelddl514@gmail.com>
2022-05-07 15:00:05 +0900

    refactor : 생성자 체이닝

--- src/main/java/wooteco/subway/domain/Line.java
+++ src/main/java/wooteco/subway/domain/Line.java
@@ -9,17 +9,16 @@ public class Line {
     public Line() {
     }
 
-    public Line(String name, String color) {
-        this.name = name;
-        this.color = color;
-    }
-
     public Line(Long id, String name, String color) {
         this.id = id;
         this.name = name;
         this.color = color;
     }
-    
+
+    public Line(String name, String color) {
+        this(null, name, color);
+    }
+
     public Long getId() {
         return id;
     }
