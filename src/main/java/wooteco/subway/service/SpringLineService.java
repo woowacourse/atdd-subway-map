aeb8af2302c2176029cdc1af2472917b305f735f
skullKim <kyk990328@gmail.com>
2022-05-07 15:06:53 +0900

    refactor(SpringLineService.java): 트랜잭션 추가
    트랜잭션 롤백을 적용하기 위해 @Transactional 어노테이션 추가
    
    BREAKING CHANGE: 디비를 접근하는 메서드에 @Transactional 어노테이션 추가
    as-is:
    @Service
    public class SpringLineService implements LineService {
    
        ...
    
        @Override
        public Line save(LineRequest lineRequest) {
            ...
        }
    
        @Override
        public Line findById(Long id) {
            ...
        }
    
        @Override
        public List<Line> findAll() {
            ...
        }
    
        @Override
        public void update(Long id, LineRequest lineRequest) {
            ...
        }
    
        @Override
        public void deleteById(Long id) {
            ...
        }
    }
    
    to-be:
    @Service
    public class SpringLineService implements LineService {
    
        ...
    
        @Override
        @Transactional
        public Line save(LineRequest lineRequest) {
            ...
        }
    
        @Override
        @Transactional(readOnly = true)
        public Line findById(Long id) {
            ...
        }
    
        @Override
        @Transactional(readOnly = true)
        public List<Line> findAll() {
            ...
        }
    
        @Override
        @Transactional
        public void update(Long id, LineRequest lineRequest) {
            ...
        }
    
        @Override
        @Transactional
        public void deleteById(Long id) {
            ...
        }
    }

--- src/main/java/wooteco/subway/service/SpringLineService.java
+++ src/main/java/wooteco/subway/service/SpringLineService.java
@@ -2,6 +2,7 @@ package wooteco.subway.service;
 
 import java.util.List;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import wooteco.subway.dao.LineDao;
 import wooteco.subway.domain.Line;
 import wooteco.subway.dto.LineRequest;
@@ -16,27 +17,32 @@ public class SpringLineService implements LineService {
     }
 
     @Override
+    @Transactional
     public Line save(LineRequest lineRequest) {
         Line line = new Line(lineRequest.getName(), lineRequest.getColor());
         return lineDao.save(line);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Line findById(Long id) {
         return lineDao.findById(id);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<Line> findAll() {
         return lineDao.findAll();
     }
 
     @Override
+    @Transactional
     public void update(Long id, LineRequest lineRequest) {
         lineDao.update(id, lineRequest);
     }
 
     @Override
+    @Transactional
     public void deleteById(Long id) {
         lineDao.deleteById(id);
     }
