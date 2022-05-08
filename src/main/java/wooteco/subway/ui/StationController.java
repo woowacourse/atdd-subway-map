b2cb4a107a142eca190f6825929f626435439106
Soomin Sohn <soomin.sohn.dev@gmail.com>
2022-05-08 15:27:38 +0900

    refactor(StationController): request to domain, domain to response 생성 위치 변경

--- src/main/java/wooteco/subway/ui/StationController.java
+++ src/main/java/wooteco/subway/ui/StationController.java
@@ -29,17 +29,17 @@ public class StationController {
 
     @PostMapping
     public ResponseEntity<StationResponse> createStation(@RequestBody StationRequest stationRequest) {
-        Station station = new Station(stationRequest.getName());
+        Station station = stationRequest.toEntity();
         Station newStation = stationService.save(station);
-        StationResponse stationResponse = new StationResponse(newStation.getId(), newStation.getName());
-        return ResponseEntity.created(URI.create("/stations/" + newStation.getId())).body(stationResponse);
+        StationResponse stationResponse = StationResponse.from(newStation);
+        return ResponseEntity.created(URI.create("/stations/" + stationResponse.getId())).body(stationResponse);
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<StationResponse>> showStations() {
         List<Station> stations = stationService.findAll();
         List<StationResponse> stationResponses = stations.stream()
-                .map(it -> new StationResponse(it.getId(), it.getName()))
+                .map(StationResponse::from)
                 .collect(Collectors.toList());
         return ResponseEntity.ok().body(stationResponses);
     }
