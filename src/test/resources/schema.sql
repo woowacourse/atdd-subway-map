d4d75a436771173dfa0a1dea09a49fb1e3c05188
seong-wooo <seongwoo0513@uos.ac.kr>
2022-05-08 19:21:08 +0900

    feat: 3단계에 맞게 schema.sql 추가

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,11 +1,15 @@
-CREATE TABLE if not exists STATION
+drop table "SECTION" if exists;
+drop table LINE if exists;
+drop table STATION if exists;
+
+CREATE TABLE IF NOT EXISTS STATION
 (
     id   bigint auto_increment not null,
     name varchar(255)          not null unique,
     primary key (id)
-    );
+);
 
-CREATE TABLE if not exists LINE
+CREATE TABLE IF NOT EXISTS LINE
 (
     id    bigint auto_increment not null,
     name  varchar(255)          not null unique,
@@ -25,5 +29,5 @@ CREATE TABLE IF NOT EXISTS "SECTION"
     foreign key (line_id) references LINE(id),
     foreign key (up_station_id) references STATION(id),
     foreign key (down_station_id) references STATION(id)
-    );
+);
 
