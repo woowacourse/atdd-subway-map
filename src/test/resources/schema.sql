7205cca5a8414f294eb098e45ade1107bec5f5f5
= <rerub0831@gmail.com>
2022-05-08 19:31:29 +0900

    feat: Line DB schema에 역 아이디, 거리 추가

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -14,6 +14,9 @@ create table if not exists LINE
     id bigint auto_increment not null,
     name varchar(255) not null unique,
     color varchar(20) not null unique,
+    upStationId bigint not null,
+    downStationId bigint not null,
+    distance int,
     primary key(id)
 );
 
