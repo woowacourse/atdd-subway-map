49b677451956fbd34f2f788d181e21d699203ee9
yangdongjue5510 <djwhy5510@gmail.com>
2022-05-08 17:45:39 +0900

    refactor(StationService): 중복되는 이름으로 역 생성시 다른 예외로 전환

--- src/main/java/wooteco/subway/dao/JdbcStationDao.java
+++ src/main/java/wooteco/subway/dao/JdbcStationDao.java
@@ -1,14 +1,12 @@
 package wooteco.subway.dao;
 
 import java.lang.reflect.Field;
-import java.sql.PreparedStatement;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
-import org.springframework.dao.DuplicateKeyException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.ReflectionUtils;
 import wooteco.subway.domain.Station;
@@ -20,28 +18,24 @@ public class JdbcStationDao implements StationDao {
 
     private static final String COLUMN_NAME = "name";
     private static final String COLUMN_ID = "id";
+    private static final String TABLE_NAME = "station";
 
     private final JdbcTemplate jdbcTemplate;
+    private final SimpleJdbcInsert stationInserter;
 
     public JdbcStationDao(final JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+        this.stationInserter = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName(TABLE_NAME)
+                .usingGeneratedKeyColumns(COLUMN_ID);
     }
 
     @Override
     public Station save(Station station) {
-        try {
-            final String sql = "INSERT INTO station SET name = ?";
-            KeyHolder keyHolder = new GeneratedKeyHolder();
-            jdbcTemplate.update(con -> {
-                PreparedStatement prepareStatement = con.prepareStatement(sql, new String[]{COLUMN_ID});
-                prepareStatement.setString(1, station.getName());
-                return prepareStatement;
-            }, keyHolder);
-            final long id = Objects.requireNonNull(keyHolder.getKey()).longValue();
-            return setId(station, id);
-        } catch (DuplicateKeyException e) {
-            throw new IllegalArgumentException(ExceptionMessage.DUPLICATED_STATION_NAME.getContent());
-        }
+        final Map<String, String> params = Map.of(COLUMN_NAME, station.getName());
+
+        long savedId = stationInserter.executeAndReturnKey(params).longValue();
+        return setId(station, savedId);
     }
 
     @Override
--- src/main/java/wooteco/subway/service/StationService.java
+++ src/main/java/wooteco/subway/service/StationService.java
@@ -2,11 +2,13 @@ package wooteco.subway.service;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 import wooteco.subway.dao.StationDao;
 import wooteco.subway.domain.Station;
 import wooteco.subway.dto.StationRequest;
 import wooteco.subway.dto.StationResponse;
+import wooteco.subway.exception.ExceptionMessage;
 
 @Service
 public class StationService {
@@ -19,8 +21,13 @@ public class StationService {
 
     public StationResponse create(StationRequest request) {
         Station station = new Station(request.getName());
-        Station savedStation = stationDao.save(station);
-        return StationResponse.of(savedStation);
+
+        try {
+            Station savedStation = stationDao.save(station);
+            return StationResponse.of(savedStation);
+        } catch (DuplicateKeyException e) {
+            throw new IllegalArgumentException(ExceptionMessage.DUPLICATED_STATION_NAME.getContent());
+        }
     }
 
     public List<StationResponse> findAll() {
