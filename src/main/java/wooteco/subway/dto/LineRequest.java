2767012801b61b2a7f67dee5d0d479fae72a026b
skullKim <kyk990328@gmail.com>
2022-05-08 18:24:49 +0900

    refactor(LineRequest.java): 필요 없는 로직 삭제
    당장은 사용하지 않는 필드와 메서드 삭제
    
    BREAKING CHANGE: 상,하행 종점, 거리에 대한 정보를 나타내는 필드와
    getter삭제
    as-is
    public class LineRequest {
        ...
        private Long upStationId;
        private Long downStationId;
        private int distance;
    
        ...
    
        public LineRequest(final String name, final String color, final Long upStationId, final Long downStationId,
                           final int distance) {
            this.name = name;
            this.color = color;
            this.upStationId = upStationId;
            this.downStationId = downStationId;
            this.distance = distance;
        }
    
        public LineRequest(final String name, final String color) {
            this(name, color, null, null, 0);
        }
    
        public Long getUpStationId() {
            return upStationId;
        }
    
        public Long getDownStationId() {
            return downStationId;
        }
    
        public int getDistance() {
            return distance;
        }
    
        ...
    }
    
    to-be:
    public class LineRequest {
        ...
    }

--- src/main/java/wooteco/subway/dto/LineRequest.java
+++ src/main/java/wooteco/subway/dto/LineRequest.java
@@ -3,24 +3,13 @@ package wooteco.subway.dto;
 public class LineRequest {
     private String name;
     private String color;
-    private Long upStationId;
-    private Long downStationId;
-    private int distance;
 
     public LineRequest() {
     }
 
-    public LineRequest(final String name, final String color, final Long upStationId, final Long downStationId,
-                       final int distance) {
+    public LineRequest(final String name, final String color) {
         this.name = name;
         this.color = color;
-        this.upStationId = upStationId;
-        this.downStationId = downStationId;
-        this.distance = distance;
-    }
-
-    public LineRequest(final String name, final String color) {
-        this(name, color, null, null, 0);
     }
 
     public String getName() {
@@ -29,18 +18,6 @@ public class LineRequest {
 
     public String getColor() {
         return color;
-    }
-
-    public Long getUpStationId() {
-        return upStationId;
-    }
-
-    public Long getDownStationId() {
-        return downStationId;
-    }
-
-    public int getDistance() {
-        return distance;
     }
 
     @Override
