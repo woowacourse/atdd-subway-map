ff1a39cc3cbd254e8c3cf259875b0d0a49335699
skullKim <kyk990328@gmail.com>
2022-05-08 21:17:24 +0900

    chore(StationControllerAdvice.java): 사용하지 않는 import 삭제

--- src/main/java/wooteco/subway/ui/StationControllerAdvice.java
+++ src/main/java/wooteco/subway/ui/StationControllerAdvice.java
@@ -5,7 +5,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import wooteco.subway.dto.ErrorResponse;
-import wooteco.subway.exception.NoStationFoundException;
 
 @RestControllerAdvice
 public class StationControllerAdvice {
