ad3f0278ace8c626acd611a13a8d726b028e84ea
kth990303 <kth990303@naver.com>
2022-05-08 21:22:47 +0900

    feat: 에러 핸들러 추가

--- src/main/java/wooteco/subway/ui/SubwayControllerAdvice.java
+++ src/main/java/wooteco/subway/ui/SubwayControllerAdvice.java
@@ -9,6 +9,11 @@ import wooteco.subway.dto.ErrorResponse;
 public class SubwayControllerAdvice {
     @ExceptionHandler
     public ResponseEntity<ErrorResponse> handleError(Exception e) {
+        return ResponseEntity.internalServerError().body(new ErrorResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleClientError(Exception e) {
         return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
     }
 }
