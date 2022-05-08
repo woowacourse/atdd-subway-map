a82c9babe109b973e4ba7a553deda061007fbeb5
nailseong <nnis9857@gmail.com>
2022-05-08 18:29:31 +0900

    feat(Section): 생성자 및 toString, eq&hc 재정의

--- src/main/java/wooteco/subway/domain/Section.java
+++ src/main/java/wooteco/subway/domain/Section.java
@@ -12,6 +12,15 @@ public class Section {
     private Long downStationId;
     private int distance;
 
+    public Section(final Long id, final Long lineId, final Long upStationId, final Long downStationId,
+                   final int distance) {
+        this.id = id;
+        this.lineId = lineId;
+        this.upStationId = upStationId;
+        this.downStationId = downStationId;
+        this.distance = distance;
+    }
+
     public Section(final Long lineId, final Long upStationId, final Long downStationId, final int distance) {
         validate(upStationId, downStationId, distance);
         this.lineId = Objects.requireNonNull(lineId);
@@ -51,5 +60,36 @@ public class Section {
 
     public int getDistance() {
         return distance;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final Section section = (Section) o;
+        return distance == section.distance && Objects.equals(id, section.id) && Objects.equals(lineId,
+                section.lineId) && Objects.equals(upStationId, section.upStationId) && Objects.equals(
+                downStationId, section.downStationId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, lineId, upStationId, downStationId, distance);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Section{");
+        sb.append("id=").append(id);
+        sb.append(", lineId=").append(lineId);
+        sb.append(", upStationId=").append(upStationId);
+        sb.append(", downStationId=").append(downStationId);
+        sb.append(", distance=").append(distance);
+        sb.append('}');
+        return sb.toString();
     }
 }
