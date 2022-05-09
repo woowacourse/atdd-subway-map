38b0cddd133019f8ebf26f6a9000bc02436d58ae
bomi <bonita9931@gmail.com>
2022-05-09 19:11:35 +0900

    feat: Section 도메인 추가

---
+++ src/main/java/wooteco/subway/domain/Section.java
@@ -0,0 +1,9 @@
+package wooteco.subway.domain;
+
+public class Section {
+    private Long id;
+    private Long distance;
+    private Long lineId;
+    private Long upStationId;
+    private Long downStationId;
+}
