6400ff10ad75ae1580e04e204b9f84f522ca449e
sinb57 <sinb57@naver.com>
2022-05-09 21:20:01 +0900

    feat: 지하철역 이름 원시값 포장 및 공백 예외처리

---
+++ src/main/java/wooteco/subway/domain/station/StationName.java
@@ -0,0 +1,21 @@
+package wooteco.subway.domain.station;
+
+public class StationName {
+
+    private final String name;
+
+    public StationName(String name) {
+        validateNameNotBlank(name);
+        this.name = name;
+    }
+
+    private void validateNameNotBlank(String name) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("지하철역 이름은 공백이 될 수 없습니다.");
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+}
