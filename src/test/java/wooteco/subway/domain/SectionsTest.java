5b43e3c263a409efb2c7bc9f2b52079543b29383
yxxnghwan <younghwan960@gmail.com>
2022-05-10 02:22:48 +0900

    test(domain): 구간 추가 기능 테스트 케이스 추가

--- src/test/java/wooteco/subway/domain/SectionsTest.java
+++ src/test/java/wooteco/subway/domain/SectionsTest.java
@@ -9,8 +9,8 @@ import org.junit.jupiter.api.Test;
 class SectionsTest {
 
     @Test
-    @DisplayName("구간 일급 컬렉션에 새 구간을 추가한다.")
-    void addSection() {
+    @DisplayName("구간 일급 컬렉션에 새 구간을 추가한다. - 맨 뒤")
+    void addSectionBack() {
         final Station station1 = new Station(1L, "강남역");
         final Station station2 = new Station(2L, "역삼역");
         final Station station3 = new Station(3L, "선릉역");
@@ -27,4 +27,60 @@ class SectionsTest {
         assertThat(sections.getValue()).isEqualTo(List.of(section1, section2, section3));
     }
 
+    @Test
+    @DisplayName("구간 일급 컬렉션에 새 구간을 추가한다. - 맨 앞")
+    void addSectionFront() {
+        final Station station1 = new Station(1L, "강남역");
+        final Station station2 = new Station(2L, "역삼역");
+        final Station station3 = new Station(3L, "선릉역");
+        final Station station4 = new Station(4L, "삼성역");
+        final Section section1 = new Section(1L, station2, station3, 10);
+        final Sections sections = new Sections(section1);
+
+        sections.addSection(station3, station4, 10);
+        sections.addSection(station1, station2, 5);
+
+        final Section section2 = new Section(2L, station3, station4, 10);
+        final Section section3 = new Section(3L, station1, station2, 5);
+
+        assertThat(sections.getValue()).isEqualTo(List.of(section3, section1, section2));
+    }
+
+    @Test
+    @DisplayName("구간 일급 컬렉션에 새 구간을 추가한다. - 상행기준 중간삽입")
+    void addSectionUpDirectionInsert() {
+        final Station station1 = new Station(1L, "강남역");
+        final Station station2 = new Station(2L, "역삼역");
+        final Station station3 = new Station(3L, "선릉역");
+        final Station station4 = new Station(4L, "삼성역");
+        final Section section1 = new Section(1L, station1, station2, 10);
+        final Sections sections = new Sections(section1);
+
+        sections.addSection(station2, station4, 10);
+        sections.addSection(station2, station3, 5);
+
+        final Section section2 = new Section(2L, station2, station3, 5);
+        final Section section3 = new Section(3L, station3, station4, 5);
+
+        assertThat(sections.getValue()).isEqualTo(List.of(section1, section2, section3));
+    }
+
+    @Test
+    @DisplayName("구간 일급 컬렉션에 새 구간을 추가한다. - 하행기준 중간삽입")
+    void addSectionDownDirectionInsert() {
+        final Station station1 = new Station(1L, "강남역");
+        final Station station2 = new Station(2L, "역삼역");
+        final Station station3 = new Station(3L, "선릉역");
+        final Station station4 = new Station(4L, "삼성역");
+        final Section section1 = new Section(1L, station1, station2, 10);
+        final Sections sections = new Sections(section1);
+
+        sections.addSection(station2, station4, 10);
+        sections.addSection(station3, station4, 5);
+
+        final Section section2 = new Section(2L, station2, station3, 5);
+        final Section section3 = new Section(3L, station3, station4, 5);
+
+        assertThat(sections.getValue()).isEqualTo(List.of(section1, section2, section3));
+    }
 }
