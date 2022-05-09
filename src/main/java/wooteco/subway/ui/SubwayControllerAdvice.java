1b1da96d57ea0b6bfa8fd631e9962973a11c8400
beomWhale <cndqjacndqja@gmail.com>
2022-05-09 10:34:20 +0900

    refactor: ControllerExceptionHandler -> SubwayControllerAdvice로 클래스명 변경

--- src/main/java/wooteco/subway/ui/SubwayControllerAdvice.java
+++ src/main/java/wooteco/subway/ui/SubwayControllerAdvice.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class ControllerExceptionHandler {
+public class SubwayControllerAdvice {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
