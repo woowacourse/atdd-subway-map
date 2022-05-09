0a8e989c3be38f72951916959a6bed08704f9b4c
SuyeonChoi <tndus970120@gmail.com>
2022-05-09 17:29:37 +0900

    feat(Station): 역 이름 길이 검증

--- src/main/java/wooteco/subway/domain/Station.java
+++ src/main/java/wooteco/subway/domain/Station.java
@@ -1,19 +1,23 @@
 package wooteco.subway.domain;
 
 public class Station {
-    private Long id;
-    private String name;
-
-    public Station() {
-    }
+    private final Long id;
+    private final String name;
 
     public Station(Long id, String name) {
+        validateNameLength(name);
         this.id = id;
         this.name = name;
     }
 
     public Station(String name) {
-        this.name = name;
+        this(null, name);
+    }
+
+    private void validateNameLength(String name) {
+        if (name.isBlank() || name.length() > 20) {
+            throw new IllegalArgumentException("역 이름은 최소 1글자이상 20글자 이하여야 합니다.");
+        }
     }
 
     public Long getId() {
@@ -24,4 +28,3 @@ public class Station {
         return name;
     }
 }
-
