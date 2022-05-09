0926b39f735d079f1e356c1cba85c6e00b0cf797
seong-wooo <seongwoo0513@uos.ac.kr>
2022-05-09 22:08:31 +0900

    feat: 역 사이에 새로운 역을 등록할 경우, 기존 역 사이 길이보다 크거나 같으면 등록할 수 없는 기능 구현 및 테스트

--- src/main/java/wooteco/subway/service/SectionService.java
+++ src/main/java/wooteco/subway/service/SectionService.java
@@ -1,5 +1,6 @@
 package wooteco.subway.service;
 
+import java.util.Optional;
 import org.springframework.stereotype.Service;
 import wooteco.subway.dao.SectionDao;
 import wooteco.subway.domain.Section;
@@ -29,6 +30,24 @@ public class SectionService {
         if (!sectionDao.existByLineIdAndStationId(lineId, sectionReq.getUpStationId())
                 && !sectionDao.existByLineIdAndStationId(lineId, sectionReq.getDownStationId())) {
             throw new IllegalArgumentException("상행, 하행이 대상 노선에 둘 다 존재하지 않습니다.");
+        }
+
+        Optional<Long> upStationId = sectionDao.findIdByLineIdAndUpStationId(lineId,
+                sectionReq.getUpStationId());
+        if (upStationId.isPresent()) {
+            int distance = sectionDao.findDistanceById(upStationId.get());
+            if (distance <= sectionReq.getDistance()) {
+                throw new IllegalArgumentException("역 사이에 새로운 역을 등록할 경우, 기존 역 사이 길이보다 크거나 같으면 등록할 수 없습니다.");
+            }
+        }
+
+        Optional<Long> downStationId = sectionDao.findIdByLineIdAndDownStationId(lineId,
+                sectionReq.getDownStationId());
+        if (downStationId.isPresent()) {
+            int distance = sectionDao.findDistanceById(downStationId.get());
+            if (distance <= sectionReq.getDistance()) {
+                throw new IllegalArgumentException("역 사이에 새로운 역을 등록할 경우, 기존 역 사이 길이보다 크거나 같으면 등록할 수 없습니다.");
+            }
         }
 
         Section section = createSection(lineId, sectionReq, sectionDao.findLineOrderById(lineId));
--- src/test/java/wooteco/subway/service/SectionServiceTest.java
+++ src/test/java/wooteco/subway/service/SectionServiceTest.java
@@ -67,4 +67,36 @@ class SectionServiceTest {
         assertThat(thrown).isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("상행, 하행이 대상 노선에 둘 다 존재하지 않습니다.");
     }
+
+    @Test
+    @DisplayName("두 개의 역 중 하나가 노선에 상행으로 존재할 때, 등록할 구간의 거리가 기존 구간의 거리보다 크면 예외가 발생한다.")
+    void upStationDistanceException() {
+        // given
+        sectionService.save(1L, GIVEN_SECTION_REQ);
+
+        // when
+        Throwable thrown = catchThrowable(() -> {
+            sectionService.save(1L, new SectionRequest(1L, 3L, 6));
+        });
+
+        // then
+        assertThat(thrown).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("역 사이에 새로운 역을 등록할 경우, 기존 역 사이 길이보다 크거나 같으면 등록할 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("두 개의 역 중 하나가 노선에 하행으로 존재할 때, 등록할 구간의 거리가 기존 구간의 거리보다 크면 예외가 발생한다.")
+    void downStationDistanceException() {
+        // given
+        sectionService.save(1L, GIVEN_SECTION_REQ);
+
+        // when
+        Throwable thrown = catchThrowable(() -> {
+            sectionService.save(1L, new SectionRequest(3L, 2L, 6));
+        });
+
+        // then
+        assertThat(thrown).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("역 사이에 새로운 역을 등록할 경우, 기존 역 사이 길이보다 크거나 같으면 등록할 수 없습니다.");
+    }
 }
