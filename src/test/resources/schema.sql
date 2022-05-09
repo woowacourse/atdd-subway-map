e679c77716d0d0035f0540fba2772e696ca45547
jojogreen91 <iawbg13@gmail.com>
2022-05-09 14:49:43 +0900

    fix: EOF 적용

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -2,7 +2,7 @@ create table if not exists STATION
 (
     id bigint auto_increment not null,
     name varchar(255) not null unique,
-    primary key(id)
+    primary key (id)
 );
 
 create table if not exists LINE
@@ -10,7 +10,7 @@ create table if not exists LINE
     id bigint auto_increment not null,
     name varchar(255) not null unique,
     color varchar(20) not null,
-    primary key(id)
+    primary key (id)
 );
 
 create table if not exists SECTION
@@ -20,5 +20,5 @@ create table if not exists SECTION
     up_station_id bigint not null,
     down_station_id bigint not null,
     distance int,
-    primary key(id)
+    primary key (id)
 );
