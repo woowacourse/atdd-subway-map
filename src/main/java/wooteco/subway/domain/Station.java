cf6f4f0e11c4bd7f58805a2ce3c354112aec87c7
sure-why-not <pkse1213@gmail.com>
2022-05-09 17:20:20 +0900

    feat: Station 이름 검증 기능 구현

---
+++ src/main/java/wooteco/subway/domain/Station.java
@@ -0,0 +1,14 @@
+package wooteco.subway.domain;
+
+public class Station {
+
+    private final String name;
+
+    public Station(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
