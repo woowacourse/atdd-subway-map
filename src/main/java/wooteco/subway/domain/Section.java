94783576e6699842c76a24c96ce751d4711aa38c
yhh1056 <yhh1056@naver.com>
2022-05-10 00:24:14 +0900

    feat: 구간의 거리 유효 검증

---
+++ src/main/java/wooteco/subway/domain/Section.java
@@ -0,0 +1,31 @@
+package wooteco.subway.domain;
+
+public class Section {
+
+    private static final int MIN_DISTANCE = 1;
+
+    private Long id;
+    private Line line;
+    private Station upStation;
+    private Station downStation;
+    private int distance;
+
+    public Section(Long id, Line line, Station upStation, Station downStation, int distance) {
+        validate(distance);
+        this.id = id;
+        this.line = line;
+        this.upStation = upStation;
+        this.downStation = downStation;
+        this.distance = distance;
+    }
+
+    public Section(Line line, Station upStation, Station downStation, int distance) {
+        this(null, line, upStation, downStation, distance);
+    }
+
+    private void validate(int distance) {
+        if (distance < MIN_DISTANCE) {
+            throw new IllegalArgumentException("거리는 1이상이어야 합니다.");
+        }
+    }
+}
