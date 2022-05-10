0c677c343189cc4eea06e52e63a92809e3f1ed90
SuyeonChoi <tndus970120@gmail.com>
2022-05-10 16:30:49 +0900

    feat(Line): 노선 이름 길이 검증

--- src/main/java/wooteco/subway/domain/Line.java
+++ src/main/java/wooteco/subway/domain/Line.java
@@ -1,19 +1,25 @@
 package wooteco.subway.domain;
 
 public class Line {
-    private Long id;
-    private String name;
-    private String color;
+    private final Long id;
+    private final String name;
+    private final String color;
 
     public Line(Long id, String name, String color) {
+        validateNameLength(name);
         this.id = id;
         this.name = name;
         this.color = color;
     }
 
     public Line(String name, String color) {
-        this.name = name;
-        this.color = color;
+        this(null, name, color);
+    }
+
+    private void validateNameLength(String name) {
+        if (name.isBlank() || name.length() > 20) {
+            throw new IllegalArgumentException("노선 이름은 최소 1글자이상 20글자 이하여야 합니다.");
+        }
     }
 
     public Long getId() {
