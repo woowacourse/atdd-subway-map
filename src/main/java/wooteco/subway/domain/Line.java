d8ca2f5d52ef7379472d9d8026fe26daf115cc41
Donggyu <a29661498@gmail.com>
2022-05-10 16:57:39 +0900

    feat: Line에 Sections 추가 및 생성자 추가

--- src/main/java/wooteco/subway/domain/Line.java
+++ src/main/java/wooteco/subway/domain/Line.java
@@ -7,6 +7,7 @@ public class Line {
     private Long id;
     private String name;
     private String color;
+    private Sections sections;
 
     public Line(String name, String color) {
         this(null, name, color);
@@ -18,6 +19,15 @@ public class Line {
         this.id = id;
         this.name = name;
         this.color = color;
+    }
+
+    public Line(Long id, String name, String color, Sections sections) {
+        validateArgument(name, color);
+
+        this.id = id;
+        this.name = name;
+        this.color = color;
+        this.sections = sections;
     }
 
     private void validateArgument(String name, String color) {
@@ -39,6 +49,10 @@ public class Line {
 
     public String getColor() {
         return color;
+    }
+
+    public Sections getSections() {
+        return sections;
     }
 
     @Override
