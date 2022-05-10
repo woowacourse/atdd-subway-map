c1d3ca00584e0a6dab5420e18a9b6e3847ae23ec
김찬 <gmelddl514@gmail.com>
2022-05-10 16:10:26 +0900

    fix : color unique 삭제

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,7 @@
+TRUNCATE TABLE STATION;
+TRUNCATE TABLE LINE;
+TRUNCATE TABLE SECTION;
+
 create table if not exists STATION
 (
     id   bigint auto_increment not null,
@@ -9,7 +13,7 @@ create table if not exists LINE
 (
     id    bigint auto_increment not null,
     name  varchar(255)          not null unique,
-    color varchar(20)           not null unique,
+    color varchar(20)           not null,
     primary key (id)
 );
 
