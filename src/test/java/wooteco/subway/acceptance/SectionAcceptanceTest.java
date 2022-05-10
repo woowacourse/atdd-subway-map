1f3e0078ed1cadc5075c71bf5ef9757bf7c59d3d
Donggyu <a29661498@gmail.com>
2022-05-10 22:18:12 +0900

    test: 환승역(교점)에 대한 구간 테스트

---
+++ src/test/java/wooteco/subway/acceptance/SectionAcceptanceTest.java
@@ -0,0 +1,62 @@
+package wooteco.subway.acceptance;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import wooteco.subway.dto.LineRequest;
+import wooteco.subway.dto.LineResponse;
+import wooteco.subway.dto.SectionRequest;
+import wooteco.subway.dto.StationRequest;
+import wooteco.subway.dto.StationResponse;
+
+public class SectionAcceptanceTest extends AcceptanceTest {
+
+    @DisplayName("환승역(교점)에 대한 구간 테스트")
+    @Test
+    public void crossSectionTest() {
+        // given
+        final Long stationId1 = extractStationIdFromName("교대역");
+        final Long stationId2 = extractStationIdFromName("강남역");
+        final Long stationId3 = extractStationIdFromName("역삼역");
+
+        final LineRequest params1 = new LineRequest("2호선", "bg-red-600", stationId1, stationId2, 10);
+        ExtractableResponse<Response> response1 = AcceptanceFixture.post(params1, "/lines");
+        Long lineId1 = extractId(response1);
+
+        SectionRequest sectionRequest1 = new SectionRequest(stationId2, stationId3, 5);
+        ExtractableResponse<Response> sectionResponse1 = AcceptanceFixture.post(sectionRequest1, "/lines/" + lineId1 + "/sections");
+
+        final Long stationId4 = extractStationIdFromName("신사역");
+        final Long stationId5 = extractStationIdFromName("양재역");
+
+        final LineRequest params2 = new LineRequest("신분당선", "bg-red-600", stationId4, stationId2, 10);
+        ExtractableResponse<Response> response2 = AcceptanceFixture.post(params2, "/lines");
+        Long lineId2 = extractId(response2);
+
+        SectionRequest sectionRequest2 = new SectionRequest(stationId2, stationId5, 5);
+        ExtractableResponse<Response> sectionResponse2 = AcceptanceFixture.post(sectionRequest2, "/lines/" + lineId2 + "/sections");
+
+        //when & then
+        assertThat(sectionResponse1.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(sectionResponse2.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    private Long extractId(ExtractableResponse<Response> response) {
+        return response.jsonPath()
+                .getObject(".", LineResponse.class)
+                .getId();
+    }
+
+    private Long extractStationIdFromName(String name) {
+        final StationRequest stationRequest = new StationRequest(name);
+        final ExtractableResponse<Response> stationResponse = AcceptanceFixture.post(stationRequest, "/stations");
+
+        return stationResponse.jsonPath()
+                .getObject(".", StationResponse.class)
+                .getId();
+    }
+}
