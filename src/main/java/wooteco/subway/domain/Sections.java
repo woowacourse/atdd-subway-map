cbe86ae4d6c896a8d2474a75cb98d76619741917
sun <syoun602@gmail.com>
2022-05-10 13:57:47 +0900

    feat: 갈래길이 아닌경우 Section을 종점으로 등록하는 기능 추가

--- src/main/java/wooteco/subway/domain/Sections.java
+++ src/main/java/wooteco/subway/domain/Sections.java
@@ -1,5 +1,6 @@
 package wooteco.subway.domain;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
@@ -9,12 +10,16 @@ public class Sections {
     private final List<Section> value;
 
     public Sections(final List<Section> value) {
-        this.value = value;
+        this.value = new ArrayList<>(value);
     }
 
     public void add(final Section section) {
         validateExist(section);
         validateSection(section);
+
+        if (!isBranched(section)) {
+            value.add(section);
+        }
     }
 
     public boolean isBranched(final Section other) {
@@ -74,5 +79,9 @@ public class Sections {
         if (!hasUpStation && !hasDownStation) {
             throw new IllegalArgumentException("상행역과 하행역 둘 중 하나도 포함되어있지 않으면 구간을 추가할 수 없습니다.");
         }
+    }
+
+    public List<Section> getSections() {
+        return value;
     }
 }
--- src/test/java/wooteco/subway/domain/SectionsTest.java
+++ src/test/java/wooteco/subway/domain/SectionsTest.java
@@ -3,6 +3,7 @@ package wooteco.subway.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -12,18 +13,23 @@ import java.util.List;
 
 class SectionsTest {
 
+    private final Station station1 = new Station(1L, "아차산역");
+    private final Station station2 = new Station(2L, "군자역");
+    private final Section section = new Section(1L, station1, station2, 10, 1L);
+    private Sections sections;
+
+    @BeforeEach
+    void setUp() {
+        sections = new Sections(List.of(section));
+    }
+
     @DisplayName("추가하려는 구간의 역들이 기존 구간에 모두 존재하지 않으면 예외를 발생한다.")
     @Test
     void add_throwsNoStationExistException() {
-        final Station station1 = new Station(1L, "아차산역");
-        final Station station2 = new Station(2L, "군자역");
-        final Section section = new Section(1L, station1, station2, 10, 1L);
 
         final Station newStation1 = new Station(3L, "여의도역");
         final Station newStation2 = new Station(4L, "마장역");
         final Section newSection = new Section(newStation1, newStation2, 5, 1L);
-
-        final Sections sections = new Sections(List.of(section));
 
         assertThatThrownBy(() -> sections.add(newSection))
                 .isInstanceOf(IllegalArgumentException.class)
@@ -33,13 +39,7 @@ class SectionsTest {
     @DisplayName("추가하려는 구간의 역들이 기존 구간에 모두 존재하면 예외를 발생한다.")
     @Test
     void add_throwsAllStationExistException() {
-        final Station station1 = new Station(1L, "아차산역");
-        final Station station2 = new Station(2L, "군자역");
-        final Section section = new Section(1L, station1, station2, 10, 1L);
-
         final Section newSection = new Section(station1, station2, 5, 1L);
-
-        final Sections sections = new Sections(List.of(section));
 
         assertThatThrownBy(() -> sections.add(newSection))
                 .isInstanceOf(IllegalArgumentException.class)
@@ -50,15 +50,9 @@ class SectionsTest {
     @ParameterizedTest
     @ValueSource(ints = {10, 11, 15})
     void add_throwsUpSectionDistanceException(final int distance) {
-        final Station station1 = new Station(1L, "아차산역");
-        final Station station2 = new Station(2L, "마장역");
-        final Section section = new Section(1L, station1, station2, 10, 1L);
-
         final Station newStation1 = new Station(1L, "아차산역");
         final Station newStation2 = new Station(3L, "군자역");
         final Section newSection = new Section(newStation1, newStation2, distance, 1L);
-
-        final Sections sections = new Sections(List.of(section));
 
         assertThatThrownBy(() -> sections.add(newSection))
                 .isInstanceOf(IllegalArgumentException.class)
@@ -69,15 +63,9 @@ class SectionsTest {
     @ParameterizedTest
     @ValueSource(ints = {10, 11, 15})
     void add_throwsDownSectionDistanceException(final int distance) {
-        final Station station1 = new Station(1L, "아차산역");
-        final Station station2 = new Station(2L, "마장역");
-        final Section section = new Section(1L, station1, station2, 10, 1L);
-
         final Station newStation1 = new Station(3L, "장한평역");
         final Station newStation2 = new Station(2L, "마장역");
         final Section newSection = new Section(newStation1, newStation2, distance, 1L);
-
-        final Sections sections = new Sections(List.of(section));
 
         assertThatThrownBy(() -> sections.add(newSection))
                 .isInstanceOf(IllegalArgumentException.class)
@@ -89,16 +77,44 @@ class SectionsTest {
     @CsvSource(value = {"1,아차산역,3,장한평역,true", "3,천호역,1,아차산역,false"})
     void isBranched(final long stationId1, final String stationName1,
                     final long stationId2, final String stationName2, final boolean expected) {
-        final Station station1 = new Station(1L, "아차산역");
-        final Station station2 = new Station(2L, "마장역");
-        final Section section = new Section(1L, station1, station2, 10, 1L);
-
         final Station newStation1 = new Station(stationId1, stationName1);
         final Station newStation2 = new Station(stationId2, stationName2);
         final Section newSection = new Section(newStation1, newStation2, 5, 1L);
 
-        final Sections sections = new Sections(List.of(section));
-
         assertThat(sections.isBranched(newSection)).isEqualTo(expected);
+    }
+
+    @DisplayName("갈래길이 아닌 경우 상행 종점으로 등록된다.")
+    @Test
+    void addUpStation() {
+        final Station newStation1 = new Station(3L, "광나루역");
+        final Station newStation2 = new Station(1L, "아차산역");
+        final Section newSection = new Section(newStation1, newStation2, 5, 1L);
+
+        sections.add(newSection);
+
+        final boolean empty = sections.getSections().stream()
+                .filter(section -> section.getDownStation().equals(newStation1))
+                .findAny()
+                .isEmpty();
+
+        assertThat(empty).isTrue();
+    }
+
+    @DisplayName("갈래길이 아닌 경우 하행 종점으로 등록된다.")
+    @Test
+    void addDownStation() {
+        final Station newStation1 = new Station(2L, "군자역");
+        final Station newStation2 = new Station(3L, "장한평역");
+        final Section newSection = new Section(newStation1, newStation2, 5, 1L);
+
+        sections.add(newSection);
+
+        final boolean empty = sections.getSections().stream()
+                .filter(section -> section.getUpStation().equals(newStation2))
+                .findAny()
+                .isEmpty();
+
+        assertThat(empty).isTrue();
     }
 }
