40b2b5de1a1e5d5afe197b07e51d00bf0e887bdb
is2js <tingstyle1@gmail.com>
2022-05-10 21:05:04 +0900

    fix: 불필요한 Line 필드 삭제

--- src/main/java/wooteco/subway/domain/Line.java
+++ src/main/java/wooteco/subway/domain/Line.java
@@ -9,36 +9,21 @@ public class Line {
     private Long id;
     private Name name;
     private String color;
-    private Long upStationId;
-    private Long downStationId;
-    private int distance;
 
 
     public Line(String name,
-                String color,
-                final Long upStationId,
-                final Long downStationId,
-                final int distance) {
-        this(null, name, color, upStationId, downStationId, distance);
+                String color) {
+        this(null, name, color);
     }
 
     public Line(Long id,
                 final String name,
-                final String color,
-                final Long upStationId,
-                final Long downStationId,
-                final int distance) {
+                final String color) {
         Objects.requireNonNull(name, ERROR_NULL);
         Objects.requireNonNull(color, ERROR_NULL);
-        Objects.requireNonNull(upStationId, ERROR_NULL);
-        Objects.requireNonNull(downStationId, ERROR_NULL);
-        Objects.requireNonNull(distance, ERROR_NULL);
         this.id = id;
         this.name = new Name(name);
         this.color = color;
-        this.upStationId = upStationId;
-        this.downStationId = downStationId;
-        this.distance = distance;
     }
 
     public Long getId() {
@@ -53,18 +38,6 @@ public class Line {
         return color;
     }
 
-    public Long getUpStationId() {
-        return upStationId;
-    }
-
-    public Long getDownStationId() {
-        return downStationId;
-    }
-
-    public int getDistance() {
-        return distance;
-    }
-
     @Override
     public boolean equals(final Object o) {
         if (this == o) {
@@ -76,24 +49,13 @@ public class Line {
 
         final Line line = (Line) o;
 
-        if (getDistance() != line.getDistance()) {
-            return false;
-        }
         if (getId() != null ? !getId().equals(line.getId()) : line.getId() != null) {
             return false;
         }
         if (getName() != null ? !getName().equals(line.getName()) : line.getName() != null) {
             return false;
         }
-        if (getColor() != null ? !getColor().equals(line.getColor()) : line.getColor() != null) {
-            return false;
-        }
-        if (getUpStationId() != null ? !getUpStationId().equals(line.getUpStationId())
-            : line.getUpStationId() != null) {
-            return false;
-        }
-        return getDownStationId() != null ? getDownStationId().equals(line.getDownStationId())
-            : line.getDownStationId() == null;
+        return getColor() != null ? getColor().equals(line.getColor()) : line.getColor() == null;
     }
 
     @Override
@@ -101,9 +63,6 @@ public class Line {
         int result = getId() != null ? getId().hashCode() : 0;
         result = 31 * result + (getName() != null ? getName().hashCode() : 0);
         result = 31 * result + (getColor() != null ? getColor().hashCode() : 0);
-        result = 31 * result + (getUpStationId() != null ? getUpStationId().hashCode() : 0);
-        result = 31 * result + (getDownStationId() != null ? getDownStationId().hashCode() : 0);
-        result = 31 * result + getDistance();
         return result;
     }
 }
