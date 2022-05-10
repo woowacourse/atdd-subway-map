693270d9052347fc0ac151e33c6551e5aab3f5c4
Nam Minjeong <nmj5012@naver.com>
2022-05-10 17:55:32 +0900

    feat: 구간 삭제 관련 Sections 로직 추가

--- src/main/java/wooteco/subway/domain/Sections.java
+++ src/main/java/wooteco/subway/domain/Sections.java
@@ -4,10 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import wooteco.subway.exception.NotFoundException;
 
 public class Sections {
     private static final String ALREADY_CONTAINS_UP_AND_DOWN_STATIONS = "상행역과 하행역이 이미 모두 노선에 등록되어 있습니다.";
     private static final String NOT_CONTAINS_UP_AND_DOWN_STATIONS = "상행역과 하행역이 모두 노선에 등록되어 있지 않습니다.";
+    private static final String STATION_NOT_FOUND_IN_SECTIONS = "해당 노선에서는 입력한 지하철 역을 찾을 수 없습니다.";
+    private static final String CAN_NOT_DELETE_MORE = "해당 노선은 더 삭제할 수 없습니다.";
 
     private final List<Section> value;
 
@@ -46,5 +49,26 @@ public class Sections {
         return stationIds.stream()
                 .distinct()
                 .collect(Collectors.toUnmodifiableList());
+    }
+
+    public List<Section> findByStationId(long stationId) {
+        List<Section> sections = value.stream()
+                .filter(section -> section.getUpStationId() == stationId || section.getDownStationId() == stationId)
+                .collect(Collectors.toUnmodifiableList());
+
+        if (sections.size() == 0) {
+            throw new NotFoundException(STATION_NOT_FOUND_IN_SECTIONS);
+        }
+        return sections;
+    }
+
+    public void checkCanDelete() {
+        if (value.size() == 1) {
+            throw new IllegalArgumentException(CAN_NOT_DELETE_MORE);
+        }
+    }
+
+    public int size() {
+        return value.size();
     }
 }
--- src/test/java/wooteco/subway/domain/SectionsTest.java
+++ src/test/java/wooteco/subway/domain/SectionsTest.java
@@ -3,19 +3,30 @@ package wooteco.subway.domain;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import wooteco.subway.exception.NotFoundException;
 
 class SectionsTest {
+    private static Section section1;
+    private static Section section2;
+    private static Section section3;
+
+
+    @BeforeAll
+    static void setUp(){
+         section1 = Section.of(1L, 2L, 10);
+         section2 = Section.of(2L, 3L, 10);
+         section3 = Section.of(5L, 4L, 10);
+    }
 
     @Test
     @DisplayName("상행과 하행이 이미 등록된 경우 에러를 발생시킨다")
     void checkSectionErrorByAlreadyExist() {
-        Section section = Section.of(1L, 2L, 10);
-        Section section2 = Section.of(2L, 3L, 10);
-        Sections sections = new Sections(List.of(section, section2));
+        Sections sections = new Sections(List.of(section1, section2));
 
-        assertThatThrownBy(() -> sections.checkSection(section))
+        assertThatThrownBy(() -> sections.checkSection(section1))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("상행역과 하행역이 이미 모두 노선에 등록되어 있습니다.");
     }
@@ -23,13 +34,31 @@ class SectionsTest {
     @Test
     @DisplayName("상행과 하행이 존재하지 않는 경우 에러를 발생시킨다")
     void checkSectionErrorByNotExist() {
-        Section section = Section.of(1L, 2L, 10);
-        Section section2 = Section.of(2L, 3L, 10);
-        Section section3 = Section.of(5L, 4L, 10);
-        Sections sections = new Sections(List.of(section, section2));
+
+        Sections sections = new Sections(List.of(section1, section2));
 
         assertThatThrownBy(() -> sections.checkSection(section3))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("상행역과 하행역이 모두 노선에 등록되어 있지 않습니다.");
+    }
+
+    @Test
+    @DisplayName("해당 노선에 해당하는 지하철 역이 없는 경우 에러를 발생시킨다.")
+    void findByStationIdNotFound() {
+        Sections sections = new Sections(List.of(section1, section2));
+
+        assertThatThrownBy(() -> sections.findByStationId(4L))
+                .isInstanceOf(NotFoundException.class)
+                .hasMessage("해당 노선에서는 입력한 지하철 역을 찾을 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("sections의 길이가 1인 경우 에러를 발생시킨다.")
+    void checkCanDeleteErrorBySizeOne() {
+        Sections sections = new Sections(List.of(section1));
+
+        assertThatThrownBy(sections::checkCanDelete)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("해당 노선은 더 삭제할 수 없습니다.");
     }
 }
