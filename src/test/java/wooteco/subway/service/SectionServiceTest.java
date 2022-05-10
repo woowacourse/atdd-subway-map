e97eb197b6f8289982f381d2876f9c1d12b572b9
Hyunta <mohai2618@gmail.com>
2022-05-10 17:53:39 +0900

    refactor: 불필요한 의존성 삭제

--- src/test/java/wooteco/subway/service/SectionServiceTest.java
+++ src/test/java/wooteco/subway/service/SectionServiceTest.java
@@ -12,55 +12,43 @@ import wooteco.subway.mock.MemoryStationDao;
 public class SectionServiceTest {
     private static final long LINE_ID = 1L;
 
-    private MemoryStationDao stationDao = new MemoryStationDao();
-    private MemorySectionDao sectionDao = new MemorySectionDao();
+    private final MemoryStationDao stationDao = new MemoryStationDao();
+    private final MemorySectionDao sectionDao = new MemorySectionDao();
 
-    private SectionService sectionService = new SectionService(sectionDao, stationDao);
+    private final SectionService sectionService = new SectionService(sectionDao);
+
+    private final Station first = stationDao.save(new Station("강남역"));
+    private final Station second = stationDao.save(new Station("역삼역"));
+    private final Station third = stationDao.save(new Station("선릉역"));
 
     @BeforeEach
     void before() {
-        stationDao.clear();
         sectionDao.clear();
+        stationDao.clear();
+        sectionDao.save(new Section(LINE_ID, first.getId(), third.getId(), 10));
     }
 
     @Test
     @DisplayName("상행역과 하행역이 동일한 경우 예외처리")
     void sameStation() {
-        Station first = stationDao.save(new Station("강남역"));
-        Station second = stationDao.save(new Station("역삼역"));
-        Station third = stationDao.save(new Station("선릉역"));
-        sectionDao.save(new Section(LINE_ID, first.getId(), third.getId(), 10));
-
         Assertions.assertThatThrownBy(
                         () -> sectionService.create(new Section(LINE_ID, first.getId(), first.getId(), 10))
                 ).isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("상행역과 하행역이 일치합니다.");
-
     }
 
     @Test
     @DisplayName("등록된 구간을 중복으로 등록하면 예외처리")
     void duplicateSection() {
-        Station first = stationDao.save(new Station("강남역"));
-        Station second = stationDao.save(new Station("역삼역"));
-        Station third = stationDao.save(new Station("선릉역"));
-        sectionDao.save(new Section(LINE_ID, first.getId(), third.getId(), 10));
-
         Assertions.assertThatThrownBy(
                         () -> sectionService.create(new Section(LINE_ID, first.getId(), third.getId(), 10))
                 ).isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("이미 등록된 구간입니다.");
-
     }
 
     @Test
     @DisplayName("입력한 역이 존재하지 않는다면 예외처리")
     void noExistStation() {
-        Station first = stationDao.save(new Station("강남역"));
-        Station second = stationDao.save(new Station("역삼역"));
-        Station third = stationDao.save(new Station("선릉역"));
-        sectionDao.save(new Section(LINE_ID, first.getId(), third.getId(), 10));
-
         Assertions.assertThatThrownBy(
                         () -> sectionService.create(new Section(LINE_ID, 4L, 5L, 10))
                 ).isInstanceOf(IllegalArgumentException.class)
@@ -70,22 +58,12 @@ public class SectionServiceTest {
     @Test
     @DisplayName("구간을 추가한다.")
     void add() {
-        Station first = stationDao.save(new Station("강남역"));
-        Station second = stationDao.save(new Station("역삼역"));
-        Station third = stationDao.save(new Station("선릉역"));
-        sectionDao.save(new Section(LINE_ID, first.getId(), third.getId(), 10));
-
         sectionService.create(new Section(LINE_ID, second.getId(), third.getId(), 5));
     }
 
     @Test
     @DisplayName("입력한 구간이 기존 구간보다 길면 예외처리")
     void longSection() {
-        Station first = stationDao.save(new Station("강남역"));
-        Station second = stationDao.save(new Station("역삼역"));
-        Station third = stationDao.save(new Station("선릉역"));
-        sectionDao.save(new Section(LINE_ID, first.getId(), third.getId(), 10));
-
         Assertions.assertThatThrownBy(
                         () -> sectionService.create(new Section(LINE_ID, second.getId(), third.getId(), 11))
                 ).isInstanceOf(IllegalArgumentException.class)
@@ -95,11 +73,6 @@ public class SectionServiceTest {
     @Test
     @DisplayName("종점을 연장할 수 있다.")
     void addExtension() {
-        Station first = stationDao.save(new Station("강남역"));
-        Station second = stationDao.save(new Station("역삼역"));
-        Station third = stationDao.save(new Station("선릉역"));
-        sectionDao.save(new Section(LINE_ID, first.getId(), third.getId(), 10));
-
         sectionService.create(new Section(LINE_ID, third.getId(), second.getId(), 100));
     }
 }
