25f2f19b7a9f708b41ac3da9f9f34d93ad8f7be4
Ohzzi <clover7even@naver.com>
2022-05-10 18:49:38 +0900

    feat: 노선에 포함되는 구간을 받아서 역 목록을 돌려주는 기능 추가

--- src/main/java/wooteco/subway/domain/Line.java
+++ src/main/java/wooteco/subway/domain/Line.java
@@ -1,18 +1,41 @@
 package wooteco.subway.domain;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Line {
     private final Long id;
     private final String name;
     private final String color;
+    private final List<Section> sections;
 
     public Line(String name, String color) {
-        this(null, name, color);
+        this(null, name, color, new ArrayList<>());
     }
 
     public Line(Long id, String name, String color) {
+        this(id, name, color, new ArrayList<>());
+    }
+
+    public Line(Long id, String name, String color, List<Section> sections) {
         this.id = id;
         this.name = name;
         this.color = color;
+        this.sections = sections;
+    }
+
+    public List<Station> getStations() {
+        List<Station> stations = new ArrayList<>();
+        for (Section section : sections) {
+            stations.add(section.getUpStation());
+            stations.add(section.getDownStation());
+        }
+        return stations.stream()
+                .distinct()
+                .sorted(Comparator.comparingLong(Station::getId))
+                .collect(Collectors.toUnmodifiableList());
     }
 
     public Long getId() {
---
+++ src/main/java/wooteco/subway/domain/Section.java
@@ -0,0 +1,36 @@
+package wooteco.subway.domain;
+
+public class Section {
+
+    private final Long id;
+    private final Station upStation;
+    private final Station downStation;
+    private final int distance;
+
+    public Section(Station upStation, Station downStation, int distance) {
+        this(null, upStation, downStation, distance);
+    }
+
+    public Section(Long id, Station upStation, Station downStation, int distance) {
+        this.id = id;
+        this.upStation = upStation;
+        this.downStation = downStation;
+        this.distance = distance;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Station getUpStation() {
+        return upStation;
+    }
+
+    public Station getDownStation() {
+        return downStation;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+}
