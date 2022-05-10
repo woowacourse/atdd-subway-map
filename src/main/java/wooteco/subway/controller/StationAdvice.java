3c2e5f8f9c9c7f955f78e4fa9a7ebc8ebc81eb29
yoon <duddbs774@gmail.com>
2022-05-10 14:19:51 +0900

    refactor: 모든 예외에 대한 핸들링을 하도록 함

--- src/main/java/wooteco/subway/controller/StationAdvice.java
+++ src/main/java/wooteco/subway/controller/StationAdvice.java
@@ -1,14 +1,14 @@
 package wooteco.subway.controller;
 
 import java.util.NoSuchElementException;
+import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class StationAdvice {
-
-    @ExceptionHandler({IllegalArgumentException.class, NoSuchElementException.class})
+    @ExceptionHandler({Exception.class})
     public ResponseEntity<Void> handle(Exception e) {
         return ResponseEntity.badRequest().build();
     }
