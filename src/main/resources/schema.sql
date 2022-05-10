0922fcb2948b8728b3c33b4f6a308a55ffb15fcf
sinb57 <sinb57@naver.com>
2022-05-10 21:07:03 +0900

    chore: schema.sql Section 테이블 DDL 작성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -11,4 +11,14 @@ CREATE TABLE IF NOT EXISTS Station
     id   integer     not null auto_increment,
     name varchar(30) not null unique,
     primary key (id)
-)
+);
+
+create table if not exists SECTION
+(
+    id bigint auto_increment not null,
+    line_id bigint not null,
+    up_station_id bigint not null,
+    down_station_id bigint not null,
+    distance int,
+    primary key(id)
+);
