63ca422d665533b36e01cad77adfd4456d6e07e2
ozragwort <ozragwort@gmail.com>
2022-05-10 13:07:13 +0900

    refactor(SectionRequest): 요청 객체 유효성 검증 어노테이션 추가

--- src/main/java/wooteco/subway/dto/SectionRequest.java
+++ src/main/java/wooteco/subway/dto/SectionRequest.java
@@ -1,8 +1,13 @@
 package wooteco.subway.dto;
 
+import javax.validation.constraints.Positive;
+
 public class SectionRequest {
+    @Positive(message = "상행 종점 id는 양수여야 합니다.")
     private Long upStationId;
+    @Positive(message = "하행 종점 id는 양수여야 합니다.")
     private Long downStationId;
+    @Positive(message = "두 종점간의 거리는 양수여야 합니다.")
     private int distance;
 
     public SectionRequest() {
