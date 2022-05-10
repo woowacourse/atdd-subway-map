e944461e36c25dc654c0322407b06af297930424
Donggyu <a29661498@gmail.com>
2022-05-10 16:58:42 +0900

    feat: response DTO에 Station에 대한 내용 추가

--- src/main/java/wooteco/subway/dto/LineResponse.java
+++ src/main/java/wooteco/subway/dto/LineResponse.java
@@ -1,11 +1,13 @@
 package wooteco.subway.dto;
 
+import java.util.List;
 import wooteco.subway.domain.Line;
 
 public class LineResponse {
     private Long id;
     private String name;
     private String color;
+    private List<StationResponse> stations;
 
     public LineResponse() {
     }
@@ -22,6 +24,13 @@ public class LineResponse {
         this.color = color;
     }
 
+    public LineResponse(Line line, List<StationResponse> stations) {
+        this.id = line.getId();
+        this.name = line.getName();
+        this.color = line.getColor();
+        this.stations = stations;
+    }
+
     public Long getId() {
         return id;
     }
@@ -32,5 +41,9 @@ public class LineResponse {
 
     public String getColor() {
         return color;
+    }
+
+    public List<StationResponse> getStations() {
+        return stations;
     }
 }
