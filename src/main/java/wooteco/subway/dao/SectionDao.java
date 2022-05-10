9ba9c1e53398233e103530aa4831038f4daaf8ec
jurlring <78091011+jurlring@users.noreply.github.com>
2022-05-10 17:33:33 +0900

    feat: lineId를 이용하여 section 조회 기능 구현

--- src/main/java/wooteco/subway/dao/SectionDao.java
+++ src/main/java/wooteco/subway/dao/SectionDao.java
@@ -1,7 +1,10 @@
 package wooteco.subway.dao;
 
+import java.util.List;
 import java.util.Objects;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
@@ -14,6 +17,13 @@ public class SectionDao {
 
     private final NamedParameterJdbcTemplate jdbcTemplate;
 
+    private final RowMapper<Section> eventRowMapper = (resultSet, rowNum)
+            -> new Section(resultSet.getLong("id")
+            , resultSet.getLong("line_id")
+            , resultSet.getLong("up_station_id")
+            , resultSet.getLong("down_station_id")
+            , resultSet.getInt("distance"));
+
     public SectionDao(NamedParameterJdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
@@ -25,5 +35,11 @@ public class SectionDao {
         KeyHolder keyHolder = new GeneratedKeyHolder();
         jdbcTemplate.update(insertSql, source, keyHolder);
         return Objects.requireNonNull(keyHolder.getKey()).longValue();
+    }
+
+    public List<Section> findByLineId(Long lineId) {
+        String selectSql = "select * from SECTION where line_id = :lineId";
+        SqlParameterSource source = new MapSqlParameterSource("lineId", lineId);
+        return jdbcTemplate.query(selectSql, source, eventRowMapper);
     }
 }
--- src/test/java/wooteco/subway/dao/SectionDaoTest.java
+++ src/test/java/wooteco/subway/dao/SectionDaoTest.java
@@ -1,7 +1,9 @@
 package wooteco.subway.dao;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -21,6 +23,8 @@ class SectionDaoTest {
     @BeforeEach
     void setUp() {
         sectionDao = new SectionDao(jdbcTemplate);
+        sectionDao.save(new Section(1L, 1L, 2L, 10));
+        sectionDao.save(new Section(1L, 2L, 3L, 5));
     }
 
     @Test
@@ -32,6 +36,23 @@ class SectionDaoTest {
         Long sectionId = sectionDao.save(section);
         //then
         assertThat(sectionId).isNotNull();
+    }
+
+    @Test
+    @DisplayName("Line-id 를 이용하여 section 을 조회한다.")
+    void findByLineId() {
+        //given
+        Long lineId = 1L;
+        //when
+        List<Section> sections = sectionDao.findByLineId(lineId);
+        long expectedIdCount = sections.stream()
+                .filter(section -> section.getLineId().equals(lineId))
+                .count();
+        //then
+        assertAll(
+                () -> assertThat(sections.size()).isEqualTo(2),
+                () -> assertThat(expectedIdCount).isEqualTo(2)
+        );
     }
 
 }
