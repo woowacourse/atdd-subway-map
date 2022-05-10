d41a8bfe97d297bfc9c28b679a6f315ff2a3216b
summerlunaa <summerlunaa9@gmail.com>
2022-05-10 16:30:02 +0900

    refactor: 구간 저장시 구간의 길이를 검증하는 기능 구현

--- src/main/java/wooteco/subway/domain/Sections.java
+++ src/main/java/wooteco/subway/domain/Sections.java
@@ -1,39 +1,57 @@
 package wooteco.subway.domain;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Sections {
 
     private final List<Section> sections;
 
-    public Sections() {
-        this(new ArrayList<>());
-    }
-
     public Sections(List<Section> sections) {
         this.sections = sections;
     }
 
-    public void add(Section section) {
+    public void add(Section newSection) {
         if (!sections.isEmpty()) {
-            validateSection(section);
+            validateSectionInLine(newSection);
+
+            if (!includeUpStation(newSection.getDownStationId()) && includeDownStation(newSection.getDownStationId())) {
+                validateSectionDistance(newSection);
+                // update
+            }
+            if (includeUpStation(newSection.getUpStationId()) && !includeDownStation(newSection.getUpStationId())) {
+                validateSectionDistance(newSection);
+                // update
+            }
         }
-        sections.add(section);
+        sections.add(newSection);
     }
 
-    private void validateSection(Section addSection) {
-        boolean existUpStation = existStationByStationId(addSection.getUpStationId());
-        boolean existDownStation = existStationByStationId(addSection.getDownStationId());
+    private void validateSectionInLine(Section newSection) {
+        boolean existUpStation = existStationByStationId(newSection.getUpStationId());
+        boolean existDownStation = existStationByStationId(newSection.getDownStationId());
 
         validateBothStationsIncludeInLine(existUpStation, existDownStation);
         validateBothStationsExcludeInLine(existUpStation, existDownStation);
     }
 
     private boolean existStationByStationId(Long stationId) {
+        return includeUpStation(stationId) || includeDownStation(stationId);
+    }
+
+    private boolean includeUpStation(Long stationId) {
         return sections.stream()
-                .anyMatch(section -> section.getUpStationId().equals(stationId)
-                        || section.getDownStationId().equals(stationId));
+                .anyMatch(section -> section.getUpStationId().equals(stationId));
+    }
+
+    private boolean includeDownStation(Long stationId) {
+        return sections.stream()
+                .anyMatch(section -> section.getDownStationId().equals(stationId));
+    }
+
+    private void validateBothStationsIncludeInLine(boolean existUpStation, boolean existDownStation) {
+        if (!(existUpStation || existDownStation)) {
+            throw new IllegalArgumentException("상행역과 하행역이 모두 노선에 포함되어있지 않습니다.");
+        }
     }
 
     private void validateBothStationsExcludeInLine(boolean existUpStation, boolean existDownStation) {
@@ -42,9 +60,17 @@ public class Sections {
         }
     }
 
-    private void validateBothStationsIncludeInLine(boolean existUpStation, boolean existDownStation) {
-        if (!(existUpStation || existDownStation)) {
-            throw new IllegalArgumentException("상행역과 하행역이 모두 노선에 포함되어있지 않습니다.");
+    private void validateSectionDistance(Section newSection) {
+        if (newSection.getDistance() >= getExistDistance(newSection)) {
+            throw new IllegalArgumentException("구간의 길이는 기존 역 사이의 길이보다 작아야합니다.");
         }
+    }
+
+    private int getExistDistance(Section newSection) {
+        return sections.stream()
+                .filter(section -> section.getUpStationId().equals(newSection.getUpStationId()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 구간입니다."))
+                .getDistance();
     }
 }
--- src/test/java/wooteco/subway/domain/SectionsTest.java
+++ src/test/java/wooteco/subway/domain/SectionsTest.java
@@ -2,6 +2,7 @@ package wooteco.subway.domain;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,9 +13,7 @@ public class SectionsTest {
     @DisplayName("지하철 구간의 상행역과 하행역이 모두 노선에 존재하지 않은 경우 예외가 발생한다.")
     @Test
     void saveNotExistStations() {
-        Sections sections = new Sections();
-
-        sections.add(SECTION);
+        Sections sections = new Sections(List.of(SECTION));
 
         assertThatThrownBy(() -> sections.add(new Section(1L, 3L, 4L, 1)))
                 .isInstanceOf(IllegalArgumentException.class)
@@ -24,12 +23,20 @@ public class SectionsTest {
     @DisplayName("지하철 구간의 상행역과 하행역이 이미 모두 노선에 포함되어 있는 경우 예외가 발생한다.")
     @Test
     void saveExistAllStations() {
-        Sections sections = new Sections();
-
-        sections.add(SECTION);
+        Sections sections = new Sections(List.of(SECTION));
 
         assertThatThrownBy(() -> sections.add(new Section(1L, 2L, 1L, 1)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("상행역과 하행역이 이미 모두 노선에 포함되어 있습니다.");
+    }
+
+    @DisplayName("지하철 구간의 길이가 기존 역 사이의 길이보다 크거나 같은 경우 예외가 발생한다.")
+    @Test
+    void saveLongerDistance() {
+        Sections sections = new Sections(List.of(SECTION));
+
+        assertThatThrownBy(() -> sections.add(new Section(1L, 1L, 3L, 3)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("구간의 길이는 기존 역 사이의 길이보다 작아야합니다.");
     }
 }
