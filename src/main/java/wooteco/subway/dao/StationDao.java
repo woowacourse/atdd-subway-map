5ccb0f4d08977f79d9fde1a9a4ef4766ffcf53c8
skullKim <kyk990328@gmail.com>
2022-05-11 20:39:30 +0900

    feat(StationRepository.java): 역에 관련된 레파지토리 생성 완료

--- src/main/java/wooteco/subway/dao/StationDao.java
+++ src/main/java/wooteco/subway/dao/StationDao.java
@@ -7,10 +7,11 @@ import javax.sql.DataSource;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import wooteco.subway.domain.Station;
 
-@Repository
+@Component
 public class StationDao implements CommonStationDao {
 
     private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
--- src/main/java/wooteco/subway/service/StationService.java
+++ src/main/java/wooteco/subway/service/StationService.java
@@ -1,49 +1,67 @@
-package wooteco.subway.service;
-
-import java.util.List;
+package wooteco.subway.service; import java.util.List;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import wooteco.subway.dao.CommonStationDao;
 import wooteco.subway.domain.Station;
 import wooteco.subway.dto.request.StationRequest;
+import wooteco.subway.repository.StationRepository;
 
 @Service
 public class StationService {
 
-    private static final int NO_ROW_AFFECTED = 0;
-    private static final String STATION_DUPLICATED = "이미 존재하는 지하철역입니다. ";
-    private static final String STATION_NOT_FOUND = "요청한 지하철 역이 존재하지 않습니다. ";
+    private final StationRepository stationRepository;
 
-    private final CommonStationDao stationDao;
-
-    public StationService(final CommonStationDao stationDao) {
-        this.stationDao = stationDao;
+    public StationService(StationRepository stationRepository) {
+        this.stationRepository = stationRepository;
     }
 
-    @Transactional
     public Station save(final StationRequest stationRequest) {
         final Station station = new Station(stationRequest.getName());
-        try {
-            return stationDao.save(station);
-        } catch (
-        DuplicateKeyException e) {
-            throw new IllegalStateException(STATION_DUPLICATED + station);
-        }
+        return stationRepository.save(station);
     }
 
-
-    @Transactional(readOnly = true)
     public List<Station> findAll() {
-        return stationDao.findAll();
+        return stationRepository.findAll();
     }
 
-
-    @Transactional
     public void deleteById(final Long id) {
-        final int theNumberOfAffectedRow = stationDao.deleteById(id);
-        if (theNumberOfAffectedRow == NO_ROW_AFFECTED) {
-            throw new IllegalStateException(STATION_NOT_FOUND + "id=" + id);
-        }
+        stationRepository.deleteById(id);
     }
+
+    //    private static final int NO_ROW_AFFECTED = 0;
+//    private static final String STATION_DUPLICATED = "이미 존재하는 지하철역입니다. ";
+//    private static final String STATION_NOT_FOUND = "요청한 지하철 역이 존재하지 않습니다. ";
+//
+//    private final CommonStationDao stationDao;
+//
+//    public StationService(final CommonStationDao stationDao) {
+//        this.stationDao = stationDao;
+//    }
+//
+//    @Transactional
+//    public Station save(final StationRequest stationRequest) {
+//        final Station station = new Station(stationRequest.getName());
+//        try {
+//            return stationDao.save(station);
+//        } catch (
+//        DuplicateKeyException e) {
+//            throw new IllegalStateException(STATION_DUPLICATED + station);
+//        }
+//    }
+//
+//
+//    @Transactional(readOnly = true)
+//    public List<Station> findAll() {
+//        return stationDao.findAll();
+//    }
+//
+//
+//    @Transactional
+//    public void deleteById(final Long id) {
+//        final int theNumberOfAffectedRow = stationDao.deleteById(id);
+//        if (theNumberOfAffectedRow == NO_ROW_AFFECTED) {
+//            throw new IllegalStateException(STATION_NOT_FOUND + "id=" + id);
+//        }
+//    }
 }
