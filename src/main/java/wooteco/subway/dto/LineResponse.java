93b41302157e1c0ea050d02435f6e0cf81add618
awesomeo184 <awesomeo184@gmail.com>
2022-05-11 18:38:32 +0900

    refactor: 사용하지 않는 메서드 제거

--- src/main/java/wooteco/subway/dto/LineResponse.java
+++ src/main/java/wooteco/subway/dto/LineResponse.java
@@ -20,29 +20,11 @@ public class LineResponse {
         this.stations = stations;
     }
 
-    public static LineResponse of(final Line line, final Section section) {
-        return new LineResponse(line.getId(), line.getName(), line.getColor(),
-                List.of(StationResponse.from(section.getUpStation()),
-                        StationResponse.from(section.getDownStation())));
-    }
-
     public static LineResponse from(final Line line) {
         List<StationResponse> stationResponses = line.getStations().stream()
                 .map(StationResponse::from)
                 .collect(Collectors.toList());
         return new LineResponse(line.getId(), line.getName(), line.getColor(), stationResponses);
-    }
-
-    public static LineResponse of(final Line line, final List<Section> sections) {
-
-        List<StationResponse> stations = new ArrayList<>();
-
-        // TODO: 2022/05/11 구간 사이에 있는 역이 두번 들어감
-        for (Section section : sections) {
-            stations.add(StationResponse.from(section.getUpStation()));
-            stations.add(StationResponse.from(section.getDownStation()));
-        }
-        return new LineResponse(line.getId(), line.getName(), line.getColor(), stations);
     }
 
     public Long getId() {
@@ -76,5 +58,15 @@ public class LineResponse {
     @Override
     public int hashCode() {
         return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "LineResponse{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                ", stations=" + stations +
+                '}';
     }
 }
