7a79c4e830b7a57249a5ee7fd1b21a4ea6bde8e6
pup-paw <wldusdl0310@gmail.com>
2022-05-11 21:21:26 +0900

    test: 검색에 대한 테스트 추가

--- src/test/java/wooteco/subway/dao/SectionDaoTest.java
+++ src/test/java/wooteco/subway/dao/SectionDaoTest.java
@@ -2,6 +2,8 @@ package wooteco.subway.dao;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -42,6 +44,31 @@ public class SectionDaoTest {
         assertThat(actualUpStationId).isEqualTo(expectedUpStationId);
         assertThat(actualDownStationId).isEqualTo(expectedDownStationId);
         assertThat(actualDistance).isEqualTo(expectedDistance);
+    }
+
+    @Test
+    @DisplayName("id로 구간을 검색한다.")
+    void findById() {
+        SectionDto expected = sectionDao.save(1L, 2L, 3L, 5);
+
+        SectionDto actual = this.sectionDao.findById(1L).get(0);
+
+        assertThat(actual.getLineId()).isEqualTo(expected.getLineId());
+        assertThat(actual.getUpStationId()).isEqualTo(expected.getUpStationId());
+        assertThat(actual.getDownStationId()).isEqualTo(expected.getDownStationId());
+        assertThat(actual.getDistance()).isEqualTo(expected.getDistance());
+    }
+
+    @Test
+    @DisplayName("모든 구간을 검색한다.")
+    void findAll() {
+        sectionDao.save(1L, 2L, 3L, 4);
+        sectionDao.save(2L, 3L, 4L, 5);
+        sectionDao.save(3L, 4L, 5L, 6);
+
+        List<SectionDto> sections = sectionDao.findAll();
+
+        assertThat(sections.size()).isEqualTo(3);
     }
 
     @Test
