f0ffec8609a0a84a757c8ce206de248dedfcdb16
Ohzzi <clover7even@naver.com>
2022-05-11 09:58:42 +0900

    refactor: 역 목록을 돌려주는 기능을 Sections에 위임

--- src/main/java/wooteco/subway/domain/Line.java
+++ src/main/java/wooteco/subway/domain/Line.java
@@ -1,41 +1,31 @@
 package wooteco.subway.domain;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Line {
     private final Long id;
     private final String name;
     private final String color;
-    private final List<Section> sections;
+    private final Sections sections;
 
     public Line(String name, String color) {
-        this(null, name, color, new ArrayList<>());
+        this(null, name, color, new Sections(new ArrayList<>()));
     }
 
     public Line(Long id, String name, String color) {
-        this(id, name, color, new ArrayList<>());
+        this(id, name, color, new Sections(new ArrayList<>()));
     }
 
-    public Line(Long id, String name, String color, List<Section> sections) {
+    public Line(Long id, String name, String color, Sections sections) {
         this.id = id;
         this.name = name;
         this.color = color;
         this.sections = sections;
     }
 
-    public List<Station> getStations() {
-        List<Station> stations = new ArrayList<>();
-        for (Section section : sections) {
-            stations.add(section.getUpStation());
-            stations.add(section.getDownStation());
-        }
-        return stations.stream()
-                .distinct()
-                .sorted(Comparator.comparingLong(Station::getId))
-                .collect(Collectors.toUnmodifiableList());
+    public List<Station> getSortedStations() {
+        return sections.getSortedStations();
     }
 
     public Long getId() {
