1bda8fbf49ee11491056823b3b7a08dce1bb3240
Seongwon97 <osw970325@naver.com>
2022-05-11 13:30:48 +0900

    refactor(ExceptionHandlerController): 예외 메시지 수정

--- src/main/java/wooteco/subway/controller/ExceptionHandlerController.java
+++ src/main/java/wooteco/subway/controller/ExceptionHandlerController.java
@@ -29,7 +29,7 @@ public class ExceptionHandlerController {
     @ExceptionHandler(EmptyResultDataAccessException.class)
     public ResponseEntity<ErrorMessageResponse> handleEmptyResultDataAccessException() {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(new ErrorMessageResponse("요청한 리소스를 DB에서 찾을 수 없습니다."));
+                .body(new ErrorMessageResponse("요청한 리소스를 찾을 수 없습니다."));
     }
 
     @ExceptionHandler(DuplicateKeyException.class)
@@ -41,6 +41,6 @@ public class ExceptionHandlerController {
     @ExceptionHandler(DataAccessException.class)
     public ResponseEntity<ErrorMessageResponse> handleDataAccessException() {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(new ErrorMessageResponse("DB관련 작업에서 데이터 관련 작업중 오류가 발생했습니다."));
+                .body(new ErrorMessageResponse("데이터 관련 작업중 오류가 발생했습니다."));
     }
 }
