556c7c32f8126f3e1e39f4d2be8e9e9392c5c14a
2yujeong <yjfflq4872@gmail.com>
2022-05-11 20:04:38 +0900

    feat: SectionService 구간 등록 기능 구현

--- src/main/java/wooteco/subway/service/SectionService.java
+++ src/main/java/wooteco/subway/service/SectionService.java
@@ -1,9 +1,11 @@
 package wooteco.subway.service;
 
+import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import wooteco.subway.dao.SectionDao;
 import wooteco.subway.domain.Section;
+import wooteco.subway.domain.Sections;
 import wooteco.subway.dto.SectionRequest;
 
 @Service
@@ -27,5 +29,31 @@ public class SectionService {
 
     private Section createSection(Long lineId, long upStationId, long downStationId, int distance, Long lineOrder) {
         return new Section(null, lineId, upStationId, downStationId, distance, lineOrder);
+    }
+
+    public void save(Long lineId, SectionRequest sectionReq) {
+        save(lineId, sectionReq.getUpStationId(), sectionReq.getDownStationId(), sectionReq.getDistance());
+    }
+
+    private void save(Long lineId, long upStationId, long downStationId, int distance) {
+        Sections sections = new Sections(sectionDao.findAllByLineId(lineId));
+        sections.validateSection(upStationId, downStationId, distance);
+        Section section = sections.findOverlapSection(upStationId, downStationId, distance);
+
+        updateSections(lineId, section);
+        sectionDao.save(createSection(lineId, upStationId, downStationId, distance, section.getLineOrder()));
+    }
+
+    private void updateSections(Long lineId, Section section) {
+        if (section.getId() != null) {
+            sectionDao.deleteById(section.getId());
+            sectionDao.save(section);
+        }
+        sectionDao.updateLineOrder(lineId, section.getLineOrder());
+    }
+
+    public List<Long> findAllStationByLineId(long lineId) {
+        Sections sections = new Sections(sectionDao.findAllByLineId(lineId));
+        return sections.getStationsId();
     }
 }
--- src/test/java/wooteco/subway/service/SectionServiceTest.java
+++ src/test/java/wooteco/subway/service/SectionServiceTest.java
@@ -1,7 +1,8 @@
 package wooteco.subway.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.catchThrowable;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -33,5 +34,78 @@ class SectionServiceTest {
         assertThatCode(() ->
             sectionService.firstSave(1L, GIVEN_SECTION_REQ))
             .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("추가할 구간의 상행, 하행이 대상 노선에 둘 다 존재하는 경우 예외가 발생한다.")
+    void bothStationExistException() {
+        // given
+        sectionService.firstSave(1L, GIVEN_SECTION_REQ);
+
+        // when
+        Throwable thrown = catchThrowable(() -> sectionService.save(1L, GIVEN_SECTION_REQ));
+
+        // then
+        assertThat(thrown).isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("상행, 하행이 대상 노선에 둘 다 존재합니다.");
+    }
+
+    @Test
+    @DisplayName("초기 등록이 아닌데 추가할 구간의 상행, 하행이 대상 노선에 둘 다 존재하지 않는 경우 예외가 발생한다.")
+    void bothStationNotExistException() {
+        // given
+        sectionService.firstSave(1L, GIVEN_SECTION_REQ);
+
+        // when
+        Throwable thrown = catchThrowable(
+            () -> sectionService.save(1L, new SectionRequest(3L, 4L, 6)));
+
+        // then
+        assertThat(thrown).isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("상행, 하행이 대상 노선에 둘 다 존재하지 않습니다.");
+    }
+
+    @Test
+    @DisplayName("두 개의 역 중 하나가 노선에 상행으로 존재할 때, 등록할 구간의 거리가 기존 구간의 거리보다 크면 예외가 발생한다.")
+    void upStationDistanceException() {
+        // given
+        sectionService.firstSave(1L, GIVEN_SECTION_REQ);
+
+        // when
+        Throwable thrown = catchThrowable(
+            () -> sectionService.save(1L, new SectionRequest(1L, 3L, 6)));
+
+        // then
+        assertThat(thrown).isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("역 사이에 새로운 역을 등록할 경우, 기존 역 사이 길이보다 크거나 같으면 등록할 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("두 개의 역 중 하나가 노선에 하행으로 존재할 때, 등록할 구간의 거리가 기존 구간의 거리보다 크면 예외가 발생한다.")
+    void downStationDistanceException() {
+        // given
+        sectionService.firstSave(1L, GIVEN_SECTION_REQ);
+
+        // when
+        Throwable thrown = catchThrowable(
+            () -> sectionService.save(1L, new SectionRequest(3L, 2L, 6)));
+
+        // then
+        assertThat(thrown).isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("역 사이에 새로운 역을 등록할 경우, 기존 역 사이 길이보다 크거나 같으면 등록할 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("구간 등록 테스트")
+    void saveTest() {
+        // given
+        sectionService.firstSave(1L, new SectionRequest(1L, 3L, 6));
+
+        // when
+        sectionService.save(1L, new SectionRequest(1L, 2L, 3));
+
+        // then
+        assertThat(sectionService.findAllStationByLineId(1L))
+            .containsExactly(1L, 2L, 3L);
     }
 }
