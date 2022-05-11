7309a32250d75b2b0f0a0d6132f7809cfdf3618a
asebn1 <asebn1@khu.ac.kr>
2022-05-12 03:05:11 +0900

    refactor: DirtiesContext 제거 후 Sql로 적용

--- src/test/java/wooteco/subway/acceptance/AcceptanceTest.java
+++ src/test/java/wooteco/subway/acceptance/AcceptanceTest.java
@@ -4,10 +4,10 @@ import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.jdbc.Sql;
 
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = {"classpath:schema.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 public class AcceptanceTest{
     @LocalServerPort
     int port;
