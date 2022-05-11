5d863338452a6f7b4164ca5c48c1512b33d3cef3
skullKim <kyk990328@gmail.com>
2022-05-11 11:17:50 +0900

    refactor(SectionsTest.java): 테스트 코드 통합
    두 개로 나눠져 있던 테스트를 하나의 ParameterizedTest로 통합
    
    BREAKING CHANGE: 섹션 추가 테스트를 하나로 통합
    as-is:
    @DisplayName("Sections 는")
    class SectionsTest {
    
        @DisplayName("추가할 섹션의 위치를 찾고 섹션을 추가해야 한다.")
        @Test
        void add_Section() {
            final Sections sections = new Sections(new ArrayList<>());
            sections.addIfPossible(SectionFactory.from("ab3"));
            sections.addIfPossible(SectionFactory.from("ac3"));
            assertThat(sections).extracting("value").isEqualTo(List.of(SectionFactory.from("ac3"),
                    SectionFactory.from("cb3")));
        }
    
        @DisplayName("추가할 섹션의 위치를 찾고 섹션을 추가해야 한다.")
        @Test
        void add_Section2() {
            final Sections sections = new Sections(new ArrayList<>());
            sections.addIfPossible(SectionFactory.from("ab3"));
            sections.addIfPossible(SectionFactory.from("cb3"));
            assertThat(sections).extracting("value").isEqualTo(List.of(
                    SectionFactory.from("ac3"),SectionFactory.from("cb3")));
        }
    }
    
    to-be:
    @DisplayName("Sections 는")
    class SectionsTest {
    
        @DisplayName("추가할 섹션의 위치를 찾고 섹션을 추가해야 한다.")
        @ParameterizedTest(name = "{index} {displayName} section={0} expectedResult={1}")
        @MethodSource("provideAddSectionSource")
        void add_Section(final Section section, final List<Section> expectedResult) {
            final Sections sections = new Sections(new ArrayList<>());
            sections.addIfPossible(SectionFactory.from("ab3"));
            sections.addIfPossible(section);
            assertThat(sections).extracting("value").isEqualTo(expectedResult);
        }
    
        private static Stream<Arguments> provideAddSectionSource() {
            return Stream.of(
                    Arguments.of(SectionFactory.from("ac3"), List.of(SectionFactory.from("ac3"),
                            SectionFactory.from("cb3"))),
                    Arguments.of(SectionFactory.from ("cb3"), List.of(
                            SectionFactory.from("ac3"),SectionFactory.from("cb3")))
            );
        }
    }

--- src/test/java/wooteco/subway/domain/SectionsTest.java
+++ src/test/java/wooteco/subway/domain/SectionsTest.java
@@ -4,31 +4,31 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 @DisplayName("Sections 는")
 class SectionsTest {
 
     @DisplayName("추가할 섹션의 위치를 찾고 섹션을 추가해야 한다.")
-    @Test
-    void add_Section() {
+    @ParameterizedTest(name = "{index} {displayName} section={0} expectedResult={1}")
+    @MethodSource("provideAddSectionSource")
+    void add_Section(final Section section, final List<Section> expectedResult) {
         final Sections sections = new Sections(new ArrayList<>());
         sections.addIfPossible(SectionFactory.from("ab3"));
-        sections.addIfPossible(SectionFactory.from("ac3"));
-        assertThat(sections).extracting("value").isEqualTo(List.of(SectionFactory.from("ac3"),
-                SectionFactory.from("cb3")));
+        sections.addIfPossible(section);
+        assertThat(sections).extracting("value").isEqualTo(expectedResult);
     }
 
-
-    @DisplayName("추가할 섹션의 위치를 찾고 섹션을 추가해야 한다.")
-    @Test
-    void add_Section2() {
-        final Sections sections = new Sections(new ArrayList<>());
-        sections.addIfPossible(SectionFactory.from("ab3"));
-        sections.addIfPossible(SectionFactory.from("cb3"));
-        assertThat(sections).extracting("value").isEqualTo(List.of(
-                SectionFactory.from("ac3"),SectionFactory.from("cb3")));
+    private static Stream<Arguments> provideAddSectionSource() {
+        return Stream.of(
+                Arguments.of(SectionFactory.from("ac3"), List.of(SectionFactory.from("ac3"),
+                        SectionFactory.from("cb3"))),
+                Arguments.of(SectionFactory.from ("cb3"), List.of(
+                        SectionFactory.from("ac3"),SectionFactory.from("cb3")))
+        );
     }
 }
