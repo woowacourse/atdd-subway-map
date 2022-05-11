78c315fcfb952b70edfc29820d8c4b450c2b18c2
ulimy <ulim130@gmail.com>
2022-05-11 21:11:05 +0900

    추가할 수 없는 구간이라면 예외 발생 기능 추가

--- src/main/java/wooteco/subway/domain/Sections.java
+++ src/main/java/wooteco/subway/domain/Sections.java
@@ -4,6 +4,11 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Sections {
+
+    private static final String ALREADY_ADDED = "이미 등록되어있는 구간입니다.";
+    private static final String NO_SECTION = "추가할 수 있는 구간이 없습니다.";
+    private static final String TOO_LONG_DISTANCE = "해당 구간의 거리가 추가하려는 구간보다 더 짧습니다.";
+
     private final List<Section> sections;
 
     public Sections(List<Section> sections) {
@@ -23,11 +28,37 @@ public class Sections {
         return sortedIds;
     }
 
+    public void validateTarget(Section target) {
+        LinkedList<Long> sortedStations = getSortedStationIds();
+
+        if (sortedStations.contains(target.getUpStationId()) && sortedStations.contains(target.getDownStationId())) {
+            throw new IllegalArgumentException(ALREADY_ADDED);
+        }
+
+        if (!sortedStations.contains(target.getUpStationId()) && !sortedStations.contains(target.getDownStationId())) {
+            throw new IllegalArgumentException(NO_SECTION);
+        }
+    }
+
     public boolean isTerminus(Section section) {
         LinkedList<Long> sortedStations = getSortedStationIds();
-        return (section.isSameDownStation(sortedStations.getFirst()) ||
-                section.isSameUpStation(sortedStations.getLast())
+
+        return (section.isSameDown(sortedStations.getFirst()) ||
+                section.isSameUp(sortedStations.getLast())
         );
+    }
+
+    public Section findSource(Section target) {
+        Section source = sections.stream()
+                .filter(s -> s.isSource(target))
+                .findAny()
+                .get();
+
+        if (source.isShorterDistance(target)) {
+            throw new IllegalArgumentException(TOO_LONG_DISTANCE);
+        }
+
+        return source;
     }
 
     private void addUpStream(LinkedList<Long> result, Long key) {
