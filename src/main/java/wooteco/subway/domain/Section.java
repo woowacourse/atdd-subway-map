d3f09f2189d068739e554a27b057a5572750db07
yeon-06 <rnjstldus2@gmail.com>
2022-05-12 15:45:12 +0900

    refactor: 검증 로직 dto -> domain 이동

--- src/main/java/wooteco/subway/domain/Section.java
+++ src/main/java/wooteco/subway/domain/Section.java
@@ -4,6 +4,10 @@ import java.util.Objects;
 
 public class Section {
 
+    private static final String INVALID_DISTANCE_ERROR_MESSAGE = "유효하지 않은 거리입니다.";
+    private static final String DUPLICATED_SECTIONS_ERROR_MESSAGE = "상행과 하행은 같은 역으로 등록할 수 없습니다.";
+    private static final int MIN_DISTANCE = 1;
+
     private Long id;
     private Long lineId;
     private Long upStationId;
@@ -11,6 +15,7 @@ public class Section {
     private int distance;
 
     public Section(Long lineId, Long upStationId, Long downStationId, int distance) {
+        validSection(upStationId, downStationId, distance);
         this.lineId = lineId;
         this.upStationId = upStationId;
         this.downStationId = downStationId;
@@ -20,6 +25,23 @@ public class Section {
     public Section(Long id, Long lineId, Long upStationId, Long downStationId, int distance) {
         this(lineId, upStationId, downStationId, distance);
         this.id = id;
+    }
+
+    private void validSection(Long upStationId, Long downStationId, int distance) {
+        validDistance(distance);
+        validStations(upStationId, downStationId);
+    }
+
+    private void validDistance(int distance) {
+        if (distance < MIN_DISTANCE) {
+            throw new IllegalArgumentException(INVALID_DISTANCE_ERROR_MESSAGE);
+        }
+    }
+
+    private void validStations(Long upStationId, Long downStationId) {
+        if (downStationId.equals(upStationId)) {
+            throw new IllegalArgumentException(DUPLICATED_SECTIONS_ERROR_MESSAGE);
+        }
     }
 
     public void reduceDistance(Section other) {
--- src/main/java/wooteco/subway/ui/dto/SectionRequest.java
+++ src/main/java/wooteco/subway/ui/dto/SectionRequest.java
@@ -4,10 +4,6 @@ import wooteco.subway.domain.Section;
 
 public class SectionRequest {
 
-    private static final String INVALID_DISTANCE_ERROR_MESSAGE = "유효하지 않은 거리입니다.";
-    private static final String DUPLICATED_SECTIONS_ERROR_MESSAGE = "상행과 하행은 같은 역으로 등록할 수 없습니다.";
-    private static final int MIN_DISTANCE = 1;
-
     private Long upStationId;
     private Long downStationId;
     private int distance;
@@ -16,7 +12,6 @@ public class SectionRequest {
     }
 
     public SectionRequest(Long upStationId, Long downStationId, int distance) {
-        validSection(upStationId, downStationId, distance);
         this.upStationId = upStationId;
         this.downStationId = downStationId;
         this.distance = distance;
@@ -25,23 +20,6 @@ public class SectionRequest {
     public static SectionRequest from(LineCreateRequest lineCreateRequest) {
         return new SectionRequest(lineCreateRequest.getUpStationId(), lineCreateRequest.getDownStationId(),
                 lineCreateRequest.getDistance());
-    }
-
-    private void validSection(Long upStationId, Long downStationId, int distance) {
-        validDistance(distance);
-        validStations(upStationId, downStationId);
-    }
-
-    private void validDistance(int distance) {
-        if (distance < MIN_DISTANCE) {
-            throw new IllegalArgumentException(INVALID_DISTANCE_ERROR_MESSAGE);
-        }
-    }
-
-    private void validStations(Long upStationId, Long downStationId) {
-        if (downStationId.equals(upStationId)) {
-            throw new IllegalArgumentException(DUPLICATED_SECTIONS_ERROR_MESSAGE);
-        }
     }
 
     public Section toEntity(Long lineId) {
