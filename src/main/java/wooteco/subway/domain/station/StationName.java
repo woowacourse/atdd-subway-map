ff61f3e2d668cdf6ae3b2f4835715f1c40cc16ca
sinb57 <sinb57@naver.com>
2022-05-12 17:58:02 +0900

    feat: StationName equals 재정의

--- src/main/java/wooteco/subway/domain/station/StationName.java
+++ src/main/java/wooteco/subway/domain/station/StationName.java
@@ -1,5 +1,7 @@
 package wooteco.subway.domain.station;
 
+import java.util.Objects;
+
 public class StationName {
 
     private final String name;
@@ -17,6 +19,23 @@ public class StationName {
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        StationName that = (StationName) o;
+        return Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 
     @Override
--- src/test/java/wooteco/subway/domain/station/StationNameTest.java
+++ src/test/java/wooteco/subway/domain/station/StationNameTest.java
@@ -1,10 +1,14 @@
 package wooteco.subway.domain.station;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import wooteco.subway.domain.line.LineColor;
 
 class StationNameTest {
 
@@ -15,5 +19,14 @@ class StationNameTest {
         assertThatThrownBy(() -> new StationName(name))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("지하철역 이름은 공백이 될 수 없습니다.");
+    }
+
+    @DisplayName("지하철역 이름을 비교한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"강남역,선릉역,false", "광교역,광교역,true"})
+    void equals(String name1, String name2, boolean expected) {
+        StationName stationName1 = new StationName(name1);
+        StationName stationName2 = new StationName(name2);
+        assertThat(stationName1.equals(stationName2)).isEqualTo(expected);
     }
 }
