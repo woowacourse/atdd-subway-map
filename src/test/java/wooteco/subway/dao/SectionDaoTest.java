ff4fb9e377cb990a23ba95007b50e693ca0ba442
Kim Seunglae <obey1342@gmail.com>
2022-05-12 17:29:00 +0900

    test(test): SectionDao 테스트 케이스 추가

--- src/test/java/wooteco/subway/dao/SectionDaoTest.java
+++ src/test/java/wooteco/subway/dao/SectionDaoTest.java
@@ -41,4 +41,33 @@ class SectionDaoTest {
 
         assertThat(actual).isEqualTo(expected);
     }
+
+
+    @Test
+    void existByLineId_메서드_해당_Line_id로_존재_하는지_확인() {
+        dao.save(new Section(1L, new Station(1L, "문정역"), new Station(2L, "장지역"), 10));
+
+        assertThat(dao.existByLineId(1L)).isTrue();
+    }
+
+    @Test
+    void deleteByLineId_메서드는_Line_id로_해당_데이터를_삭제한다() {
+        dao.save(new Section(1L, new Station(1L, "문정역"), new Station(2L, "장지역"), 10));
+        dao.deleteByLineId(1L);
+
+        assertThat(dao.existByLineId(1L)).isFalse();
+    }
+
+    @Test
+    void saveSections_메서드는_구간들을_저장한다() {
+        Sections sections = new Sections(List.of(
+            new Section(1L, new Station(1L, "복정역"), new Station(2L, "가천대역"), 10),
+            new Section(2L, new Station(2L, "가천대역"), new Station(3L, "태평역"), 4)
+        ));
+        Line line = new Line(1L, "분당선", "노란색", sections);
+
+        dao.saveSections(line);
+
+        assertThatCode(() -> dao.saveSections(line)).doesNotThrowAnyException();
+    }
 }
