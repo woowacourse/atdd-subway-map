676b5987c6e223cd52bf24c5b56552a4ec3f8b55
nailseong <nnis9857@gmail.com>
2022-05-12 20:40:47 +0900

    feat(ExceptionAdvice): HttpMessageNotReadableException 핸들링 메서드

--- src/main/java/wooteco/subway/ui/ExceptionAdvice.java
+++ src/main/java/wooteco/subway/ui/ExceptionAdvice.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -16,6 +17,7 @@ import wooteco.subway.exception.SubwayMapException;
 @RestControllerAdvice
 public class ExceptionAdvice {
 
+    private static final String INVALID_REQUEST_FORMAT = "요청 값 형식이 올바르지 않습니다.";
     private static final String INTERNAL_SERVER_ERROR_MESSAGE = "서버가 요청을 처리할 수 없습니다.";
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -39,6 +41,13 @@ public class ExceptionAdvice {
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(body);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleHttpMessageException(final HttpMessageNotReadableException e) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ErrorResponse.from(INVALID_REQUEST_FORMAT));
     }
 
     @ExceptionHandler(SubwayMapException.class)
