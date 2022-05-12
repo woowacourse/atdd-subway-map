e0c0dac5a7c885191adcf160b4f775edb55c7dc4
hyewoncc <hyewoncc@gmail.com>
2022-05-12 17:15:14 +0900

    refactor: 레포지토리 - dao 계층 통일

--- src/main/java/wooteco/subway/repository/StationRepository.java
+++ src/main/java/wooteco/subway/repository/StationRepository.java
@@ -1,5 +1,6 @@
 package wooteco.subway.repository;
 
+import java.util.List;
 import org.springframework.stereotype.Repository;
 import wooteco.subway.dao.StationDao;
 import wooteco.subway.domain.Station;
@@ -13,7 +14,19 @@ public class StationRepository {
         this.stationDao = stationDao;
     }
 
+    public Station save(Station station) {
+        return stationDao.save(station);
+    }
+
+    public List<Station> findAll() {
+        return stationDao.findAll();
+    }
+
     public Station findById(Long id) {
         return stationDao.findById(id);
+    }
+
+    public void deleteById(Long id) {
+        stationDao.deleteById(id);
     }
 }
