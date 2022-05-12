3ebba4b92f170521f26dd120730ee41d022e5661
Seongwon97 <osw970325@naver.com>
2022-05-13 00:40:53 +0900

    test(StationAcceptanceTest): 인수 테스트 성격에 맞게 테스트 수정

--- src/test/java/wooteco/subway/controller/StationAcceptanceTest.java
+++ src/test/java/wooteco/subway/controller/StationAcceptanceTest.java
@@ -2,13 +2,9 @@ package wooteco.subway.controller;
 
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import wooteco.subway.dao.StationDao;
-import wooteco.subway.domain.Station;
 import wooteco.subway.dto.StationRequest;
 import wooteco.subway.dto.StationResponse;
 
@@ -16,72 +12,89 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
+@SuppressWarnings("NonAsciiCharacters")
 public class StationAcceptanceTest extends AcceptanceTest {
 
-    @Autowired
-    private StationDao stationDao;
-
-    private Station savedStation1;
-    private Station savedStation2;
     private final String basicPath = "/stations";
-
-    @BeforeEach
-    void setUpStations() {
-        savedStation1 = stationDao.insert(new Station("선릉역"));
-        savedStation2 = stationDao.insert(new Station("선정릉역"));
-    }
 
     @DisplayName("지하철역을 생성한다.")
     @Test
     void createStation() {
+        // given
         StationRequest request = new StationRequest("강남역");
 
+        // when
         ExtractableResponse<Response> response =
                 RestAssuredConvenienceMethod.postRequest(request, basicPath);
 
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
-        assertThat(response.header("Location")).isNotBlank();
+        // then
+        Long ResponseStationId = response.jsonPath().getObject(".", StationResponse.class).getId();
+        assertAll(
+                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value()),
+                () -> assertThat(response.header("Location")).isEqualTo("/stations/" + ResponseStationId)
+        );
     }
 
     @DisplayName("비어있는 값으로 이름을 생성하면 400번 코드를 반환한다.")
     @Test
     void createStationWithInvalidDataSize() {
+        // given
         StationRequest request = new StationRequest("");
 
+        // when
         ExtractableResponse<Response> response =
                 RestAssuredConvenienceMethod.postRequest(request, basicPath);
 
+        // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 
     @DisplayName("기존에 존재하는 지하철역 이름으로 지하철역을 생성하면 400번 코드를 반환한다.")
     @Test
     void createStationWithDuplicateName() {
+        // given
+        RestAssuredConvenienceMethod.postRequest(new StationRequest("선릉역"), basicPath);
         StationRequest request = new StationRequest("선릉역");
 
+        // when
         ExtractableResponse<Response> response =
                 RestAssuredConvenienceMethod.postRequest(request, basicPath);
 
+        // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 
     @DisplayName("지하철역을 조회한다.")
     @Test
     void getStations() {
+        // given
+        Long 선릉역_id = RestAssuredConvenienceMethod.postRequest(new StationRequest("선릉역"), basicPath)
+                .jsonPath().getObject(".", StationResponse.class).getId();
+        Long 선정릉역_id = RestAssuredConvenienceMethod.postRequest(new StationRequest("선정릉역"), basicPath)
+                .jsonPath().getObject(".", StationResponse.class).getId();
+
+        // when
         ExtractableResponse<Response> response = RestAssuredConvenienceMethod.getRequest(basicPath);
 
+        // then
         List<Long> resultLineIds = response.jsonPath().getList(".", StationResponse.class).stream()
                 .map(StationResponse::getId)
                 .collect(Collectors.toList());
-        assertThat(resultLineIds).containsAll(List.of(savedStation1.getId(), savedStation2.getId()));
+        assertThat(resultLineIds).containsAll(List.of(선릉역_id, 선정릉역_id));
     }
 
     @DisplayName("지하철역을 제거한다.")
     @Test
     void deleteStation() {
+        // given
+        Long 선릉역_id = RestAssuredConvenienceMethod.postRequest(new StationRequest("선릉역"), basicPath)
+                .jsonPath().getObject(".", StationResponse.class).getId();
+
+        // when
         ExtractableResponse<Response> response =
-                RestAssuredConvenienceMethod.deleteRequest("/stations/" + savedStation1.getId());
+                RestAssuredConvenienceMethod.deleteRequest("/stations/" + 선릉역_id);
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
     }
@@ -89,9 +102,11 @@ public class StationAcceptanceTest extends AcceptanceTest {
     @DisplayName("존재하지 않는 지하철역을 삭제하려하면 400번 코드를 반환한다.")
     @Test
     void deleteStationWithNotExistData() {
+        // when
         ExtractableResponse<Response> response =
                 RestAssuredConvenienceMethod.deleteRequest("/stations/" + 100L);
 
+        // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 }
