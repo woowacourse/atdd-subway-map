e3053795313393518f99e315bb18e334b5466886
jojogreen91 <iawbg13@gmail.com>
2022-05-12 16:45:51 +0900

    docs: jdbcSectionDaoSetting.sql 수정

--- src/test/resources/jdbcSectionDaoSetting.sql
+++ src/test/resources/jdbcSectionDaoSetting.sql
@@ -1,33 +1,6 @@
-drop table SECTION if exists;
-drop table LINE if exists;
-drop table STATION if exists;
-
-create table if not exists STATION
-(
-    id bigint auto_increment not null,
-    name varchar(255) not null unique,
-    primary key (id)
-    );
-
-create table if not exists LINE
-(
-    id bigint auto_increment not null,
-    name varchar(255) not null unique,
-    color varchar(20) not null,
-    primary key (id)
-    );
-
-create table if not exists SECTION
-(
-    id bigint auto_increment not null,
-    line_id bigint not null,
-    up_station_id bigint not null,
-    down_station_id bigint not null,
-    distance int,
-    primary key (id),
-    foreign key (line_id) references LINE (id) on delete cascade
-    );
-
+alter table SECTION alter column id restart with 1;
+alter table LINE alter column id restart with 1;
+alter table STATION alter column id restart with 1;
 
 insert into STATION(name) values ('강남역');
 insert into STATION(name) values ('역삼역');
