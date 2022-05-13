dad54be6674238552bea4ef3072c9c379d7fb6dc
RunaNam <nmj5012@naver.com>
2022-05-13 23:54:15 +0900

    refactor(Section): 상수값 추출

--- src/main/java/wooteco/subway/domain/Section.java
+++ src/main/java/wooteco/subway/domain/Section.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 public class Section {
     private static final String SAME_UP_AND_DOWN_STATION_ERROR = "상행과 하행의 지하철 역이 같을 수 없습니다.";
     private static final String NON_POSITIVE_DISTANCE_ERROR = "거리는 양수여야 합니다.";
+    private static final String DISTANCE_IS_LONGER_THAN_THIS = "기존 역 사이보다 긴 길이를 등록할 수 없습니다.";
 
     private final Long id;
     private final Long upStationId;
@@ -70,7 +71,7 @@ public class Section {
 
     private void checkDistanceIsLongerThan(Section section) {
         if (distance <= section.distance) {
-            throw new IllegalArgumentException("기존 역 사이보다 긴 길이를 등록할 수 없습니다.");
+            throw new IllegalArgumentException(DISTANCE_IS_LONGER_THAN_THIS);
         }
     }
 }
