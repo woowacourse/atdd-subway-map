5fe751f33be46c1816b75b10a661e509f177f411
jayjaehunchoi <wogns0108@naver.com>
2022-05-13 20:53:52 +0900

    refactor : 에러에 대한 내용 Logging

--- src/main/java/wooteco/subway/ui/ControllerAdvice.java
+++ src/main/java/wooteco/subway/ui/ControllerAdvice.java
@@ -1,5 +1,7 @@
 package wooteco.subway.ui;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,21 +11,27 @@ import wooteco.subway.utils.exception.SubwayException;
 @RestControllerAdvice
 public class ControllerAdvice {
 
+    private final Logger logger;
+
+    public ControllerAdvice() {
+        logger = LoggerFactory.getLogger(this.getClass());
+    }
+
     @ExceptionHandler(SubwayException.class)
     public ResponseEntity<ErrorResponse> subwayExceptionHandler(SubwayException e) {
-        e.printStackTrace();
+        logger.error(e.getMessage(), e);
         return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
     }
 
     @ExceptionHandler(NullPointerException.class)
     public ResponseEntity<ErrorResponse> nullPointerExceptionHandler(NullPointerException e) {
-        e.printStackTrace();
+        logger.error(e.getMessage(), e);
         return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> ExceptionHandler(Exception e) {
-        e.printStackTrace();
+        logger.error(e.getMessage(), e);
         return ResponseEntity.internalServerError().body(new ErrorResponse("[ERROR] 데이터를 조회할 수 없습니다."));
     }
 }
