e97ad1fc9dc818c28bd19ad11a8f6c588a6372c5
skullKim <kyk990328@gmail.com>
2022-05-14 21:03:32 +0900

    fix(LineAcceptanceTest.java): 깨지는 테스트 수정
    요청에 필요한 데이터가 바뀜에 따라 꺠지는 테스트를 통과되게 수정
    
    BREAKING CHANGE: 라인 생성 전 필요한 역을 생성하는 기능 추가
    as-is:
    @DisplayName("노선 관련 기능")
    public class LineAcceptanceTest extends AcceptanceTest {
        ...
    
         private ExtractableResponse<Response> createLine(final String name, final String color) {
            final Map<String, String> params = new HashMap<>();
            params.put("name", name);
            params.put("color", color);
            return RestAssured.given().log().all()
                ...
            }
    
        ...
    }
    
    to-be:
    @DisplayName("노선 관련 기능")
    public class LineAcceptanceTest extends AcceptanceTest {
        ...
    
        private ExtractableResponse<Response> createLineAfterCreatingStation(final String name, final String color,
                                                                             final String upStationName,
                                                                             final String downStationName,
                                                                             final int distance) {
            final ExtractableResponse<Response> stationResponse1 = createStation(upStationName);
            final ExtractableResponse<Response> stationResponse2 = createStation(downStationName);
            final Long upStationId = stationResponse1.jsonPath().getObject(".", StationResponse.class).getId();
            final Long downStationId = stationResponse2.jsonPath().getObject(".", StationResponse.class).getId();
            final Map<String, Object> params = new HashMap<>();
            params.put("name", name);
            params.put("color", color);
            params.put("upStationId", upStationId);
            params.put("downStationId", downStationId);
            params.put("distance", distance);
            return RestAssured.given().log().all()
                    .body(params)
                    .contentType(MediaType.APPLICATION_JSON_VALUE)
                    .when()
                    .post("/lines")
                    .then().log().all()
                    .extract();
        }
    
        ...
    
        private ExtractableResponse<Response> createStation(final String name) {
            final Map<String, String> params = new HashMap<>();
            params.put("name", name);
            return RestAssured.given().log().all()
                    .body(params)
                    .contentType(MediaType.APPLICATION_JSON_VALUE)
                    .when()
                    .post("/stations")
                    .then().log().all()
                    .extract();
        }
    
    }

--- src/test/java/wooteco/subway/acceptance/LineAcceptanceTest.java
+++ src/test/java/wooteco/subway/acceptance/LineAcceptanceTest.java
@@ -18,14 +18,16 @@ import org.junit.jupiter.api.TestFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import wooteco.subway.dto.response.LineResponse;
+import wooteco.subway.dto.response.StationResponse;
 
 @DisplayName("노선 관련 기능")
 public class LineAcceptanceTest extends AcceptanceTest {
 
     @DisplayName("노선을 생성한다.")
     @Test
-    void createLine() {
-        final ExtractableResponse<Response> response = createLine("신분당선", "bg-red-600");
+    void createLineAfterCreatingStation() {
+        final ExtractableResponse<Response> response = createLineAfterCreatingStation("신분당선", "bg-red-600",
+                "a", "b", 5);
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
         assertThat(response.header("Location")).isNotBlank();
@@ -34,8 +36,8 @@ public class LineAcceptanceTest extends AcceptanceTest {
     @DisplayName("기존에 존재하는 노선 이름으로 노선 생성 시도 시 Bad request가 응답된다.")
     @Test
     void createLineWithDuplicateName() {
-        createLine("강남역", "bg-red-600");
-        final ExtractableResponse<Response> response = createLine("강남역", "bg-red-600");
+        createLineAfterCreatingStation("8호선", "bg-red-600", "a", "b", 5);
+        final ExtractableResponse<Response> response = createLineAfterCreatingStation("8호선", "bg-red-600", "c", "d", 5);
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
@@ -43,8 +45,10 @@ public class LineAcceptanceTest extends AcceptanceTest {
     @DisplayName("노선을 조회한다.")
     @TestFactory
     Stream<DynamicTest> getLinesTest() {
-        final ExtractableResponse<Response> createResponse1 = createLine("1호선", "bg-red-600");
-        final ExtractableResponse<Response> createResponse2 = createLine("2호선", "bg-green-600");
+        final ExtractableResponse<Response> createResponse1 = createLineAfterCreatingStation("1호선", "bg-red-600",
+                "a", "B", 10);
+        final ExtractableResponse<Response> createResponse2 = createLineAfterCreatingStation("2호선", "bg-green-600",
+                "c", "d", 10);
 
         return Stream.of(
                 DynamicTest.dynamicTest("생성된 노선 목록을 불러온다", () -> {
@@ -69,7 +73,8 @@ public class LineAcceptanceTest extends AcceptanceTest {
     @DisplayName("노선을 제거한다.")
     @Test
     void deleteLine() {
-        final ExtractableResponse<Response> createResponse = createLine("1호선", "bg-red-600");
+        final ExtractableResponse<Response> createResponse = createLineAfterCreatingStation("1호선", "bg-red-600",
+                "a", "b", 10);
 
         final String uri = createResponse.header("Location");
         final ExtractableResponse<Response> response = RestAssured.given().log().all()
@@ -84,7 +89,8 @@ public class LineAcceptanceTest extends AcceptanceTest {
     @DisplayName("특정 노선을 조회한다.")
     @Test
     void getLine() {
-        final ExtractableResponse<Response> createResponse = createLine("1호선", "bg-red-600");
+        final ExtractableResponse<Response> createResponse = createLineAfterCreatingStation("1호선", "bg-red-600",
+                "a", "b", 10);
         final LineResponse expected = createResponse.jsonPath().getObject(".", LineResponse.class);
 
         final ExtractableResponse<Response> response = getLine(createResponse.header("Location"));
@@ -97,7 +103,8 @@ public class LineAcceptanceTest extends AcceptanceTest {
     @DisplayName("노선을 갱신한다.")
     @Test
     void updateLine() {
-        final ExtractableResponse<Response> createResponse = createLine("1호선", "bg-red-600");
+        final ExtractableResponse<Response> createResponse = createLineAfterCreatingStation("1호선", "bg-red-600",
+                "a", "b", 10);
         final LineResponse expected = createResponse.jsonPath().getObject(".", LineResponse.class);
 
         final String newLineName = "11호선";
@@ -128,10 +135,20 @@ public class LineAcceptanceTest extends AcceptanceTest {
 
     }
 
-    private ExtractableResponse<Response> createLine(final String name, final String color) {
-        final Map<String, String> params = new HashMap<>();
+    private ExtractableResponse<Response> createLineAfterCreatingStation(final String name, final String color,
+                                                                         final String upStationName,
+                                                                         final String downStationName,
+                                                                         final int distance) {
+        final ExtractableResponse<Response> stationResponse1 = createStation(upStationName);
+        final ExtractableResponse<Response> stationResponse2 = createStation(downStationName);
+        final Long upStationId = stationResponse1.jsonPath().getObject(".", StationResponse.class).getId();
+        final Long downStationId = stationResponse2.jsonPath().getObject(".", StationResponse.class).getId();
+        final Map<String, Object> params = new HashMap<>();
         params.put("name", name);
         params.put("color", color);
+        params.put("upStationId", upStationId);
+        params.put("downStationId", downStationId);
+        params.put("distance", distance);
         return RestAssured.given().log().all()
                 .body(params)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -153,6 +170,18 @@ public class LineAcceptanceTest extends AcceptanceTest {
         return RestAssured.given().log().all()
                 .when()
                 .get(url)
+                .then().log().all()
+                .extract();
+    }
+
+    private ExtractableResponse<Response> createStation(final String name) {
+        final Map<String, String> params = new HashMap<>();
+        params.put("name", name);
+        return RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post("/stations")
                 .then().log().all()
                 .extract();
     }
