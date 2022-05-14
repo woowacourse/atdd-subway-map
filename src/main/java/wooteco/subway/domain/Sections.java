1c7f569c553d2ccd5c7b02fa4018eb2e5257f6cd
pup-paw <wldusdl0310@gmail.com>
2022-05-14 17:50:25 +0900

    refactor: 작은 기능 별로 메서드 분리

--- src/main/java/wooteco/subway/domain/Sections.java
+++ src/main/java/wooteco/subway/domain/Sections.java
@@ -49,51 +49,6 @@ public class Sections {
         }
     }
 
-    public void deleteSection(Station station) {
-        if (sections.size() == 1) {
-            throw new IllegalArgumentException("마지막 구간은 삭제할 수 없습니다.");
-        }
-        Optional<Section> sameUpStation = findSameUpStation(station);
-        Optional<Section> sameDownStation = findSameDownStation(station);
-        if (sameUpStation.isPresent() && sameDownStation.isPresent()) {
-            Section upSection = sameDownStation.get();
-            Section downSection = sameUpStation.get();
-
-            Section concatSection = upSection.concatSections(downSection);
-            sections.remove(upSection);
-            sections.remove(downSection);
-            sections.add(concatSection);
-            return;
-        }
-        throw new IllegalArgumentException("해당 역이 포함된 구간이 존재하지 않습니다.");
-    }
-
-    public void updateSection(Station upStation, Station downStation, int distance) {
-        Optional<Section> sameUpStation = findSameUpStation(upStation);
-        if (sameUpStation.isPresent()) {
-            Section section = sameUpStation.get();
-            List<Section> splitSections = section.splitSectionIfSameUpStation(downStation, distance);
-            sections.add(splitSections.get(0));
-            sections.add(splitSections.get(1));
-            sections.remove(section);
-            return;
-        }
-        Optional<Section> sameDownStation = findSameDownStation(downStation);
-        if (sameDownStation.isPresent()) {
-            Section section = sameDownStation.get();
-            List<Section> splitSections = section.splitSectionIfSameDownStation(upStation, distance);
-            sections.add(splitSections.get(0));
-            sections.add(splitSections.get(1));
-            sections.remove(section);
-            return;
-        }
-        sections.add(new Section(getLineId(), upStation, downStation, distance));
-    }
-
-    public long getLineId() {
-        return sections.get(0).getLineId();
-    }
-
     private Optional<Section> findSameUpStation(Station upStation) {
         return sections.stream()
                 .filter(section -> section.getUpStation().equals(upStation))
@@ -104,6 +59,61 @@ public class Sections {
         return sections.stream()
                 .filter(section -> section.getDownStation().equals(downStation))
                 .findFirst();
+    }
+
+    public void updateSection(Station upStation, Station downStation, int distance) {
+        Optional<Section> sameUpStation = findSameUpStation(upStation);
+        if (sameUpStation.isPresent()) {
+            Section section = sameUpStation.get();
+            List<Section> splitSections = section.splitSectionIfSameUpStation(downStation, distance);
+            update(section, splitSections);
+            return;
+        }
+
+        Optional<Section> sameDownStation = findSameDownStation(downStation);
+        if (sameDownStation.isPresent()) {
+            Section section = sameDownStation.get();
+            List<Section> splitSections = section.splitSectionIfSameDownStation(upStation, distance);
+            update(section, splitSections);
+            return;
+        }
+
+        sections.add(new Section(getLineId(), upStation, downStation, distance));
+    }
+
+    private void update(Section section, List<Section> splitSections) {
+        sections.add(splitSections.get(0));
+        sections.add(splitSections.get(1));
+        sections.remove(section);
+    }
+
+    public void deleteSection(Station station) {
+        if (sections.size() == 1) {
+            throw new IllegalArgumentException("마지막 구간은 삭제할 수 없습니다.");
+        }
+
+        Optional<Section> sameUpStation = findSameUpStation(station);
+        Optional<Section> sameDownStation = findSameDownStation(station);
+        if (sameUpStation.isPresent() && sameDownStation.isPresent()) {
+            Section upSection = sameDownStation.get();
+            Section downSection = sameUpStation.get();
+            Section concatSection = upSection.concatSections(downSection);
+
+            delete(upSection, downSection, concatSection);
+            return;
+        }
+
+        throw new IllegalArgumentException("해당 역이 포함된 구간이 존재하지 않습니다.");
+    }
+
+    private void delete(Section upSection, Section downSection, Section concatSection) {
+        sections.remove(upSection);
+        sections.remove(downSection);
+        sections.add(concatSection);
+    }
+
+    public long getLineId() {
+        return sections.get(0).getLineId();
     }
 
     public boolean isPresent() {
