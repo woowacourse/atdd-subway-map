27b89982552d300512db9bd126a205c7864cbae5
sinb57 <sinb57@naver.com>
2022-05-15 04:52:09 +0900

    test: LineDao 테스트 보충 및 리팩터링

--- src/test/java/wooteco/subway/repository/dao/LineDaoTest.java
+++ src/test/java/wooteco/subway/repository/dao/LineDaoTest.java
@@ -1,34 +1,36 @@
 package wooteco.subway.repository.dao;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
 
-import java.util.List;
 import java.util.Optional;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 
 import javax.sql.DataSource;
-import wooteco.subway.domain.line.Line;
-import wooteco.subway.domain.section.Section;
-import wooteco.subway.domain.station.Station;
-import wooteco.subway.repository.dao.entity.EntityAssembler;
 import wooteco.subway.repository.dao.entity.line.LineEntity;
 
+@DisplayName("지하철노선 DB")
 @JdbcTest
 class LineDaoTest {
 
-    private static final List<Section> sections = List.of(
-            new Section(new Station(1L, "강남역"), new Station(2L, "선릉역"), 3)
-    );
-    
+    private static final String LINE_NAME = "신분당선";
+    private static final String LINE_COLOR = "red";
+    private static final LineEntity LINE_ENTITY = new LineEntity(1L, LINE_NAME, LINE_COLOR);
+
     @Autowired
     private DataSource dataSource;
-    private wooteco.subway.repository.dao.LineDao lineDao;
+    private LineDao lineDao;
 
     @BeforeEach
     void setUp() {
@@ -38,78 +40,105 @@ class LineDaoTest {
     @DisplayName("지하철노선을 저장한다.")
     @Test
     void save() {
-        LineEntity lineEntity = EntityAssembler.lineEntity(new Line(sections, "신분당선", "bg-red-600"));
-        assertThat(lineDao.save(lineEntity)).isGreaterThan(0);
+        Long actual = lineDao.save(LINE_ENTITY);
+        assertThat(actual).isGreaterThan(0);
     }
 
-    @DisplayName("지하철노선 목록을 조회한다.")
-    @Test
-    void findAll() {
-        List<LineEntity> lines = List.of(
-                EntityAssembler.lineEntity(new Line(sections, "신분당선", "bg-red-600")),
-                EntityAssembler.lineEntity(new Line(sections, "1호선", "bg-red-601")),
-                EntityAssembler.lineEntity(new Line(sections, "2호선", "bg-red-602"))
-        );
-        lines.forEach(lineDao::save);
-        assertThat(lineDao.findAll()).hasSize(3);
+    @DisplayName("노선 목록을 조회한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {5})
+    void findAll(int expected) {
+        LongStream.rangeClosed(1, expected)
+                .mapToObj(id -> new LineEntity(id, "호선" + id, "색상" + id))
+                .forEach(lineDao::save);
+
+        assertThat(lineDao.findAll()).hasSize(expected);
     }
 
-    @DisplayName("지하철노선을 조회한다.")
+    @DisplayName("노선을 조회한다.")
     @Test
     void findById() {
-        LineEntity expected = EntityAssembler.lineEntity(new Line(sections, "신분당선", "bg-red-600"));
-        Long lineId = lineDao.save(expected);
-        Optional<LineEntity> lineEntity = lineDao.findById(lineId);
-        assertAll(
-                () -> assertThat(lineEntity.isPresent()).isTrue(),
-                () -> assertThat(lineEntity.get()).usingRecursiveComparison()
-                        .ignoringFields("id")
-                        .isEqualTo(expected)
-        );
+        LineEntity expected = LINE_ENTITY;
+        Optional<LineEntity> actual = lineDao.findById(lineDao.save(expected));
+
+        assertThat(actual).isPresent();
+        assertThat(actual.get()).usingRecursiveComparison()
+                .ignoringFields("id")
+                .isEqualTo(expected);
     }
 
-    @DisplayName("존재하지 않는 지하철노선을 조회한다.")
+    @DisplayName("존재하지 않는 노선을 조회한다.")
     @Test
     void findWithNonexistentId() {
-        Optional<LineEntity> lineEntity = lineDao.findById(1L);
-        assertThat(lineEntity.isEmpty()).isTrue();
+        Optional<LineEntity> actual = lineDao.findById(1L);
+        assertThat(actual).isEmpty();
     }
 
-    @DisplayName("지하철 노선 정보를 수정한다.")
-    @Test
-    void update() {
-        Long lineId = lineDao.save(EntityAssembler.lineEntity(new Line(sections, "신분당선", "bg-red-600")));
-        LineEntity expected = EntityAssembler.lineEntity(new Line(lineId, sections, "분당선", "bg-blue-600"));
-        lineDao.update(expected);
-        Optional<LineEntity> actual = lineDao.findById(lineId);
-        assertAll(
-                () -> assertThat(actual.isPresent()).isTrue(),
-                () -> assertThat(actual.get()).usingRecursiveComparison()
-                        .isEqualTo(expected)
-        );
-    }
+    @DisplayName("해당 식별자의 노선이 존재하는지 확인한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"0,true", "1,false"})
+    void existsById(Long difference, boolean expected) {
+        Long lineId = lineDao.save(LINE_ENTITY);
 
-    @DisplayName("지하철 노선을 삭제한다.")
-    @Test
-    void remove() {
-        Long lineId = lineDao.save(EntityAssembler.lineEntity(new Line(sections, "신분당선", "bg-red-600")));
-        lineDao.remove(lineId);
-        assertThat(lineDao.findAll()).isEmpty();
+        boolean actual = lineDao.existsById(lineId + difference);
+        assertThat(actual).isEqualTo(expected);
     }
 
     @DisplayName("해당 이름의 노선이 존재하는지 확인한다.")
-    @Test
-    void existsByName() {
-        LineEntity lineEntity = EntityAssembler.lineEntity(new Line(sections, "신분당선", "bg-red-600"));
-        lineDao.save(lineEntity);
-        assertThat(lineDao.existsByName(lineEntity.getName())).isTrue();
+    @ParameterizedTest
+    @MethodSource("provideForExistsByName")
+    void existsByName(String lineName, boolean expected) {
+        lineDao.save(LINE_ENTITY);
+
+        boolean actual = lineDao.existsByName(lineName);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> provideForExistsByName() {
+        return Stream.of(
+                Arguments.of(LINE_NAME, true),
+                Arguments.of(LINE_NAME + "temp", false));
     }
 
     @DisplayName("해당 색상의 노선이 존재하는지 확인한다.")
+    @ParameterizedTest
+    @MethodSource("provideForExistsByColor")
+    void existsByColor(String lineColor, boolean expected) {
+        lineDao.save(LINE_ENTITY);
+
+        boolean actual = lineDao.existsByColor(lineColor);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> provideForExistsByColor() {
+        return Stream.of(
+                Arguments.of(LINE_COLOR, true),
+                Arguments.of(LINE_COLOR + "temp", false));
+    }
+
+    @DisplayName("노선 정보를 수정한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"2호선,black", "1호선,white"})
+    void update(String lineName, String lineColor) {
+        Long lineId = lineDao.save(LINE_ENTITY);
+
+        LineEntity expected = new LineEntity(lineId, lineName, lineColor);
+        lineDao.update(expected);
+
+        Optional<LineEntity> actual = lineDao.findById(lineId);
+
+        assertThat(actual).isPresent();
+        assertThat(actual.get()).usingRecursiveComparison()
+                .isEqualTo(expected);
+    }
+
+    @DisplayName("노선을 삭제한다.")
     @Test
-    void existsByColor() {
-        LineEntity lineEntity = EntityAssembler.lineEntity(new Line(sections, "신분당선", "bg-red-600"));
-        lineDao.save(lineEntity);
-        assertThat(lineDao.existsByColor(lineEntity.getColor())).isTrue();
+    void remove() {
+        Stream.of(LINE_ENTITY)
+                .map(lineDao::save)
+                .forEach(lineDao::remove);
+
+        assertThat(lineDao.findAll()).isEmpty();
     }
 }
