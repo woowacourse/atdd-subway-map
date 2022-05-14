7f5fa61612c43abad52ecc9315ec843135f46ec8
hyeonic <dev.hyeonic@gmail.com>
2022-05-14 15:57:42 +0900

    feat: Section 동등성 보장을 위한 equals hashCode 추가

--- src/main/java/wooteco/subway/domain/Section.java
+++ src/main/java/wooteco/subway/domain/Section.java
@@ -1,5 +1,7 @@
 package wooteco.subway.domain;
 
+import java.util.Objects;
+
 public class Section {
 
     private final Long id;
@@ -68,5 +70,24 @@ public class Section {
 
     public int getDistance() {
         return distance;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Section section = (Section) o;
+        return distance == section.distance && Objects.equals(id, section.id) && Objects.equals(lineId,
+                section.lineId) && Objects.equals(upStationId, section.upStationId) && Objects.equals(
+                downStationId, section.downStationId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, lineId, upStationId, downStationId, distance);
     }
 }
