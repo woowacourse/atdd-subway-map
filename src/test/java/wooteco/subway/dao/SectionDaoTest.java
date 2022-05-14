5cb2d7ec9410e96973c19561e47e505c8f55361c
corinne <yoo77hyeon@gmail.com>
2022-05-15 02:04:53 +0900

    test: jdbctest에 transactional 제거

--- src/test/java/wooteco/subway/dao/SectionDaoTest.java
+++ src/test/java/wooteco/subway/dao/SectionDaoTest.java
@@ -17,7 +17,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 @JdbcTest
-@Transactional
 class SectionDaoTest {
 
     private final SectionDao sectionDao;
