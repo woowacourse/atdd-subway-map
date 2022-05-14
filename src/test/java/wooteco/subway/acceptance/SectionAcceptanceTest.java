6456cf34dcc4c8afd8cb7de3a5677d14c6464475
seong-wooo <seongwoo0513@uos.ac.kr>
2022-05-14 19:08:34 +0900

    refactor: 구간 추가 테스트에서 추가된 구간을 조회하는 테스트 작성

--- src/test/java/wooteco/subway/acceptance/SectionAcceptanceTest.java
+++ src/test/java/wooteco/subway/acceptance/SectionAcceptanceTest.java
@@ -1,11 +1,13 @@
 package wooteco.subway.acceptance;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
 
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,6 +15,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import wooteco.subway.dto.SectionRequest;
+import wooteco.subway.dto.StationResponse;
 
 @Sql("/sectionTestSchema.sql")
 public class SectionAcceptanceTest extends AcceptanceTest {
@@ -27,13 +30,14 @@ public class SectionAcceptanceTest extends AcceptanceTest {
         params.put("upStationId", "1");
         params.put("downStationId", "2");
         params.put("distance", "10");
-        RestAssured.given().log().all()
+        String location = RestAssured.given().log().all()
                 .body(params)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when()
                 .post("/lines")
                 .then().log().all()
-                .extract();
+                .extract()
+                .header("location");
 
         SectionRequest sectionRequest = new SectionRequest(2L, 3L, 6);
 
@@ -46,8 +50,17 @@ public class SectionAcceptanceTest extends AcceptanceTest {
                 .then().log().all()
                 .extract();
 
+        ExtractableResponse<Response> lineResponse = RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .get(location)
+                .then().log().all()
+                .extract();
+        List<StationResponse> stations = lineResponse.jsonPath().getList("stations", StationResponse.class);
         //then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(stations).extracting("id", "name")
+                .containsExactly(tuple(1L, "신도림역"), tuple(2L, "왕십리역"), tuple(3L, "용산역"));
     }
 
     @Test
