e63ed02e1bd874bfde42c2756a1029a767d62d0f
corinne <yoo77hyeon@gmail.com>
2022-05-15 02:00:11 +0900

    refactor: Line equals hashcode 삭제

--- src/main/java/wooteco/subway/domain/Line.java
+++ src/main/java/wooteco/subway/domain/Line.java
@@ -32,17 +32,4 @@ public class Line {
     public String getColor() {
         return color;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Line)) return false;
-        Line line = (Line) o;
-        return Objects.equals(id, line.id) && Objects.equals(name, line.name) && Objects.equals(color, line.color);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, color);
-    }
 }
