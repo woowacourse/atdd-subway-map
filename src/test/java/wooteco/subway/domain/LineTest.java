b5f70feb18798222734b5651f4f691eeeaa2aeea
yangdongjue5510 <djwhy5510@gmail.com>
2022-05-15 20:06:25 +0900

    refactor(Line): Sections의 책임을 Line으로 이동

--- src/test/java/wooteco/subway/domain/LineTest.java
+++ src/test/java/wooteco/subway/domain/LineTest.java
@@ -3,12 +3,25 @@ package wooteco.subway.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import wooteco.subway.exception.ExceptionMessage;
 
 class LineTest {
+
+    private Line line;
+
+    @BeforeEach
+    void setUp() {
+        Section section1 = new Section(1L, 4L, 2L, 10);
+        Section section2 = new Section(1L, 2L, 3L, 10);
+        Section section3 = new Section(1L, 3L, 1L, 10);
+        line = new Line("1호선", "red", List.of(section1, section2, section3));
+    }
 
     @ParameterizedTest
     @DisplayName("노선 이름이 공백이면 예외가 발생한다")
@@ -27,5 +40,37 @@ class LineTest {
 
         // then
         assertThat(line).isNotNull();
+    }
+
+    @Test
+    @DisplayName("섹션들에서 역 id 찾기")
+    void findStationIds() {
+        // when
+        List<Long> ids = line.getSortedStationId();
+
+        // then
+        assertThat(ids).containsExactly(4L, 2L, 3L, 1L);
+    }
+
+    @Test
+    @DisplayName("구간들에서 특정역에 따라 삭제할 구간 찾기")
+    void findNearByStationId() {
+        // when
+        List<Section> nearBySections = line.findDeletableByStationId(2L);
+
+        // then
+        assertThat(nearBySections).hasSize(2);
+    }
+
+    @Test
+    @DisplayName("구간이 하나일 때 특정역에 따라 삭제할 구간 찾으려 하면 예외")
+    void findNearByStationId_invalid() {
+        // when
+        Line onlyOneLine = new Line("onlyOne", "red", List.of(new Section(1L, 1L, 2L, 10)));
+
+        // then
+        assertThatThrownBy(() -> onlyOneLine.findDeletableByStationId(1L))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ExceptionMessage.SECTIONS_NOT_DELETABLE.getContent());
     }
 }
