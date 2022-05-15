3c6250ffcec83c1758ba4f57a853bc8b5c1dd07a
sinb57 <sinb57@naver.com>
2022-05-15 09:42:07 +0900

    test: LineService 테스트 보충 및 리팩터링

--- src/test/java/wooteco/subway/service/LineServiceTest.java
+++ src/test/java/wooteco/subway/service/LineServiceTest.java
@@ -5,11 +5,13 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
@@ -26,50 +28,55 @@ import wooteco.subway.service.dto.line.LineRequest;
 import wooteco.subway.service.dto.line.LineResponse;
 import wooteco.subway.service.dto.line.LineUpdateRequest;
 
+@DisplayName("지하철노선 Service")
 @JdbcTest
 class LineServiceTest {
 
     @Autowired
     private DataSource dataSource;
     private LineService lineService;
-    private Long stationId1;
-    private Long stationId2;
+    private Long upStationId;
+    private Long downStationId;
 
     @BeforeEach
     void setUp() {
-        wooteco.subway.repository.dao.LineDao lineDao = new LineDao(dataSource);
-        wooteco.subway.repository.dao.SectionDao sectionDao = new SectionDao(dataSource);
-        wooteco.subway.repository.dao.StationDao stationDao = new StationDao(dataSource);
+        LineDao lineDao = new LineDao(dataSource);
+        SectionDao sectionDao = new SectionDao(dataSource);
+        StationDao stationDao = new StationDao(dataSource);
 
         LineRepository lineRepository = new SubwayRepository(lineDao, sectionDao, stationDao);
         this.lineService = new LineService(lineRepository);
 
         StationService stationService = new StationService(new SubwayRepository(lineDao, sectionDao, stationDao));
-        this.stationId1 = stationService.create("강남역").getId();
-        this.stationId2 = stationService.create("광교역").getId();
+        this.upStationId = stationService.create("강남역").getId();
+        this.downStationId = stationService.create("광교역").getId();
+    }
+
+    private LineRequest createLineRequest(String name, String color) {
+        return new LineRequest(name, color, upStationId, downStationId, 10);
     }
 
     @DisplayName("지하철노선을 저장한다.")
-    @Test
-    void create() {
-        LineRequest lineRequest = new LineRequest("신분당선", "color", stationId1, stationId2, 10);
-        LineResponse lineResponse = lineService.create(lineRequest);
+    @ParameterizedTest
+    @CsvSource(value = {"신분당선,red"})
+    void create(String name, String color) {
+        LineResponse actual = lineService.create(createLineRequest(name, color));
         assertAll(
-                () -> assertThat(lineResponse.getId()).isGreaterThan(0),
-                () -> assertThat(lineResponse.getName()).isEqualTo("신분당선"),
-                () -> assertThat(lineResponse.getColor()).isEqualTo("color")
+                () -> assertThat(actual.getId()).isGreaterThan(0),
+                () -> assertThat(actual.getName()).isEqualTo(name),
+                () -> assertThat(actual.getColor()).isEqualTo(color)
         );
     }
 
     @DisplayName("이미 존재하는 이름으로 지하철노선을 생성할 수 없다.")
     @ParameterizedTest
     @ValueSource(strings = {"신분당선"})
-    void duplicateNameException(String name) {
-        LineRequest lineRequest1 = new LineRequest(name, "color1", stationId1, stationId2, 10);
-        LineRequest lineRequest2 = new LineRequest(name, "color2", stationId1, stationId2, 10);
+    void createWithDuplicatedName(String name) {
+        LineRequest firstRequest = createLineRequest(name, "color1");
+        LineRequest secondRequest = createLineRequest(name, "color2");
 
-        lineService.create(lineRequest1);
-        assertThatThrownBy(() -> lineService.create(lineRequest2))
+        lineService.create(firstRequest);
+        assertThatThrownBy(() -> lineService.create(secondRequest))
                 .isInstanceOf(DuplicateLineNameException.class)
                 .hasMessageContaining("해당 이름의 지하철노선은 이미 존재합니다");
     }
@@ -77,54 +84,59 @@ class LineServiceTest {
     @DisplayName("이미 존재하는 색상으로 지하철노선을 생성할 수 없다.")
     @ParameterizedTest
     @ValueSource(strings = {"color"})
-    void duplicateColorException(String color) {
-        LineRequest lineRequest1 = new LineRequest("신분당선", color, stationId1, stationId2, 10);
-        LineRequest lineRequest2 = new LineRequest("분당선", color, stationId1, stationId2, 10);
+    void createWithDuplicatedColor(String color) {
+        LineRequest firstRequest = createLineRequest("신분당선", color);
+        LineRequest secondRequest = createLineRequest("분당선", color);
 
-        lineService.create(lineRequest1);
-        assertThatThrownBy(() -> lineService.create(lineRequest2))
+        lineService.create(firstRequest);
+        assertThatThrownBy(() -> lineService.create(secondRequest))
                 .isInstanceOf(DuplicateLineColorException.class)
                 .hasMessageContaining("해당 색상의 지하철노선은 이미 존재합니다");
     }
 
     @DisplayName("지하철노선 목록을 조회한다.")
-    @Test
-    void findAll() {
-        lineService.create(new LineRequest("1호선", "color1", stationId1, stationId2, 10));
-        lineService.create(new LineRequest("2호선", "color2", stationId1, stationId2, 10));
-        lineService.create(new LineRequest("3호선", "color3", stationId1, stationId2, 10));
-        List<LineResponse> lines = lineService.findAll();
-        assertThat(lines).hasSize(3);
+    @ParameterizedTest
+    @ValueSource(ints = {4})
+    void findAll(int expected) {
+        IntStream.rangeClosed(1, expected)
+                .mapToObj(id -> createLineRequest("호선" + id, "color" + id))
+                .forEach(lineService::create);
+
+        List<LineResponse> actual = lineService.findAll();
+        assertThat(actual).hasSize(expected);
     }
 
     @DisplayName("지하철노선을 조회한다.")
     @Test
     void findOne() {
-        LineResponse expected = lineService.create(new LineRequest("1호선", "color1", stationId1, stationId2, 10));
+        LineResponse expected = lineService.create(createLineRequest("1호선", "color1"));
         LineResponse actual = lineService.findOne(expected.getId());
         assertThat(actual).usingRecursiveComparison()
-                .isEqualTo(actual);
+                .isEqualTo(expected);
     }
 
     @DisplayName("지하철노선을 수정한다.")
-    @Test
-    void update() {
-        LineResponse line = lineService.create(new LineRequest("1호선", "color1", stationId1, stationId2, 10));
-        lineService.update(line.getId(), new LineUpdateRequest("분당선", "color2"));
+    @ParameterizedTest
+    @CsvSource(value = {"신분당선,red"})
+    void update(String name, String color) {
+        Long lineId = lineService.create(createLineRequest("1호선", "color1")).getId();
+        lineService.update(lineId, new LineUpdateRequest(name, color));
 
-        LineResponse updatedLine = lineService.findOne(line.getId());
+        LineResponse actual = lineService.findOne(lineId);
         assertAll(
-                () -> assertThat(updatedLine.getId()).isEqualTo(line.getId()),
-                () -> assertThat(updatedLine.getName()).isEqualTo("분당선"),
-                () -> assertThat(updatedLine.getColor()).isEqualTo("color2")
+                () -> assertThat(actual.getId()).isEqualTo(lineId),
+                () -> assertThat(actual.getName()).isEqualTo(name),
+                () -> assertThat(actual.getColor()).isEqualTo(color)
         );
     }
 
     @DisplayName("지하철노선을 삭제한다.")
     @Test
     void remove() {
-        LineResponse line = lineService.create(new LineRequest("1호선", "color1", stationId1, stationId2, 10));
-        lineService.remove(line.getId());
-        assertThat(lineService.findAll()).isEmpty();
+        LineResponse lineResponse = lineService.create(createLineRequest("1호선", "color1"));
+        lineService.remove(lineResponse.getId());
+
+        List<LineResponse> actual = lineService.findAll();
+        assertThat(actual).isEmpty();
     }
 }
