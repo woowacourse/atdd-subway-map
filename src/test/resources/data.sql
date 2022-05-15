d9623aa992a5f3f7f65da7e12b6f68428d654eeb
giron <ggyucheol0@gmail.com>
2022-05-15 18:41:49 +0900

    refactor: sql 테이블 구조 적지 않도록 수정

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,33 +1,3 @@
-drop table if exists station;
-drop table if exists section;
-drop table if exists line;
-
-create table station
-(
-    id   bigint auto_increment not null,
-    name varchar(255) not null unique,
-    primary key (id)
-);
-
-create table line
-(
-    id    bigint auto_increment not null,
-    name  varchar(255) not null unique,
-    color varchar(20)  not null,
-    primary key (id)
-);
-
-create table section
-(
-    id              bigint auto_increment not null,
-    line_id         bigint not null,
-    up_station_id   bigint not null,
-    down_station_id bigint not null,
-    distance        int    not null,
-    primary key (id),
-    foreign key (line_id) references line (id) on delete cascade
-);
-
 INSERT INTO station (name) VALUES ('합정역');
 INSERT INTO station (name) VALUES ('홍대입구역');
 INSERT INTO station (name) VALUES ('신촌역');
