cb90e6ebed29083f32b571142a15201a41f5a151
김찬 <gmelddl514@gmail.com>
2022-05-15 18:05:04 +0900

    refactor : Service 테스트 중복 제거

--- src/test/java/wooteco/subway/service/LineServiceTest.java
+++ src/test/java/wooteco/subway/service/LineServiceTest.java
@@ -6,6 +6,7 @@ import static org.assertj.core.groups.Tuple.tuple;
 
 import java.util.List;
 import javax.sql.DataSource;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,7 @@ import org.springframework.test.context.jdbc.Sql;
 import wooteco.subway.dao.LineDao;
 import wooteco.subway.dao.SectionDao;
 import wooteco.subway.dao.StationDao;
+import wooteco.subway.domain.Station;
 import wooteco.subway.dto.LineRequest;
 import wooteco.subway.dto.LineResponse;
 
@@ -22,43 +24,44 @@ import wooteco.subway.dto.LineResponse;
 @Sql("/schema.sql")
 class LineServiceTest {
 
+    private final StationDao stationDao;
     private final LineService lineService;
 
     @Autowired
     public LineServiceTest(JdbcTemplate jdbcTemplate, DataSource dataSource) {
+        this.stationDao = new StationDao(jdbcTemplate, dataSource);
         this.lineService = new LineService(
                 new LineDao(jdbcTemplate, dataSource),
                 new SectionDao(jdbcTemplate, dataSource),
-                new StationDao(jdbcTemplate, dataSource));
+                stationDao);
+    }
+
+    @BeforeEach
+    void setUp() {
+        long 강남역 = stationDao.save(new Station("강남역"));
+        long 선릉역 = stationDao.save(new Station("선릉역"));
+        lineService.save(new LineRequest("line1", "red", 강남역, 선릉역, 5));
+
     }
 
     @Test
     @DisplayName("지하철 노선 추가, 조회, 삭제 테스트")
     void LineCRDTest() {
-        lineService.save(new LineRequest("line1", "red", 1L, 2L, 0));
-        lineService.save(new LineRequest("line2", "yellow", 1L, 2L, 0));
-        lineService.save(new LineRequest("line3", "blue", 1L, 2L, 0));
-
         List<LineResponse> lines = lineService.findAll();
 
-        assertThat(lines).hasSize(3)
+        assertThat(lines).hasSize(1)
                 .extracting("name", "color")
-                .containsExactly(tuple("line1", "red"), tuple("line2", "yellow"), tuple("line3", "blue"));
+                .containsExactly(tuple("line1", "red"));
 
         lineService.delete(lines.get(0).getId());
-        lineService.delete(lines.get(1).getId());
-        lineService.delete(lines.get(2).getId());
-
         assertThat(lineService.findAll()).hasSize(0);
     }
 
     @Test
     @DisplayName("중복된 노선 이름 입력 시 예외 발생 테스트")
     void validateDuplicationNameTest() {
-        lineService.save(new LineRequest("line1", "red", 1L, 2L, 0));
-
         assertThatThrownBy(() -> lineService.save(
-                new LineRequest("line1", "yellow", 1L, 2L, 0)))
+                new LineRequest("line1", "yellow", 1L, 2L, 5)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("이미 존재하는 노선 이름입니다.");
     }
@@ -66,14 +69,10 @@ class LineServiceTest {
     @Test
     @DisplayName("노선을 수정한다.")
     void updateTest() {
-        LineResponse lineResponse = lineService.save(
-                new LineRequest("line1", "red", 1L, 2L, 0));
-        long lineId = lineResponse.getId();
+        lineService.update(1L, new LineRequest(
+                "line2", "yellow", 1L, 2L, 5));
 
-        lineService.update(lineId, new LineRequest(
-                "line2", "yellow", 1L, 2L, 0));
-
-        assertThat(lineService.findById(lineId))
+        assertThat(lineService.findById(1L))
                 .extracting("name", "color")
                 .containsExactly("line2", "yellow");
     }
--- src/test/java/wooteco/subway/service/SectionServiceTest.java
+++ src/test/java/wooteco/subway/service/SectionServiceTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThatCode;
 
 import javax.sql.DataSource;
 import org.assertj.core.groups.Tuple;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,33 +37,32 @@ class SectionServiceTest {
         sectionService = new SectionService(lineDao, sectionDao, stationDao);
     }
 
+    @BeforeEach
+    void setUp() {
+        lineDao.save(new Line("신분당선", "red"));
+        stationDao.save(new Station("강남역"));
+        stationDao.save(new Station("선릉역"));
+        stationDao.save(new Station("역삼역"));
+    }
+
     @Test
     @DisplayName("구간을 등록한다")
     void add() {
-        long lineId = lineDao.save(new Line("신분당선", "red"));
-        long stationId1 = stationDao.save(new Station("강남역"));
-        long stationId2 = stationDao.save(new Station("선릉역"));
-        long stationId3 = stationDao.save(new Station("역삼역"));
-        sectionDao.save(new Section(lineId, stationId1, stationId2, 5));
-
-        assertThatCode(() -> sectionService.add(lineId, new SectionRequest(stationId2, stationId3, 5)))
+        sectionDao.save(new Section(1L, 1L, 2L, 5));
+        assertThatCode(() -> sectionService.add(1L, new SectionRequest(2L, 3L, 5)))
                 .doesNotThrowAnyException();
     }
 
     @Test
     @DisplayName("구간을 제거한다")
     void delete() {
-        long lineId = lineDao.save(new Line("신분당선", "red"));
-        long stationId1 = stationDao.save(new Station("강남역"));
-        long stationId2 = stationDao.save(new Station("선릉역"));
-        long stationId3 = stationDao.save(new Station("역삼역"));
-        sectionDao.save(new Section(lineId, stationId1, stationId2, 5));
-        sectionDao.save(new Section(lineId, stationId2, stationId3, 5));
+        sectionDao.save(new Section(1L, 1L, 2L, 5));
+        sectionDao.save(new Section(1L, 2L, 3L, 5));
 
-        sectionService.delete(lineId, stationId2);
+        sectionService.delete(1L, 2L);
 
-        assertThat(sectionDao.findByLineId(lineId)).hasSize(1)
+        assertThat(sectionDao.findByLineId(1L)).hasSize(1)
                 .extracting("upStationId", "downStationId", "distance")
-                .containsExactly(Tuple.tuple(stationId1, stationId3, 10));
+                .containsExactly(Tuple.tuple(1L, 3L, 10));
     }
 }
