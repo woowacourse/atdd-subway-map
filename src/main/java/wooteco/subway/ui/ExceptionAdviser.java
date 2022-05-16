4558373676ba597d3b7bf7d658415b2e51f98b6f
cjlee38 <cjlee38@github.com>
2022-05-17 01:57:49 +0900

    feat: 파일 로깅 설정 추가

--- src/main/java/wooteco/subway/ui/ExceptionAdviser.java
+++ src/main/java/wooteco/subway/ui/ExceptionAdviser.java
@@ -1,5 +1,7 @@
 package wooteco.subway.ui;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,15 +12,17 @@ import wooteco.subway.exception.CustomException;
 @RestControllerAdvice
 public class ExceptionAdviser {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass().getSimpleName());
+
     @ExceptionHandler
     public ResponseEntity<ExceptionResponse> handle(CustomException exception) {
+        logger.info(exception.getClass().getName() + " -> " + exception.getMessage());
         return ResponseEntity.badRequest().body(ExceptionResponse.of(exception));
     }
 
-    @ExceptionHandler(RuntimeException.class)
+    @ExceptionHandler
     public ResponseEntity<ExceptionResponse> handle(RuntimeException exception) {
-        // TODO : logback
-        exception.printStackTrace();
+        logger.warn(exception.getClass().getName() + " -> " + exception.getMessage());
         return ResponseEntity.internalServerError().body(ExceptionResponse.of(exception));
     }
 
