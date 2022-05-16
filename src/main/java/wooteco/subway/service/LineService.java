9150bf230ac741801a0b9c031970aaf77ae466b6
yangdongjue5510 <djwhy5510@gmail.com>
2022-05-16 20:48:50 +0900

    refactor(LineService): 구간들을 필요 이상으로 조회하는 문제 수정

--- src/main/java/wooteco/subway/service/LineService.java
+++ src/main/java/wooteco/subway/service/LineService.java
@@ -16,7 +16,7 @@ import wooteco.subway.dto.SectionSaveRequest;
 import wooteco.subway.entity.LineEntity;
 import wooteco.subway.exception.ExceptionMessage;
 import wooteco.subway.exception.domain.LineException;
-import wooteco.subway.exception.notfound.NotFoundException;
+import wooteco.subway.exception.notfound.LineNotFoundException;
 
 @Service
 @Transactional
@@ -35,44 +35,34 @@ public class LineService {
     public LineResponse save(final LineRequest request) {
         try {
             LineEntity saved = lineDao.save(new LineEntity(null, request.getName(), request.getColor()));
-            sectionService.save(
-                    new SectionSaveRequest(saved.getId(), request.getUpStationId(), request.getDownStationId(),
-                            request.getDistance()));
-            List<Section> sections = sectionService.findByLineId(saved.getId());
-            Line line = new Line(saved.getId(), saved.getName(), saved.getColor(), sections);
-            return LineResponse.of(line, getStationsByLine(line));
+            sectionService.save(new SectionSaveRequest(saved.getId(), request.getUpStationId(),
+                    request.getDownStationId(), request.getDistance()));
+            return createResponseFrom(saved);
         } catch (DuplicateKeyException e) {
             throw new LineException(ExceptionMessage.DUPLICATED_LINE_NAME.getContent());
         }
     }
 
-    private List<Station> getStationsByLine(Line line) {
-        List<Section> sections = sectionService.findByLineId(line.getId());
+    private LineResponse createResponseFrom(LineEntity entity) {
+        List<Section> sections = sectionService.findByLineId(entity.getId());
+        Line line = new Line(entity.getId(), entity.getName(), entity.getColor(), sections);
+        return LineResponse.of(line, getSortedStationsBySections(sections));
+    }
+
+    private List<Station> getSortedStationsBySections(List<Section> sections) {
         return stationService.getSortedStations(new Sections(sections));
     }
 
     public List<LineResponse> findAll() {
         return lineDao.findAll().stream()
-                .map(lineEntity -> new Line(lineEntity.getId(), lineEntity.getName(), lineEntity.getColor(),
-                        sectionService.findByLineId(lineEntity.getId())))
-                .map(line -> LineResponse.of(line, getStationsByLine(line)))
+                .map(this::createResponseFrom)
                 .collect(Collectors.toList());
     }
 
-    private Line toLine(LineEntity entity) {
-        return new Line(entity.getId(), entity.getName(), entity.getColor(),
-                sectionService.findByLineId(entity.getId()));
-    }
-
     public LineResponse findById(Long id) {
-        Line line = getLine(id);
-        return LineResponse.of(line, getStationsByLine(line));
-    }
-
-    private Line getLine(Long id) {
         return lineDao.findById(id)
-                .map(this::toLine)
-                .orElseThrow(NotFoundException::new);
+                .map(this::createResponseFrom)
+                .orElseThrow(LineNotFoundException::new);
     }
 
     public void updateById(final Long id, final LineRequest request) {
