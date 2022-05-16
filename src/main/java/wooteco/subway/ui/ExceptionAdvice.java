1c496ec87a9c6964b63c251375dedd3afe88d021
Kim Seunglae <obey1342@gmail.com>
2022-05-16 21:41:39 +0900

    feat: valid exception handler 구현

--- src/main/java/wooteco/subway/ui/ExceptionAdvice.java
+++ src/main/java/wooteco/subway/ui/ExceptionAdvice.java
@@ -1,7 +1,10 @@
 package wooteco.subway.ui;
 
+import java.util.stream.Collectors;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import wooteco.subway.exception.NotFoundException;
@@ -23,5 +26,16 @@ public class ExceptionAdvice {
     @ExceptionHandler
     public ResponseEntity<String> handleException(NotFoundException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<String> handleException(MethodArgumentNotValidException e) {
+        String errorMessage = e.getBindingResult()
+            .getAllErrors()
+            .stream()
+            .map(DefaultMessageSourceResolvable::getDefaultMessage)
+            .collect(Collectors.joining(", "));
+
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
     }
 }
