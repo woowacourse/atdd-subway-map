ab7f1c53fb6a795af513c5d63afb9a86e5692701
Hongdonggeon <ghd700@naver.com>
2022-05-16 18:14:36 +0900

    feat: Section 객체 검증 로직 추가

--- src/main/java/wooteco/subway/domain/Section.java
+++ src/main/java/wooteco/subway/domain/Section.java
@@ -4,6 +4,9 @@ import java.util.Objects;
 
 public class Section {
 
+    private static final String STATION_DISTANCE_NEGATIVE_ERROR = "두 역간의 거리는 양수여야 합니다.";
+    private static final String ALL_STATION_SAME_ERROR = "구간의 두 지하철역은 같을 수 없습니다.";
+
     private Long id;
     private Long lineId;
     private Long upStationId;
@@ -11,6 +14,8 @@ public class Section {
     private int distance;
 
     public Section(Long id, Long lineId, Long upStationId, Long downStationId, int distance) {
+        validateDistance(distance);
+        validateDistance(upStationId, downStationId);
         this.id = id;
         this.lineId = lineId;
         this.upStationId = upStationId;
@@ -20,6 +25,29 @@ public class Section {
 
     public Section(Long lineId, Long upStationId, Long downStationId, int distance) {
         this(null, lineId, upStationId, downStationId, distance);
+    }
+
+    private void validateDistance(Long upStationId, Long downStationId) {
+        if (upStationId.equals(downStationId)) {
+            throw new IllegalArgumentException(ALL_STATION_SAME_ERROR);
+        }
+    }
+
+    private void validateDistance(int distance) {
+        if (distance < 0) {
+            throw new IllegalArgumentException(STATION_DISTANCE_NEGATIVE_ERROR);
+        }
+    }
+
+    public boolean isExistSameStation(Section anotherSection) {
+        return (upStationId == anotherSection.upStationId) || (upStationId == anotherSection.downStationId)
+                || (downStationId == anotherSection.upStationId) || (downStationId == anotherSection.downStationId);
+    }
+
+    public void update(Long inputUpStationId, Long inputDownStationId, int distance) {
+        this.upStationId = inputUpStationId;
+        this.downStationId = inputDownStationId;
+        this.distance = distance;
     }
 
     public Long getId() {
@@ -40,17 +68,6 @@ public class Section {
 
     public int getDistance() {
         return distance;
-    }
-
-    public boolean isExistSameStation(Section anotherSection) {
-        return (upStationId == anotherSection.upStationId) || (upStationId == anotherSection.downStationId)
-                || (downStationId == anotherSection.upStationId) || (downStationId == anotherSection.downStationId);
-    }
-
-    public void update(Long inputUpStationId, Long inputDownStationId, int distance) {
-        this.upStationId = inputUpStationId;
-        this.downStationId = inputDownStationId;
-        this.distance = distance;
     }
 
     @Override
